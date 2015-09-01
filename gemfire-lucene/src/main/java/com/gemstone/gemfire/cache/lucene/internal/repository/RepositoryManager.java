6f9401e19cef1dd370ea209bfa3b8c64883d1764
Ashvin Agrawal <ashvin@apache.org>
2015-09-01 11:30:43 -0700

    GEODE-11: Add utility method to discover IndexRepo
    * Add method to discover IndexRepositories for a given region and buckets

--- gemfire-lucene/src/main/java/com/gemstone/gemfire/cache/lucene/internal/repository/RepositoryManager.java
+++ gemfire-lucene/src/main/java/com/gemstone/gemfire/cache/lucene/internal/repository/RepositoryManager.java
@@ -1,9 +1,27 @@
 package com.gemstone.gemfire.cache.lucene.internal.repository;
 
-import com.gemstone.gemfire.cache.Region;
+import java.util.Collection;
 
+import com.gemstone.gemfire.cache.Region;
+import com.gemstone.gemfire.internal.cache.BucketNotFoundException;
+
+/**
+ * {@link RepositoryManager} instances will be used to get {@link IndexRepository} instances hosting index data for
+ * {@link Region}s
+ */
 public interface RepositoryManager {
 
   IndexRepository getRepository(Region region, Object key);
 
+  /**
+   * Returns a collection of {@link IndexRepository} instances hosting index data of the input list of bucket ids. The
+   * bucket needs to be present on this member.
+   * 
+   * @param region
+   * @param bucketIds buckets of a Partitioned region for which {@link IndexRepository}s needs to be discovered. null
+   *          for all primary buckets on this member or if the region is Replicated.
+   * @return a collection of {@link IndexRepository} instances
+   * @throws BucketNotFoundException if any of the requested buckets is not found on this member
+   */
+  Collection<IndexRepository> getRepositories(Region region, int[] bucketIds) throws BucketNotFoundException;
 }
