62952c17c3c2e4af37d52174137b8534814faab0
Dan Smith <upthewaterspout@apache.org>
2015-10-09 10:17:56 -0700

    Changing a reference from GemFire to Geode

--- gemfire-lucene/src/main/java/com/gemstone/gemfire/cache/lucene/LuceneQueryProvider.java
+++ gemfire-lucene/src/main/java/com/gemstone/gemfire/cache/lucene/LuceneQueryProvider.java
@@ -8,7 +8,7 @@ import com.gemstone.gemfire.cache.query.QueryException;
 
 /**
  * The instances of this class will be used for distributing Lucene Query objects and re-constructing the Query object.
- * If necessary the implementation needs to take care of serializing and de-serializing Lucene Query object. GemFire
+ * If necessary the implementation needs to take care of serializing and de-serializing Lucene Query object. Geode
  * respects the DataSerializable contract to provide optimal object serialization. For instance,
  * {@link LuceneQueryProvider}'s toData method will be used to serialize it when it is sent to another member of the
  * distributed system. Implementation of DataSerializable can provide a zero-argument constructor that will be invoked
