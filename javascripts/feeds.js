11710481b97cc5935b73e9178f7a5972721f2ad9
William Markito <wmarkito@pivotal.io>
2015-12-08 16:50:15 -0800

    GEODE-53 - Applying Apache Geode new website layout

--- javascripts/feeds.js
+++
@@ -1,6 +0,0 @@
-function loadFeedControl() {
-  var feed = "https://www.google.com/calendar/feeds/gejamo1ppmtohgneonsen1a9vc%40group.calendar.google.com/public/basic?"+new Date().getTime();
-  var fg = new GFdynamicFeedControl(feed, "calendar");
-}
-google.load("feeds", "1");
-google.setOnLoadCallback(loadFeedControl);
