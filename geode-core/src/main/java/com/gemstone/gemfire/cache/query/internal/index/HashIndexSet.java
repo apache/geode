9f896420b248c5415e6d46d7f59122882f0d59f7
Sai Boorlagadda <sboorlagadda@pivotal.io>
2016-02-22 12:16:37 -0800

    GEODE-906: Clean up source header / modification text
    * HashIndexSet to use the standard Apache source header with a separate modification statement
    * IdentityArrayList to use the original license header with a separate modificaiton statement
    * Added fastutil copyright to the NOTICE

--- geode-core/src/main/java/com/gemstone/gemfire/cache/query/internal/index/HashIndexSet.java
+++ geode-core/src/main/java/com/gemstone/gemfire/cache/query/internal/index/HashIndexSet.java
@@ -19,19 +19,6 @@
  * insertionIndex(), index(), trimToSize() are based on code provided by fastutil
  * They are based from add(), contains() and other methods from ObjectOpenHashSet
  * We have used the traversing mechanism and the HashCommon.mix()
- * Copyright (C) 2002-2014 Sebastiano Vigna 
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License. 
  */
 package com.gemstone.gemfire.cache.query.internal.index;
 
