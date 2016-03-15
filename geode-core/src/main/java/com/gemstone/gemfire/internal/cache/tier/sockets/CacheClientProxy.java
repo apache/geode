be74e3a1e9ea0699743dd7ec9e63774402d3542e
agingade <agingade@pivotal.io>
2016-03-15 15:31:37 -0700

    GEODE-934: CI failure: DurableClientSimpleDUnitTest.testReadyForEventsNotCalledImplicitlyWithCacheXML.
    In this test the client sees previously delivered events, when it disconnects and reconnects immediately.
    This happens when the message dispatcher is in the process of removing the messages while its in the pause state; and client reconnects at the same time. When client reconnects, it clears out the acked-event list, this causes the message dispatcher thread not to remove dispatched event from the queue. The fix is to take the pause lock and start removing the dispatched event, this blocks the client reconnect process to wait tille the messages are removed.
    
    Testing:
    The test used to fail once in 4 runs. With the change it run for 50 times.
    precheckin

--- geode-core/src/main/java/com/gemstone/gemfire/internal/cache/tier/sockets/CacheClientProxy.java
+++ geode-core/src/main/java/com/gemstone/gemfire/internal/cache/tier/sockets/CacheClientProxy.java
@@ -2582,21 +2582,20 @@ public class CacheClientProxy implements ClientSession {
         try {
           // If paused, wait to be told to resume (or interrupted if stopped)
           if (getProxy().isPaused()) {
-            try {
-              // ARB: Before waiting for resumption, process acks from client. 
-              // This will reduce the number of duplicates that a client receives after
-              // reconnecting.
-              if (this._messageQueue.size() > 0) {
-                Thread.sleep(50);
-              }
-              logger.info("available ids = " + this._messageQueue.size()+ " , isEmptyAckList =" + this._messageQueue.isEmptyAckList() 
+            // ARB: Before waiting for resumption, process acks from client. 
+            // This will reduce the number of duplicates that a client receives after
+            // reconnecting.
+            synchronized (_pausedLock) {
+              try {
+                logger.info("available ids = " + this._messageQueue.size()+ " , isEmptyAckList =" + this._messageQueue.isEmptyAckList() 
                             + ", peekInitialized = " + this._messageQueue.isPeekInitialized());
-              while (!this._messageQueue.isEmptyAckList()&& this._messageQueue.isPeekInitialized()) {
-                this._messageQueue.remove();
+                while (!this._messageQueue.isEmptyAckList()&& this._messageQueue.isPeekInitialized()) {
+                  this._messageQueue.remove();
+                }
               }
-            }
-            catch (InterruptedException ex) {
-              logger.warn(LocalizedMessage.create(LocalizedStrings.CacheClientProxy_0_SLEEP_INTERRUPTED, this));
+              catch (InterruptedException ex) {
+                logger.warn(LocalizedMessage.create(LocalizedStrings.CacheClientProxy_0_SLEEP_INTERRUPTED, this));
+              }
             }
             waitForResumption();
           }
