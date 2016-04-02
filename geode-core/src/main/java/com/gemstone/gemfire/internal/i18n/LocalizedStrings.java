92f174134d185ed72795ccbad6c57b0b8e8437b3
Nitin Lamba <nlamba@apache.org>
2016-04-02 01:52:17 -0700

    GEODE-1163 Fix failing I18n test for one of the strings

--- geode-core/src/main/java/com/gemstone/gemfire/internal/i18n/LocalizedStrings.java
+++ geode-core/src/main/java/com/gemstone/gemfire/internal/i18n/LocalizedStrings.java
@@ -2151,7 +2151,7 @@ public class LocalizedStrings extends ParentLocalizedStrings {
 
   // Developer REST interface
   public static final StringId SwaggerConfig_VENDOR_PRODUCT_LINE = new StringId(6617, "Apache Geode Developer REST API");
-  public static final StringId SwaggerConfig_DESCRIPTOR = new StringId(6618, "Developer REST API and interface to Geode's distributed, in-memory data grid and cache.");
+  public static final StringId SwaggerConfig_DESCRIPTOR = new StringId(6618, "Developer REST API and interface to Geode''s distributed, in-memory data grid and cache.");
   public static final StringId SwaggerConfig_EULA_LINK = new StringId(6619, "http://www.apache.org/licenses/");
   public static final StringId SwaggerConfig_SUPPORT_LINK = new StringId(6620, "dev@geode.incubator.apache.org");
   public static final StringId SwaggerConfig_DOC_TITLE = new StringId(6621, "Apache Geode Documentation");
