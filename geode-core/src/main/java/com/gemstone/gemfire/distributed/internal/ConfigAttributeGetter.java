047bf8bdddfc5faf0d61a15c89d1a43da65ecd4c
Jinmei Liao <jiliao@pivotal.io>
2016-04-22 15:54:12 -0700

    GEODE-17: Delete the author tag

--- geode-core/src/main/java/com/gemstone/gemfire/distributed/internal/ConfigAttributeGetter.java
+++ geode-core/src/main/java/com/gemstone/gemfire/distributed/internal/ConfigAttributeGetter.java
@@ -21,9 +21,6 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-/**
- * Created by jiliao on 2/3/16.
- */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface ConfigAttributeGetter {
