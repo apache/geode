ce43082f9cf0cd1702bd0de5e6be6b314e063396
Jens Deppe <jdeppe@pivotal.io>
2016-04-29 09:23:09 -0700

    GEODE-1326: Add test for gfsh function execution with ResultCollector

---
+++ geode-core/src/test/java/com/gemstone/gemfire/management/internal/cli/commands/ToUpperResultCollector.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.gemstone.gemfire.management.internal.cli.commands;
+
+import com.gemstone.gemfire.cache.execute.FunctionException;
+import com.gemstone.gemfire.cache.execute.ResultCollector;
+import com.gemstone.gemfire.distributed.DistributedMember;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public class ToUpperResultCollector implements ResultCollector {
+
+  private List<Object> results = new ArrayList<>();
+
+  private CountDownLatch latch = new CountDownLatch(1);
+
+  @Override
+  public Object getResult() throws FunctionException {
+    try {
+      latch.await();
+    } catch (InterruptedException e) {
+      throw new FunctionException("Interrupted waiting for results", e);
+    }
+    return results;
+  }
+
+  @Override
+  public Object getResult(long timeout, TimeUnit unit) throws FunctionException, InterruptedException {
+    latch.await(timeout, unit);
+    return results;
+  }
+
+  @Override
+  public void addResult(DistributedMember memberID, Object resultOfSingleExecution) {
+    results.add(resultOfSingleExecution.toString().toUpperCase());
+  }
+
+  @Override
+  public void endResults() {
+    latch.countDown();
+  }
+
+  @Override
+  public void clearResults() {
+    results.clear();
+    latch = new CountDownLatch(1);
+  }
+}
