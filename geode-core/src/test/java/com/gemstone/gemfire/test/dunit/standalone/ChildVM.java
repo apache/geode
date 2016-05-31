b3613606edf154315f98dbc3f9f0bfb9e2fb9a73
Kevin J. Duling <kduling@pivotal.io>
2016-05-31 15:39:16 -0700

    GEODE-308: Separate hydra from dunit and junit tests in gemfire-core
    Removed hydra classes.

--- geode-core/src/test/java/com/gemstone/gemfire/test/dunit/standalone/ChildVM.java
+++ geode-core/src/test/java/com/gemstone/gemfire/test/dunit/standalone/ChildVM.java
@@ -18,13 +18,11 @@ package com.gemstone.gemfire.test.dunit.standalone;
 
 import java.rmi.Naming;
 
-import hydra.HydraRuntimeException;
-import hydra.Log;
-import org.apache.logging.log4j.Logger;
-
 import com.gemstone.gemfire.internal.OSProcess;
 import com.gemstone.gemfire.internal.logging.LogService;
 import com.gemstone.gemfire.test.dunit.standalone.DUnitLauncher.MasterRemote;
+
+import org.apache.logging.log4j.Logger;
 
 /**
  *
@@ -39,24 +37,19 @@ public class ChildVM {
   public static void stopVM() {
     stopMainLoop = true;
   }
-  
-  static {
-    createHydraLogWriter();
-  }
-  
+
   private final static Logger logger = LogService.getLogger();
-  private static RemoteDUnitVM dunitVM;
-  
+
   public static void main(String[] args) throws Throwable {
     try {
-      int namingPort = Integer.getInteger(DUnitLauncher.RMI_PORT_PARAM).intValue();
-      int vmNum = Integer.getInteger(DUnitLauncher.VM_NUM_PARAM).intValue();
+      int namingPort = Integer.getInteger(DUnitLauncher.RMI_PORT_PARAM);
+      int vmNum = Integer.getInteger(DUnitLauncher.VM_NUM_PARAM);
       int pid = OSProcess.getId();
       logger.info("VM" + vmNum + " is launching" + (pid > 0? " with PID " + pid : ""));
       MasterRemote holder = (MasterRemote) Naming.lookup("//localhost:" + namingPort + "/" + DUnitLauncher.MASTER_PARAM);
       DUnitLauncher.init(holder);
       DUnitLauncher.locatorPort = holder.getLocatorPort();
-      dunitVM = new RemoteDUnitVM();
+      final RemoteDUnitVM dunitVM = new RemoteDUnitVM();
       Naming.rebind("//localhost:" + namingPort + "/vm" + vmNum, dunitVM);
       holder.signalVMReady();
       //This loop is here so this VM will die even if the master is mean killed.
@@ -67,13 +60,6 @@ public class ChildVM {
     } catch (Throwable t) {
       t.printStackTrace();
       System.exit(1);
-    }
-  }
-
-  private static void createHydraLogWriter() {
-    try {
-      Log.createLogWriter("dunit-childvm", "fine");
-    } catch (HydraRuntimeException ignore) {
     }
   }
 }
