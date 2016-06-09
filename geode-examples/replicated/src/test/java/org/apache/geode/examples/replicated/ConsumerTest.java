bd75c732aa21d4d92e78d5cd960a507039d0fd40
William Markito <wmarkito@pivotal.io>
2016-06-08 20:30:32 -0700

    [GEODE-33] - Adding geode-example module Creating a simple example for replicated regions to test the proposed example model Adding script tests Adding server folders to gitignore Adding gitignore and readme files

---
+++ geode-examples/replicated/src/test/java/org/apache/geode/examples/replicated/ConsumerTest.java
@@ -0,0 +1,66 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.geode.examples.replicated;
+
+import com.gemstone.gemfire.cache.Region;
+import com.gemstone.gemfire.cache.client.ClientCache;
+import com.gemstone.gemfire.cache.client.NoAvailableLocatorsException;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.Set;
+
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+public class ConsumerTest {
+
+  @Rule
+  public ExpectedException expectedException = ExpectedException.none();
+
+  private Consumer consumer;
+  private ClientCache clientCache = mock(ClientCache.class);
+  private Region region = mock(Region.class);
+  private Set keys = mock(Set.class);
+
+  @Before
+  public void setup() {
+    when(region.getName()).thenReturn(Consumer.REGION_NAME);
+    when(keys.size()).thenReturn(Consumer.NUM_ENTRIES);
+    when(region.keySetOnServer()).thenReturn(keys);
+    when(clientCache.getRegion(any())).thenReturn(region);
+    consumer = new Consumer(clientCache);
+    consumer.setRegion(region);
+  }
+
+  @Test
+  public void countEntriesOnARegion() throws Exception {
+    assertEquals(consumer.NUM_ENTRIES, consumer.countEntries());
+  }
+
+  @Test
+  public void countEntriesOnARegionWithNoConnection() throws Exception {
+    consumer = new Consumer();
+    expectedException.expect(NoAvailableLocatorsException.class);
+    assertEquals(consumer.NUM_ENTRIES, consumer.countEntries());
+  }
+
+}
