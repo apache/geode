c3162e0b9d7315c4665ebd05e26515228f9e89de
zhouxh <gzhou@pivotal.io>
2016-10-10 11:20:25 -0700

    GEODE-1956: fix the race condition that cause the vm only hosts 1 primary bucket

--- geode-lucene/src/test/java/com/gemstone/gemfire/cache/lucene/LuceneQueriesPRBase.java
+++ geode-lucene/src/test/java/com/gemstone/gemfire/cache/lucene/LuceneQueriesPRBase.java
@@ -49,7 +49,7 @@ import com.gemstone.gemfire.test.dunit.VM;
  *
  */
 public abstract class LuceneQueriesPRBase extends LuceneQueriesBase {
-  protected static final int NUM_BUCKETS = 7;
+  protected static final int NUM_BUCKETS = 10;
 
   @After
   public void cleanupRebalanceCallback() {
