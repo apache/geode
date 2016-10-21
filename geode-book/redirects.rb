0055b112d5c1e9966e9361a1d70e7dc7a5e68d3b
Joey McAllister <jmcallister@pivotal.io>
2016-10-21 15:52:38 -0700

    Fix redirects for docs/guide

--- geode-book/redirects.rb
+++ geode-book/redirects.rb
@@ -14,5 +14,5 @@
 #permissions and limitations under the License.
 
 r301 %r{/releases/latest/javadoc/(.*)}, 'http://geode.incubator.apache.org/releases/latest/javadoc/$1'
-rewrite '/', '/guide/about_geode.html'
-rewrite '/index.html', '/guide/about_geode.html'
+rewrite '/', '/docs/guide/about_geode.html'
+rewrite '/index.html', '/docs/guide/about_geode.html'
