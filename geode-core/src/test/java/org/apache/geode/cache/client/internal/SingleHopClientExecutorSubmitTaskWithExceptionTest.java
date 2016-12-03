0e29f8c234006fa74f761ef4f5bfdcaa7288f32f
Deepak Dixit <deepakdixit2011@gmail.com>
2016-12-03 20:40:12 +0530

    GEODE-2109: Replacing ExecutorService.submit calls with ExecutorService.execute call. Applying formatting changes.

--- geode-core/src/test/java/org/apache/geode/cache/client/internal/SingleHopClientExecutorSubmitTaskWithExceptionTest.java
+++ geode-core/src/test/java/org/apache/geode/cache/client/internal/SingleHopClientExecutorSubmitTaskWithExceptionTest.java
@@ -14,8 +14,8 @@ import org.junit.contrib.java.lang.system.SystemErrRule;
 import org.junit.experimental.categories.Category;
 
 /**
- *  Test if exceptions are logged when thread is submitted using
- *  {@code SingleHopClientExecutor#submitTask} method.
+ * Test if exceptions are logged when thread is submitted using
+ * {@code SingleHopClientExecutor#submitTask} method.
  */
 @Category(UnitTest.class)
 public class SingleHopClientExecutorSubmitTaskWithExceptionTest {
@@ -24,23 +24,24 @@ public class SingleHopClientExecutorSubmitTaskWithExceptionTest {
   public SystemErrRule systemErrRule = new SystemErrRule().enableLog();
 
   /**
-   * Refer: GEODE-2109
-   * This test verifies that any exception thrown from forked thread
-   * is logged into log.
+   * Refer: GEODE-2109 This test verifies that any exception thrown from forked thread is logged
+   * into log.
    */
   @Test
   public void submittedTaskShouldLogFailure() {
     String erroMsg = "I am expecting this to be logged";
 
     SingleHopClientExecutor.submitTask(new Runnable() {
-      @Override public void run() {
+      @Override
+      public void run() {
         // test piece throwing exception
         throw new RuntimeException(erroMsg);
       }
     });
 
     final Callable<Boolean> isLogFound = new Callable<Boolean>() {
-      @Override public Boolean call() throws Exception {
+      @Override
+      public Boolean call() throws Exception {
         return systemErrRule.getLog().contains(erroMsg) == true;
       }
     };
@@ -48,8 +49,8 @@ public class SingleHopClientExecutorSubmitTaskWithExceptionTest {
     /**
      * Sometimes need to wait for more than sec as thread execution takes time.
      */
-    Awaitility.await("Waiting for exception").atMost(1000 * 2l,TimeUnit.MILLISECONDS)
-      .until(isLogFound);
+    Awaitility.await("Waiting for exception").atMost(1000 * 2l, TimeUnit.MILLISECONDS)
+        .until(isLogFound);
     assertTrue(systemErrRule.getLog().contains(erroMsg));
   }
 
