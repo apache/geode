796ecee10827679206d2b77d22ddaef71fb1df02
Jacob Barrett <jbarrett@pivotal.io>
2017-01-16 22:45:45 -0800

    GEODE-2309: Replace Pivotal Copyright and add Apache License notice.

--- src/com/gemstone/gemfire/plugins/sqlite/AssemblyInfo.cs
+++ src/com/gemstone/gemfire/plugins/sqlite/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Gemstone.Gemfire.Plugins.SqLite")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Licensed to the Apache Software Foundation (ASF) under one or more contributor license agreements.  See the NOTICE file distributed with this work for additional information regarding copyright ownership.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- src/cppcache/src/gfcppBanner.cpp
+++ src/cppcache/src/gfcppBanner.cpp
@@ -2,21 +2,23 @@
 using namespace gemfire;
 std::string gfcppBanner::getBanner() {
   std::string str =
-      ""
-      " -----------------------------------------------------------------------"
-      "----\n"
-      " Copyright © 1997-2014 Pivotal Software, Inc. All Rights Reserved. This "
-      "product is\n"
-      " protected by U.S. and international copyright and intellectual "
-      "property\n"
-      " laws. Pivotal products are covered by one or more patents listed at \n"
-      " http://www.pivotal.io/patents.  Pivotal is a registered trademark or\n"
-      " trademark of Pivotal Software, Inc. in the United States and/or other "
-      "jurisdictions.\n"
-      " All other marks and names mentioned herein may be trademarks of their "
+      "---------------------------------------------------------------------------\n"
+      "Licensed to the Apache Software Foundation (ASF) under one or more\n"
+      "contributor license agreements.  See the NOTICE file distributed with\n"
+      "this work for additional information regarding copyright ownership.\n"
+      "The ASF licenses this file to You under the Apache License, Version 2.0\n"
+      "(the \"License\"); you may not use this file except in compliance with\n"
+      "the License.  You may obtain a copy of the License at\n"
       "\n"
-      " respective companies.\n"
-      " -----------------------------------------------------------------------"
-      "----\n";
+      "     http://www.apache.org/licenses/LICENSE-2.0\n"
+      "\n"
+      "Unless required by applicable law or agreed to in writing, software\n"
+      "distributed under the License is distributed on an \"AS IS\" BASIS,\n"
+      "WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n"
+      "See the License for the specific language governing permissions and\n"
+      "limitations under the License.\n"
+      "---------------------------------------------------------------------------\n";
   return str;
 }
+
+
