01c0286795d0a7ed0755fd81c8935160d5c7a8f4
Jacob Barrett <jbarrett@pivotal.io>
2017-01-26 13:45:23 -0800

    GEODE-2308: Rebrand shared library names.
    * GemStone.GemFire.Cache.dll -> Apache.Geode.dll
    * gfcppcache.dll -> apache-geode.dll
    
    This closes #371

--- examples/dist/cacheRunner/buildit.bat
+++ examples/dist/cacheRunner/buildit.bat
@@ -2,6 +2,6 @@
 
 rem GFCPP must be set
 
-cl /MD /Zc:wchar_t /EHsc /GR /wd4996 /D_EXAMPLE /D_CRT_SECURE_NO_DEPRECATE /D_CRT_NON_CONFORMING_SWPRINTFS /DWINVER=0x0500 /DBUILD_TESTOBJECT /I%GFCPP%/include /Fecacherunner.exe *.cpp %GFCPP%/lib/gfcppcache.lib
+cl /MD /Zc:wchar_t /EHsc /GR /wd4996 /D_EXAMPLE /D_CRT_SECURE_NO_DEPRECATE /D_CRT_NON_CONFORMING_SWPRINTFS /DWINVER=0x0500 /DBUILD_TESTOBJECT /I%GFCPP%/include /Fecacherunner.exe *.cpp %GFCPP%/lib/apache-geode.lib
 
 del *.obj
