340f2fca80d9388155ed0911712f9a830211b32b
Ernest Burghardt <eburghardt@pivotal.io>
2017-02-02 14:03:10 -0800

    GEODE-2421: Adding packer portion of making a VS2015 dev AMI
    This closes #384

---
+++ packer/windows/install-vs-2015-community.ps1
@@ -0,0 +1,9 @@
+# TODO AdminDeploy.xml
+# vs_community.exe /AdminFile C:\Users\Administrator\AdminDeployment.xml /Log setup.log /Passive
+Set-PSDebug -Trace 0
+
+Import-Module Packer
+
+$log = "vs_community.log"
+
+choco install visualstudio2015community -confirm
