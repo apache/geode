e148bcaa0fcd2a3ebf3fe624f5e79cc7a4035ad9
Ernest Burghardt <eburghardt@pivotal.io>
2017-02-03 13:32:34 -0800

    GEODE-2421: fix install of vs2015 to use PowerShell Install-Package

--- packer/windows/install-vs-2015-community.ps1
+++ packer/windows/install-vs-2015-community.ps1
@@ -6,4 +6,7 @@ Import-Module Packer
 
 $log = "vs_community.log"
 
-choco install visualstudio2015community -confirm
+Install-Package https://download.microsoft.com/download/0/B/C/0BC321A4-013F-479C-84E6-4A2F90B11269/vs_community.exe `
+    -Hash ED8D88D0AB88832754302BFC2A374E803B3A21C1590B428092944272F9EA30FE `
+    -ArgumentList @("/quiet", "/log", $log) `
+    -Log $log -Verbose
