60fc526d3bc119673e1b80cec3d509f5dd0d1975
Kirk Lund <klund@apache.org>
2017-02-09 15:26:09 -0800

    GEODE-2324: update test to Awaility 2.0

--- geode-core/src/test/java/org/apache/geode/internal/cache/tier/sockets/AcceptorImplDUnitTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/tier/sockets/AcceptorImplDUnitTest.java
@@ -15,7 +15,7 @@
 
 package org.apache.geode.internal.cache.tier.sockets;
 
-import com.jayway.awaitility.Awaitility;
+import org.awaitility.Awaitility;
 import org.apache.geode.cache.CacheFactory;
 import org.apache.geode.cache.EntryEvent;
 import org.apache.geode.cache.GemFireCache;
