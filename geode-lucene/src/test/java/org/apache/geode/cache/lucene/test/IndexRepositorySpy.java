e9b4250fe57818ae5e81aaefc8777a18d672cbdd
Dan Smith <upthewaterspout@apache.org>
2017-02-16 17:34:46 -0800

    Make sure IndexRepositorySpy doesn't mock the same object twice
    In some cases we can retry an operation and end up trying to spy a spy.

--- geode-lucene/src/test/java/org/apache/geode/cache/lucene/test/IndexRepositorySpy.java
+++ geode-lucene/src/test/java/org/apache/geode/cache/lucene/test/IndexRepositorySpy.java
@@ -58,6 +58,10 @@ public class IndexRepositorySpy extends IndexRepositoryFactory {
     if (indexRepo == null) {
       return null;
     }
+    if (mockingDetails(indexRepo).isSpy()) {
+      return indexRepo;
+    }
+
     final IndexRepository spy = Mockito.spy(indexRepo);
 
     Answer invokeBeforeWrite = invocation -> {
