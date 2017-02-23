55a4c7aef2ad3d9f1f0bb4957a76489a3d73de84
Jason Huynh <huynhja@gmail.com>
2017-02-22 17:06:52 -0800

    GEODE-2522: Removed debugging log

--- geode-lucene/src/test/java/org/apache/geode/cache/lucene/LuceneQueriesAccessorBase.java
+++ geode-lucene/src/test/java/org/apache/geode/cache/lucene/LuceneQueriesAccessorBase.java
@@ -101,7 +101,6 @@ public class LuceneQueriesAccessorBase extends LuceneDUnitTest {
       query = service.createLuceneQueryFactory().setResultLimit(1000).setPageSize(1000)
           .create(INDEX_NAME, REGION_NAME, queryString, defaultField);
       Collection<?> results = query.findKeys();
-      System.out.println("JASON Lucene Query returned:" + results.size());
 
       assertEquals(expectedResultsSize, results.size());
     });
