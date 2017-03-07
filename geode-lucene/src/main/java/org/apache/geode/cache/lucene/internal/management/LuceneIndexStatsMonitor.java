cecda0015583decaffd0929464a75a578fd4ac55
nabarun <nnag@pivotal.io>
2017-03-07 15:05:47 -0800

    GEODE-2620: Rate stats removed from LuceneIndexMetrics
    	* Commit Rate, update rate and query rate removed from LuceneIndexMetrics
    	* They can be added back again when the getRate method in StatsRate.java is fixed

--- geode-lucene/src/main/java/org/apache/geode/cache/lucene/internal/management/LuceneIndexStatsMonitor.java
+++ geode-lucene/src/main/java/org/apache/geode/cache/lucene/internal/management/LuceneIndexStatsMonitor.java
@@ -24,15 +24,9 @@ import org.apache.geode.management.internal.beans.stats.StatsRate;
 
 public class LuceneIndexStatsMonitor extends MBeanStatsMonitor {
 
-  private StatsRate updateRate;
-
   private StatsAverageLatency updateRateAverageLatency;
 
-  private StatsRate commitRate;
-
   private StatsAverageLatency commitRateAverageLatency;
-
-  private StatsRate queryRate;
 
   private StatsAverageLatency queryRateAverageLatency;
 
@@ -45,11 +39,6 @@ public class LuceneIndexStatsMonitor extends MBeanStatsMonitor {
   }
 
   private void configureMetrics() {
-    this.queryRate = new StatsRate(StatsKey.QUERIES, StatType.INT_TYPE, this);
-
-    this.updateRate = new StatsRate(StatsKey.UPDATES, StatType.INT_TYPE, this);
-
-    this.commitRate = new StatsRate(StatsKey.COMMITS, StatType.INT_TYPE, this);
 
     this.queryRateAverageLatency =
         new StatsAverageLatency(StatsKey.QUERIES, StatType.INT_TYPE, StatsKey.QUERY_TIME, this);
@@ -64,29 +53,26 @@ public class LuceneIndexStatsMonitor extends MBeanStatsMonitor {
   protected LuceneIndexMetrics getIndexMetrics(LuceneIndex index) {
     int queryExecutions = getStatistic(StatsKey.QUERIES).intValue();
     long queryExecutionTime = getStatistic(StatsKey.QUERY_TIME).longValue();
-    float queryRateValue = this.queryRate.getRate();
     long queryRateAverageLatencyValue = this.queryRateAverageLatency.getAverageLatency();
     int queryExecutionsInProgress = getStatistic(StatsKey.QUERIES_IN_PROGRESS).intValue();
     long queryExecutionTotalHits = getStatistic(StatsKey.QUERIES_TOTAL_HITS).longValue();
 
     int updates = getStatistic(StatsKey.UPDATES).intValue();
     long updateTime = getStatistic(StatsKey.UPDATE_TIME).longValue();
-    float updateRateValue = this.updateRate.getRate();
     long updateRateAverageLatencyValue = this.updateRateAverageLatency.getAverageLatency();
     int updatesInProgress = getStatistic(StatsKey.UPDATES_IN_PROGRESS).intValue();
 
     int commits = getStatistic(StatsKey.COMMITS).intValue();
     long commitTime = getStatistic(StatsKey.COMMIT_TIME).longValue();
-    float commitRateValue = this.commitRate.getRate();
     long commitRateAverageLatencyValue = this.commitRateAverageLatency.getAverageLatency();
     int commitsInProgress = getStatistic(StatsKey.COMMITS_IN_PROGRESS).intValue();
 
     int documents = getStatistic(StatsKey.DOCUMENTS).intValue();
 
     return new LuceneIndexMetrics(index.getRegionPath(), index.getName(), queryExecutions,
-        queryExecutionTime, queryRateValue, queryRateAverageLatencyValue, queryExecutionsInProgress,
-        queryExecutionTotalHits, updates, updateTime, updateRateValue,
-        updateRateAverageLatencyValue, updatesInProgress, commits, commitTime, commitRateValue,
-        commitRateAverageLatencyValue, commitsInProgress, documents);
+        queryExecutionTime, queryRateAverageLatencyValue, queryExecutionsInProgress,
+        queryExecutionTotalHits, updates, updateTime, updateRateAverageLatencyValue,
+        updatesInProgress, commits, commitTime, commitRateAverageLatencyValue, commitsInProgress,
+        documents);
   }
 }
