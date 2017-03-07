9d597dce47c3f9edc52cab0b70d9ca912a0c94d2
nabarun <nnag@pivotal.io>
2017-03-06 16:51:01 -0800

    GEODE-2604: Added javadoc comments to LuceneIndexMetrics.java

--- geode-lucene/src/main/java/org/apache/geode/cache/lucene/management/LuceneIndexMetrics.java
+++ geode-lucene/src/main/java/org/apache/geode/cache/lucene/management/LuceneIndexMetrics.java
@@ -14,6 +14,8 @@
  */
 package org.apache.geode.cache.lucene.management;
 
+import org.apache.geode.cache.lucene.LuceneIndex;
+
 import java.beans.ConstructorProperties;
 
 public class LuceneIndexMetrics {
@@ -92,82 +94,182 @@ public class LuceneIndexMetrics {
     this.documents = documents;
   }
 
+  /**
+   * Returns the {@link String} path for the region on which the {@link LuceneIndex} is created
+   *
+   * @return String value of the region path on the Lucene Index is created
+   */
   public String getRegionPath() {
     return this.regionPath;
   }
 
+  /**
+   * Returns the {@link String} name of the {@link LuceneIndex} created
+   *
+   * @return String value of the index name
+   */
   public String getIndexName() {
     return this.indexName;
   }
 
+  /**
+   * Returns the number of query executions using the {@link LuceneIndex}
+   *
+   * @return Number of queries executed using this Lucene index
+   */
   public int getQueryExecutions() {
     return this.queryExecutions;
   }
 
+  /**
+   * Returns the time duration for execution of queries using the {@link LuceneIndex}
+   *
+   * @return long value for the time consumed in the execution of queries using this Lucene Index
+   */
   public long getQueryExecutionTime() {
     return this.queryExecutionTime;
   }
 
+  /**
+   * Returns the rate of query execution using the {@link LuceneIndex}
+   *
+   * @return the rate of query execution using the Lucene Index
+   */
   public float getQueryRate() {
     return this.queryRate;
   }
 
+  /**
+   * Returns the average latency for query executions using the {@link LuceneIndex}
+   *
+   * @return the average latency for query executions using the Lucene Index
+   */
   public long getQueryRateAverageLatency() {
     return this.queryRateAverageLatency;
   }
 
+  /**
+   * Returns the number of query executions in progress which are using the {@link LuceneIndex}
+   *
+   * @return the number of query executions in progress which are using the Lucene Index
+   */
   public int getQueryExecutionsInProgress() {
     return this.queryExecutionsInProgress;
   }
 
+  /**
+   * Returns the number of hits for the query execution using the {@link LuceneIndex}
+   *
+   * @return the number of hit for the query execution using the Lucene Index
+   */
   public long getQueryExecutionTotalHits() {
     return this.queryExecutionTotalHits;
   }
 
+  /**
+   * Returns the number of update operations on the {@link LuceneIndex}
+   *
+   * @return the number of update operations on the Lucene Index
+   */
   public int getUpdates() {
     return this.updates;
   }
 
+  /**
+   * Returns the time consumed for the update operations on the {@link LuceneIndex}
+   *
+   * @return the time consumed for the update operations on the Lucene Index
+   */
   public long getUpdateTime() {
     return this.updateTime;
   }
 
+  /**
+   * Returns the rate at which update operations are executed on the {@link LuceneIndex}
+   *
+   * @return rate at which update operations are executed on the {@link LuceneIndex}
+   */
   public float getUpdateRate() {
     return this.updateRate;
   }
 
+  /**
+   * Returns the average latency for the update operations on the {@link LuceneIndex}
+   *
+   * @return the average latency for the update operations on the Lucene Index
+   */
   public long getUpdateRateAverageLatency() {
     return this.updateRateAverageLatency;
   }
 
+  /**
+   * Returns the number of update operations in progress for the {@link LuceneIndex}
+   *
+   * @return the number of update operations in progress for the Lucene Index
+   */
   public int getUpdatesInProgress() {
     return this.updatesInProgress;
   }
 
+  /**
+   * Returns the number of commit operation executed on the {@link LuceneIndex}
+   *
+   * @return the number of commit operation executed on the Lucene Index
+   */
   public int getCommits() {
     return this.commits;
   }
 
+  /**
+   * Returns the time consumed by the commit operations on the {@link LuceneIndex}
+   *
+   * @return the time consumed by the commit operations on the Lucene Index
+   */
   public long getCommitTime() {
     return this.commitTime;
   }
 
+  /**
+   * Returns the rate of commit operations on the {@link LuceneIndex}
+   *
+   * @return the rate of commit operations on the Lucene Index
+   */
   public float getCommitRate() {
     return this.commitRate;
   }
 
+  /**
+   * Returns the average latency for the commit operations using the {@link LuceneIndex}
+   *
+   * @return Returns the average latency for the commit operations using the Lucene Index
+   */
   public long getCommitRateAverageLatency() {
     return this.commitRateAverageLatency;
   }
 
+  /**
+   * Returns the number of commit operations in progress for the {@link LuceneIndex}
+   *
+   * @return Returns the number of commit operations in progress for the Lucene Indexes
+   */
   public int getCommitsInProgress() {
     return this.commitsInProgress;
   }
 
+  /**
+   * Returns the number of documents indexed by {@link LuceneIndex}
+   *
+   * @return Returns the number of documents indexed by Lucene
+   */
   public int getDocuments() {
     return documents;
   }
 
+  /**
+   * Outputs the string message containing all the stats stored for the {@link LuceneIndex}
+   *
+   * @return the string message containing all the stats stored for the {@link LuceneIndex}
+   */
   @Override
   public String toString() {
     return new StringBuilder().append(getClass().getSimpleName()).append("[").append("regionPath=")
