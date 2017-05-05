b29e1bda431c55b4287582672c7f7c5d3e684386
Kirk Lund <klund@apache.org>
2017-05-05 15:33:27 -0700

    GEODE-2887: exclude build-* output directories from rat

--- geode-core/src/test/java/org/apache/geode/cache/query/LocalQueryServiceJUnitTest.java
+++ geode-core/src/test/java/org/apache/geode/cache/query/LocalQueryServiceJUnitTest.java
@@ -14,88 +14,74 @@
  */
 package org.apache.geode.cache.query;
 
-import org.apache.geode.internal.net.SocketCreatorFactory;
-import org.apache.geode.distributed.internal.DistributionConfigImpl;
+import static org.apache.geode.test.dunit.IgnoredException.addIgnoredException;
+import static org.junit.Assert.assertEquals;
+
 import org.apache.geode.cache.CacheTransactionManager;
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.client.ClientCache;
 import org.apache.geode.cache.client.ClientCacheFactory;
 import org.apache.geode.cache.client.ClientRegionFactory;
 import org.apache.geode.cache.client.ClientRegionShortcut;
-import org.apache.geode.test.junit.categories.UnitTest;
+import org.apache.geode.test.dunit.IgnoredException;
+import org.apache.geode.test.junit.categories.IntegrationTest;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import java.io.IOException;
-import java.util.Properties;
 
-import static org.junit.Assert.assertEquals;
-
-@Category(UnitTest.class)
+@Category(IntegrationTest.class)
 public class LocalQueryServiceJUnitTest {
+
+  private IgnoredException ignoredException;
+  private ClientCache clientCache;
+  private int numEntries;
+
+  @Before
+  public void before() throws Exception {
+    this.ignoredException = addIgnoredException(IOException.class.getName());
+
+    this.clientCache = new ClientCacheFactory().create();
+    ClientRegionFactory factory =
+        this.clientCache.createClientRegionFactory(ClientRegionShortcut.LOCAL);
+    Region region = factory.create("localRegion");
+    this.numEntries = 10;
+    for (int i = 0; i < this.numEntries; i++) {
+      region.put(i, i);
+    }
+  }
+
+  @After
+  public void after() throws Exception {
+    if (this.clientCache != null) {
+      this.clientCache.close();
+    }
+    this.ignoredException.remove();
+  }
 
   @Test
   public void testLocalQueryService() throws Exception {
-    ClientCache c = null;
-    try {
-      c = new ClientCacheFactory().create();
-      addExpectedException(c, IOException.class.getName());
-      Region r = createRegion(c);
-      int numEntries = 10;
-      loadRegion(r, numEntries);
-      SelectResults sR = executeLocalQuery(c, "SELECT * from /localRegion");
-      assertEquals(numEntries, sR.size());
-    } finally {
-      if (c != null) {
-        c.close();
-      }
-    }
+    SelectResults sR = executeLocalQuery(this.clientCache, "SELECT * from /localRegion");
+
+    assertEquals(this.numEntries, sR.size());
   }
 
   @Test
   public void testLocalQueryServiceWithTransaction() throws Exception {
-    ClientCache c = null;
-    try {
-      SocketCreatorFactory.setDistributionConfig(new DistributionConfigImpl(new Properties()));
-      c = new ClientCacheFactory().create();
-      addExpectedException(c, IOException.class.getName());
-      Region r = createRegion(c);
-      int numEntries = 10;
-      loadRegion(r, numEntries);
-      CacheTransactionManager cacheTransactionManager = c.getCacheTransactionManager();
-      cacheTransactionManager.begin();
-      SelectResults sR = executeLocalQuery(c, "SELECT * from /localRegion");
-      assertEquals(numEntries, sR.size());
-      cacheTransactionManager.commit();
-    } finally {
-      if (c != null) {
-        c.close();
-      }
-    }
+    CacheTransactionManager cacheTransactionManager = clientCache.getCacheTransactionManager();
+    cacheTransactionManager.begin();
+    SelectResults selectResults = executeLocalQuery(this.clientCache, "SELECT * from /localRegion");
+
+    assertEquals(this.numEntries, selectResults.size());
+
+    cacheTransactionManager.commit();
   }
 
-  private Region createRegion(ClientCache c) {
-    ClientRegionFactory factory = c.createClientRegionFactory(ClientRegionShortcut.LOCAL);
-    return factory.create("localRegion");
-  }
-
-  private void loadRegion(Region r, int numEntries) {
-    for (int i = 0; i < numEntries; i++) {
-      r.put(i, i);
-    }
-  }
-
-  private SelectResults executeLocalQuery(ClientCache c, String qS) throws QueryException {
+  private SelectResults executeLocalQuery(ClientCache c, String query) throws QueryException {
     QueryService qs = c.getLocalQueryService();
-    Query q = qs.newQuery("SELECT * from /localRegion");
+    Query q = qs.newQuery(query);
     return (SelectResults) q.execute();
-  }
-
-  private void addExpectedException(ClientCache c, String exception) {
-    c.getLogger().info("<ExpectedException action=add>" + exception + "</ExpectedException>");
-  }
-
-  private void removeExpectedException(ClientCache c, String exception) {
-    c.getLogger().info("<ExpectedException action=remove>" + exception + "</ExpectedException>");
   }
 }
