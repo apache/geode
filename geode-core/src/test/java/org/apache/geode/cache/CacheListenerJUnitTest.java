92cc47148b33a0e8bad9e85d62a397fae2e07f94
Shankar <shankar@ampool.io>
2017-05-10 12:59:42 +0530

    GEODE-231: Remove deprecated AttributesMutator.setCacheListener
    Removed commented test

--- geode-core/src/test/java/org/apache/geode/cache/CacheListenerJUnitTest.java
+++ geode-core/src/test/java/org/apache/geode/cache/CacheListenerJUnitTest.java
@@ -156,12 +156,6 @@ public class CacheListenerJUnitTest {
     } catch (IllegalStateException expected) {
     }
 
-    // commented this test as this is no more valid test scenario with new APIs
-    // try {
-    // am.setCacheListener(cl1);
-    // fail("expected IllegalStateException");
-    // } catch (IllegalStateException expected) {
-    // }
     am.removeCacheListener(cl1);
     assertEquals(Arrays.asList(new CacheListener[] {cl2}), Arrays.asList(ra.getCacheListeners()));
     am.removeCacheListener(cl1);
