7987502527d27259cf0b95b749da5c6cc7397271
masaki.yamakawa <masaki.yamakawa@ulsystems.co.jp>
2017-05-13 00:35:49 +0900

    GEODE-2853: Change of locator list request interval
    Replace the manual cleanup of system properties with the Rule.

--- geode-core/src/test/java/org/apache/geode/cache/client/internal/AutoConnectionSourceImplJUnitTest.java
+++ geode-core/src/test/java/org/apache/geode/cache/client/internal/AutoConnectionSourceImplJUnitTest.java
@@ -40,7 +40,9 @@ import org.apache.geode.test.junit.categories.ClientServerTest;
 import org.apache.geode.test.junit.categories.IntegrationTest;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.RestoreSystemProperties;
 import org.junit.experimental.categories.Category;
 
 import java.io.IOException;
@@ -80,6 +82,9 @@ public class AutoConnectionSourceImplJUnitTest {
   private ScheduledExecutorService background;
   private PoolStats poolStats;
 
+  @Rule
+  public final RestoreSystemProperties restoreSystemProperties = new RestoreSystemProperties();
+
   @Before
   public void setUp() throws Exception {
     Properties props = new Properties();
@@ -110,7 +115,6 @@ public class AutoConnectionSourceImplJUnitTest {
 
   @After
   public void tearDown() {
-    System.clearProperty(DistributionConfig.GEMFIRE_PREFIX + "LOCATOR_UPDATE_INTERVAL");
     background.shutdownNow();
     try {
       cache.close();
