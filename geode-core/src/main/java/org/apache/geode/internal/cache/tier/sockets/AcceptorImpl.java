9354cacfc6a8fa79746e8240730f6a209199bb89
Galen O'Sullivan <gosullivan@pivotal.io>
2017-06-07 17:04:17 -0700

    GEODE-3051: Removing obsolete SSL handling in `AcceptorImpl.accept` catch block

--- geode-core/src/main/java/org/apache/geode/internal/cache/tier/sockets/AcceptorImpl.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/tier/sockets/AcceptorImpl.java
@@ -1249,19 +1249,6 @@ public class AcceptorImpl extends Acceptor implements Runnable {
           }
         }
       } catch (IOException e) {
-        if (isRunning()) {
-          if (e instanceof SSLException) {
-            try {
-              // Try to send a proper rejection message
-              ServerHandShakeProcessor.refuse(s.getOutputStream(), e.toString(),
-                  HandShake.REPLY_EXCEPTION_AUTHENTICATION_FAILED);
-            } catch (IOException ex) {
-              if (logger.isDebugEnabled()) {
-                logger.debug("Bridge server: Unable to write SSL error");
-              }
-            }
-          }
-        }
         closeSocket(s);
         if (isRunning()) {
           if (!this.loggedAcceptError) {
@@ -1269,8 +1256,6 @@ public class AcceptorImpl extends Acceptor implements Runnable {
             logger.error(LocalizedMessage.create(
                 LocalizedStrings.AcceptorImpl_CACHE_SERVER_UNEXPECTED_IOEXCEPTION_FROM_ACCEPT, e));
           }
-          // Why sleep?
-          // try {Thread.sleep(3000);} catch (InterruptedException ie) {}
         }
       } catch (CancelException e) {
         closeSocket(s);
