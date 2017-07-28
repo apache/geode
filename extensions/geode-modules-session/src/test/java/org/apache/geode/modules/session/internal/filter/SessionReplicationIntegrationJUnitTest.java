9d59402b71beea84199c79399aa0260955a19d2c
Lynn Hughes-Godfrey <lhughesgodfrey@pivotal.io>
2017-07-28 16:15:01 -0700

    GEODE-2226: SessionReplicationIntegrationTests do not run on Windows
    * Disable SessionReplicationIntegrationTests on Windows
    
    This closes #669

--- extensions/geode-modules-session/src/test/java/org/apache/geode/modules/session/internal/filter/SessionReplicationIntegrationJUnitTest.java
+++ extensions/geode-modules-session/src/test/java/org/apache/geode/modules/session/internal/filter/SessionReplicationIntegrationJUnitTest.java
@@ -35,6 +35,7 @@ import org.eclipse.jetty.http.HttpTester;
 import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.junit.After;
+import org.junit.Assume;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
@@ -83,6 +84,9 @@ public class SessionReplicationIntegrationJUnitTest {
 
   @Before
   public void setUp() throws Exception {
+
+    Assume.assumeFalse(System.getProperty("os.name").toLowerCase().contains("win"));
+
     File gemfireLogFile = new File(tmpdir.newFolder(), "gemfire_modules.log");
 
     request = HttpTester.newRequest();
