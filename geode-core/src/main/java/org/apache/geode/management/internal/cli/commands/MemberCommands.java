47377c8086e57a88a87244ed77cad48e43495dd5
Kirk Lund <klund@apache.org>
2017-08-12 09:38:20 -0700

    Revert "GEODE-3264: Refactoring MemberCommands"
    This reverts commit d27f8b956de7d9c5d95ebdc68dfc67ee8b2d7b51.

--- geode-core/src/main/java/org/apache/geode/management/internal/cli/commands/MemberCommands.java
+++ geode-core/src/main/java/org/apache/geode/management/internal/cli/commands/MemberCommands.java
@@ -12,40 +12,93 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package org.apache.geode.management.internal.cli.commands;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.springframework.shell.core.annotation.CliCommand;
 import org.springframework.shell.core.annotation.CliOption;
 
 import org.apache.geode.cache.CacheClosedException;
+import org.apache.geode.cache.execute.FunctionInvocationTargetException;
 import org.apache.geode.cache.execute.ResultCollector;
 import org.apache.geode.distributed.DistributedMember;
+import org.apache.geode.internal.cache.InternalCache;
 import org.apache.geode.management.cli.CliMetaData;
 import org.apache.geode.management.cli.ConverterHint;
 import org.apache.geode.management.cli.Result;
 import org.apache.geode.management.internal.cli.CliUtil;
+import org.apache.geode.management.internal.cli.LogWrapper;
 import org.apache.geode.management.internal.cli.domain.CacheServerInfo;
 import org.apache.geode.management.internal.cli.domain.MemberInformation;
 import org.apache.geode.management.internal.cli.functions.GetMemberInformationFunction;
 import org.apache.geode.management.internal.cli.i18n.CliStrings;
 import org.apache.geode.management.internal.cli.result.CompositeResultData;
+import org.apache.geode.management.internal.cli.result.CompositeResultData.SectionResultData;
 import org.apache.geode.management.internal.cli.result.ResultBuilder;
+import org.apache.geode.management.internal.cli.result.TabularResultData;
 import org.apache.geode.management.internal.security.ResourceOperation;
-import org.apache.geode.security.ResourcePermission;
+import org.apache.geode.security.ResourcePermission.Operation;
+import org.apache.geode.security.ResourcePermission.Resource;
 
-public class DescribeMemberCommand implements GfshCommand {
+/**
+ * @since GemFire 7.0
+ */
+public class MemberCommands implements GfshCommand {
+
   private static final GetMemberInformationFunction getMemberInformation =
       new GetMemberInformationFunction();
 
+  @CliCommand(value = {CliStrings.LIST_MEMBER}, help = CliStrings.LIST_MEMBER__HELP)
+  @CliMetaData(shellOnly = false, relatedTopic = CliStrings.TOPIC_GEODE_SERVER)
+  @ResourceOperation(resource = Resource.CLUSTER, operation = Operation.READ)
+  public Result listMember(@CliOption(key = {CliStrings.GROUP}, unspecifiedDefaultValue = "",
+      optionContext = ConverterHint.MEMBERGROUP,
+      help = CliStrings.LIST_MEMBER__GROUP__HELP) String group) {
+    Result result = null;
+
+    // TODO: Add the code for identifying the system services
+    try {
+      Set<DistributedMember> memberSet = new TreeSet<DistributedMember>();
+      InternalCache cache = getCache();
+
+      // default get all the members in the DS
+      if (group.isEmpty()) {
+        memberSet.addAll(CliUtil.getAllMembers(cache));
+      } else {
+        memberSet.addAll(cache.getDistributedSystem().getGroupMembers(group));
+      }
+
+      if (memberSet.isEmpty()) {
+        result = ResultBuilder.createInfoResult(CliStrings.LIST_MEMBER__MSG__NO_MEMBER_FOUND);
+      } else {
+        TabularResultData resultData = ResultBuilder.createTabularResultData();
+        Iterator<DistributedMember> memberIters = memberSet.iterator();
+        while (memberIters.hasNext()) {
+          DistributedMember member = memberIters.next();
+          resultData.accumulate("Name", member.getName());
+          resultData.accumulate("Id", member.getId());
+        }
+
+        result = ResultBuilder.buildResult(resultData);
+      }
+    } catch (Exception e) {
+
+      result = ResultBuilder
+          .createGemFireErrorResult("Could not fetch the list of members. " + e.getMessage());
+      LogWrapper.getInstance().warning(e.getMessage(), e);
+    }
+
+    return result;
+  }
+
   @CliCommand(value = {CliStrings.DESCRIBE_MEMBER}, help = CliStrings.DESCRIBE_MEMBER__HELP)
-  @CliMetaData(relatedTopic = CliStrings.TOPIC_GEODE_SERVER)
-  @ResourceOperation(resource = ResourcePermission.Resource.CLUSTER,
-      operation = ResourcePermission.Operation.READ)
+  @CliMetaData(shellOnly = false, relatedTopic = CliStrings.TOPIC_GEODE_SERVER)
+  @ResourceOperation(resource = Resource.CLUSTER, operation = Operation.READ)
   public Result describeMember(@CliOption(key = CliStrings.DESCRIBE_MEMBER__IDENTIFIER,
       optionContext = ConverterHint.ALL_MEMBER_IDNAME,
       help = CliStrings.DESCRIBE_MEMBER__IDENTIFIER__HELP,
@@ -57,6 +110,12 @@ public class DescribeMemberCommand implements GfshCommand {
           CliUtil.getDistributedMemberByNameOrId(memberNameOrId);
 
       if (memberToBeDescribed != null) {
+        // This information should be available through the MBeans too. We might not need
+        // the function.
+
+        // Yes, but then the command is subject to Mbean availability, which would be
+        // affected once MBean filters are used.
+
         ResultCollector<?, ?> rc =
             CliUtil.executeFunction(getMemberInformation, null, memberToBeDescribed);
 
@@ -73,7 +132,7 @@ public class DescribeMemberCommand implements GfshCommand {
           memberInformation.setHost(memberToBeDescribed.getHost());
           memberInformation.setProcessId("" + memberToBeDescribed.getProcessId());
 
-          CompositeResultData.SectionResultData section = crd.addSection();
+          SectionResultData section = crd.addSection();
           section.addData("Name", memberInformation.getName());
           section.addData("Id", memberInformation.getId());
           section.addData("Host", memberInformation.getHost());
@@ -95,7 +154,7 @@ public class DescribeMemberCommand implements GfshCommand {
           section.addData("Locators", memberInformation.getLocators());
 
           if (memberInformation.isServer()) {
-            CompositeResultData.SectionResultData clientServiceSection = crd.addSection();
+            SectionResultData clientServiceSection = crd.addSection();
             List<CacheServerInfo> csList = memberInformation.getCacheServeInfo();
 
             if (csList != null) {
@@ -124,10 +183,14 @@ public class DescribeMemberCommand implements GfshCommand {
         result = ResultBuilder.createInfoResult(CliStrings
             .format(CliStrings.DESCRIBE_MEMBER__MSG__NOT_FOUND, new Object[] {memberNameOrId}));
       }
-    } catch (CacheClosedException ignored) {
+    } catch (CacheClosedException e) {
+
+    } catch (FunctionInvocationTargetException e) {
+      result = ResultBuilder.createGemFireErrorResult(e.getMessage());
     } catch (Exception e) {
       result = ResultBuilder.createGemFireErrorResult(e.getMessage());
     }
     return result;
   }
+
 }
--- geode-core/src/test/java/org/apache/geode/management/internal/security/TestCommand.java
+++ geode-core/src/test/java/org/apache/geode/management/internal/security/TestCommand.java
@@ -218,7 +218,7 @@ public class TestCommand {
     // createTestCommand("stop locator --name=locator1", clusterManage);
     // createTestCommand("stop server --name=server1", clusterManage);
 
-    // DescribeMemberCommand, ListMemberCommand
+    // MemberCommands
     createTestCommand("describe member --name=server1", clusterRead);
     createTestCommand("list members", clusterRead);
 
