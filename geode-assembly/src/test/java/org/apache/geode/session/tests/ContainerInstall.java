be305896685e1b7d4d1f37724f673701277e5a76
Jason Huynh <huynhja@gmail.com>
2017-08-15 11:46:16 -0700

    Removed unused import

--- geode-assembly/src/test/java/org/apache/geode/session/tests/ContainerInstall.java
+++ geode-assembly/src/test/java/org/apache/geode/session/tests/ContainerInstall.java
@@ -18,7 +18,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URI;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Properties;
