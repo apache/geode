fbc31a84a968be56fee1fb39085da7795c84f650
Patrick Rhomberg <prhomberg@pivotal.io>
2017-09-12 11:42:52 -0700

    GEODE-3426: Ignore RestServersJUnitTest when default port is unavailable.

--- geode-assembly/src/test/java/org/apache/geode/rest/internal/web/RestServersJUnitTest.java
+++ geode-assembly/src/test/java/org/apache/geode/rest/internal/web/RestServersJUnitTest.java
@@ -15,46 +15,54 @@
 
 package org.apache.geode.rest.internal.web;
 
+import static org.apache.geode.distributed.internal.DistributionConfig.DEFAULT_HTTP_SERVICE_PORT;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assume.assumeTrue;
 
-import org.apache.geode.distributed.internal.DistributionConfig;
-import org.apache.geode.test.dunit.rules.ServerStarterRule;
-import org.apache.geode.test.junit.categories.IntegrationTest;
-import org.apache.geode.test.junit.categories.RestAPITest;
 import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.json.JSONArray;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import org.apache.geode.internal.AvailablePort;
+import org.apache.geode.test.dunit.rules.ServerStarterRule;
+import org.apache.geode.test.junit.categories.IntegrationTest;
+import org.apache.geode.test.junit.categories.RestAPITest;
+
 @Category({IntegrationTest.class, RestAPITest.class})
 public class RestServersJUnitTest {
 
   @ClassRule
-  public static ServerStarterRule serverStarter =
-      new ServerStarterRule().withRestService(true).withAutoStart();
+  public static ServerStarterRule serverStarter = new ServerStarterRule().withRestService(true);
+
 
   private static GeodeRestClient restClient;
 
   @BeforeClass
   public static void before() throws Exception {
-    assertThat(serverStarter.getHttpPort()).isEqualTo(DistributionConfig.DEFAULT_HTTP_SERVICE_PORT);
+    assumeTrue(
+        "Default port was unavailable for testing.  Please ensure the testing environment is clean.",
+        AvailablePort.isPortAvailable(DEFAULT_HTTP_SERVICE_PORT, AvailablePort.SOCKET));
+    serverStarter.startServer();
+    assertThat(serverStarter.getHttpPort()).isEqualTo(DEFAULT_HTTP_SERVICE_PORT);
     restClient = new GeodeRestClient("localhost", serverStarter.getHttpPort());
   }
 
   @Test
   public void testGet() throws Exception {
     HttpResponse response = restClient.doGet("/", null, null);
-    assertThat(GeodeRestClient.getCode(response)).isEqualTo(200);
+    assertThat(GeodeRestClient.getCode(response)).isEqualTo(HttpStatus.SC_OK);
   }
 
   @Test
   public void testServerStartedOnDefaultPort() throws Exception {
     HttpResponse response = restClient.doGet("/servers", null, null);
+    assertThat(GeodeRestClient.getCode(response)).isEqualTo(HttpStatus.SC_OK);
     JSONArray body = GeodeRestClient.getJsonArray(response);
     assertThat(body.length()).isEqualTo(1);
-    assertThat(body.getString(0))
-        .isEqualTo("http://localhost:" + DistributionConfig.DEFAULT_HTTP_SERVICE_PORT);
+    assertThat(body.getString(0)).isEqualTo("http://localhost:" + DEFAULT_HTTP_SERVICE_PORT);
   }
 }
