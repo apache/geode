fd7bf1832fe497905dfe8df2f156e9b7cff50995
Lynn Gallinat <lgallinat@pivotal.io>
2017-09-20 16:09:43 -0700

    GEODE-3187 Fix backup inspector and restore script for windows. Refactor RestoreScript.java and BackupInspector.java

--- geode-core/src/main/java/org/apache/geode/internal/cache/persistence/BackupInspector.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/persistence/BackupInspector.java
@@ -24,6 +24,8 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.apache.geode.internal.lang.SystemUtils;
+
 /**
  * Inspects a completed backup and parses the operation log file data from the restore script
  * produced by a previous backup.
@@ -34,17 +36,17 @@ public abstract class BackupInspector {
    * files. These lines will be added to future restore scripts if the operation logs are still
    * relevant to the member.
    */
-  protected Map<String, String> oplogLineMap = new HashMap<String, String>();
+  final Map<String, String> oplogLineMap = new HashMap<>();
 
   /**
    * Contains the unique set of operation log file names contained in the restore script.
    */
-  protected Set<String> oplogFileNames = new HashSet<String>();
+  final Set<String> oplogFileNames = new HashSet<>();
 
   /**
    * Root directory for a member's backup.
    */
-  protected File backupDir = null;
+  private final File backupDir;
 
   /**
    * Returns a BackupInspector for a member's backup directory.
@@ -53,8 +55,8 @@ public abstract class BackupInspector {
    * @return a new BackupInspector.
    * @throws IOException the backup directory was malformed.
    */
-  public static BackupInspector createInspector(File backupDir) throws IOException {
-    if (isWindows()) {
+  public static BackupInspector createInspector(final File backupDir) throws IOException {
+    if (SystemUtils.isWindows()) {
       return new WindowsBackupInspector(backupDir);
     }
 
@@ -67,56 +69,39 @@ public abstract class BackupInspector {
    * @param backupDir a a previous backup for a member.
    * @throws IOException an error occurred while parsing the restore script.
    */
-  public BackupInspector(File backupDir) throws IOException {
-    this.backupDir = backupDir;
-
+  BackupInspector(final File backupDir) throws IOException {
     if (!backupDir.exists()) {
       throw new IOException("Backup directory " + backupDir.getAbsolutePath() + " does not exist.");
     }
 
-    File restoreFile = getRestoreFile(backupDir);
+    this.backupDir = backupDir;
 
+    File restoreFile = getRestoreFile(backupDir);
     if (!restoreFile.exists()) {
       throw new IOException("Restore file " + restoreFile.getName() + " does not exist.");
     }
 
-    BufferedReader reader = null;
-
-    try {
-      reader = new BufferedReader(new FileReader(restoreFile));
+    try (BufferedReader reader = new BufferedReader(new FileReader(restoreFile))) {
       parseRestoreFile(reader);
-    } finally {
-      if (null != reader) {
-        reader.close();
-      }
     }
   }
 
   /**
-   * Searches for the incremental backup marker.
+   * Searches for the incremental backup marker and parses the incremental portion.
    * 
    * @param reader restore file reader.
-   * @throws IOException
    */
-  private void parseRestoreFile(BufferedReader reader) throws IOException {
+  private void parseRestoreFile(final BufferedReader reader) throws IOException {
     boolean markerFound = false;
-    String line = null;
-    String incrementalMarker = getIncrementalMarker();
 
+    String line = null;
     while (!markerFound && (null != (line = reader.readLine()))) {
-      markerFound = line.startsWith(incrementalMarker);
+      markerFound = line.contains(RestoreScript.INCREMENTAL_MARKER_COMMENT);
     }
 
     if (markerFound) {
       parseOplogLines(reader);
     }
-  }
-
-  /**
-   * @return true if the host operating system is windows.
-   */
-  public static boolean isWindows() {
-    return (System.getProperty("os.name").indexOf("Windows") != -1);
   }
 
   /**
@@ -138,7 +123,7 @@ public abstract class BackupInspector {
    * 
    * @param oplogFileName an operation log file.
    */
-  public String getScriptLineForOplogFile(String oplogFileName) {
+  public String getScriptLineForOplogFile(final String oplogFileName) {
     return this.oplogLineMap.get(oplogFileName);
   }
 
@@ -149,11 +134,6 @@ public abstract class BackupInspector {
   public Set<String> getIncrementalOplogFileNames() {
     return Collections.unmodifiableSet(this.oplogFileNames);
   }
-
-  /**
-   * @return the incremental marke contained in the backup restore script.
-   */
-  protected abstract String getIncrementalMarker();
 
   /**
    * Returns the restore script for the backup.
@@ -167,180 +147,19 @@ public abstract class BackupInspector {
    * 
    * @param oplogFileName an operation log file.
    */
-  public abstract String getCopyToForOplogFile(String oplogFileName);
+  public abstract String getCopyToForOplogFile(final String oplogFileName);
 
   /**
    * Returns the copy from operation log file path for an operation log file name.
    * 
    * @param oplogFileName an operation log file.
    */
-  public abstract String getCopyFromForOplogFile(String oplogFileName);
+  public abstract String getCopyFromForOplogFile(final String oplogFileName);
 
   /**
    * Parses out operation log data from the incremental backup portion of the restore script.
    * 
    * @param reader restore file reader.
-   * @throws IOException
    */
-  protected abstract void parseOplogLines(BufferedReader reader) throws IOException;
-}
-
-
-/**
- * A BackupInspector for the Windows platform(s).
- *
- */
-class WindowsBackupInspector extends BackupInspector {
-  /**
-   * When found indicates that the restore script was produced from an incremental backup.
-   */
-  private static final String INCREMENTAL_MARKER = "rem Incremental backup";
-
-  /**
-   * Restore file for windows platform.
-   */
-  static final String RESTORE_FILE = "restore.bat";
-
-  WindowsBackupInspector(File backupDir) throws IOException {
-    super(backupDir);
-  }
-
-  @Override
-  public String getCopyFromForOplogFile(String oplogFileName) {
-    String copyFrom = null;
-    String line = this.oplogLineMap.get(oplogFileName);
-
-    if (null != line) {
-      String[] parts = line.split("\\s");
-      copyFrom = parts[1].substring(1, parts[1].length() - 1) + File.separator + parts[3];
-    }
-
-    return copyFrom;
-  }
-
-  @Override
-  public String getCopyToForOplogFile(String oplogFileName) {
-    String copyTo = null;
-    String line = this.oplogLineMap.get(oplogFileName);
-
-    if (null != line) {
-      String[] parts = line.split("\\s");
-      copyTo = parts[2].substring(1, parts[2].length() - 1) + File.separator + parts[3];
-    }
-
-    return copyTo;
-  }
-
-  @Override
-  /**
-   * Parses out operation log data from the incremental backup portion of the restore script.
-   * 
-   * @param reader restore file reader.
-   * @throws IOException
-   */
-  protected void parseOplogLines(BufferedReader reader) throws IOException {
-    String line = null;
-
-    int beginIndex, endIndex;
-    String oplogName = "";
-    while (null != (line = reader.readLine())) {
-
-      if (line.startsWith("robocopy")) {
-        beginIndex = line.lastIndexOf("\"") + 2;
-        endIndex = line.indexOf("/njh", beginIndex) - 1;
-        oplogName = line.substring(beginIndex, endIndex);
-        this.oplogFileNames.add(oplogName);
-        this.oplogLineMap.put(oplogName, line);
-      } else if (line.startsWith("IF")) {
-        continue;
-      } else if (line.contains(RestoreScript.EXIT_MARKER)) {
-        break;
-      }
-    }
-  }
-
-  @Override
-  protected String getIncrementalMarker() {
-    return INCREMENTAL_MARKER;
-  }
-
-  @Override
-  protected File getRestoreFile(final File backupDir) {
-    return new File(backupDir, RESTORE_FILE);
-  }
-}
-
-
-/**
- * A BackupInspector for Unix platforms.
- */
-class UnixBackupInspector extends BackupInspector {
-  /**
-   * When found indicates that the restore script was produced from an incremental backup.
-   */
-  private static final String INCREMENTAL_MARKER = "#Incremental backup";
-
-  /**
-   * Restore file for windows platform.
-   */
-  static final String RESTORE_FILE = "restore.sh";
-
-  UnixBackupInspector(File backupDir) throws IOException {
-    super(backupDir);
-  }
-
-  @Override
-  public String getCopyFromForOplogFile(String oplogFileName) {
-    String copyFrom = null;
-    String line = this.oplogLineMap.get(oplogFileName);
-
-    if (null != line) {
-      String[] parts = line.split("\\s");
-      copyFrom = parts[2].substring(1, parts[2].length() - 1);
-    }
-
-    return copyFrom;
-  }
-
-  @Override
-  public String getCopyToForOplogFile(String oplogFileName) {
-    String copyTo = null;
-    String line = this.oplogLineMap.get(oplogFileName);
-
-    if (null != line) {
-      String[] parts = line.split("\\s");
-      copyTo = parts[3].substring(1, parts[3].length() - 1);
-    }
-
-    return copyTo;
-  }
-
-  @Override
-  /**
-   * Parses out operation log data from the incremental backup portion of the restore script.
-   * 
-   * @param reader restore file reader.
-   * @throws IOException
-   */
-  protected void parseOplogLines(BufferedReader reader) throws IOException {
-    String line = null;
-
-    while (null != (line = reader.readLine())) {
-      int beginIndex = line.lastIndexOf(File.separator) + 1;
-      int endIndex = line.length() - 1;
-      String oplogName = line.substring(beginIndex, endIndex);
-      this.oplogFileNames.add(oplogName);
-      this.oplogLineMap.put(oplogName, line);
-    }
-  }
-
-  @Override
-  protected String getIncrementalMarker() {
-    return INCREMENTAL_MARKER;
-  }
-
-  @Override
-  protected File getRestoreFile(final File backupDir) {
-    return new File(backupDir, RESTORE_FILE);
-  }
+  protected abstract void parseOplogLines(final BufferedReader reader) throws IOException;
 }
