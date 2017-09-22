c0aed25ff70896abcd6b1cc693ccb57fd64535eb
Anton Mironenko <anton.mironenko@gmail.com>
2017-09-22 15:12:42 +0300

    CacheXmlNotFoundUnitTest renamed to CacheXmlNotFoundRegressionTest

--- geode-core/src/test/java/org/apache/geode/cache30/CacheXmlNotFoundRegressionTest.java
+++ geode-core/src/test/java/org/apache/geode/cache30/CacheXmlNotFoundRegressionTest.java
@@ -29,15 +29,15 @@ import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.fail;
 
 @Category(IntegrationTest.class)
-public class CacheXmlNotFoundUnitTest {
+public class CacheXmlNotFoundRegressionTest {
 
   /**
    * unit test for <a href="https://issues.apache.org/jira/browse/GEODE-3038">GEODE-3038</a> Tests
    * that an error about missing cache-xml file is indeed printed in the text log file. The test
-   * {@link org.apache.geode.cache30.CacheXml66DUnitTest#testNonExistentFile()} is supposed to test
-   * the same, but is not enough. It only checks for an CacheXmlException exception to be thrown.
-   * Also in that test a log is printed into STDOUT, and we do see our error there, but that is not
-   * the case when we work with the real text log, specified via "log-file" param.
+   * {@link CacheXml66DUnitTest#testNonExistentFile()} is supposed to test the same, but is not
+   * enough. It only checks for an CacheXmlException exception to be thrown. Also in that test a log
+   * is printed into STDOUT, and we do see our error there, but that is not the case when we work
+   * with the real text log, specified via "log-file" param.
    */
   @Test
   public void testCacheXmlNotFoundInRealLog() throws Exception {
