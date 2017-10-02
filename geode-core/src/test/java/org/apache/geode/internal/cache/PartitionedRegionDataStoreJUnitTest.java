5cb2a591199845190751f52e4da758c5ccd4d44e
Darrel Schneider <dschneider@pivotal.io>
2017-10-02 13:46:07 -0700

    Revert "GEODE-3647: Fix race condition"
    This reverts commit 96149530d82e0b62e9df1a043bfd7d0e01d3411a.

--- geode-core/src/test/java/org/apache/geode/internal/cache/PartitionedRegionDataStoreJUnitTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/PartitionedRegionDataStoreJUnitTest.java
@@ -15,7 +15,10 @@
 package org.apache.geode.internal.cache;
 
 import org.apache.geode.cache.*;
+import org.apache.geode.cache.partition.PartitionRegionHelper;
 import org.apache.geode.distributed.DistributedSystem;
+import org.apache.geode.internal.cache.partitioned.PartitionedRegionObserverAdapter;
+import org.apache.geode.internal.cache.partitioned.PartitionedRegionObserverHolder;
 import org.apache.geode.test.junit.categories.IntegrationTest;
 import org.junit.After;
 import org.junit.Before;
@@ -23,6 +26,8 @@ import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 import static org.apache.geode.distributed.ConfigurationProperties.MCAST_PORT;
 import static org.junit.Assert.*;
@@ -101,6 +106,64 @@ public class PartitionedRegionDataStoreJUnitTest {
     pr.put(key, value);
     assertEquals(pr.get(key), value);
 
+  }
+
+  @Test
+  public void testChangeCacheLoaderDuringBucketCreation() throws Exception {
+    final PartitionedRegion pr =
+        (PartitionedRegion) cache.createRegionFactory(RegionShortcut.PARTITION)
+            .create("testChangeCacheLoaderDuringBucketCreation");
+
+    // Add an observer which will block bucket creation and wait for a loader to be added
+    final CountDownLatch loaderAdded = new CountDownLatch(1);
+    final CountDownLatch bucketCreated = new CountDownLatch(1);
+    PartitionedRegionObserverHolder.setInstance(new PartitionedRegionObserverAdapter() {
+      @Override
+      public void beforeAssignBucket(PartitionedRegion partitionedRegion, int bucketId) {
+        try {
+          // Indicate that the bucket has been created
+          bucketCreated.countDown();
+
+          // Wait for the loader to be added. if the synchronization
+          // is correct, this would wait for ever because setting the
+          // cache loader will wait for this method. So time out after
+          // 1 second, which should be good enough to cause a failure
+          // if the synchronization is broken.
+          loaderAdded.await(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+          throw new RuntimeException("Interrupted");
+        }
+      }
+    });
+
+    Thread createBuckets = new Thread() {
+      public void run() {
+        PartitionRegionHelper.assignBucketsToPartitions(pr);
+      }
+    };
+
+    createBuckets.start();
+
+    CacheLoader loader = new CacheLoader() {
+      @Override
+      public void close() {}
+
+      @Override
+      public Object load(LoaderHelper helper) throws CacheLoaderException {
+        return null;
+      }
+    };
+
+    bucketCreated.await();
+    pr.getAttributesMutator().setCacheLoader(loader);
+    loaderAdded.countDown();
+    createBuckets.join();
+
+
+    // Assert that all buckets have received the cache loader
+    for (BucketRegion bucket : pr.getDataStore().getAllLocalBucketRegions()) {
+      assertEquals(loader, bucket.getCacheLoader());
+    }
   }
 
   /**
