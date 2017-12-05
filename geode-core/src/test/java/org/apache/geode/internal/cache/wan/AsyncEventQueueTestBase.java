48a0ff4c4026415aed36490123f8ddbc795ec820
Dan Smith <upthewaterspout@apache.org>
2017-12-05 15:56:50 -0800

    GEODE-4058: Always delete disk files after every test runs
    Avoid hangs due to leftover disk files by making sure that all tests
    that use JUnit4DistributedCase have their disk files cleaned up after
    the test, including a locators ConfigDiskDir_

--- geode-core/src/test/java/org/apache/geode/internal/cache/wan/AsyncEventQueueTestBase.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/wan/AsyncEventQueueTestBase.java
@@ -99,7 +99,6 @@ import org.apache.geode.test.dunit.LogWriterUtils;
 import org.apache.geode.test.dunit.VM;
 import org.apache.geode.test.dunit.Wait;
 import org.apache.geode.test.dunit.WaitCriterion;
-import org.apache.geode.test.dunit.cache.internal.JUnit4CacheTestCase;
 import org.apache.geode.test.dunit.internal.JUnit4DistributedTestCase;
 import org.apache.geode.test.junit.categories.DistributedTest;
 
@@ -1558,7 +1557,7 @@ public class AsyncEventQueueTestBase extends JUnit4DistributedTestCase {
 
   public static void cleanupVM() throws IOException {
     closeCache();
-    JUnit4CacheTestCase.cleanDiskDirs();
+    JUnit4DistributedTestCase.cleanDiskDirs();
   }
 
   public static void closeCache() throws IOException {
