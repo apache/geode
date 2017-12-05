7497ecc5a78f98b54bb9f54e7413bd812bbfeac7
Anil <agingade@pivotal.io>
2017-12-05 15:14:17 -0800

    Added unit test for LRUListWithSyncSort

--- geode-core/src/test/java/org/apache/geode/internal/cache/eviction/LRUListWithSyncSortingTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/eviction/LRUListWithSyncSortingTest.java
@@ -15,16 +15,20 @@
 package org.apache.geode.internal.cache.eviction;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.atLeast;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.RestoreSystemProperties;
 import org.junit.experimental.categories.Category;
 import org.junit.rules.ExpectedException;
 
 import org.apache.geode.internal.cache.BucketRegion;
+import org.apache.geode.internal.lang.SystemPropertyHelper;
 import org.apache.geode.test.junit.categories.UnitTest;
 
 @Category(UnitTest.class)
@@ -32,6 +36,9 @@ public class LRUListWithSyncSortingTest {
 
   @Rule
   public ExpectedException thrown = ExpectedException.none();
+
+  @Rule
+  public RestoreSystemProperties restoreSystemProperties = new RestoreSystemProperties();
 
   private BucketRegion bucketRegion;
   private InternalEvictionStatistics stats;
@@ -60,6 +67,26 @@ public class LRUListWithSyncSortingTest {
     when(node.previous()).thenReturn(list.head);
     assertThat(list.getEvictableEntry()).isSameAs(node);
     assertThat(list.size()).isZero();
+  }
+
+  @Test
+  public void doesNotEvictRecentlyUsed() throws Exception {
+    LRUListWithSyncSorting list = new LRUListWithSyncSorting(stats, bucketRegion);
+    EvictionNode recentlyUsedNode = mock(EvictableEntry.class);
+    list.appendEntry(recentlyUsedNode);
+    when(recentlyUsedNode.isRecentlyUsed()).thenReturn(true);
+    EvictionNode node = mock(EvictableEntry.class);
+    list.appendEntry(node);
+
+    when(recentlyUsedNode.next()).thenReturn(node).thenReturn(null);
+    when(recentlyUsedNode.previous()).thenReturn(list.head);
+    when(node.next()).thenReturn(list.tail);
+    when(node.previous()).thenReturn(recentlyUsedNode);
+
+    assertThat(list.getEvictableEntry()).isSameAs(node);
+    assertThat(list.tail.previous()).isSameAs(recentlyUsedNode);
+    verify(recentlyUsedNode, atLeast(1)).unsetRecentlyUsed();
+    assertThat(list.size()).isOne();
   }
 
   @Test
@@ -98,6 +125,29 @@ public class LRUListWithSyncSortingTest {
     when(node.previous()).thenReturn(list.head);
     assertThat(list.getEvictableEntry()).isSameAs(node);
     assertThat(list.size()).isZero();
+  }
+
+  @Test
+  public void verifyRecentlyUsedNodeIsGreedilyEvicted() throws Exception {
+    System.setProperty("geode." + SystemPropertyHelper.EVICTION_SEARCH_MAX_ENTRIES, "1");
+
+    LRUListWithSyncSorting list = new LRUListWithSyncSorting(stats, bucketRegion);
+    EvictionNode recentlyUsedNode1 = mock(EvictableEntry.class, "RecentlyUsed1");
+    list.appendEntry(recentlyUsedNode1);
+    when(recentlyUsedNode1.isRecentlyUsed()).thenReturn(true);
+    EvictionNode recentlyUsedNode2 = mock(EvictableEntry.class, "RecentlyUsed2");
+    when(recentlyUsedNode2.isRecentlyUsed()).thenReturn(true);
+    list.appendEntry(recentlyUsedNode2);
+
+    when(recentlyUsedNode1.next()).thenReturn(recentlyUsedNode2).thenReturn(null);
+    when(recentlyUsedNode1.previous()).thenReturn(list.head);
+    when(recentlyUsedNode2.next()).thenReturn(list.tail);
+    when(recentlyUsedNode2.previous()).thenReturn(recentlyUsedNode1);
+
+    assertThat(list.getEvictableEntry()).isSameAs(recentlyUsedNode2);
+    assertThat(list.tail.previous()).isSameAs(recentlyUsedNode1);
+    verify(recentlyUsedNode1, atLeast(1)).unsetRecentlyUsed();
+    assertThat(list.size()).isOne();
   }
 
 }
