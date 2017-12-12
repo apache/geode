3b4a4a68ffb6c8a6d56484e4d38ba11451134658
Anil <agingade@pivotal.io>
2017-12-11 17:57:52 -0800

    GEODE-3869: Fix early eviction.
    While code review found that the refactoring moved "evictionNode.unsetRecentlyUsed()" to
    "appendEntry()" which gets call by other places (adding entry into the list); this causes
    the recently used entry to be evicted early.

--- geode-core/src/test/java/org/apache/geode/internal/cache/eviction/LRUListWithAsyncSortingTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/eviction/LRUListWithAsyncSortingTest.java
@@ -177,9 +177,9 @@ public class LRUListWithAsyncSortingTest {
     when(recentlyUsedNode.next()).thenReturn(list.tail);
     list.incrementRecentlyUsed();
 
-    // unsetRecentlyUsed() is called once on appendEntry(...) and once during scan
+    // unsetRecentlyUsed() is called once during scan
     await().atMost(10, TimeUnit.SECONDS)
-        .until(() -> verify(recentlyUsedNode, times(2)).unsetRecentlyUsed());
+        .until(() -> verify(recentlyUsedNode, times(1)).unsetRecentlyUsed());
     realExecutor.shutdown();
   }
 
@@ -196,9 +196,9 @@ public class LRUListWithAsyncSortingTest {
     when(recentlyUsedNode.next()).thenReturn(recentlyUsedNode);
     list.incrementRecentlyUsed();
 
-    // unsetRecentlyUsed() is called once on appendEntry(...) and once during scan
+    // unsetRecentlyUsed() is called once during scan
     await().atMost(10, TimeUnit.SECONDS)
-        .until(() -> verify(recentlyUsedNode, times(2)).unsetRecentlyUsed());
+        .until(() -> verify(recentlyUsedNode, times(1)).unsetRecentlyUsed());
     realExecutor.shutdown();
   }
 
@@ -226,9 +226,9 @@ public class LRUListWithAsyncSortingTest {
     when(recentlyUsedNode.isRecentlyUsed()).thenReturn(true);
     list.incrementRecentlyUsed();
 
-    // unsetRecentlyUsed() is called once on appendEntry(...) and once during scan
+    // unsetRecentlyUsed() is called once during scan
     await().atMost(10, TimeUnit.SECONDS)
-        .until(() -> verify(recentlyUsedNode, times(2)).unsetRecentlyUsed());
+        .until(() -> verify(recentlyUsedNode, times(1)).unsetRecentlyUsed());
     assertThat(list.tail.previous()).isEqualTo(recentlyUsedNode);
     realExecutor.shutdown();
   }
