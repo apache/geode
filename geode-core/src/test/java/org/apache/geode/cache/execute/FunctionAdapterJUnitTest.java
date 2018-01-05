55d65212c8399ec51ed8c82c2e682e7b8d087d8d
Jason Huynh <huynhja@gmail.com>
2018-01-05 11:12:20 -0800

    GEODE-4231: Renamed FunctionAdapter test resource file to have .ser extension

--- geode-core/src/test/java/org/apache/geode/cache/execute/FunctionAdapterJUnitTest.java
+++ geode-core/src/test/java/org/apache/geode/cache/execute/FunctionAdapterJUnitTest.java
@@ -1,7 +1,7 @@
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
  * agreements. See the NOTICE file distributed with this work for additional information regarding
- * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * copyright ownership. The ASF licenses this file to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
  *
@@ -12,6 +12,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
+
 package org.apache.geode.cache.execute;
 
 import static org.junit.Assert.*;
@@ -79,7 +80,7 @@ public class FunctionAdapterJUnitTest {
       throws Exception {
     FileInputStream fis =
         new FileInputStream(TestUtil.getResourcePath(getClass(), getClass().getSimpleName() + "."
-            + "serializedFunctionAdapterWithDifferentSerialVersionUID"));
+            + "serializedFunctionAdapterWithDifferentSerialVersionUID.ser"));
 
     DataInputStream dis = new VersionedDataInputStream(new DataInputStream(fis), Version.GFE_82);
     Object o = InternalDataSerializer.basicReadObject(dis);
