0d23b4fe937dc7f24b55ee290531b759e1e1e257
nabarun <nnag@pivotal.io>
2018-01-05 18:22:26 -0800

    GEODE-4227 : Cluster config updated during Lucene Reindex
    	* Cluster config is updated when Lucene indexes are created after the region is created.

--- geode-lucene/src/main/java/org/apache/geode/cache/lucene/internal/cli/functions/LuceneCreateIndexFunction.java
+++ geode-lucene/src/main/java/org/apache/geode/cache/lucene/internal/cli/functions/LuceneCreateIndexFunction.java
@@ -33,7 +33,9 @@ import org.apache.geode.cache.lucene.internal.LuceneServiceImpl;
 import org.apache.geode.cache.lucene.internal.cli.LuceneCliStrings;
 import org.apache.geode.cache.lucene.internal.cli.LuceneIndexDetails;
 import org.apache.geode.cache.lucene.internal.cli.LuceneIndexInfo;
+import org.apache.geode.cache.lucene.internal.xml.LuceneXmlConstants;
 import org.apache.geode.internal.InternalEntity;
+import org.apache.geode.internal.cache.xmlcache.CacheXml;
 import org.apache.geode.management.internal.cli.CliUtil;
 import org.apache.geode.management.internal.cli.functions.CliFunctionResult;
 import org.apache.geode.management.internal.cli.i18n.CliStrings;
@@ -65,10 +67,13 @@ public class LuceneCreateIndexFunction implements InternalEntity, Function {
     try {
       final LuceneIndexInfo indexInfo = (LuceneIndexInfo) context.getArguments();
       final Cache cache = context.getCache();
+      final String indexName = indexInfo.getIndexName();
+      final String regionPath = indexInfo.getRegionPath();
+
       memberId = cache.getDistributedSystem().getDistributedMember().getId();
       LuceneService service = LuceneServiceProvider.get(cache);
 
-      INDEX_NAME.validateName(indexInfo.getIndexName());
+      INDEX_NAME.validateName(indexName);
 
       String[] fields = indexInfo.getSearchableFieldNames();
       String[] analyzerName = indexInfo.getFieldAnalyzers();
@@ -93,21 +98,28 @@ public class LuceneCreateIndexFunction implements InternalEntity, Function {
         indexFactory.setLuceneSerializer(toSerializer(serializerName));
       }
 
-      REGION_PATH.validateName(indexInfo.getRegionPath());
+      XmlEntity xmlEntity = null;
+      REGION_PATH.validateName(regionPath);
       if (LuceneServiceImpl.LUCENE_REINDEX) {
-        indexFactory.create(indexInfo.getIndexName(), indexInfo.getRegionPath(), true);
+        indexFactory.create(indexName, regionPath, true);
+        if (cache.getRegion(regionPath) != null) {
+          xmlEntity = getXmlEntity(indexName, regionPath);
+        }
       } else {
-        indexFactory.create(indexInfo.getIndexName(), indexInfo.getRegionPath(), false);
+        indexFactory.create(indexName, regionPath, false);
       }
 
-      // TODO - update cluster configuration by returning a valid XmlEntity
-      XmlEntity xmlEntity = null;
       context.getResultSender().lastResult(new CliFunctionResult(memberId, xmlEntity));
     } catch (Exception e) {
       String exceptionMessage = CliStrings.format(CliStrings.EXCEPTION_CLASS_AND_MESSAGE,
           e.getClass().getName(), e.getMessage());
       context.getResultSender().lastResult(new CliFunctionResult(memberId, e, e.getMessage()));
     }
+  }
+
+  protected XmlEntity getXmlEntity(String indexName, String regionPath) {
+    String regionName = StringUtils.stripStart(regionPath, "/");
+    return new XmlEntity(CacheXml.REGION, "name", regionName);
   }
 
   private LuceneSerializer toSerializer(String serializerName)
