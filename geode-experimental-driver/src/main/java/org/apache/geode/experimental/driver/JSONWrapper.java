6af617091b7e3da1bf1813e33b5c40f1806cd8b6
Bruce Schuchardt <bschuchardt@pivotal.io>
2018-01-19 15:09:41 -0800

    GEODE-4315 Convert JSONWrapper to an interface
    JSONWrapper is now an interface.  JSONWrapperImpl is what will be returned
    by the experimental driver but it will accept anything implementing
    JSONWrapper as input.

--- geode-experimental-driver/src/main/java/org/apache/geode/experimental/driver/JSONWrapper.java
+++ geode-experimental-driver/src/main/java/org/apache/geode/experimental/driver/JSONWrapper.java
@@ -33,44 +33,51 @@ import org.apache.geode.annotations.Experimental;
  *
  */
 @Experimental
-public class JSONWrapper implements Comparable<JSONWrapper> {
-
-  protected final String jsonDocument;
+public interface JSONWrapper extends Comparable<JSONWrapper> {
 
   public static JSONWrapper wrapJSON(String jsonDocument) {
     if (jsonDocument == null) {
       throw new IllegalArgumentException("wrapped document may not be null");
     }
-    return new JSONWrapper(jsonDocument);
+    return new JSONWrapperImpl(jsonDocument);
   }
 
-  private JSONWrapper(String jsonDocument) {
-    this.jsonDocument = jsonDocument;
-  }
+  public String getJSON();
 
-  public String getJSON() {
-    return jsonDocument;
-  }
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+  static class JSONWrapperImpl implements JSONWrapper {
+
+
+    protected final String jsonDocument;
+
+    private JSONWrapperImpl(String jsonDocument) {
+      this.jsonDocument = jsonDocument;
     }
-    if (!(o instanceof JSONWrapper)) {
-      return false;
+
+    public String getJSON() {
+      return jsonDocument;
     }
-    JSONWrapper that = (JSONWrapper) o;
-    return jsonDocument.equals(that.jsonDocument);
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hash(jsonDocument);
-  }
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (!(o instanceof JSONWrapper)) {
+        return false;
+      }
+      JSONWrapper that = (JSONWrapper) o;
+      return jsonDocument.equals(that.getJSON());
+    }
 
-  @Override
-  public int compareTo(JSONWrapper o) {
-    return jsonDocument.compareTo(o.jsonDocument);
+    @Override
+    public int hashCode() {
+      return Objects.hash(jsonDocument);
+    }
+
+    @Override
+    public int compareTo(JSONWrapper o) {
+      return jsonDocument.compareTo(o.getJSON());
+    }
   }
 }
