4aede33bb142bd9e1ac0dd1433c446661e8b9ffc
Kirk Lund <klund@apache.org>
2018-01-31 14:11:44 -0800

    fixup

--- geode-core/src/test/java/org/apache/geode/internal/cache/EnforceUniqueHostForLonerIntegrationTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/EnforceUniqueHostForLonerIntegrationTest.java
@@ -20,30 +20,14 @@ import static org.apache.geode.distributed.ConfigurationProperties.ENFORCE_UNIQU
 import static org.apache.geode.distributed.ConfigurationProperties.LOCATORS;
 import static org.apache.geode.distributed.ConfigurationProperties.MCAST_PORT;
 import static org.apache.geode.distributed.ConfigurationProperties.REDUNDANCY_ZONE;
-import static org.apache.geode.distributed.internal.DistributionConfig.GEMFIRE_PREFIX;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.assertEquals;
-
-import java.util.Properties;
 
 import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.contrib.java.lang.system.RestoreSystemProperties;
 import org.junit.experimental.categories.Category;
 
-import org.apache.geode.cache.AttributesFactory;
 import org.apache.geode.cache.Cache;
-import org.apache.geode.cache.CacheExistsException;
 import org.apache.geode.cache.CacheFactory;
-import org.apache.geode.cache.PartitionAttributes;
-import org.apache.geode.cache.PartitionAttributesFactory;
-import org.apache.geode.cache.Region;
-import org.apache.geode.cache.RegionAttributes;
-import org.apache.geode.distributed.internal.DistributionConfig;
-import org.apache.geode.distributed.internal.InternalDistributedSystem;
-import org.apache.geode.test.dunit.Assert;
 import org.apache.geode.test.junit.categories.IntegrationTest;
 
 /**
@@ -57,18 +41,9 @@ import org.apache.geode.test.junit.categories.IntegrationTest;
  */
 
 @Category(IntegrationTest.class)
-public class EnforceUniqueHostInLonerIntegrationTest {
+public class EnforceUniqueHostForLonerIntegrationTest {
 
   private Cache cache;
-
-  @Rule
-  public RestoreSystemProperties restoreSystemProperties = new RestoreSystemProperties();
-
-  @Before
-  public void setUp() {
-    System.setProperty(GEMFIRE_PREFIX + "DISABLE_DISCONNECT_DS_ON_CACHE_CLOSE", "true");
-    cache = new CacheFactory().set(MCAST_PORT, "0").set(LOCATORS, "").set(ENFORCE_UNIQUE_HOST, "true").set(REDUNDANCY_ZONE, "zone1").create();
-  }
 
   @After
   public void tearDown() {
@@ -76,24 +51,12 @@ public class EnforceUniqueHostInLonerIntegrationTest {
   }
 
   /**
-   * Added for defect #47181. Use below combination to reproduce the issue: mcast-port = 0 Locators
-   * should be empty enforce-unique-host = true redundancy-zone = "zone"
+   * Creation of cache should not throw UnsupportedOperationException
    */
   @Test
-  public void testEnforceUniqueHostForLonerDistributedSystem() throws Exception {
-    AttributesFactory attr = new AttributesFactory();
-    PartitionAttributesFactory paf = new PartitionAttributesFactory();
-    PartitionAttributes prAttr = paf.create();
-    attr.setPartitionAttributes(prAttr);
-    RegionAttributes regionAttribs = attr.create();
-
-    Region<String, Integer> region = cache.createRegion("PR1", regionAttribs);
-
-    for (int i = 0; i < 113; i++) {
-      region.put("Key_" + i, i);
-    }
-
-    assertThat(region.size()).isEqualTo(113);
+  public void enforceUniqueHostShouldBeUsableForLoner() throws Exception {
+    cache = new CacheFactory().set(MCAST_PORT, "0").set(LOCATORS, "").set(ENFORCE_UNIQUE_HOST, "true").set(REDUNDANCY_ZONE, "zone1").create();
+    assertThat(cache.isClosed()).isFalse();
   }
 
 }
