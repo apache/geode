178dd46042ac94fa3fb0da1328dbf2ee00e1b349
Kirk Lund <klund@apache.org>
2018-02-06 14:17:13 -0800

    GEODE-1720: Cleanup PRQueryRegionCloseDUnitTest

--- geode-core/src/test/java/org/apache/geode/cache/query/partitioned/PRQueryRegionCloseDUnitTest.java
+++ geode-core/src/test/java/org/apache/geode/cache/query/partitioned/PRQueryRegionCloseDUnitTest.java
@@ -12,212 +12,133 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package org.apache.geode.cache.query.partitioned;
 
 import static org.apache.geode.cache.query.Utils.createPortfolioData;
+import static org.apache.geode.distributed.ConfigurationProperties.SERIALIZABLE_OBJECT_FILTER;
+import static org.apache.geode.test.dunit.Host.getHost;
+import static org.apache.geode.test.dunit.Invoke.invokeInEveryVM;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 import java.util.Random;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import org.apache.geode.cache.query.data.PortfolioData;
-import org.apache.geode.distributed.ConfigurationProperties;
-import org.apache.geode.internal.cache.ForceReattemptException;
-import org.apache.geode.test.dunit.Assert;
 import org.apache.geode.test.dunit.AsyncInvocation;
-import org.apache.geode.test.dunit.Host;
-import org.apache.geode.test.dunit.Invoke;
-import org.apache.geode.test.dunit.LogWriterUtils;
-import org.apache.geode.test.dunit.ThreadUtils;
 import org.apache.geode.test.dunit.VM;
-import org.apache.geode.test.dunit.Wait;
 import org.apache.geode.test.dunit.cache.CacheTestCase;
-import org.apache.geode.test.dunit.standalone.DUnitLauncher;
 import org.apache.geode.test.junit.categories.DistributedTest;
-import org.apache.geode.test.junit.categories.FlakyTest;
 
 @Category(DistributedTest.class)
 public class PRQueryRegionCloseDUnitTest extends CacheTestCase {
 
-  /**
-   * Tear down a PartitionedRegionTestCase by cleaning up the existing cache (mainly because we want
-   * to destroy any existing PartitionedRegions)
-   */
+  private static final String PARTITIONED_REGION_NAME = "Portfolios";
+  private static final String LOCAL_REGION_NAME = "LocalPortfolios";
+  private static final int CNT = 0;
+  private static final int CNT_DEST = 50;
+  private static final int REDUNDANCY = 1;
+  private static final int LOOP_SLEEP_TIME = 500;
+  private static final int NUMBER_OF_TEST_LOOPS = 10;
+
+  private VM vm0;
+  private VM vm1;
+  private VM vm2;
+  private List<VM> vmList;
+  private Random random;
+  private PortfolioData[] portfolio;
+  private PRQueryDUnitHelper prQueryDUnitHelper;
+
+  @Before
+  public void setUp() throws Exception {
+    vm0 = getHost(0).getVM(0);
+    vm1 = getHost(0).getVM(1);
+    vm2 = getHost(0).getVM(2);
+    setCacheInVMs(vm0, vm1, vm2);
+
+    vmList = new ArrayList<>();
+    vmList.add(vm1);
+    vmList.add(vm2);
+
+    random = new Random();
+    portfolio = createPortfolioData(CNT, CNT_DEST);
+    prQueryDUnitHelper = new PRQueryDUnitHelper();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    disconnectAllFromDS();
+    invokeInEveryVM(() -> PRQueryDUnitHelper.setCache(null));
+  }
+
   @Override
-  public final void preTearDownCacheTestCase() throws Exception {
-    preTearDownPartitionedRegionDUnitTest();
-    closeCache();
-    Invoke.invokeInEveryVM(org.apache.geode.cache30.CacheTestCase.class, "closeCache");
+  public Properties getDistributedSystemProperties() {
+    Properties config = new Properties();
+    config.put(SERIALIZABLE_OBJECT_FILTER, "org.apache.geode.cache.query.data.**");
+    return config;
   }
 
-  protected void preTearDownPartitionedRegionDUnitTest() throws Exception {}
+  /**
+   * 1. Creates PR regions across with scope = DACK, one accessor node & 2 datastores
+   * <p>
+   * 2. Creates a Local region on one of the VM's
+   * <p>
+   * 3. Puts in the same data both in PR region & the Local Region
+   * <p>
+   * 4. Queries the data both in local & PR
+   * <p>
+   * 5. Also calls Region.close() randomly on one of the datastore VM's with delay
+   * <p>
+   * 6. then recreates the PR on the same VM
+   * <p>
+   * 7. Verifies the size, type, and contents of both the resultSets obtained
+   */
+  @Test
+  public void testPRWithRegionCloseInOneDatastoreWithoutDelay() throws Exception {
+    vm0.invoke(prQueryDUnitHelper.getCacheSerializableRunnableForPRAccessorCreate(
+        PARTITIONED_REGION_NAME, REDUNDANCY, PortfolioData.class));
 
-  @BeforeClass
-  public static void caseSetUp() {
-    DUnitLauncher.launchIfNeeded();
-    // this makes sure we don't have any connection left over from previous tests
-    disconnectAllFromDS();
+    vm1.invoke(prQueryDUnitHelper.getCacheSerializableRunnableForPRCreate(PARTITIONED_REGION_NAME,
+        REDUNDANCY, PortfolioData.class));
+    vm2.invoke(prQueryDUnitHelper.getCacheSerializableRunnableForPRCreate(PARTITIONED_REGION_NAME,
+        REDUNDANCY, PortfolioData.class));
+
+    // creating a local region on one of the JVM's
+    vm0.invoke(prQueryDUnitHelper.getCacheSerializableRunnableForLocalRegionCreation(
+        LOCAL_REGION_NAME, PortfolioData.class));
+
+    // Putting the data into the accessor node
+    vm0.invoke(prQueryDUnitHelper.getCacheSerializableRunnableForPRPuts(PARTITIONED_REGION_NAME,
+        portfolio, CNT, CNT_DEST));
+
+    // Putting the same data in the local region created
+    vm0.invoke(prQueryDUnitHelper.getCacheSerializableRunnableForPRPuts(LOCAL_REGION_NAME,
+        portfolio, CNT, CNT_DEST));
+
+    AsyncInvocation async0 =
+        vm0.invokeAsync(prQueryDUnitHelper.getCacheSerializableRunnableForPRQueryAndCompareResults(
+            PARTITIONED_REGION_NAME, LOCAL_REGION_NAME));
+
+    for (int i = 0; i < NUMBER_OF_TEST_LOOPS; i++) {
+      int whichVM = random.nextInt(vmList.size());
+      if (whichVM > 0) {
+        vmList.get(whichVM).invoke(prQueryDUnitHelper.getCacheSerializableRunnableForRegionClose(
+            PARTITIONED_REGION_NAME, REDUNDANCY, PortfolioData.class));
+        Thread.sleep(LOOP_SLEEP_TIME);
+      }
+    }
+
+    async0.await();
   }
 
-  @AfterClass
-  public static void caseTearDown() {
-    // this makes sure we don't leave anything for the next tests
-    disconnectAllFromDS();
-  }
-
-  public PRQueryRegionCloseDUnitTest() {
-    super();
-  }
-
-  public void setCacheInVMs(VM... vms) {
+  private void setCacheInVMs(VM... vms) {
     for (VM vm : vms) {
       vm.invoke(() -> PRQueryDUnitHelper.setCache(getCache()));
     }
   }
-
-
-  int threadSleepTime = 500;
-
-  int queryTestCycle = 10;
-
-  PRQueryDUnitHelper PRQHelp = new PRQueryDUnitHelper();
-
-  final String name = "Portfolios";
-
-  final String localName = "LocalPortfolios";
-
-  final int cnt = 0, cntDest = 50;
-
-  final int redundancy = 1;
-
-  @Override
-  public Properties getDistributedSystemProperties() {
-    Properties properties = super.getDistributedSystemProperties();
-    properties.put(ConfigurationProperties.SERIALIZABLE_OBJECT_FILTER,
-        "org.apache.geode.cache.query.data.**");
-    return properties;
-  }
-
-
-  /**
-   * This test <br>
-   * 1. Creates PR regions across with scope = DACK, one accessor node & 2 datastores <br>
-   * 2. Creates a Local region on one of the VM's <br>
-   * 3. Puts in the same data both in PR region & the Local Region <br>
-   * 4. Queries the data both in local & PR <br>
-   * 5. Also calls Region.close() randomly on one of the datastore VM's with delay <br>
-   * 6. then recreates the PR on the same VM <br>
-   * 7. Verfies the size , type , contents of both the resultSets Obtained <br>
-   */
-  @Category(FlakyTest.class) // GEODE-1720
-  @Test
-  public void testPRWithRegionCloseInOneDatastoreWithoutDelay() throws Exception
-
-  {
-
-    LogWriterUtils.getLogWriter().info(
-        "PRQueryRegionCloseDUnitTest#testPRWithRegionCloseInOneDatastoreWithoutDelay: Querying PR Test with region Close PR operation*****");
-    Host host = Host.getHost(0);
-    VM vm0 = host.getVM(0);
-    VM vm1 = host.getVM(1);
-    VM vm2 = host.getVM(2);
-    setCacheInVMs(vm0, vm1, vm2);
-    List vmList = new LinkedList();
-    vmList.add(vm1);
-    vmList.add(vm2);
-
-    LogWriterUtils.getLogWriter().info(
-        "PRQueryRegionCloseDUnitTest#testPRWithRegionCloseInOneDatastoreWithoutDelay: Creating Accessor node on VM0");
-    vm0.invoke(PRQHelp.getCacheSerializableRunnableForPRAccessorCreate(name, redundancy,
-        PortfolioData.class));
-    LogWriterUtils.getLogWriter().info(
-        "PRQueryRegionCloseDUnitTest#testPRWithRegionCloseInOneDatastoreWithoutDelay: Successfully Created Accessor node on VM0");
-
-    LogWriterUtils.getLogWriter().info(
-        "PRQueryRegionCloseDUnitTest#testPRWithRegionCloseInOneDatastoreWithoutDelay: Creating PR's across all VM1 , VM2");
-    vm1.invoke(
-        PRQHelp.getCacheSerializableRunnableForPRCreate(name, redundancy, PortfolioData.class));
-    vm2.invoke(
-        PRQHelp.getCacheSerializableRunnableForPRCreate(name, redundancy, PortfolioData.class));
-    LogWriterUtils.getLogWriter().info(
-        "PRQueryRegionCloseDUnitTest#testPRWithRegionCloseInOneDatastoreWithoutDelay: Successfully Created PR on VM1 , VM2");
-
-    // creating a local region on one of the JVM's
-    LogWriterUtils.getLogWriter().info(
-        "PRQueryRegionCloseDUnitTest#testPRWithRegionCloseInOneDatastoreWithoutDelay: Creating Local Region on VM0");
-    vm0.invoke(
-        PRQHelp.getCacheSerializableRunnableForLocalRegionCreation(localName, PortfolioData.class));
-    LogWriterUtils.getLogWriter().info(
-        "PRQueryRegionCloseDUnitTest#testPRWithRegionCloseInOneDatastoreWithoutDelay: Successfully Created Local Region on VM0");
-
-    // Generating portfolio object array to be populated across the PR's & Local
-    // Regions
-
-    final PortfolioData[] portfolio = createPortfolioData(cnt, cntDest);
-
-    // Putting the data into the accessor node
-    LogWriterUtils.getLogWriter().info(
-        "PRQueryRegionCloseDUnitTest#testPRWithRegionCloseInOneDatastoreWithoutDelay: Inserting Portfolio data through the accessor node");
-    vm0.invoke(PRQHelp.getCacheSerializableRunnableForPRPuts(name, portfolio, cnt, cntDest));
-    LogWriterUtils.getLogWriter().info(
-        "PRQueryRegionCloseDUnitTest#testPRWithRegionCloseInOneDatastoreWithoutDelay: Successfully Inserted Portfolio data through the accessor node");
-
-    // Putting the same data in the local region created
-    LogWriterUtils.getLogWriter().info(
-        "PRQueryRegionCloseDUnitTest#testPRWithRegionCloseInOneDatastoreWithoutDelay: Inserting Portfolio data on local node  VM0 for result Set Comparison");
-    vm0.invoke(PRQHelp.getCacheSerializableRunnableForPRPuts(localName, portfolio, cnt, cntDest));
-    LogWriterUtils.getLogWriter().info(
-        "PRQueryRegionCloseDUnitTest#testPRWithRegionCloseInOneDatastoreWithoutDelay: Successfully Inserted Portfolio data on local node  VM0 for result Set Comparison");
-
-    Random random = new Random();
-    AsyncInvocation async0;
-    // querying the VM for data
-    LogWriterUtils.getLogWriter().info(
-        "PRQueryRegionCloseDUnitTest#testPRWithRegionCloseInOneDatastoreWithoutDelay: Querying on VM0 both on PR Region & local ,also  Comparing the Results sets from both");
-    async0 = vm0.invokeAsync(
-        PRQHelp.getCacheSerializableRunnableForPRQueryAndCompareResults(name, localName));
-
-    LogWriterUtils.getLogWriter().info(
-        "PRQueryRegionCloseDUnitTest#testPRWithRegionCloseInOneDatastoreWithoutDelay: Calling for Region.close() on either of the Datastores VM1 , VM2 at random and then recreating the cache, with a predefined Delay ");
-    for (int j = 0; j < queryTestCycle; j++) {
-      int k = (random.nextInt(vmList.size()));
-      if (0 != k) {
-        ((VM) (vmList.get(k))).invoke(PRQHelp.getCacheSerializableRunnableForRegionClose(name,
-            redundancy, PortfolioData.class));
-        Wait.pause(threadSleepTime);
-      }
-    }
-    ThreadUtils.join(async0, 30 * 1000);
-
-    if (async0.exceptionOccurred()) {
-      // for now, certain exceptions when a region is closed are acceptable
-      // including ForceReattemptException (e.g. resulting from RegionDestroyed)
-      boolean isForceReattempt = false;
-      Throwable t = async0.getException();
-      do {
-        if (t instanceof ForceReattemptException) {
-          isForceReattempt = true;
-          break;
-        }
-        t = t.getCause();
-      } while (t != null);
-
-      if (!isForceReattempt) {
-        Assert.fail("Unexpected exception during query", async0.getException());
-      }
-    }
-
-    LogWriterUtils.getLogWriter().info(
-        "PRQueryRegionCloseDUnitTest#testPRWithRegionCloseInOneDatastoreWithoutDelay: Querying with PR Operations ENDED*****");
-  }
-
-
-
 }
