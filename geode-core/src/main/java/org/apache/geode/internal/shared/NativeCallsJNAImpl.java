c076bef6471f43e227531c4829dd90b32931dd23
Anthony Baker <abaker@apache.org>
2018-02-06 17:17:07 -0800

    GEODE-4622 Update library dependencies
    The following libraries have been updated to more recent versions:
      commons-fileupload 1.3.2 => 1.3.3
      commons-io 2.5 => 2.6
      fastutil 7.1.0 => 8.1.1
      google-gson 2.8.0 => 2.8.2
      guava 21.0 => 24.0-jie
      HikariCP 2.7.4 => 2.7.6
      jackson 2.9.2 => 2.9.4
      jansi 1.14 => 1.17
      jgroups 3.6.10.Final => 3.6.14.Final
      jna 4.0.0 => 4.5.1
      jopt-simple 5.0.3 => 5.0.4
      lucene 6.4.1 => 6.6.2
      mx4j 3.0.1 => 3.0.2
      netty 4.1.8.Final => 4.1.21.Final
      protobuf 3.3.1 => 3.5.1
      shiro 1.3.2 => 1.4.0
      slf4j-api 1.7.24 => 1.7.25
      spring-hateoas 0.23.0.RELEASE => 0.24.0.RELEASE
      spring-ldap-core 2.3.1.RELEASE => 2.3.2.RELEASE
      spring-security 4.2.3.RELEASE => 4.2.4.RELEASE
      spring 4.3.13.RELEASE => 4.3.14.RELEASE
      springfox 2.6.1 => 2.8.0
    
    Add LICENSE entry for reflections. Update NOTICE versions.  Remove
    explicit dependency on commons-beanutils that is not required.

--- geode-core/src/main/java/org/apache/geode/internal/shared/NativeCallsJNAImpl.java
+++ geode-core/src/main/java/org/apache/geode/internal/shared/NativeCallsJNAImpl.java
@@ -897,7 +897,7 @@ public class NativeCallsJNAImpl {
       public int keepaliveinterval;
 
       @Override
-      protected List<?> getFieldOrder() {
+      protected List<String> getFieldOrder() {
         return Arrays.asList(new String[] {"onoff", "keepalivetime", "keepaliveinterval"});
       }
     }
