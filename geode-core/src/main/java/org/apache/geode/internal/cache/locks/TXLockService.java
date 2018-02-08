1520879454a0e18c6b3ab2146ebe66097cbde091
Darrel Schneider <dschneider@pivotal.io>
2018-02-08 11:20:29 -0800

    GEODE-4588: remove call of getAnyInstance (#1394)
    Now an InternalDistributedSystem is passed in to the TXLockServiceImpl
    constructor. Note that this service depends on the distributed system
    instead of the cache.

--- geode-core/src/main/java/org/apache/geode/internal/cache/locks/TXLockService.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/locks/TXLockService.java
@@ -18,6 +18,7 @@ package org.apache.geode.internal.cache.locks;
 import java.util.*;
 
 import org.apache.geode.cache.CommitConflictException;
+import org.apache.geode.distributed.internal.InternalDistributedSystem;
 import org.apache.geode.distributed.internal.locks.*;
 
 /**
@@ -45,10 +46,10 @@ public abstract class TXLockService {
   }
 
   /** Returns (or creates) the instance of the distributed TXLockService */
-  public static TXLockService createDTLS() {
+  public static TXLockService createDTLS(InternalDistributedSystem system) {
     synchronized (TXLockService.class) {
       if (DTLS == null || DTLS.isDestroyed()) {
-        DTLS = new TXLockServiceImpl(DLockService.DTLS);
+        DTLS = new TXLockServiceImpl(DLockService.DTLS, system);
       }
       return DTLS;
     }
