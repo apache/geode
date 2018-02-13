954ccb545d24a9c9a35cbd84023a4d7e07032de0
jinmeiliao <jiliao@pivotal.io>
2018-02-13 11:15:38 -0800

    GEODE-3974: revert a change to log the exception in the server logs (#1444)

--- geode-core/src/main/java/org/apache/geode/internal/cache/MemberFunctionStreamingMessage.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/MemberFunctionStreamingMessage.java
@@ -215,9 +215,10 @@ public class MemberFunctionStreamingMessage extends DistributionMessage
       rex = new ReplyException(thr);
       replyWithException(dm, rex);
     } catch (Exception exception) {
-      logger.error("Exception occurred on remote member while executing Function: {}",
-          this.functionObject.getId(), exception);
-
+      if (logger.isDebugEnabled()) {
+        logger.debug("Exception occurred on remote member while executing Function: {}",
+            this.functionObject.getId(), exception);
+      }
       stats.endFunctionExecutionWithException(this.functionObject.hasResult());
       rex = new ReplyException(exception);
       replyWithException(dm, rex);
