3b6a9aaf9d1ae6608a1fd9ffee01b399a6a11452
Darrel Schneider <dschneider@pivotal.io>
2018-02-23 11:32:44 -0800

    GEODE-4559: pass the Cache to a Declarable (#1422)
    * Declarable now has a default method named "initialize(Cache, Properties)".
    This allows a Declarable to know the cache that created it.
    Deprecated Declarable.init(Properties).
    Note that for backwards compatibility, the product calls
    both these methods. Also the two Declarables that the product
    implements, AutoBalancer and ReflectionBasedAutoSerializer,
    implement both these methods but after the first calls subsequent
    calls of init or initialize will be noops.
    
    * initialize on ReflectionBasedAutoSerializer now call setRegionService with the cache.
    init and initialize can now be called multiple times and each time the properties will
    be set again. This is for backwards compatibility.
    
    * The AutoBalancer no longer looks up the static cache.
    but instead of given one by the product calling setCache
    during initialization.

--- geode-core/src/main/java/org/apache/geode/management/internal/cli/functions/RegionCreateFunction.java
+++ geode-core/src/main/java/org/apache/geode/management/internal/cli/functions/RegionCreateFunction.java
@@ -190,12 +190,13 @@ public class RegionCreateFunction implements InternalFunction {
     }
 
     if (regionCreateArgs.getEntryIdleTimeCustomExpiry() != null) {
-      factory
-          .setCustomEntryIdleTimeout(regionCreateArgs.getEntryIdleTimeCustomExpiry().newInstance());
+      factory.setCustomEntryIdleTimeout(
+          regionCreateArgs.getEntryIdleTimeCustomExpiry().newInstance(cache));
     }
 
     if (regionCreateArgs.getEntryTTLCustomExpiry() != null) {
-      factory.setCustomEntryTimeToLive(regionCreateArgs.getEntryTTLCustomExpiry().newInstance());
+      factory
+          .setCustomEntryTimeToLive(regionCreateArgs.getEntryTTLCustomExpiry().newInstance(cache));
     }
 
     final RegionFunctionArgs.ExpirationAttrs entryExpirationTTL =
@@ -287,7 +288,7 @@ public class RegionCreateFunction implements InternalFunction {
     if (cacheListeners != null && !cacheListeners.isEmpty()) {
       List<CacheListener<K, V>> newListeners = new ArrayList<>();
       for (ClassName<CacheListener> cacheListener : cacheListeners) {
-        newListeners.add(cacheListener.newInstance());
+        newListeners.add(cacheListener.newInstance(cache));
       }
       factory.initCacheListeners(newListeners.toArray(new CacheListener[0]));
     }
@@ -302,12 +303,12 @@ public class RegionCreateFunction implements InternalFunction {
 
     final ClassName<CacheLoader> cacheLoader = regionCreateArgs.getCacheLoader();
     if (cacheLoader != null) {
-      factory.setCacheLoader(cacheLoader.newInstance());
+      factory.setCacheLoader(cacheLoader.newInstance(cache));
     }
 
     final ClassName<CacheWriter> cacheWriter = regionCreateArgs.getCacheWriter();
     if (cacheWriter != null) {
-      factory.setCacheWriter(cacheWriter.newInstance());
+      factory.setCacheWriter(cacheWriter.newInstance(cache));
     }
 
     // If a region path indicates a sub-region,
