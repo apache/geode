22be0df1dedc6d8567d3de4c9bda55e8f6dc73a5
Sai Boorlagadda <sai.boorlagadda@gmail.com>
2018-03-05 17:35:00 -0800

    no-sandbox option for selenium to run chrome

--- geode-pulse/src/test/java/org/apache/geode/tools/pulse/tests/rules/WebDriverRule.java
+++ geode-pulse/src/test/java/org/apache/geode/tools/pulse/tests/rules/WebDriverRule.java
@@ -91,6 +91,7 @@ public class WebDriverRule extends ExternalResource {
   private void setUpWebDriver() {
     ChromeOptions options = new ChromeOptions();
     options.addArguments("headless");
+    options.addArguments("no-sandbox");
     options.addArguments("window-size=1200x600");
     driver = new ChromeDriver(options);
     driver.manage().window().maximize();
