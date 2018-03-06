441733d5a60d036cc07f9ee8edc47584507fa7c0
Darrel Schneider <dschneider@pivotal.io>
2018-03-06 11:59:44 -0800

    added jdbc client end to end test that covers all supported pdx fields using the reflection based auto serializer

--- geode-connectors/src/test/java/org/apache/geode/connectors/jdbc/JdbcDUnitTest.java
+++ geode-connectors/src/test/java/org/apache/geode/connectors/jdbc/JdbcDUnitTest.java
@@ -23,22 +23,35 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 import org.awaitility.Awaitility;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.RestoreSystemProperties;
 import org.junit.experimental.categories.Category;
 
 import org.apache.geode.cache.Region;
+import org.apache.geode.cache.RegionShortcut;
+import org.apache.geode.cache.client.ClientCacheFactory;
+import org.apache.geode.cache.client.ClientRegionShortcut;
 import org.apache.geode.pdx.PdxInstance;
+import org.apache.geode.pdx.ReflectionBasedAutoSerializer;
+import org.apache.geode.pdx.internal.AutoSerializableManager;
 import org.apache.geode.test.dunit.IgnoredException;
+import org.apache.geode.test.dunit.VM;
+import org.apache.geode.test.dunit.rules.ClientVM;
 import org.apache.geode.test.dunit.rules.ClusterStartupRule;
+import org.apache.geode.test.dunit.rules.DistributedRestoreSystemProperties;
 import org.apache.geode.test.dunit.rules.MemberVM;
 import org.apache.geode.test.junit.categories.DistributedTest;
 import org.apache.geode.test.junit.rules.GfshCommandRule;
+import org.apache.geode.test.junit.rules.RuleList;
 import org.apache.geode.test.junit.rules.serializable.SerializableTestName;
 
 /**
@@ -54,30 +67,51 @@ public class JdbcDUnitTest implements Serializable {
   private static final String CONNECTION_URL = "jdbc:derby:memory:" + DB_NAME + ";create=true";
   private static final String CONNECTION_NAME = "TestConnection";
 
+
   @Rule
   public transient GfshCommandRule gfsh = new GfshCommandRule();
 
   @Rule
-  public ClusterStartupRule startupRule = new ClusterStartupRule();
+  public transient ClusterStartupRule startupRule = new ClusterStartupRule();
 
   @Rule
   public SerializableTestName testName = new SerializableTestName();
 
+  @Rule
+  public DistributedRestoreSystemProperties restoreSystemProperties =
+      new DistributedRestoreSystemProperties();
+
   private MemberVM server;
+  private MemberVM locator;
 
   @Before
   public void setup() throws Exception {
-    MemberVM locator = startupRule.startLocatorVM(0);
+    locator = startupRule.startLocatorVM(0);
     gfsh.connectAndVerify(locator);
-    server = startupRule.startServerVM(1, locator.getPort());
-    server.invoke(() -> createTable());
   }
 
   private void createTable() throws SQLException {
-    Connection connection = DriverManager.getConnection(CONNECTION_URL);
-    Statement statement = connection.createStatement();
-    statement.execute("Create Table " + TABLE_NAME
-        + " (id varchar(10) primary key not null, name varchar(10), age int)");
+    server = startupRule.startServerVM(1, x -> x.withConnectionToLocator(locator.getPort()));
+    server.invoke(() -> {
+      Connection connection = DriverManager.getConnection(CONNECTION_URL);
+      Statement statement = connection.createStatement();
+      statement.execute("Create Table " + TABLE_NAME
+          + " (id varchar(10) primary key not null, name varchar(10), age int)");
+    });
+  }
+
+  private void createTableForAllSupportedFields() throws SQLException {
+    server = startupRule.startServerVM(1,
+        x -> x.withConnectionToLocator(locator.getPort()).withPDXReadSerialized());
+    server.invoke(() -> {
+      Connection connection = DriverManager.getConnection(CONNECTION_URL);
+      Statement statement = connection.createStatement();
+      statement.execute("Create Table " + TABLE_NAME + " (id varchar(10) primary key not null, "
+          + "aboolean smallint, " + "abyte smallint, " + "ashort smallint, " + "anint int, "
+          + "along bigint, " + "afloat float, " + "adouble float, " + "astring varchar(10), "
+          + "adate timestamp, " + "anobject varchar(20), " + "abytearray blob(100), "
+          + "achar char(1))");
+    });
   }
 
   @After
@@ -105,7 +139,8 @@ public class JdbcDUnitTest implements Serializable {
 
   @Test
   public void throwsExceptionWhenNoMappingExistsUsingWriter() throws Exception {
-    createRegion(true, false, false);
+    createTable();
+    createRegionUsingGfsh(true, false, false);
     createJdbcConnection();
 
     server.invoke(() -> {
@@ -121,8 +156,9 @@ public class JdbcDUnitTest implements Serializable {
 
   @Test
   public void throwsExceptionWhenNoMappingExistsUsingAsyncWriter() throws Exception {
+    createTable();
     IgnoredException.addIgnoredException("JdbcConnectorException");
-    createRegion(false, true, false);
+    createRegionUsingGfsh(false, true, false);
     createJdbcConnection();
 
     server.invoke(() -> {
@@ -143,7 +179,8 @@ public class JdbcDUnitTest implements Serializable {
 
   @Test
   public void throwsExceptionWhenNoMappingMatches() throws Exception {
-    createRegion(true, false, false);
+    createTable();
+    createRegionUsingGfsh(true, false, false);
     createJdbcConnection();
     createMapping("NoSuchRegion", CONNECTION_NAME);
 
@@ -160,7 +197,8 @@ public class JdbcDUnitTest implements Serializable {
 
   @Test
   public void throwsExceptionWhenNoConnectionExists() throws Exception {
-    createRegion(true, false, false);
+    createTable();
+    createRegionUsingGfsh(true, false, false);
     createMapping(REGION_NAME, CONNECTION_NAME);
 
     server.invoke(() -> {
@@ -176,7 +214,8 @@ public class JdbcDUnitTest implements Serializable {
 
   @Test
   public void putWritesToDB() throws Exception {
-    createRegion(true, false, false);
+    createTable();
+    createRegionUsingGfsh(true, false, false);
     createJdbcConnection();
     createMapping(REGION_NAME, CONNECTION_NAME);
     server.invoke(() -> {
@@ -192,7 +231,8 @@ public class JdbcDUnitTest implements Serializable {
 
   @Test
   public void putAsyncWritesToDB() throws Exception {
-    createRegion(true, false, false);
+    createTable();
+    createRegionUsingGfsh(true, false, false);
     createJdbcConnection();
     createMapping(REGION_NAME, CONNECTION_NAME);
     server.invoke(() -> {
@@ -208,7 +248,8 @@ public class JdbcDUnitTest implements Serializable {
 
   @Test
   public void getReadsFromEmptyDB() throws Exception {
-    createRegion(false, false, true);
+    createTable();
+    createRegionUsingGfsh(false, false, true);
     createJdbcConnection();
     createMapping(REGION_NAME, CONNECTION_NAME);
     server.invoke(() -> {
@@ -221,7 +262,8 @@ public class JdbcDUnitTest implements Serializable {
 
   @Test
   public void getReadsFromDB() throws Exception {
-    createRegion(true, false, true);
+    createTable();
+    createRegionUsingGfsh(true, false, true);
     createJdbcConnection();
     createMapping(REGION_NAME, CONNECTION_NAME);
     server.invoke(() -> {
@@ -244,7 +286,8 @@ public class JdbcDUnitTest implements Serializable {
 
   @Test
   public void getReadsFromDBWithPdxClassName() throws Exception {
-    createRegion(true, false, true);
+    createTable();
+    createRegionUsingGfsh(true, false, true);
     createJdbcConnection();
     createMapping(REGION_NAME, CONNECTION_NAME, Employee.class.getName());
     server.invoke(() -> {
@@ -257,6 +300,46 @@ public class JdbcDUnitTest implements Serializable {
       Employee result = (Employee) region.get(key);
       assertThat(result.getName()).isEqualTo("Emp1");
       assertThat(result.getAge()).isEqualTo(55);
+    });
+  }
+
+  @Test
+  public void clientGetReadsFromDBWithPdxClassName() throws Exception {
+    createTableForAllSupportedFields();
+    ClientVM client = getClientVM();
+    createClientRegion(client);
+
+    createRegionUsingGfsh(true, false, true);
+    createJdbcConnection();
+    createMapping(REGION_NAME, CONNECTION_NAME, ClassWithSupportedPdxFields.class.getName());
+    client.invoke(() -> {
+      String key = "id1";
+      ClassWithSupportedPdxFields value = new ClassWithSupportedPdxFields(true, (byte) 1, (short) 2,
+          3, 4, 5.5f, 6.0, "BigEmp", new Date(100000), "BigEmpObject", new byte[] {1, 2}, 'c');
+      Region<String, ClassWithSupportedPdxFields> region =
+          ClusterStartupRule.getClientCache().getRegion(REGION_NAME);
+      region.put(key, value);
+      region.invalidate(key);
+
+      ClassWithSupportedPdxFields result = region.get(key);
+      assertThat(result).isEqualTo(value);
+    });
+  }
+
+  private ClientVM getClientVM() throws Exception {
+    Consumer<ClientCacheFactory> cacheSetup = (Serializable & Consumer<ClientCacheFactory>) cf -> {
+      System.setProperty(AutoSerializableManager.NO_HARDCODED_EXCLUDES_PARAM, "true");
+      cf.addPoolLocator("localhost", locator.getPort());
+      cf.setPdxSerializer(
+          new ReflectionBasedAutoSerializer(ClassWithSupportedPdxFields.class.getName()));
+    };
+    return startupRule.startClientVM(2, new Properties(), cacheSetup);
+  }
+
+  private void createClientRegion(ClientVM client) {
+    client.invoke(() -> {
+      ClusterStartupRule.getClientCache().createClientRegionFactory(ClientRegionShortcut.PROXY)
+          .create(REGION_NAME);
     });
   }
 
@@ -273,7 +356,8 @@ public class JdbcDUnitTest implements Serializable {
     gfsh.executeAndAssertThat(commandStr).statusIsSuccess();
   }
 
-  private void createRegion(boolean withCacheWriter, boolean withAsyncWriter, boolean withLoader) {
+  private void createRegionUsingGfsh(boolean withCacheWriter, boolean withAsyncWriter,
+      boolean withLoader) {
     StringBuffer createRegionCmd = new StringBuffer();
     createRegionCmd.append("create region --name=" + REGION_NAME + " --type=REPLICATE ");
     if (withCacheWriter) {
