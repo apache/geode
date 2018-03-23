9af64809f27573768d9af4306d22a84b67633107
Nick Reich <nreich@pivotal.io>
2018-03-23 13:58:12 -0700

    GEODE-4933: Include members with NORMAL and PRELOADED data policy in expiration
      * If the data policy for a replicate region is PRELOADED, for example, if
        eviction is used and local destroy is the eviction action, that member was
        being excluded from the set of members to get last access time before
        expiration. Those members are now included.
    
      * Members that were caching proxies, thus having the NORMAL data policy,
        were also being excluded and are not included in the last access time
        lookup.

--- geode-core/src/test/java/org/apache/geode/internal/cache/ReplicateEntryIdleExpirationDistributedTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/ReplicateEntryIdleExpirationDistributedTest.java
@@ -17,6 +17,7 @@ package org.apache.geode.internal.cache;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.apache.geode.cache.ExpirationAction.DESTROY;
 import static org.apache.geode.cache.RegionShortcut.REPLICATE;
+import static org.apache.geode.cache.RegionShortcut.REPLICATE_PROXY;
 import static org.apache.geode.test.dunit.Host.getHost;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
@@ -32,9 +33,12 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import org.apache.geode.cache.DataPolicy;
+import org.apache.geode.cache.EvictionAttributes;
 import org.apache.geode.cache.ExpirationAttributes;
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.RegionFactory;
+import org.apache.geode.cache.Scope;
 import org.apache.geode.test.dunit.AsyncInvocation;
 import org.apache.geode.test.dunit.VM;
 import org.apache.geode.test.dunit.rules.CacheRule;
@@ -60,7 +64,7 @@ public class ReplicateEntryIdleExpirationDistributedTest implements Serializable
 
   @Rule
   public CacheRule cacheRule = CacheRule.builder().createCacheIn(member1).createCacheIn(member2)
-      .createCacheIn(member3).build();
+      .createCacheIn(member3).createCacheIn(getHost(0).getVM(3)).build();
 
   @Before
   public void setUp() throws Exception {
@@ -120,9 +124,100 @@ public class ReplicateEntryIdleExpirationDistributedTest implements Serializable
     memberReading.await();
   }
 
+  @Test
+  public void readsNormalMemberShouldPreventExpiration() throws Exception {
+    VM member4 = getHost(0).getVM(3);
+    member4.invoke(() -> {
+      KEEP_READING.set(true);
+      ExpiryTask.suspendExpiration();
+
+      RegionFactory<String, String> factory = cacheRule.getCache().createRegionFactory();
+      factory.setDataPolicy(DataPolicy.NORMAL).setScope(Scope.DISTRIBUTED_ACK);
+      factory.setEntryIdleTimeout(new ExpirationAttributes(1, DESTROY));
+      factory.create(regionName);
+    });
+    AsyncInvocation<?> memberReading = member4.invokeAsync(() -> {
+      Region<String, String> region = cacheRule.getCache().getRegion(regionName);
+      region.put(KEY, VALUE);
+      while (KEEP_READING.get()) {
+        region.get(KEY);
+        Thread.sleep(10);
+      }
+    });
+
+    member2.invoke(() -> {
+      Region<String, String> region = cacheRule.getCache().getRegion(regionName);
+      await().atMost(30, SECONDS).until(() -> region.containsKey(KEY));
+      assertThat(region.containsKey(KEY)).isTrue();
+    });
+
+    member1.invoke(() -> {
+      Region<String, String> region = cacheRule.getCache().getRegion(regionName);
+      await().atMost(30, SECONDS).until(() -> region.containsKey(KEY));
+      assertThat(region.containsKey(KEY)).isTrue();
+
+      ExpiryTask.permitExpiration();
+      Stopwatch stopwatch = Stopwatch.createStarted();
+      while (stopwatch.elapsed(SECONDS) <= 5 && region.containsKey(KEY)) {
+        Thread.sleep(10);
+      }
+      assertThat(region.containsKey(KEY)).isTrue();
+    });
+
+    member4.invoke(() -> KEEP_READING.set(false));
+
+    memberReading.await();
+  }
+
+  @Test
+  public void readsInOtherMemberShouldPreventExpirationWhenEvictionEnabled() throws Exception {
+    String evictionRegionName = "evictionRegion";
+    member1.invoke(() -> createEvictionRegion(evictionRegionName));
+    member2.invoke(() -> createEvictionRegion(evictionRegionName));
+    member3.invoke(() -> createEvictionRegion(evictionRegionName));
+    AsyncInvocation<?> memberReading = member3.invokeAsync(() -> {
+      Region<String, String> region = cacheRule.getCache().getRegion(evictionRegionName);
+      region.put(KEY, VALUE);
+      while (KEEP_READING.get()) {
+        region.get(KEY);
+        Thread.sleep(10);
+      }
+    });
+
+    member2.invoke(() -> {
+      Region<String, String> region = cacheRule.getCache().getRegion(evictionRegionName);
+      await().atMost(30, SECONDS).until(() -> region.containsKey(KEY));
+      assertThat(region.containsKey(KEY)).isTrue();
+    });
+
+    member1.invoke(() -> {
+      Region<String, String> region = cacheRule.getCache().getRegion(evictionRegionName);
+      await().atMost(30, SECONDS).until(() -> region.containsKey(KEY));
+      assertThat(region.containsKey(KEY)).isTrue();
+
+      ExpiryTask.permitExpiration();
+      Stopwatch stopwatch = Stopwatch.createStarted();
+      while (stopwatch.elapsed(SECONDS) <= 5 && region.containsKey(KEY)) {
+        Thread.sleep(10);
+      }
+      assertThat(region.containsKey(KEY)).isTrue();
+    });
+
+    member3.invoke(() -> KEEP_READING.set(false));
+
+    memberReading.await();
+  }
+
   protected void createRegion() {
     RegionFactory<String, String> factory = cacheRule.getCache().createRegionFactory(REPLICATE);
     factory.setEntryIdleTimeout(new ExpirationAttributes(1, DESTROY));
+    factory.create(regionName);
+  }
+
+  private void createEvictionRegion(String regionName) {
+    RegionFactory<String, String> factory = cacheRule.getCache().createRegionFactory(REPLICATE);
+    factory.setEntryIdleTimeout(new ExpirationAttributes(1, DESTROY));
+    factory.setEvictionAttributes(EvictionAttributes.createLRUEntryAttributes(100));
     factory.create(regionName);
   }
 }
