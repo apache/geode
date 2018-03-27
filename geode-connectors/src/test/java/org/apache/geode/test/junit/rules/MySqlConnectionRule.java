a2b6962d1092ff8b4fb582000b08a65ab9601edc
Nick Reich <nreich@pivotal.io>
2018-03-27 10:00:02 -0700

    GEODE-4947: Add test using MySQL for JDBC connector
      * Add rule for standing up Docker instance of MySQL to enable testing

---
+++ geode-connectors/src/test/java/org/apache/geode/test/junit/rules/MySqlConnectionRule.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.geode.test.junit.rules;
+
+import com.palantir.docker.compose.DockerComposeRule;
+
+public class MySqlConnectionRule extends DatabaseConnectionRule {
+  private static final String CONNECTION_STRING =
+      "jdbc:mysql://$HOST:$EXTERNAL_PORT?user=root&useSSL=false";
+
+  protected MySqlConnectionRule(DockerComposeRule dockerRule, String serviceName, int port,
+      String dbName) {
+    super(dockerRule, serviceName, port, dbName);
+  }
+
+  @Override
+  protected String getConnectionString() {
+    return getDockerPort().inFormat(CONNECTION_STRING);
+  }
+
+  public static class Builder extends DatabaseConnectionRule.Builder {
+
+    public Builder() {
+      super();
+    }
+
+    @Override
+    public MySqlConnectionRule build() {
+      return new MySqlConnectionRule(createDockerRule(), getServiceName(), getPort(), getDbName());
+    }
+  }
+}
