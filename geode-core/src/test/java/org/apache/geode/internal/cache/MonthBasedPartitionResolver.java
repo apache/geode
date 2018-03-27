cb6cd2d295cb007615d479df9ab02211f72c3304
Kirk Lund <klund@apache.org>
2018-03-26 17:31:15 -0700

    GEODE-4876: Update PRCustomPartitioningDistributedTest use of VM and NetworkUtil
    * rename PRCustomPartitioningDUnitTest as PRCustomPartitioningDistributedTest

--- geode-core/src/test/java/org/apache/geode/internal/cache/MonthBasedPartitionResolver.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/MonthBasedPartitionResolver.java
@@ -28,7 +28,7 @@ import org.apache.geode.internal.cache.xmlcache.Declarable2;
  * partitioning.
  *
  * <p>
- * Extracted from {@link PRCustomPartitioningDUnitTest}.
+ * Extracted from {@link PRCustomPartitioningDistributedTest}.
  */
 class MonthBasedPartitionResolver implements PartitionResolver, Declarable2 {
 
