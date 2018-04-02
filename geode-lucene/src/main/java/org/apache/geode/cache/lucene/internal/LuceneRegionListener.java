cfee45a0c6ca668244a11acabaf1f423ac05d24e
Jason Huynh <huynhja@gmail.com>
2018-04-02 15:56:34 -0700

    GEODE-3926: Lucene Query Exception is thrown if queries are executed in the middle of reindexing a region
      * Add new internal exception, LuceneIndexCreationInProgressException
      * Index the region even if it's empty
      * Added awaitility clauses to reindexing tests
      * getRepositories will throw an exception if the index is not available
      * getRepositories will not throw an exception if the data region is empty (this scenario can occur when creating the index before the region)

--- geode-lucene/src/main/java/org/apache/geode/cache/lucene/internal/LuceneRegionListener.java
+++ geode-lucene/src/main/java/org/apache/geode/cache/lucene/internal/LuceneRegionListener.java
@@ -26,6 +26,7 @@ import org.apache.geode.cache.asyncqueue.internal.AsyncEventQueueImpl;
 import org.apache.geode.cache.lucene.LuceneSerializer;
 import org.apache.geode.internal.cache.InternalCache;
 import org.apache.geode.internal.cache.InternalRegionArguments;
+import org.apache.geode.internal.cache.PartitionedRegion;
 import org.apache.geode.internal.cache.RegionListener;
 
 public class LuceneRegionListener implements RegionListener {
@@ -108,6 +109,7 @@ public class LuceneRegionListener implements RegionListener {
     if (region.getFullPath().equals(this.regionPath)
         && this.afterCreateInvoked.compareAndSet(false, true)) {
       this.service.afterDataRegionCreated(this.luceneIndex);
+      this.service.createLuceneIndexOnDataRegion((PartitionedRegion) region, luceneIndex);
     }
   }
 
