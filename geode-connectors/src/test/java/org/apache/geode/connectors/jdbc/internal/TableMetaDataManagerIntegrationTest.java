5413739ea8f44e4a2415326dbba2a67fdcbafda2
Nick Reich <nreich@pivotal.io>
2018-04-03 14:56:53 -0700

    GEODE-4947: Make new category for tests that require external services

--- geode-connectors/src/test/java/org/apache/geode/connectors/jdbc/internal/TableMetaDataManagerIntegrationTest.java
+++ geode-connectors/src/test/java/org/apache/geode/connectors/jdbc/internal/TableMetaDataManagerIntegrationTest.java
@@ -28,9 +28,9 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-import org.apache.geode.test.junit.categories.IntegrationTest;
+import org.apache.geode.test.junit.categories.ExternalServiceTest;
 
-@Category(IntegrationTest.class)
+@Category(ExternalServiceTest.class)
 public abstract class TableMetaDataManagerIntegrationTest {
 
   private static final String REGION_TABLE_NAME = "employees";
