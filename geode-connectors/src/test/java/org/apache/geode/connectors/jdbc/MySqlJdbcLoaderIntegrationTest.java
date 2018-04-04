e9859d6600eb3e5c30ab80a23de7358611055727
Nick Reich <nreich@pivotal.io>
2018-04-04 08:41:54 -0700

    Revert "GEODE-4947: Make new category for tests that require external services"
    This reverts commit 5413739ea8f44e4a2415326dbba2a67fdcbafda2.

--- geode-connectors/src/test/java/org/apache/geode/connectors/jdbc/MySqlJdbcLoaderIntegrationTest.java
+++ geode-connectors/src/test/java/org/apache/geode/connectors/jdbc/MySqlJdbcLoaderIntegrationTest.java
@@ -22,11 +22,11 @@ import java.sql.Statement;
 import org.junit.ClassRule;
 import org.junit.experimental.categories.Category;
 
-import org.apache.geode.test.junit.categories.ExternalServiceTest;
+import org.apache.geode.test.junit.categories.IntegrationTest;
 import org.apache.geode.test.junit.rules.DatabaseConnectionRule;
 import org.apache.geode.test.junit.rules.MySqlConnectionRule;
 
-@Category(ExternalServiceTest.class)
+@Category(IntegrationTest.class)
 public class MySqlJdbcLoaderIntegrationTest extends JdbcLoaderIntegrationTest {
 
   private static final URL COMPOSE_RESOURCE_PATH =
