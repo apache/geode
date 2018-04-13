0d918359d0f9b2f06eff4fc2586e8df54d79d983
Jinmei Liao <jiliao@pivotal.io>
2018-04-13 14:27:57 -0700

    GEODE-5048: add missing availability indicators for certain commands

--- geode-core/src/test/java/org/apache/geode/management/internal/cli/commands/CommandAvailabilityIndicatorTest.java
+++ geode-core/src/test/java/org/apache/geode/management/internal/cli/commands/CommandAvailabilityIndicatorTest.java
@@ -25,6 +25,7 @@ import org.junit.experimental.categories.Category;
 import org.springframework.shell.core.CommandMarker;
 import org.springframework.shell.core.annotation.CliCommand;
 
+import org.apache.geode.internal.cache.extension.mock.MockExtensionCommands;
 import org.apache.geode.management.cli.CliMetaData;
 import org.apache.geode.management.cli.GfshCommand;
 import org.apache.geode.management.internal.cli.CommandManager;
@@ -35,31 +36,40 @@ import org.apache.geode.test.junit.categories.UnitTest;
 public class CommandAvailabilityIndicatorTest {
 
   @Test
-  public void allOnlineCommandsHaveAvailabilityIndicator() throws Exception {
+  public void allOnlineCommandsHaveAvailabilityIndicator() {
     CommandManager manager = new CommandManager();
-    List<CommandMarker> commandMarkers = manager.getCommandMarkers();
+    assertOnlineCommandsHasAvailabilityIndicator(manager);
+  }
 
+  public static void assertOnlineCommandsHasAvailabilityIndicator(CommandManager manager) {
+    List<CommandMarker> commandMarkers = manager.getCommandMarkers();
     for (CommandMarker commandMarker : commandMarkers) {
       // ignore all the other commands beside GfshCommand
       if (!GfshCommand.class.isAssignableFrom(commandMarker.getClass())) {
         continue;
       }
 
+      if (commandMarker instanceof MockExtensionCommands) {
+        continue;
+      }
+
       for (Method method : commandMarker.getClass().getMethods()) {
         CliCommand cliCommand = method.getAnnotation(CliCommand.class);
-        CliMetaData cliMetaData = method.getAnnotation(CliMetaData.class);
+        if (cliCommand == null) {
+          // the method is not a command method
+          continue;
+        }
 
+        CliMetaData cliMetaData = method.getAnnotation(CliMetaData.class);
         // all the online commands have availability indicator defined in the commandManager
-        if (cliMetaData != null && !cliMetaData.shellOnly()) {
+        if (cliMetaData == null || cliMetaData != null && !cliMetaData.shellOnly()) {
           assertThat(manager.getHelper().hasAvailabilityIndicator(cliCommand.value()[0]))
-              .describedAs(cliCommand.value()[0] + " has no availability indicator defined. "
+              .describedAs(cliCommand.value()[0] + " in " + commandMarker.getClass()
+                  + " has no availability indicator defined. "
                   + "Please add the command in the CommandAvailabilityIndicator")
               .isTrue();
         }
       }
     }
   }
-
-
-
 }
