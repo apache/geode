068cc825a9589abc27b0064906308b172f4286b4
Darrel Schneider <dschneider@pivotal.io>
2018-04-23 12:57:31 -0700

    GEODE-5113: fix javadocs on getMaximum

--- geode-core/src/main/java/org/apache/geode/cache/EvictionAttributes.java
+++ geode-core/src/main/java/org/apache/geode/cache/EvictionAttributes.java
@@ -423,7 +423,12 @@ public abstract class EvictionAttributes implements DataSerializable {
 
   /**
    * The unit of this value is determined by the definition of the {@link EvictionAlgorithm} set by
-   * one of the creation methods e.g. {@link EvictionAttributes#createLRUEntryAttributes()}
+   * one of the creation methods e.g. {@link EvictionAttributes#createLRUEntryAttributes()}.
+   * If the algorithm is LRU_ENTRY then the unit is entries.
+   * If the algorithm is LRU_MEMORY then the unit is megabytes.
+   * If the algorithm is LRU_HEAP then the unit is undefined and this method always returns zero.
+   * Note, in geode 1.4 and earlier, this method would throw UnsupportedOperationException for
+   * LRU_HEAP.
    *
    * @return maximum value used by the {@link EvictionAlgorithm} which determines when the
    *         {@link EvictionAction} is performed.
