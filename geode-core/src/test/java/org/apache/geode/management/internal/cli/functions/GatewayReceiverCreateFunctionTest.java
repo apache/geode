b2a4da289d52a5298a95b038741c1d2d1e9b3594
Patrick Rhomberg <prhomberg@pivotal.io>
2018-05-15 09:44:39 -0700

    GEODE-5185: Correct backwards-compatibility violation in 'create gateway-receiver'.  (#1920)
    * This commit also refactors the 'create gateway-receiver' command and function in accordance with GEODE-4858 and GEODE-5011, concerning persistence service and ResultModel respectively.

--- geode-core/src/test/java/org/apache/geode/management/internal/cli/functions/GatewayReceiverCreateFunctionTest.java
+++ geode-core/src/test/java/org/apache/geode/management/internal/cli/functions/GatewayReceiverCreateFunctionTest.java
@@ -55,7 +55,6 @@ public class GatewayReceiverCreateFunctionTest {
     doCallRealMethod().when(function).execute(context);
     doReturn(false).when(function).gatewayReceiverExists(any());
     doReturn(receiver).when(function).createGatewayReceiver(cache, args);
-    doReturn(null).when(function).getXmlEntity(args);
 
     doReturn(5555).when(receiver).getPort();
   }
