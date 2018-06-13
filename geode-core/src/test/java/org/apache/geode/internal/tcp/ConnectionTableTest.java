3f4c33b0588bfc147bdb052e45d149a7397dffe9
Bruce Schuchardt <bschuchardt@pivotal.io>
2018-06-13 15:29:43 -0700

    GEODE-5320 add a test for thread-owned connection cleanup
    added a unit test to ensure that thread-owned connection cleanup is
    functioning properly

--- geode-core/src/test/java/org/apache/geode/internal/tcp/ConnectionTableTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/tcp/ConnectionTableTest.java
@@ -20,12 +20,15 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import org.apache.geode.CancelCriterion;
+import org.apache.geode.distributed.DistributedMember;
 import org.apache.geode.distributed.internal.DMStats;
 import org.apache.geode.distributed.internal.DistributionManager;
 import org.apache.geode.distributed.internal.InternalDistributedSystem;
@@ -92,5 +95,21 @@ public class ConnectionTableTest {
 
     connectionTable.acceptConnection(socket, factory);
     assertEquals(1, connectionTable.getNumberOfReceivers());
+  }
+
+  @Test
+  public void testThreadOwnedSocketsAreRemoved() throws Exception {
+    Boolean wantsResources = ConnectionTable.getThreadOwnsResourcesRegistration();
+    ConnectionTable.threadWantsOwnResources();
+    try {
+      Map<DistributedMember, Connection> threadConnectionMap = new HashMap<>();
+      connectionTable.threadOrderedConnMap.set(threadConnectionMap);
+      ConnectionTable.releaseThreadsSockets();
+      assertEquals(0, threadConnectionMap.size());
+    } finally {
+      if (wantsResources != Boolean.FALSE) {
+        ConnectionTable.threadWantsSharedResources();
+      }
+    }
   }
 }
