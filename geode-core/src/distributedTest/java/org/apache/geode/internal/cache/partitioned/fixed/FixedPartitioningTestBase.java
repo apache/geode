4edacb13854c35e73dc1b8b27b39c9bf9bb24179
Dan Smith <dsmith@pivotal.io>
2018-07-19 09:35:37 -0700

    GEODE-567: Wait for async datastore creation to reach appropriate point (#2143)
    GEODE-567: Wait for async datastore creation to reach appropriate point
    
    This test appears like it was trying to test what happens if a put
    happens when data store creation is at a particular point.
    
    Unfortunately, it failed to do so correctly because it just spun off the
    data store creation in separate threads and then did the put. The wait
    criterion used was also completely wrong, so the test would always wait
    for 1 minute.
    
    Finally, there were some race conditions caused by passing the same
    array list to multiple async tasks.
    
    Changed the code to wait until the data stores are at the given point in
    region creation and then do the put.

--- geode-core/src/distributedTest/java/org/apache/geode/internal/cache/partitioned/fixed/FixedPartitioningTestBase.java
+++ geode-core/src/distributedTest/java/org/apache/geode/internal/cache/partitioned/fixed/FixedPartitioningTestBase.java
@@ -32,6 +32,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Properties;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
@@ -56,9 +57,7 @@ import org.apache.geode.distributed.internal.membership.InternalDistributedMembe
 import org.apache.geode.internal.cache.FixedPartitionAttributesImpl;
 import org.apache.geode.internal.cache.HARegion;
 import org.apache.geode.internal.cache.InternalCache;
-import org.apache.geode.internal.cache.PartitionRegionConfig;
 import org.apache.geode.internal.cache.PartitionedRegion;
-import org.apache.geode.internal.cache.PartitionedRegionHelper;
 import org.apache.geode.internal.cache.execute.data.CustId;
 import org.apache.geode.internal.cache.execute.data.Customer;
 import org.apache.geode.internal.cache.execute.data.Order;
@@ -71,8 +70,6 @@ import org.apache.geode.internal.cache.partitioned.PartitionedRegionObserverHold
 import org.apache.geode.test.dunit.Host;
 import org.apache.geode.test.dunit.LogWriterUtils;
 import org.apache.geode.test.dunit.VM;
-import org.apache.geode.test.dunit.Wait;
-import org.apache.geode.test.dunit.WaitCriterion;
 import org.apache.geode.test.dunit.internal.JUnit4DistributedTestCase;
 
 /**
@@ -207,7 +204,7 @@ public class FixedPartitioningTestBase extends JUnit4DistributedTestCase {
     return dir;
   }
 
-  public static void putThorughAccessor(String regionName) {
+  public static void putThroughAccessor(String regionName) {
     region_FPR = (PartitionedRegion) cache.getRegion(regionName);
     assertNotNull(region_FPR);
     for (Months_Accessor month : Months_Accessor.values()) {
@@ -219,7 +216,7 @@ public class FixedPartitioningTestBase extends JUnit4DistributedTestCase {
     }
   }
 
-  public static void putThorughAccessor_Immediate(String regionName) {
+  public static void putThroughAccessor_Immediate(String regionName) {
     region_FPR = (PartitionedRegion) cache.getRegion(regionName);
     assertNotNull(region_FPR);
     for (int i = 1; i < 10; i++) {
@@ -1191,29 +1188,13 @@ public class FixedPartitioningTestBase extends JUnit4DistributedTestCase {
     origObserver =
         PartitionedRegionObserverHolder.setInstance(new PartitionedRegionObserverAdapter() {
           public void beforeCalculatingStartingBucketId() {
-            WaitCriterion wc = new WaitCriterion() {
-              String excuse;
-
-              public boolean done() {
-                Region prRoot = PartitionedRegionHelper.getPRRoot(cache);
-                PartitionRegionConfig regionConfig = (PartitionRegionConfig) prRoot.get("#Quarter");
-                if (regionConfig == null) {
-                  return false;
-                } else {
-                  if (!regionConfig.isFirstDataStoreCreated()) {
-                    return true;
-                  } else {
-                    return false;
-                  }
-                }
-              }
-
-              public String description() {
-                return excuse;
-              }
-            };
-            Wait.waitForCriterion(wc, 20000, 500, false);
-            LogWriterUtils.getLogWriter().info("end of beforeCalculatingStartingBucketId");
+            getBlackboard().signalGate("waiting");
+            try {
+              getBlackboard().waitForGate("done", 1, TimeUnit.MINUTES);
+            } catch (Exception e) {
+              throw new RuntimeException("Failed waiting to be signaled to continue in PR observer",
+                  e);
+            }
           }
         });
   }
