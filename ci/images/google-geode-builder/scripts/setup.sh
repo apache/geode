c431d24be1c00e25feefbeca26268079882c464f
Sean Goller <sgoller@pivotal.io>
2018-08-02 11:33:32 -0700

    [GEODE-5519] Add stackdriver monitoring to instances.
    * Add stackdriver agent to google-geode-builder.
    * Add labels identifying heavy-lifters
    
    Signed-off-by: Patrick Rhomberg <prhomberg@pivotal.io>

--- ci/images/google-geode-builder/scripts/setup.sh
+++ ci/images/google-geode-builder/scripts/setup.sh
@@ -55,6 +55,9 @@ pushd /tmp
   curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${CLOUD_SDK_VERSION}-linux-x86_64.tar.gz
   tar xzf google-cloud-sdk-${CLOUD_SDK_VERSION}-linux-x86_64.tar.gz -C /
   rm google-cloud-sdk-${CLOUD_SDK_VERSION}-linux-x86_64.tar.gz
+  curl -sSO https://dl.google.com/cloudagents/install-monitoring-agent.sh
+  bash install-monitoring-agent.sh
+  rm install-monitoring-agent.sh
 popd
 export PATH=/google-cloud-sdk/bin:${PATH}
 gcloud config set core/disable_usage_reporting true
