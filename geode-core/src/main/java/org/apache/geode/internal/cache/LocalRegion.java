2fe1696907d6b4eace05d725e62817b7b268d9f2
Bruce Schuchardt <bschuchardt@pivotal.io>
2018-08-17 10:51:44 -0700

    GEODE-5596 Client ends up with destroyed entry after invalidate()
    Modifying client cache behavior to create an Invalid entry when an application
    invokes invalid() and the entry is currently in Destroyed state on the client
    but not on servers.

--- geode-core/src/main/java/org/apache/geode/internal/cache/LocalRegion.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/LocalRegion.java
@@ -4955,7 +4955,9 @@ public class LocalRegion extends AbstractRegion implements LoaderHelperFactory,
    */
   private void basicInvalidate(final EntryEventImpl event, boolean invokeCallbacks)
       throws EntryNotFoundException {
-    basicInvalidate(event, invokeCallbacks, false);
+    final boolean forceNewEntryInClientCache = this.serverRegionProxy != null
+        && getConcurrencyChecksEnabled();
+    basicInvalidate(event, invokeCallbacks, forceNewEntryInClientCache);
   }
 
   /**
