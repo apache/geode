2f32947bd290b61114a21cd5410e3a6b2fc0419e
Juan Jose Ramos Cassella <jujoramos@gmail.com>
2018-08-17 12:24:50 +0100

    GEODE-5531: Fixed Performance Tests
    The class `GfshJuniTest` resulted in failures, not related to this PR,
    when the new `StressTest` pipeline CI was ran.
    The problem is basically that some methods check the singleton
    `LogManager` class, and the interleaving execution of those methods can
    lead to a race condition for which some loggers are not redirected and,
    as such, the tests fail.
    I have split `GfshJuniTest` in two different suites to avoid the
    problem and also have different classes to verify the different use
    cases of Gfsh: `GfshConsoleModeUnitTest` and `GfshHeadlessModeUnitTest`.

--- geode-core/src/test/java/org/apache/geode/management/internal/cli/shell/GfshAbstractUnitTest.java
+++ geode-core/src/test/java/org/apache/geode/management/internal/cli/shell/GfshAbstractUnitTest.java
@@ -20,10 +20,6 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-import java.util.Enumeration;
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
-
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.shell.core.CommandResult;
@@ -31,9 +27,9 @@ import org.springframework.shell.core.CommandResult;
 import org.apache.geode.management.internal.cli.result.LegacyCommandResult;
 
 
-public class GfshJunitTest {
-  private String testString;
-  private Gfsh gfsh;
+public class GfshAbstractUnitTest {
+  protected Gfsh gfsh;
+  protected String testString;
 
   @Before
   public void before() {
@@ -68,55 +64,6 @@ public class GfshJunitTest {
     assertThat(gfsh.getEnvAppContextPath()).isEqualTo("");
     gfsh.setEnvProperty(Gfsh.ENV_APP_CONTEXT_PATH, "test");
     assertThat(gfsh.getEnvAppContextPath()).isEqualTo("test");
-  }
-
-  @Test
-  public void loggerParentInHeadlessMode() {
-    gfsh = new Gfsh(false, null, new GfshConfig());
-    LogManager logManager = LogManager.getLogManager();
-    Enumeration<String> loggerNames = logManager.getLoggerNames();
-    while (loggerNames.hasMoreElements()) {
-      String loggerName = loggerNames.nextElement();
-      Logger logger = logManager.getLogger(loggerName);
-      // make sure jdk's logging goes to the gfsh log file
-      if (loggerName.startsWith("java")) {
-        assertThat(logger.getParent().getName()).endsWith("LogWrapper");
-      }
-      // make sure Gfsh's logging goes to the gfsh log file
-      else if (loggerName.endsWith(".Gfsh")) {
-        assertThat(logger.getParent().getName()).endsWith("LogWrapper");
-      }
-      // make sure SimpleParser's logging will still show up in the console
-      else if (loggerName.endsWith(".SimpleParser")) {
-        assertThat(logger.getParent().getName()).doesNotEndWith("LogWrapper");
-      }
-    }
-  }
-
-  @Test
-  public void loggerParentInConsoleMode() {
-    gfsh = new Gfsh(true, null, new GfshConfig());
-    LogManager logManager = LogManager.getLogManager();
-    Enumeration<String> loggerNames = logManager.getLoggerNames();
-    // when initialized in console mode, all log messages will show up in console
-    // initially. so that we see messages when "start locator", "start server" command
-    // are executed. Only after connection, JDK's logging is turned off
-    while (loggerNames.hasMoreElements()) {
-      String loggerName = loggerNames.nextElement();
-      Logger logger = logManager.getLogger(loggerName);
-      // make sure jdk's logging goes to the gfsh log file
-      if (loggerName.startsWith("java")) {
-        assertThat(logger.getParent().getName()).endsWith("LogWrapper");
-      }
-      // make sure Gfsh's logging goes to the gfsh log file
-      else if (loggerName.endsWith(".Gfsh")) {
-        assertThat(logger.getParent().getName()).doesNotEndWith("LogWrapper");
-      }
-      // make sure SimpleParser's logging will still show up in the console
-      else if (loggerName.endsWith(".SimpleParser")) {
-        assertThat(logger.getParent().getName()).doesNotEndWith("LogWrapper");
-      }
-    }
   }
 
   @Test
