12c48f7feb15f6a47a46c560efeae14d062607e8
Sai Boorlagadda <sboorlagadda@pivotal.io>
2018-08-23 17:32:15 -0700

    Added client server negative cases

--- geode-core/src/distributedTest/java/org/apache/geode/cache/client/internal/ClientServerHostNameVerificationDistributedTest.java
+++ geode-core/src/distributedTest/java/org/apache/geode/cache/client/internal/ClientServerHostNameVerificationDistributedTest.java
@@ -16,24 +16,30 @@ package org.apache.geode.cache.client.internal;
 
 import static org.apache.geode.security.SecurableCommunicationChannels.ALL;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import java.io.IOException;
 import java.net.InetAddress;
+import java.security.GeneralSecurityException;
 import java.util.Properties;
 
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.RegionFactory;
 import org.apache.geode.cache.RegionShortcut;
+import org.apache.geode.cache.client.ClientCache;
 import org.apache.geode.cache.client.ClientCacheFactory;
 import org.apache.geode.cache.client.ClientRegionFactory;
 import org.apache.geode.cache.client.ClientRegionShortcut;
+import org.apache.geode.cache.client.NoAvailableServersException;
 import org.apache.geode.cache.ssl.CertStores;
 import org.apache.geode.cache.ssl.TestSSLUtils.CertificateBuilder;
-import org.apache.geode.test.dunit.SerializableConsumerIF;
+import org.apache.geode.distributed.internal.tcpserver.LocatorCancelException;
+import org.apache.geode.internal.net.SocketCreatorFactory;
+import org.apache.geode.test.dunit.IgnoredException;
 import org.apache.geode.test.dunit.rules.ClientVM;
 import org.apache.geode.test.dunit.rules.ClusterStartupRule;
 import org.apache.geode.test.dunit.rules.MemberVM;
@@ -47,11 +53,45 @@ public class ClientServerHostNameVerificationDistributedTest {
   private static MemberVM server2;
   private static ClientVM client;
 
-  @ClassRule
-  public static ClusterStartupRule cluster = new ClusterStartupRule();
+  @Rule
+  public ClusterStartupRule cluster = new ClusterStartupRule();
 
-  @BeforeClass
-  public static void setupCluster() throws Exception {
+  private void setupCluster(Properties locatorSSLProps, Properties serverSSLProps) {
+    // create a cluster
+    setupLocators(locatorSSLProps);
+    setupServers(serverSSLProps);
+
+    // create region
+    server.invoke(ClientServerHostNameVerificationDistributedTest::createServerRegion);
+    server2.invoke(ClientServerHostNameVerificationDistributedTest::createServerRegion);
+    locator.waitUntilRegionIsReadyOnExactlyThisManyServers("/region", 2);
+  }
+
+  private void setupServers(Properties serverSSLProps) {
+    server = cluster.startServerVM(2, serverSSLProps, locator.getPort());
+    server2 = cluster.startServerVM(3, serverSSLProps, locator.getPort());
+  }
+
+  private void setupLocators(Properties locatorSSLProps) {
+    locator = cluster.startLocatorVM(0, locatorSSLProps);
+    locator2 = cluster.startLocatorVM(1, locatorSSLProps, locator.getPort());
+  }
+
+  private static void createServerRegion() {
+    RegionFactory factory =
+        ClusterStartupRule.getCache().createRegionFactory(RegionShortcut.REPLICATE);
+    Region r = factory.create("region");
+    r.put("serverkey", "servervalue");
+  }
+
+  private static void doServerRegionTest() {
+    Region<String, String> region = ClusterStartupRule.getCache().getRegion("region");
+    assertThat("servervalue").isEqualTo(region.get("serverkey"));
+    assertThat("clientvalue").isEqualTo(region.get("clientkey"));
+  }
+
+  @Test
+  public void clientValidatesHostNameOfLocatorAndServerDuringSSLConnection() throws Exception {
     CertificateBuilder locatorCertificate = new CertificateBuilder()
         .commonName("locator")
         // ClusterStartupRule uses 'localhost' as locator host
@@ -76,79 +116,109 @@ public class ClientServerHostNameVerificationDistributedTest {
     CertStores clientStore = CertStores.clientStore();
     clientStore.withCertificate(clientCertificate);
 
+    validateClientConnection(locatorCertificate, serverCertificate, clientCertificate, true, true,
+        true,
+        null);
+  }
+
+  @Test
+  public void expectConnectionFailureWhenNoHostNameInLocatorKey() throws Exception {
+    CertificateBuilder locatorCertificate = new CertificateBuilder()
+        .commonName("locator");
+
+    CertificateBuilder serverCertificate = new CertificateBuilder()
+        .commonName("server");
+
+    CertificateBuilder clientCertificate = new CertificateBuilder()
+        .commonName("client");
+
+    validateClientConnection(locatorCertificate, serverCertificate, clientCertificate, false, false,
+        true,
+        LocatorCancelException.class);
+  }
+
+  @Test
+  public void expectConnectionFailureWhenNoHostNameInServerKey() throws Exception {
+    CertificateBuilder locatorCertificateWithSan = new CertificateBuilder()
+        .commonName("locator")
+        .sanDnsName(InetAddress.getLoopbackAddress().getHostName())
+        .sanDnsName(InetAddress.getLocalHost().getHostName())
+        .sanIpAddress(InetAddress.getLocalHost());
+
+    CertificateBuilder serverCertificateWithNoSan = new CertificateBuilder()
+        .commonName("server");
+
+    CertificateBuilder clientCertificate = new CertificateBuilder()
+        .commonName("client");
+
+    validateClientConnection(locatorCertificateWithSan, serverCertificateWithNoSan,
+        clientCertificate, false, false, true,
+        NoAvailableServersException.class);
+  }
+
+  private void validateClientConnection(CertificateBuilder locatorCertificate,
+      CertificateBuilder serverCertificate, CertificateBuilder clientCertificate,
+      boolean enableHostNameVerficiationForLocator, boolean enableHostNameVerificationForServer,
+      boolean enableHostNameVerificationForClient, Class expectedExceptionOnClient)
+      throws GeneralSecurityException, IOException {
+    CertStores locatorStore = CertStores.locatorStore();
+    locatorStore.withCertificate(locatorCertificate);
+
+    CertStores serverStore = CertStores.serverStore();
+    serverStore.withCertificate(serverCertificate);
+
+    CertStores clientStore = CertStores.clientStore();
+    clientStore.withCertificate(clientCertificate);
+
     Properties locatorSSLProps = locatorStore
         .trustSelf()
-        .trust(serverStore.alias(), serverStore.certificate())
         .trust(clientStore.alias(), clientStore.certificate())
-        .propertiesWith(ALL);
+        .trust(serverStore.alias(), serverStore.certificate())
+        .propertiesWith(ALL, true, enableHostNameVerficiationForLocator);
 
     Properties serverSSLProps = serverStore
         .trustSelf()
         .trust(locatorStore.alias(), locatorStore.certificate())
         .trust(clientStore.alias(), clientStore.certificate())
-        .propertiesWith(ALL);
+        .propertiesWith(ALL, true, enableHostNameVerificationForServer);
 
     Properties clientSSLProps = clientStore
         .trust(locatorStore.alias(), locatorStore.certificate())
         .trust(serverStore.alias(), serverStore.certificate())
-        .propertiesWith(ALL);
+        .propertiesWith(ALL, true, enableHostNameVerificationForClient);
 
-    // create a cluster
-    locator = cluster.startLocatorVM(0, locatorSSLProps);
-    locator2 = cluster.startLocatorVM(1, locatorSSLProps, locator.getPort());
-    server = cluster.startServerVM(2, serverSSLProps, locator.getPort());
-    server2 = cluster.startServerVM(3, serverSSLProps, locator.getPort());
+    setupCluster(locatorSSLProps, serverSSLProps);
 
-    // create region
-    server.invoke(ClientServerHostNameVerificationDistributedTest::createServerRegion);
-    server2.invoke(ClientServerHostNameVerificationDistributedTest::createServerRegion);
-    locator.waitUntilRegionIsReadyOnExactlyThisManyServers("/region", 2);
+    final int locatorPort = locator.getPort();
+    final String locatorHost = locator.getVM().getHost().getHostName();
 
-    // setup client
-    setupClient(clientSSLProps, locator.getPort(), locator.getVM().getHost().getHostName());
-  }
+    ClientCacheFactory clientCacheFactory = new ClientCacheFactory(clientSSLProps);
+    clientCacheFactory.setPoolSubscriptionEnabled(true)
+        .addPoolLocator(locatorHost, locatorPort);
 
-  private static void createServerRegion() {
-    RegionFactory factory =
-        ClusterStartupRule.getCache().createRegionFactory(RegionShortcut.REPLICATE);
-    Region r = factory.create("region");
-    r.put("serverkey", "servervalue");
-  }
+    ClientCache clientCache = clientCacheFactory.create();
 
-  private static void setupClient(Properties clientSSLProps, int locatorPort,
-      String locatorHost) throws Exception {
-    SerializableConsumerIF<ClientCacheFactory> clientSetup =
-        cf -> cf.addPoolLocator(locatorHost, locatorPort);
-    client = cluster.startClientVM(4, clientSSLProps, clientSetup);
-
-    // create a client region
-    client.invoke(ClientServerHostNameVerificationDistributedTest::createClientRegion);
-  }
-
-  private static void createClientRegion() {
     ClientRegionFactory<String, String> regionFactory =
-        ClusterStartupRule.getClientCache().createClientRegionFactory(ClientRegionShortcut.PROXY);
-    Region<String, String> region = regionFactory.create("region");
-    assertThat(region).isNotNull();
-  }
+        clientCache.createClientRegionFactory(ClientRegionShortcut.PROXY);
 
-  @Test
-  public void clientValidatesHostNameOfLocatorAndServerDuringSSLConnection() {
-    client.invoke(ClientServerHostNameVerificationDistributedTest::doClientRegionTest);
-    server.invoke(ClientServerHostNameVerificationDistributedTest::doServerRegionTest);
-  }
+    if (expectedExceptionOnClient != null) {
+      IgnoredException.addIgnoredException("javax.net.ssl.SSLHandshakeException");
+      IgnoredException.addIgnoredException("java.net.SocketException");
 
-  private static void doClientRegionTest() {
-    Region<String, String> region = ClusterStartupRule.getClientCache().getRegion("region");
-    assertThat("servervalue").isEqualTo(region.get("serverkey"));
+      Region<String, String> clientRegion = regionFactory.create("region");
+      assertThatExceptionOfType(expectedExceptionOnClient)
+          .isThrownBy(() -> clientRegion.put("1", "1"));
 
-    region.put("clientkey", "clientvalue");
-    assertThat("clientvalue").isEqualTo(region.get("clientkey"));
-  }
+      IgnoredException.removeAllExpectedExceptions();
+    } else {
+      // test client can read and write to server
+      Region<String, String> clientRegion = regionFactory.create("region");
+      assertThat("servervalue").isEqualTo(clientRegion.get("serverkey"));
+      clientRegion.put("clientkey", "clientvalue");
 
-  private static void doServerRegionTest() {
-    Region<String, String> region = ClusterStartupRule.getCache().getRegion("region");
-    assertThat("servervalue").isEqualTo(region.get("serverkey"));
-    assertThat("clientvalue").isEqualTo(region.get("clientkey"));
+      // test server can see data written by client
+      server.invoke(ClientServerHostNameVerificationDistributedTest::doServerRegionTest);
+    }
+    SocketCreatorFactory.close();
   }
 }
