02e85b77b715ea1f2ce8f97751b9872f8d881398
Ken Howe <khowe@pivotal.io>
2018-08-27 09:37:12 -0700

    Debugging out of memeory failures

--- geode-assembly/src/acceptanceTest/java/org/apache/geode/management/internal/cli/commands/DeployWithLargeJarTest.java
+++ geode-assembly/src/acceptanceTest/java/org/apache/geode/management/internal/cli/commands/DeployWithLargeJarTest.java
@@ -36,7 +36,7 @@ public class DeployWithLargeJarTest {
     File libDir = gfsh.getGfshPath().getParent().getParent().resolve("lib").toFile();
     String commonLibs = Arrays.stream(libDir.listFiles(x -> x.getName().startsWith("commons")))
         .map(File::getAbsolutePath).collect(Collectors.joining(","));
-    GfshExecution execution = GfshScript.of("start locator --name=locator --max-heap=128m",
+    GfshExecution execution = GfshScript.of("start locator --name=locator --max-heap=140" + "m",
         "start server --name=server --max-heap=128m", "sleep --time=1",
         "deploy --jars=" + commonLibs).execute(gfsh);
   }
