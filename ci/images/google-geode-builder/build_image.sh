aa915c2ba7d07a641c8ec65f473dd60f44806606
FSOUTHERLAND <39743785+FSOUTHERLAND@users.noreply.github.com>
2018-08-28 10:30:24 -0700

    GEODE-5608 truncate fork/branch names in image/instance naming (#2390)
    * GEODE-5608 truncate fork/branch names in image/instance naming (#2363)
    
    Co-authored-by: Jacob Barrett <jbarrett@pivotal.io>
    Co-authored-by: Finn Southerland <fsoutherland@pivotal.io>
    Co-authored-by: Robert Houghton <rhoughton@pivotal.io>

--- ci/images/google-geode-builder/build_image.sh
+++ ci/images/google-geode-builder/build_image.sh
@@ -26,8 +26,6 @@ SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
 pushd ${SCRIPTDIR}
 
-GEODE_BRANCH=${GEODE_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
-SANITIZED_GEODE_BRANCH=$(echo ${GEODE_BRANCH} | tr "/" "-" | tr '[:upper:]' '[:lower:]')
 IMAGE_FAMILY_PREFIX=""
 GEODE_DOCKER_IMAGE=${GEODE_DOCKER_IMAGE:-"gcr.io/apachegeode-ci/test-container"}
 if [[ -z "${GEODE_FORK}" ]]; then
@@ -35,9 +33,12 @@ if [[ -z "${GEODE_FORK}" ]]; then
   exit 1
 fi
 
+GEODE_BRANCH=${GEODE_BRANCH:-$(git rev-parse --abbrev-ref HEAD)}
+SANITIZED_GEODE_BRANCH=$(echo ${GEODE_BRANCH} | tr "/" "-" | tr '[:upper:]' '[:lower:]' | cut -c 1-20)
+SANITIZED_GEODE_FORK=$(echo ${GEODE_FORK} | tr "/" "-" | tr '[:upper:]' '[:lower:]' | cut -c 1-16)
 
-if [[ "${GEODE_FORK}" != "apache" ]]; then
-  IMAGE_FAMILY_PREFIX="${GEODE_FORK}-${SANITIZED_GEODE_BRANCH}-"
+if [[ "${SANITIZED_GEODE_FORK}" != "apache" ]]; then
+  IMAGE_FAMILY_PREFIX="${SANITIZED_GEODE_FORK}-${SANITIZED_GEODE_BRANCH}-"
 fi
 
 echo "Running packer"
