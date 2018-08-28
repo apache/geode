5717a2483aec1f523452392972453f98d2ae72e2
FSOUTHERLAND <39743785+FSOUTHERLAND@users.noreply.github.com>
2018-08-28 10:30:24 -0700

    GEODE-5633 pr pipeline name has fork and branch
    (for staging pipelines)
    Authored-by: Finn Southerland <fsoutherland@pivotal.io>

--- ci/pipelines/pull-request/deploy_pr_pipeline.sh
+++ ci/pipelines/pull-request/deploy_pr_pipeline.sh
@@ -46,7 +46,11 @@ if [ "${GEODE_BRANCH}" = "HEAD" ]; then
   exit 1
 fi
 
-SANITIZED_GEODE_BRANCH=$(echo ${GEODE_BRANCH} | tr "/" "-" | tr '[:upper:]' '[:lower:]')
+
+. ${SCRIPTDIR}/../shared/utilities.sh
+
+SANITIZED_GEODE_BRANCH=$(getSanitizedBranch ${GEODE_BRANCH})
+SANITIZED_GEODE_FORK=$(getSanitizedFork ${GEODE_FORK})
 
 OUTPUT_DIRECTORY=${OUTPUT_DIRECTORY:-$SCRIPTDIR}
 
@@ -62,17 +66,17 @@ TARGET="geode"
 
 TEAM=${CONCOURSE_TEAM:-main}
 
-if [[ "${GEODE_FORK}" == "apache" ]]; then
-  PIPELINE_PREFIX=""
+if [[ "${SANITIZED_GEODE_FORK}" == "apache" ]]; then
+  PIPELINE_NAME="pr-${SANITIZED_GEODE_BRANCH}"
   DOCKER_IMAGE_PREFIX=""
 else
-  PIPELINE_PREFIX="${GEODE_FORK}-${SANITIZED_GEODE_BRANCH}-"
+  PIPELINE_NAME="pr-${SANITIZED_GEODE_FORK}-${SANITIZED_GEODE_BRANCH}"
   DOCKER_IMAGE_PREFIX=${PIPELINE_PREFIX}
 fi
 
 pushd ${SCRIPTDIR} 2>&1 > /dev/null
   # Template and output share a directory with this script, but variables are shared in the parent directory.
-  python3 ../render.py jinja.template.yml ../shared/jinja.variables.yml generated-pipeline.yml || exit 1
+  python3 ../render.py $(basename ${SCRIPTDIR}) || exit 1
 
   fly login -t ${TARGET} \
             -n ${TEAM} \
@@ -81,11 +85,11 @@ pushd ${SCRIPTDIR} 2>&1 > /dev/null
             -p ${CONCOURSE_PASSWORD}
 
   fly -t ${TARGET} set-pipeline \
-      --non-interactive \
-      --pipeline pr-${SANITIZED_GEODE_BRANCH} \
-      --config generated-pipeline.yml \
-      --var docker-image-prefix=${DOCKER_IMAGE_PREFIX} \
-      --var concourse-team=${TEAM}
+    --non-interactive \
+    --pipeline ${PIPELINE_NAME} \
+    --config ${SCRIPTDIR}/generated-pipeline.yml \
+    --var docker-image-prefix=${DOCKER_IMAGE_PREFIX} \
+    --var concourse-team=${TEAM}
 
 popd 2>&1 > /dev/null
 
