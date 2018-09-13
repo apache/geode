e9ca513a7a72416f4747ff07a455d01165fc2e49
Ryan McMahon <rmcmahon@pivotal.io>
2018-09-13 12:45:19 -0700

    GEODE-5725: Use values large enough (> 8 bytes) to force off-heap storage (#2459)
    Off-heap is only used if values are greater than the size of an off-heap address i.e. greater than 8 bytes.  These tests were using longs as values, so off-heap was no actually being exercised as expected.  Changing the values to strings of 10 byte minimum size will force off-heap storage to be used.

--- geode-wan/src/distributedTest/java/org/apache/geode/internal/cache/wan/WANTestBase.java
+++ geode-wan/src/distributedTest/java/org/apache/geode/internal/cache/wan/WANTestBase.java
@@ -2227,7 +2227,7 @@ public class WANTestBase extends DistributedTestCase {
       Region r = cache.getRegion(Region.SEPARATOR + regionName);
       assertNotNull(r);
       for (long i = 0; i < numPuts; i++) {
-        r.put(i, i);
+        r.put(i, "Value_" + i);
       }
     } finally {
       exp1.remove();
@@ -2239,7 +2239,7 @@ public class WANTestBase extends DistributedTestCase {
     Region r = cache.getRegion(Region.SEPARATOR + regionName);
     assertNotNull(r);
     for (long i = 300; i < numPuts; i++) {
-      r.put(i, i);
+      r.put(i, "Value_" + i);
     }
   }
 
@@ -2247,7 +2247,7 @@ public class WANTestBase extends DistributedTestCase {
     Region r = cache.getRegion(Region.SEPARATOR + regionName);
     assertNotNull(r);
     for (long i = from; i < numPuts; i++) {
-      r.put(i, i);
+      r.put(i, "Value_" + i);
     }
   }
 
@@ -2265,7 +2265,7 @@ public class WANTestBase extends DistributedTestCase {
     for (long i = 0; i < numPuts; i++) {
       Map putAllMap = new HashMap();
       for (long j = 0; j < size; j++) {
-        putAllMap.put((size * i) + j, i);
+        putAllMap.put((size * i) + j, "Value_" + i);
       }
       r.putAll(putAllMap, "putAllCallback");
       putAllMap.clear();
@@ -2277,7 +2277,7 @@ public class WANTestBase extends DistributedTestCase {
     Region r = cache.getRegion(Region.SEPARATOR + regionName);
     assertNotNull(r);
     for (long i = 0; i < numPuts; i++) {
-      r.put("Object_" + i, i);
+      r.put("Object_" + i, "Value_" + i);
     }
   }
 
