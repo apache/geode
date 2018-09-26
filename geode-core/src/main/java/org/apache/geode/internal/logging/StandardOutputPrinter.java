333a162974616665ae45eabd73d63c750cf2d864
Kirk Lund <klund@apache.org>
2018-09-26 09:37:00 -0700

    GEODE-2644: Cleanup logging classes

--- geode-core/src/main/java/org/apache/geode/internal/logging/StandardOutputPrinter.java
+++ geode-core/src/main/java/org/apache/geode/internal/logging/StandardOutputPrinter.java
@@ -16,16 +16,15 @@ package org.apache.geode.internal.logging;
 
 /**
  * Prints messages formatted like GemFire log messages to stderr.
- *
  */
-public class StandardOutputPrinter extends LocalLogWriter {
+class StandardOutputPrinter extends LocalLogWriter {
 
   /**
    * Creates a writer that logs to <code>System.err</code>. All messages will be logged.
    *
    * @throws IllegalArgumentException if level is not in legal range
    */
-  public StandardOutputPrinter() {
+  StandardOutputPrinter() {
     this(InternalLogWriter.ALL_LEVEL);
   }
 
@@ -33,10 +32,10 @@ public class StandardOutputPrinter extends LocalLogWriter {
    * Creates a writer that logs to <code>System.err</code>.
    *
    * @param level only messages greater than or equal to this value will be logged.
+   *
    * @throws IllegalArgumentException if level is not in legal range
    */
-  public StandardOutputPrinter(final int level) {
+  StandardOutputPrinter(final int level) {
     super(level, System.out);
   }
-
 }
