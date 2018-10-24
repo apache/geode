e7a326e0fd53c0e0a8f0bc76498ab456278cc216
Darrel Schneider <dschneider@pivotal.io>
2018-10-24 16:04:40 -0700

    The create jndi-binding --datasource-config-properties will now check each name for a "pool." prefix. If found then that property, minus the prefix, will be added to the properties that the PooledDataSourceFactory gets to configure the pool. The other datasource config properties are still used to configure the database. The help strings on create jndi-binding have been updated.

--- geode-core/src/test/java/org/apache/geode/internal/datasource/DataSourceFactoryTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/datasource/DataSourceFactoryTest.java
@@ -17,27 +17,64 @@
 package org.apache.geode.internal.datasource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import javax.sql.DataSource;
+
 import org.junit.Test;
+
+import org.apache.geode.datasource.PooledDataSourceFactory;
 
 public class DataSourceFactoryTest {
 
   private final Map<String, String> inputs = new HashMap<>();
+  private final List<ConfigProperty> configProperties = new ArrayList<>();
 
   @Test
   public void creatPoolPropertiesWithNullInputReturnsEmptyOutput() {
-    Properties output = DataSourceFactory.createPoolProperties(null);
+    Properties output = DataSourceFactory.createPoolProperties(null, null);
 
     assertThat(output.isEmpty()).isTrue();
   }
 
   @Test
+  public void creatPoolPropertiesWithOneConfigDataSourcePropertyReturnsEmptyOutput() {
+    configProperties.add(new ConfigProperty("n1", "v1", null));
+
+    Properties output = DataSourceFactory.createPoolProperties(inputs, configProperties);
+
+    assertThat(output.isEmpty()).isTrue();
+  }
+
+  @Test
+  public void creatPoolPropertiesWithOneConfigPoolPropertyReturnsOneOutput() {
+    configProperties.add(new ConfigProperty("pool.n1", "v1", null));
+
+    Properties output = DataSourceFactory.createPoolProperties(inputs, configProperties);
+
+    assertThat(output.size()).isEqualTo(1);
+    assertThat(output.getProperty("n1")).isEqualTo("v1");
+  }
+
+  @Test
+  public void creatPoolPropertiesWithOneUpperCaseConfigPoolPropertyReturnsOneOutput() {
+    configProperties.add(new ConfigProperty("POOL.N1", "v1", null));
+
+    Properties output = DataSourceFactory.createPoolProperties(inputs, configProperties);
+
+    assertThat(output.size()).isEqualTo(1);
+    assertThat(output.getProperty("N1")).isEqualTo("v1");
+  }
+
+  @Test
   public void creatPoolPropertiesWithEmptyInputReturnsEmptyOutput() {
-    Properties output = DataSourceFactory.createPoolProperties(inputs);
+    Properties output = DataSourceFactory.createPoolProperties(inputs, configProperties);
 
     assertThat(output.isEmpty()).isTrue();
   }
@@ -46,7 +83,7 @@ public class DataSourceFactoryTest {
   public void creatPoolPropertiesWithNullValueInputReturnsEmptyOutput() {
     inputs.put("name", null);
 
-    Properties output = DataSourceFactory.createPoolProperties(inputs);
+    Properties output = DataSourceFactory.createPoolProperties(inputs, configProperties);
 
     assertThat(output.isEmpty()).isTrue();
   }
@@ -55,7 +92,7 @@ public class DataSourceFactoryTest {
   public void creatPoolPropertiesWithEmptyStringValueInputReturnsEmptyOutput() {
     inputs.put("name", "");
 
-    Properties output = DataSourceFactory.createPoolProperties(inputs);
+    Properties output = DataSourceFactory.createPoolProperties(inputs, configProperties);
 
     assertThat(output.isEmpty()).isTrue();
   }
@@ -64,7 +101,7 @@ public class DataSourceFactoryTest {
   public void creatPoolPropertiesWithOneInputReturnsOneOutput() {
     inputs.put("name", "value");
 
-    Properties output = DataSourceFactory.createPoolProperties(inputs);
+    Properties output = DataSourceFactory.createPoolProperties(inputs, configProperties);
 
     assertThat(output.size()).isEqualTo(1);
     assertThat(output.getProperty("name")).isEqualTo("value");
@@ -79,7 +116,7 @@ public class DataSourceFactoryTest {
     inputs.put("managed-conn-factory-class", "value");
     inputs.put("xa-datasource-class", "value");
 
-    Properties output = DataSourceFactory.createPoolProperties(inputs);
+    Properties output = DataSourceFactory.createPoolProperties(inputs, configProperties);
 
     assertThat(output.isEmpty()).isTrue();
   }
@@ -96,12 +133,91 @@ public class DataSourceFactoryTest {
     inputs.put("xa-datasource-class", "value");
     inputs.put("validname1", "value1");
     inputs.put("validname2", "value2");
+    configProperties.add(new ConfigProperty("pool.n1", "v1", null));
+    configProperties.add(new ConfigProperty("dataSourceProp", "dataSourceValue", null));
+    configProperties.add(new ConfigProperty("pool.n2", "v2", null));
 
-    Properties output = DataSourceFactory.createPoolProperties(inputs);
+    Properties output = DataSourceFactory.createPoolProperties(inputs, configProperties);
 
-    assertThat(output.size()).isEqualTo(2);
+    assertThat(output.size()).isEqualTo(4);
     assertThat(output.getProperty("validname1")).isEqualTo("value1");
     assertThat(output.getProperty("validname2")).isEqualTo("value2");
+    assertThat(output.getProperty("n1")).isEqualTo("v1");
+    assertThat(output.getProperty("n2")).isEqualTo("v2");
+  }
+
+  @Test
+  public void createDataSourcePropertiesWithNullReturnsEmpty() {
+    Properties output = DataSourceFactory.createDataSourceProperties(null);
+
+    assertThat(output).isEmpty();
+  }
+
+  @Test
+  public void createDataSourcePropertiesWithEmptyListReturnsEmpty() {
+    Properties output = DataSourceFactory.createDataSourceProperties(configProperties);
+
+    assertThat(output).isEmpty();
+  }
+
+  @Test
+  public void createDataSourcePropertiesWithPoolPropertyReturnsEmpty() {
+    configProperties.add(new ConfigProperty("pool.n1", "v1", null));
+
+    Properties output = DataSourceFactory.createDataSourceProperties(configProperties);
+
+    assertThat(output).isEmpty();
+  }
+
+  @Test
+  public void createDataSourcePropertiesWithNonPoolPropertyReturnsOne() {
+    configProperties.add(new ConfigProperty("n1", "v1", null));
+
+    Properties output = DataSourceFactory.createDataSourceProperties(configProperties);
+
+    assertThat(output.size()).isEqualTo(1);
+    assertThat(output.getProperty("n1")).isEqualTo("v1");
+  }
+
+  @Test
+  public void createDataSourcePropertiesWithMuliplePropertiesReturnsJustNonPool() {
+    configProperties.add(new ConfigProperty("n1", "v1", null));
+    configProperties.add(new ConfigProperty("pool.n3", "v3", null));
+    configProperties.add(new ConfigProperty("n2", "v2", null));
+
+    Properties output = DataSourceFactory.createDataSourceProperties(configProperties);
+
+    assertThat(output.size()).isEqualTo(2);
+    assertThat(output.getProperty("n1")).isEqualTo("v1");
+    assertThat(output.getProperty("n2")).isEqualTo("v2");
+  }
+
+  public static class TestPooledDataSourceFactory implements PooledDataSourceFactory {
+    @Override
+    public DataSource createDataSource(Properties poolProperties, Properties dataSourceProperties) {
+      return null;
+    }
+  }
+
+  @Test
+  public void getPooledDataSourceUsesConnPooledDataSourceClass() throws DataSourceCreateException {
+    inputs.put("conn-pooled-datasource-class",
+        "org.apache.geode.internal.datasource.DataSourceFactoryTest$TestPooledDataSourceFactory");
+
+    DataSource dataSource = DataSourceFactory.getPooledDataSource(inputs, configProperties);
+
+    assertThat(dataSource).isNull();
+  }
+
+  @Test
+  public void getPooledDataSourceFailsIfClassDoesNotExist() throws DataSourceCreateException {
+    inputs.put("conn-pooled-datasource-class", "doesNotExist");
+
+    Throwable throwable =
+        catchThrowable(() -> DataSourceFactory.getPooledDataSource(inputs, configProperties));
+
+    assertThat(throwable).isInstanceOf(DataSourceCreateException.class).hasMessage(
+        "DataSourceFactory::getPooledDataSource:Exception creating ConnectionPoolDataSource.Exception string=java.lang.ClassNotFoundException: doesNotExist");
   }
 
 }
