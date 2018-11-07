6081821bc76aee3e0fb40d9175b97873b10c0f68
Ryan McMahon <rmcmahon@pivotal.io>
2018-11-06 17:50:44 -0800

    Revert "GEODE-5729: when DistributedCacheOperation needs 2 messages, should let (#2458)"
    This reverts commit 49eb1c5fd13aefff0995d76ec7864c82d5730dd8.

--- geode-assembly/geode-assembly-test/src/main/java/org/apache/geode/test/junit/rules/HttpResponseAssert.java
+++ geode-assembly/geode-assembly-test/src/main/java/org/apache/geode/test/junit/rules/HttpResponseAssert.java
@@ -20,8 +20,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.Collectors;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
