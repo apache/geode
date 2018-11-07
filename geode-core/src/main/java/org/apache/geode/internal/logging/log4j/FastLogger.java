81c0c38ac0a593c095c6cf0ec325791563964e73
Kirk Lund <klund@apache.org>
2018-11-06 16:06:02 -0800

    GEODE-5997: Override ExtendedLogger methods in FastLogger
    The isDebugEnabled and isTraceEnabled gaurd clauses around logger debug
    and trace log statements can now be deleted. Any altered log statements
    should use log4j substitution parameters. Any parameters that are
    particularly expensive should be supplied as a lambda.
    
    See FastLoggerBenchmark and FasterLoggerParamsBenchmark for benchmarks
    that should help guide log statement paramterization.
    
    It would be a good idea to consider writing additional benchmarks for
    any sensitive codepaths with debug or trace log statements that are
    changed.

--- geode-core/src/main/java/org/apache/geode/internal/logging/log4j/FastLogger.java
+++ geode-core/src/main/java/org/apache/geode/internal/logging/log4j/FastLogger.java
@@ -14,21 +14,25 @@
  */
 package org.apache.geode.internal.logging.log4j;
 
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.message.Message;
 import org.apache.logging.log4j.message.MessageFactory;
 import org.apache.logging.log4j.spi.ExtendedLogger;
 import org.apache.logging.log4j.spi.ExtendedLoggerWrapper;
 import org.apache.logging.log4j.status.StatusLogger;
+import org.apache.logging.log4j.util.MessageSupplier;
+import org.apache.logging.log4j.util.Supplier;
 
 /**
- * Overrides is-enabled checks for log levels below INFO to avoid performance penalties when the log
- * level is INFO or above. If delegating is true then it will always delegate to
- * ExtendedLoggerWrapper for is-enabled checks.
- *
+ * Overrides is-enabled checks for log levels below {@code INFO} to avoid performance penalties when
+ * the log level is {@code INFO} or above.
  */
 public class FastLogger extends ExtendedLoggerWrapper {
   private static final long serialVersionUID = 7084130827962463327L;
+
+  private static final Level DEFAULT_LEVEL = Level.INFO;
 
   private static volatile boolean delegating = true;
 
@@ -47,30 +51,333 @@ public class FastLogger extends ExtendedLoggerWrapper {
   }
 
   @Override
-  public boolean isDebugEnabled() {
-    return delegating && super.isDebugEnabled();
+  public boolean isEnabled(Level level, Marker marker, Message message, Throwable t) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, t);
   }
 
   @Override
-  public boolean isDebugEnabled(final Marker marker) {
-    return delegating && super.isDebugEnabled(marker);
+  public boolean isEnabled(Level level, Marker marker, CharSequence message, Throwable t) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, t);
   }
 
   @Override
-  public boolean isTraceEnabled() {
-    return delegating && super.isTraceEnabled();
+  public boolean isEnabled(Level level, Marker marker, Object message, Throwable t) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, t);
   }
 
   @Override
-  public boolean isTraceEnabled(final Marker marker) {
-    return delegating && super.isTraceEnabled(marker);
+  public boolean isEnabled(Level level, Marker marker, String message, Throwable t) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, t);
   }
 
-  public boolean isDelegating() {
+  @Override
+  public boolean isEnabled(Level level, Marker marker, String message) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message);
+  }
+
+  @Override
+  public boolean isEnabled(Level level, Marker marker, String message, Object... params) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, params);
+  }
+
+  @Override
+  public boolean isEnabled(Level level, Marker marker, String message, Object p0) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, p0);
+  }
+
+  @Override
+  public boolean isEnabled(Level level, Marker marker, String message, Object p0, Object p1) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, p0, p1);
+  }
+
+  @Override
+  public boolean isEnabled(Level level, Marker marker, String message, Object p0, Object p1,
+      Object p2) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, p0, p1, p2);
+  }
+
+  @Override
+  public boolean isEnabled(Level level, Marker marker, String message, Object p0, Object p1,
+      Object p2, Object p3) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, p0, p1, p2, p3);
+  }
+
+  @Override
+  public boolean isEnabled(Level level, Marker marker, String message, Object p0, Object p1,
+      Object p2, Object p3,
+      Object p4) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, p0, p1, p2, p3, p4);
+  }
+
+  @Override
+  public boolean isEnabled(Level level, Marker marker, String message, Object p0, Object p1,
+      Object p2, Object p3,
+      Object p4, Object p5) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, p0, p1, p2, p3, p4, p5);
+  }
+
+  @Override
+  public boolean isEnabled(Level level, Marker marker, String message, Object p0, Object p1,
+      Object p2, Object p3,
+      Object p4, Object p5, Object p6) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, p0, p1, p2, p3, p4, p5, p6);
+  }
+
+  @Override
+  public boolean isEnabled(Level level, Marker marker, String message, Object p0, Object p1,
+      Object p2, Object p3,
+      Object p4, Object p5, Object p6, Object p7) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, p0, p1, p2, p3, p4, p5, p6, p7);
+  }
+
+  @Override
+  public boolean isEnabled(Level level, Marker marker, String message, Object p0, Object p1,
+      Object p2, Object p3,
+      Object p4, Object p5, Object p6, Object p7, Object p8) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, p0, p1, p2, p3, p4, p5, p6, p7, p8);
+  }
+
+  @Override
+  public boolean isEnabled(Level level, Marker marker, String message, Object p0, Object p1,
+      Object p2, Object p3,
+      Object p4, Object p5, Object p6, Object p7, Object p8, Object p9) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return false;
+    }
+    return super.isEnabled(level, marker, message, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, Message message, Throwable t) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, t);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, CharSequence message,
+      Throwable t) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, t);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, Object message, Throwable t) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, t);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, String message, Throwable t) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, t);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, String message) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, String message,
+      Object... params) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, params);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, String message, Object p0) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, p0);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, String message, Object p0,
+      Object p1) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, p0, p1);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, String message, Object p0,
+      Object p1, Object p2) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, p0, p1, p2);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, String message, Object p0,
+      Object p1, Object p2,
+      Object p3) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, p0, p1, p2, p3);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, String message, Object p0,
+      Object p1, Object p2,
+      Object p3, Object p4) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, p0, p1, p2, p3, p4);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, String message, Object p0,
+      Object p1, Object p2,
+      Object p3, Object p4, Object p5) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, p0, p1, p2, p3, p4, p5);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, String message, Object p0,
+      Object p1, Object p2,
+      Object p3, Object p4, Object p5, Object p6) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, p0, p1, p2, p3, p4, p5, p6);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, String message, Object p0,
+      Object p1, Object p2,
+      Object p3, Object p4, Object p5, Object p6, Object p7) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, p0, p1, p2, p3, p4, p5, p6, p7);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, String message, Object p0,
+      Object p1, Object p2,
+      Object p3, Object p4, Object p5, Object p6, Object p7, Object p8) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, p0, p1, p2, p3, p4, p5, p6, p7, p8);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, String message, Object p0,
+      Object p1, Object p2,
+      Object p3, Object p4, Object p5, Object p6, Object p7, Object p8, Object p9) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
+  }
+
+  @Override
+  public void logMessage(String fqcn, Level level, Marker marker, Message message, Throwable t) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logMessage(fqcn, level, marker, message, t);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, MessageSupplier msgSupplier,
+      Throwable t) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, msgSupplier, t);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, String message,
+      Supplier<?>... paramSuppliers) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, message, paramSuppliers);
+  }
+
+  @Override
+  public void logIfEnabled(String fqcn, Level level, Marker marker, Supplier<?> msgSupplier,
+      Throwable t) {
+    if (level.intLevel() > DEFAULT_LEVEL.intLevel() && !delegating) {
+      return;
+    }
+    super.logIfEnabled(fqcn, level, marker, msgSupplier, t);
+  }
+
+  boolean isDelegating() {
     return delegating;
   }
 
-  public ExtendedLogger getExtendedLogger() {
+  ExtendedLogger getExtendedLogger() {
     return super.logger;
   }
 }
