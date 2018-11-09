00f4f22b354490e0578224d50e441b52e40331fe
bross <bross@pivotal.io>
2018-11-09 11:42:57 -0800

    GEODE-6029: Added test for describe jndi-binding
    - Cleaned up unessessary code in Describe JNDI-Binding Command and
    Create JNDI-Binding Command unit test
    
    [GEODE-6029]
    
    Co-authored-by: Ben Ross <bross@pivotal.io>
    Co-authored-by: Jianxia Chen <jchen21@apache.org>

--- geode-core/src/test/java/org/apache/geode/management/internal/cli/commands/CreateJndiBindingCommandTest.java
+++ geode-core/src/test/java/org/apache/geode/management/internal/cli/commands/CreateJndiBindingCommandTest.java
@@ -25,7 +25,6 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
@@ -33,13 +32,10 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.UnaryOperator;
 
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.xml.sax.SAXException;
 
 import org.apache.geode.cache.configuration.CacheConfig;
 import org.apache.geode.cache.configuration.JndiBindingsType;
@@ -122,8 +118,7 @@ public class CreateJndiBindingCommandTest {
   }
 
   @Test
-  public void returnsErrorIfBindingAlreadyExistsAndIfUnspecified()
-      throws ParserConfigurationException, SAXException, IOException {
+  public void returnsErrorIfBindingAlreadyExistsAndIfUnspecified() {
     InternalConfigurationPersistenceService clusterConfigService =
         mock(InternalConfigurationPersistenceService.class);
     CacheConfig cacheConfig = mock(CacheConfig.class);
@@ -139,8 +134,7 @@ public class CreateJndiBindingCommandTest {
   }
 
   @Test
-  public void skipsIfBindingAlreadyExistsAndIfSpecified()
-      throws ParserConfigurationException, SAXException, IOException {
+  public void skipsIfBindingAlreadyExistsAndIfSpecified() {
     InternalConfigurationPersistenceService clusterConfigService =
         mock(InternalConfigurationPersistenceService.class);
     CacheConfig cacheConfig = mock(CacheConfig.class);
