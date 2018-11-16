e0c3b8bb05b4d6881605b598d9543a7b207c443f
Jens Deppe <jdeppe@pivotal.io>
2018-11-16 08:39:56 -0800

    GEODE-5971: Update assembly_contents.txt for new the interface

--- geode-core/src/main/java/org/apache/geode/management/cli/UpdateAllConfigurationGroups.java
+++ geode-core/src/main/java/org/apache/geode/management/cli/UpdateAllConfigurationGroups.java
@@ -14,5 +14,13 @@
  */
 package org.apache.geode.management.cli;
 
+/**
+ * This interface is implemented by gfsh commands that can potentially update the configuration for
+ * all groups (including the cluster-wide group, "cluster").
+ * </p>
+ * CommandExecutor will builds the list of groups for configuration updating to include all groups
+ * if the command implements UpdateAllConfigurationGroups. Otherwise, the list of groups are those
+ * specified with a command --group option, or "cluster" if there is no --group option.
+ */
 public interface UpdateAllConfigurationGroups {
 }
