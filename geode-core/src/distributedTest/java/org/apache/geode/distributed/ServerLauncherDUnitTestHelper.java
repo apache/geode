351a80c2925a6b62b6de603eb04a7d0eb68f7767
Jens Deppe <jdeppe@pivotal.io>
2018-11-28 12:27:51 -0800

    GEODE-4883: Set server port correctly for an ephemeral port

--- geode-core/src/distributedTest/java/org/apache/geode/distributed/ServerLauncherDUnitTestHelper.java
+++ geode-core/src/distributedTest/java/org/apache/geode/distributed/ServerLauncherDUnitTestHelper.java
@@ -21,12 +21,16 @@ public class ServerLauncherDUnitTestHelper {
     int locatorPort = Integer.parseInt(args[0]);
 
     System.setProperty("gemfire.disableShutdownHook", "true");
-    System.setProperty("gemfire.server-port", "0");
 
     final ServerLauncher serverLauncher =
         new ServerLauncher.Builder().setCommand(ServerLauncher.Command.START)
-            .setMemberName("server1").set("locators", "localhost[" + locatorPort + "]")
-            .set("log-level", "config").set("log-file", "").setDebug(true).build();
+            .setMemberName("server1")
+            .set("locators", "localhost[" + locatorPort + "]")
+            .set("log-level", "config")
+            .set("log-file", "")
+            .setServerPort(0)
+            .setDebug(true)
+            .build();
 
     serverLauncher.start();
 
