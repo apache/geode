4fa89430d2ae0fbc3b3d9a8374bc77d7b606e62d
bross <bross@pivotal.io>
2018-11-29 11:28:44 -0800

    Completed Unit test and added DUnit test for DestroyDataSourceCommand

---
+++ geode-connectors/src/distributedTest/java/org/apache/geode/connectors/jdbc/internal/cli/DestroyDataSourceCommandDUnitTest.java
@@ -0,0 +1,101 @@
+package org.apache.geode.connectors.jdbc.internal.cli;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+
+import org.apache.geode.distributed.internal.InternalConfigurationPersistenceService;
+import org.apache.geode.distributed.internal.InternalLocator;
+import org.apache.geode.internal.jndi.JNDIInvoker;
+import org.apache.geode.management.internal.configuration.domain.Configuration;
+import org.apache.geode.management.internal.configuration.utils.XmlUtils;
+import org.apache.geode.test.dunit.rules.ClusterStartupRule;
+import org.apache.geode.test.dunit.rules.MemberVM;
+import org.apache.geode.test.junit.categories.GfshTest;
+import org.apache.geode.test.junit.rules.GfshCommandRule;
+import org.apache.geode.test.junit.rules.VMProvider;
+
+@Category(GfshTest.class)
+public class DestroyDataSourceCommandDUnitTest {
+  private static MemberVM locator, server1, server2;
+
+  @ClassRule
+  public static ClusterStartupRule cluster = new ClusterStartupRule();
+
+  @ClassRule
+  public static GfshCommandRule gfsh = new GfshCommandRule();
+
+  @BeforeClass
+  public static void before() throws Exception {
+    locator = cluster.startLocatorVM(0);
+    server1 = cluster.startServerVM(1, locator.getPort());
+    server2 = cluster.startServerVM(2, locator.getPort());
+
+    gfsh.connectAndVerify(locator);
+
+    gfsh.execute(
+        "create data-source --name=datasource1 --url=\"jdbc:derby:newDB;create=true\"");
+  }
+
+  @Test
+  public void testDestroyDataSource() {
+    // assert that there is a datasource
+    VMProvider
+        .invokeInEveryMember(
+            () -> assertThat(JNDIInvoker.getBindingNamesRecursively(JNDIInvoker.getJNDIContext()))
+                .containsKey("java:datasource1").containsValue(
+                    "org.apache.geode.internal.datasource.GemFireBasicDataSource"),
+            server1, server2);
+
+    gfsh.executeAndAssertThat("destroy data-source --name=datasource1").statusIsSuccess()
+        .tableHasColumnOnlyWithValues("Member", "server-1", "server-2")
+        .tableHasColumnOnlyWithValues("Message",
+            "Data source \"datasource1\" destroyed on \"server-1\"",
+            "Data source \"datasource1\" destroyed on \"server-2\"");
+
+    // verify cluster config is updated
+    locator.invoke(() -> {
+      InternalLocator internalLocator = ClusterStartupRule.getLocator();
+      AssertionsForClassTypes.assertThat(internalLocator).isNotNull();
+      InternalConfigurationPersistenceService ccService =
+          internalLocator.getConfigurationPersistenceService();
+      Configuration configuration = ccService.getConfiguration("cluster");
+      Document document = XmlUtils.createDocumentFromXml(configuration.getCacheXmlContent());
+      NodeList jndiBindings = document.getElementsByTagName("jndi-binding");
+
+      AssertionsForClassTypes.assertThat(jndiBindings.getLength()).isEqualTo(0);
+
+      boolean found = false;
+      for (int i = 0; i < jndiBindings.getLength(); i++) {
+        Element eachBinding = (Element) jndiBindings.item(i);
+        if (eachBinding.getAttribute("jndi-name").equals("datasource1")) {
+          found = true;
+          break;
+        }
+      }
+      AssertionsForClassTypes.assertThat(found).isFalse();
+    });
+
+    // verify datasource does not exists
+    VMProvider.invokeInEveryMember(
+        () -> AssertionsForClassTypes.assertThat(JNDIInvoker.getNoOfAvailableDataSources())
+            .isEqualTo(0),
+        server1, server2);
+
+    // bounce server1 and assert that there is still no datasource received from cluster config
+    server1.stop(false);
+    server1 = cluster.startServerVM(1, locator.getPort());
+
+    // verify no datasource from cluster config
+    server1.invoke(() -> {
+      AssertionsForClassTypes.assertThat(JNDIInvoker.getNoOfAvailableDataSources()).isEqualTo(0);
+    });
+  }
+}
--- geode-connectors/src/test/java/org/apache/geode/connectors/jdbc/internal/cli/DestroyDataSourceCommandTest.java
+++ geode-connectors/src/test/java/org/apache/geode/connectors/jdbc/internal/cli/DestroyDataSourceCommandTest.java
@@ -1,189 +1,205 @@
 package org.apache.geode.connectors.jdbc.internal.cli;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doAnswer;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.UnaryOperator;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.configuration.CacheConfig;
 import org.apache.geode.cache.configuration.JndiBindingsType;
 import org.apache.geode.distributed.DistributedMember;
 import org.apache.geode.distributed.internal.InternalConfigurationPersistenceService;
 import org.apache.geode.internal.cache.InternalCache;
-import org.apache.geode.management.internal.cli.commands.DestroyJndiBindingCommand;
 import org.apache.geode.management.internal.cli.functions.CliFunctionResult;
 import org.apache.geode.management.internal.cli.functions.DestroyJndiBindingFunction;
 import org.apache.geode.management.internal.configuration.domain.Configuration;
 import org.apache.geode.test.junit.rules.GfshParserRule;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Test;
-import org.mockito.ArgumentCaptor;
-
-import java.util.*;
-import java.util.function.UnaryOperator;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 public class DestroyDataSourceCommandTest {
-    @ClassRule
-    public static GfshParserRule gfsh = new GfshParserRule();
+  @ClassRule
+  public static GfshParserRule gfsh = new GfshParserRule();
 
-    private DestroyJndiBindingCommand command;
-    private InternalCache cache;
-    private CacheConfig cacheConfig;
-    private InternalConfigurationPersistenceService ccService;
+  private DestroyDataSourceCommand command;
+  private InternalCache cache;
+  private CacheConfig cacheConfig;
+  private InternalConfigurationPersistenceService ccService;
 
-    private static String COMMAND = "destroy data-source ";
+  private static String COMMAND = "destroy data-source ";
 
-    @Before
-    public void setUp() throws Exception {
-        cache = mock(InternalCache.class);
-        command = spy(DestroyDataSourceCommand.class);
-        doReturn(cache).when(command).getCache();
-        cacheConfig = mock(CacheConfig.class);
-        ccService = mock(InternalConfigurationPersistenceService.class);
+  @Before
+  public void setUp() throws Exception {
+    cache = mock(InternalCache.class);
+    command = spy(DestroyDataSourceCommand.class);
+    doReturn(cache).when(command).getCache();
+    cacheConfig = mock(CacheConfig.class);
+    ccService = mock(InternalConfigurationPersistenceService.class);
 
-        doReturn(Collections.emptySet()).when(command).findMembers(any(), any());
-        doReturn(ccService).when(command).getConfigurationPersistenceService();
-        when(ccService.getCacheConfig(any())).thenReturn(cacheConfig);
-        doAnswer(invocation -> {
-            UnaryOperator<CacheConfig> mutator = invocation.getArgument(1);
-            mutator.apply(cacheConfig);
-            return null;
-        }).when(ccService).updateCacheConfig(any(), any());
+    doReturn(Collections.emptySet()).when(command).findMembers(any(), any());
+    doReturn(ccService).when(command).getConfigurationPersistenceService();
+    when(ccService.getCacheConfig(any())).thenReturn(cacheConfig);
+    doAnswer(invocation -> {
+      UnaryOperator<CacheConfig> mutator = invocation.getArgument(1);
+      mutator.apply(cacheConfig);
+      return null;
+    }).when(ccService).updateCacheConfig(any(), any());
 
-        when(ccService.getConfigurationRegion()).thenReturn(mock(Region.class));
-        when(ccService.getConfiguration(any())).thenReturn(mock(Configuration.class));
-    }
+    when(ccService.getConfigurationRegion()).thenReturn(mock(Region.class));
+    when(ccService.getConfiguration(any())).thenReturn(mock(Configuration.class));
+  }
 
-    @Test
-    public void missingMandatory() {
-        gfsh.executeAndAssertThat(command, COMMAND).statusIsError().containsOutput("Invalid command");
-    }
+  @Test
+  public void missingMandatory() {
+    gfsh.executeAndAssertThat(command, COMMAND).statusIsError().containsOutput("Invalid command");
+  }
 
-    @Test
-    public void returnsErrorIfBindingDoesNotExistAndIfExistsUnspecified() {
-        gfsh.executeAndAssertThat(command, COMMAND + " --name=name").statusIsError()
-                .containsOutput("does not exist.");
-    }
+  @Test
+  public void returnsErrorIfBindingDoesNotExistAndIfExistsUnspecified() {
+    gfsh.executeAndAssertThat(command, COMMAND + " --name=name").statusIsError()
+        .containsOutput("does not exist.");
+  }
 
-    @Test
-    public void skipsIfBindingDoesNotExistAndIfExistsSpecified() {
-        gfsh.executeAndAssertThat(command, COMMAND + " --name=name --if-exists").statusIsSuccess()
-                .containsOutput("does not exist.");
-    }
+  @Test
+  public void skipsIfBindingDoesNotExistAndIfExistsSpecified() {
+    gfsh.executeAndAssertThat(command, COMMAND + " --name=name --if-exists").statusIsSuccess()
+        .containsOutput("does not exist.");
+  }
 
-    @Test
-    public void skipsIfBindingDoesNotExistAndIfExistsSpecifiedTrue() {
-        gfsh.executeAndAssertThat(command, COMMAND + " --name=name --if-exists=true").statusIsSuccess()
-                .containsOutput("does not exist.");
-    }
+  @Test
+  public void skipsIfBindingDoesNotExistAndIfExistsSpecifiedTrue() {
+    gfsh.executeAndAssertThat(command, COMMAND + " --name=name --if-exists=true").statusIsSuccess()
+        .containsOutput("does not exist.");
+  }
 
-    @Test
-    public void returnsErrorIfBindingDoesNotExistAndIfExistsSpecifiedFalse() {
-        gfsh.executeAndAssertThat(command, COMMAND + " --name=name --if-exists=false").statusIsError()
-                .containsOutput("does not exist.");
-    }
+  @Test
+  public void returnsErrorIfBindingDoesNotExistAndIfExistsSpecifiedFalse() {
+    gfsh.executeAndAssertThat(command, COMMAND + " --name=name --if-exists=false").statusIsError()
+        .containsOutput("does not exist.");
+  }
 
-    @Test
-    public void whenNoMembersFoundAndNoClusterConfigServiceRunningThenError() {
-        doReturn(Collections.emptySet()).when(command).findMembers(any(), any());
-        doReturn(null).when(command).getConfigurationPersistenceService();
+  @Test
+  public void whenNoMembersFoundAndNoClusterConfigServiceRunningThenError() {
+    doReturn(Collections.emptySet()).when(command).findMembers(any(), any());
+    doReturn(null).when(command).getConfigurationPersistenceService();
 
-        gfsh.executeAndAssertThat(command, COMMAND + " --name=name").statusIsSuccess()
-                .containsOutput("No members found").containsOutput(
-                "Cluster configuration service is not running. Configuration change is not persisted.");
-    }
+    gfsh.executeAndAssertThat(command, COMMAND + " --name=name").statusIsSuccess()
+        .containsOutput("No members found").containsOutput(
+            "Cluster configuration service is not running. Configuration change is not persisted.");
+  }
 
-    @Test
-    public void whenNoMembersFoundAndClusterConfigRunningThenUpdateClusterConfig() {
-        List<JndiBindingsType.JndiBinding> bindings = new ArrayList<>();
-        JndiBindingsType.JndiBinding jndiBinding = new JndiBindingsType.JndiBinding();
-        jndiBinding.setJndiName("name");
-        bindings.add(jndiBinding);
-        doReturn(bindings).when(cacheConfig).getJndiBindings();
+  @Test
+  public void whenNoMembersFoundAndClusterConfigRunningThenUpdateClusterConfig() {
+    List<JndiBindingsType.JndiBinding> bindings = new ArrayList<>();
+    JndiBindingsType.JndiBinding jndiBinding = new JndiBindingsType.JndiBinding();
+    jndiBinding.setJndiName("name");
+    bindings.add(jndiBinding);
+    doReturn(bindings).when(cacheConfig).getJndiBindings();
 
-        gfsh.executeAndAssertThat(command, COMMAND + " --name=name").statusIsSuccess()
-                .containsOutput("No members found.")
-                .containsOutput("Changes to configuration for group 'cluster' are persisted.");
+    gfsh.executeAndAssertThat(command, COMMAND + " --name=name").statusIsSuccess()
+        .containsOutput("No members found.")
+        .containsOutput("Changes to configuration for group 'cluster' are persisted.");
 
-        verify(ccService).updateCacheConfig(any(), any());
-        verify(command).updateConfigForGroup(eq("cluster"), eq(cacheConfig), any());
-    }
+    verify(ccService).updateCacheConfig(any(), any());
+    verify(command).updateConfigForGroup(eq("cluster"), eq(cacheConfig), any());
+  }
 
-    @Test
-    public void whenMembersFoundAndNoClusterConfigRunningThenOnlyInvokeFunction() {
-        Set<DistributedMember> members = new HashSet<>();
-        members.add(mock(DistributedMember.class));
+  @Test
+  public void whenMembersFoundAndNoClusterConfigRunningThenOnlyInvokeFunction() {
+    Set<DistributedMember> members = new HashSet<>();
+    members.add(mock(DistributedMember.class));
 
-        CliFunctionResult result =
-                new CliFunctionResult("server1", true, "Jndi binding \"name\" destroyed on \"server1\"");
-        List<CliFunctionResult> results = new ArrayList<>();
-        results.add(result);
+    CliFunctionResult result =
+        new CliFunctionResult("server1", true, "Data source \"name\" destroyed on \"server1\"");
+    List<CliFunctionResult> results = new ArrayList<>();
+    results.add(result);
 
-        doReturn(members).when(command).findMembers(any(), any());
-        doReturn(null).when(command).getConfigurationPersistenceService();
-        doReturn(results).when(command).executeAndGetFunctionResult(any(), any(), any());
+    doReturn(members).when(command).findMembers(any(), any());
+    doReturn(null).when(command).getConfigurationPersistenceService();
+    doReturn(results).when(command).executeAndGetFunctionResult(any(), any(), any());
 
-        gfsh.executeAndAssertThat(command, COMMAND + " --name=name").statusIsSuccess()
-                .tableHasColumnOnlyWithValues("Member", "server1")
-                .tableHasColumnOnlyWithValues("Status", "OK")
-                .tableHasColumnOnlyWithValues("Message", "Jndi binding \"name\" destroyed on \"server1\"");
+    gfsh.executeAndAssertThat(command, COMMAND + " --name=name").statusIsSuccess()
+        .tableHasColumnOnlyWithValues("Member", "server1")
+        .tableHasColumnOnlyWithValues("Status", "OK")
+        .tableHasColumnOnlyWithValues("Message", "Data source \"name\" destroyed on \"server1\"");
 
-        verify(ccService, times(0)).updateCacheConfig(any(), any());
+    verify(ccService, times(0)).updateCacheConfig(any(), any());
 
-        ArgumentCaptor<DestroyJndiBindingFunction> function =
-                ArgumentCaptor.forClass(DestroyJndiBindingFunction.class);
-        ArgumentCaptor<String> jndiName = ArgumentCaptor.forClass(String.class);
-        ArgumentCaptor<Set<DistributedMember>> targetMembers = ArgumentCaptor.forClass(Set.class);
-        verify(command, times(1)).executeAndGetFunctionResult(function.capture(), jndiName.capture(),
-                targetMembers.capture());
+    ArgumentCaptor<DestroyJndiBindingFunction> function =
+        ArgumentCaptor.forClass(DestroyJndiBindingFunction.class);
+    ArgumentCaptor<Object[]> arguments = ArgumentCaptor.forClass(Object[].class);
 
-        assertThat(function.getValue()).isInstanceOf(DestroyJndiBindingFunction.class);
-        assertThat(jndiName.getValue()).isNotNull();
-        assertThat(jndiName.getValue()).isEqualTo("name");
-        assertThat(targetMembers.getValue()).isEqualTo(members);
-    }
+    ArgumentCaptor<Set<DistributedMember>> targetMembers = ArgumentCaptor.forClass(Set.class);
+    verify(command, times(1)).executeAndGetFunctionResult(function.capture(), arguments.capture(),
+        targetMembers.capture());
 
-    @Test
-    public void whenMembersFoundAndClusterConfigRunningThenUpdateClusterConfigAndInvokeFunction() {
-        List<JndiBindingsType.JndiBinding> bindings = new ArrayList<>();
-        JndiBindingsType.JndiBinding jndiBinding = new JndiBindingsType.JndiBinding();
-        jndiBinding.setJndiName("name");
-        bindings.add(jndiBinding);
-        doReturn(bindings).when(cacheConfig).getJndiBindings();
+    String jndiName = (String) arguments.getValue()[0];
+    boolean destroyingDataSource = (boolean) arguments.getValue()[1];
 
-        Set<DistributedMember> members = new HashSet<>();
-        members.add(mock(DistributedMember.class));
+    assertThat(function.getValue()).isInstanceOf(DestroyJndiBindingFunction.class);
+    assertThat(jndiName).isEqualTo("name");
+    assertThat(destroyingDataSource).isEqualTo(true);
+    assertThat(targetMembers.getValue()).isEqualTo(members);
+  }
 
-        CliFunctionResult result =
-                new CliFunctionResult("server1", true, "Jndi binding \"name\" destroyed on \"server1\"");
-        List<CliFunctionResult> results = new ArrayList<>();
-        results.add(result);
+  @Test
+  public void whenMembersFoundAndClusterConfigRunningThenUpdateClusterConfigAndInvokeFunction() {
+    List<JndiBindingsType.JndiBinding> bindings = new ArrayList<>();
+    JndiBindingsType.JndiBinding jndiBinding = new JndiBindingsType.JndiBinding();
+    jndiBinding.setJndiName("name");
+    bindings.add(jndiBinding);
+    doReturn(bindings).when(cacheConfig).getJndiBindings();
 
-        doReturn(members).when(command).findMembers(any(), any());
-        doReturn(results).when(command).executeAndGetFunctionResult(any(), any(), any());
+    Set<DistributedMember> members = new HashSet<>();
+    members.add(mock(DistributedMember.class));
 
-        gfsh.executeAndAssertThat(command, COMMAND + " --name=name").statusIsSuccess()
-                .tableHasColumnOnlyWithValues("Member", "server1")
-                .tableHasColumnOnlyWithValues("Status", "OK")
-                .tableHasColumnOnlyWithValues("Message", "Jndi binding \"name\" destroyed on \"server1\"");
+    CliFunctionResult result =
+        new CliFunctionResult("server1", true, "Data source \"name\" destroyed on \"server1\"");
+    List<CliFunctionResult> results = new ArrayList<>();
+    results.add(result);
 
-        assertThat(cacheConfig.getJndiBindings().isEmpty()).isTrue();
-        verify(command).updateConfigForGroup(eq("cluster"), eq(cacheConfig), any());
+    doReturn(members).when(command).findMembers(any(), any());
+    doReturn(results).when(command).executeAndGetFunctionResult(any(), any(), any());
 
-        ArgumentCaptor<DestroyJndiBindingFunction> function =
-                ArgumentCaptor.forClass(DestroyJndiBindingFunction.class);
-        ArgumentCaptor<String> jndiName = ArgumentCaptor.forClass(String.class);
-        ArgumentCaptor<Set<DistributedMember>> targetMembers = ArgumentCaptor.forClass(Set.class);
-        verify(command, times(1)).executeAndGetFunctionResult(function.capture(), jndiName.capture(),
-                targetMembers.capture());
+    gfsh.executeAndAssertThat(command, COMMAND + " --name=name").statusIsSuccess()
+        .tableHasColumnOnlyWithValues("Member", "server1")
+        .tableHasColumnOnlyWithValues("Status", "OK")
+        .tableHasColumnOnlyWithValues("Message", "Data source \"name\" destroyed on \"server1\"");
 
-        assertThat(function.getValue()).isInstanceOf(DestroyJndiBindingFunction.class);
-        assertThat(jndiName.getValue()).isNotNull();
-        assertThat(jndiName.getValue()).isEqualTo("name");
-        assertThat(targetMembers.getValue()).isEqualTo(members);
-    }
+    assertThat(cacheConfig.getJndiBindings().isEmpty()).isTrue();
+    verify(command).updateConfigForGroup(eq("cluster"), eq(cacheConfig), any());
+
+    ArgumentCaptor<DestroyJndiBindingFunction> function =
+        ArgumentCaptor.forClass(DestroyJndiBindingFunction.class);
+    ArgumentCaptor<Object[]> arguments = ArgumentCaptor.forClass(Object[].class);
+
+    ArgumentCaptor<Set<DistributedMember>> targetMembers = ArgumentCaptor.forClass(Set.class);
+    verify(command, times(1)).executeAndGetFunctionResult(function.capture(), arguments.capture(),
+        targetMembers.capture());
+
+    String jndiName = (String) arguments.getValue()[0];
+    boolean destroyingDataSource = (boolean) arguments.getValue()[1];
+
+    assertThat(function.getValue()).isInstanceOf(DestroyJndiBindingFunction.class);
+    assertThat(jndiName).isEqualTo("name");
+    assertThat(destroyingDataSource).isEqualTo(true);
+    assertThat(targetMembers.getValue()).isEqualTo(members);
+  }
 }
