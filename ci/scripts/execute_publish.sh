50ea625cbe1c660d9cdfe79c310a88704afcbe88
Robert Houghton <rhoughton@pivotal.io>
2019-01-10 12:02:18 -0800

    GEODE-6259: Refine version specification and consumption (#3058)
    * Streamline version specification -- version can be specified directly, rather than as a combination of versionNumber, releaseQualifier, and releaseType
    * Concourse meta-property MAVEN_SNAPSNOT_BUCKET should now be the complete bucket path.
    * Concourse uses 'SNAPSHOT' as the semver pre-release token for develop
    * Concourse uses semver resource as the source-of-truth for binary build-id and version
    * Use semver content to help determine release or snapshot in concourse
    * Zero-pad build-id number for publications
    
    Co-authored-by: Robert Houghton <rhoughton@pivotal.io>
    Co-authored-by: Patrick Rhomberg <prhomberg@pivotal.io>

--- ci/scripts/execute_publish.sh
+++ ci/scripts/execute_publish.sh
@@ -37,7 +37,6 @@ EMAIL_BODY="results/body"
 
 GEODE_BUILD_VERSION_FILE=${ROOT_DIR}/geode-build-version/number
 GEODE_RESULTS_VERSION_FILE=${ROOT_DIR}/results/number
-GEODE_BUILD_VERSION_NUMBER=$(echo "${GRADLE_GLOBAL_ARGS}"|tr ' ' '\n' | grep "versionNumber *=" - geode/gradle.properties | awk -F "=" '{print $2; exit}' | tr -d ' ')
 GEODE_BUILD_DIR=/tmp/geode-build
 GEODE_PULL_REQUEST_ID_FILE=${ROOT_DIR}/geode/.git/id
 
@@ -45,14 +44,23 @@ if [ -e "${GEODE_PULL_REQUEST_ID_FILE}" ]; then
   GEODE_PULL_REQUEST_ID=$(cat ${GEODE_PULL_REQUEST_ID_FILE})
   FULL_PRODUCT_VERSION="geode-pr-${GEODE_PULL_REQUEST_ID}"
 else
+  # semver resource, e.g., "1.9.0-SNAPSHOT.325"
   CONCOURSE_VERSION=$(cat ${GEODE_BUILD_VERSION_FILE})
-  CONCOURSE_PRODUCT_VERSION=${CONCOURSE_VERSION%%-*}
-  GEODE_PRODUCT_VERSION=${GEODE_BUILD_VERSION_NUMBER}
+  # Prune all after '-', yielding e.g., "1.9.0"
+  GEODE_PRODUCT_VERSION=${CONCOURSE_VERSION%%-*}
+  # Prune all before '-', yielding e.g., "SNAPSHOT.325"
   CONCOURSE_BUILD_SLUG=${CONCOURSE_VERSION##*-}
-  BUILD_ID=${CONCOURSE_VERSION##*.}
-  FULL_PRODUCT_VERSION=${GEODE_PRODUCT_VERSION}-${CONCOURSE_BUILD_SLUG}
+  # Prune all before '.', yielding e.g., "SNAPSHOT"
+  SNAPSHOT_SLUG=${CONCOURSE_BUILD_SLUG##*.}
+  # Prune all before '.', yielding e.g., "325"
+  BUILD_ID=$(printf "%04d" ${CONCOURSE_VERSION##*.})
+
+  # Rebuild version, zero-padded
+  FULL_PRODUCT_VERSION=${GEODE_PRODUCT_VERSION}-${SNAPSHOT_SLUG}.${BUILD_ID}
+
   echo "Concourse VERSION is ${CONCOURSE_VERSION}"
   echo "Geode product VERSION is ${GEODE_PRODUCT_VERSION}"
+  echo "Full product VERSION is ${FULL_PRODUCT_VERSION}"
   echo "Build ID is ${BUILD_ID}"
 fi
 
@@ -68,7 +76,10 @@ SET_JAVA_HOME="export JAVA_HOME=/usr/lib/jvm/java-${JAVA_BUILD_VERSION}-openjdk-
 GRADLE_COMMAND="./gradlew \
     ${DEFAULT_GRADLE_TASK_OPTIONS} \
     ${GRADLE_GLOBAL_ARGS} \
-    -PbuildId=${BUILD_ID} -PmavenSnapshotBucket=${MAVEN_SNAPSHOT_BUCKET} publish"
+    -Pversion=${FULL_PRODUCT_VERSION} \
+    -PbuildId=${BUILD_ID} \
+    -PmavenRepository=\"${MAVEN_SNAPSHOT_BUCKET}\" \
+    publish"
 
 echo "${GRADLE_COMMAND}"
 ssh ${SSH_OPTIONS} geode@${INSTANCE_IP_ADDRESS} "mkdir -p tmp && cd geode && ${SET_JAVA_HOME} && ${GRADLE_COMMAND}"
