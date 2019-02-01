0a92ce2be2061cbab5e8e89c132641778d3ba6b0
Mark Hanson <mhanson@pivotal.io>
2019-02-01 15:31:57 -0800

    GEODE-6345: Moving StatSamplerStatsTest to mocks
    Keeping JvmPausesIsALong to validate type

--- geode-core/src/test/java/org/apache/geode/internal/statistics/StatSamplerStatsTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/statistics/StatSamplerStatsTest.java
@@ -15,8 +15,10 @@
 package org.apache.geode.internal.statistics;
 
 import static org.apache.geode.internal.statistics.StatSamplerStats.jvmPausesId;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -30,13 +32,15 @@ import org.apache.geode.StatisticsType;
 public class StatSamplerStatsTest {
   private static final String TEXT_ID = "statSampler";
 
+  private StatisticsType statisticsType;
+  private StatisticsFactory statisticsFactory;
   private Statistics statistics;
   private StatSamplerStats statSamplerStats;
 
   @Before
   public void setUp() {
-    StatisticsType statisticsType = StatSamplerStats.getStatisticsType();
-    StatisticsFactory statisticsFactory = mock(StatisticsFactory.class);
+    statisticsType = StatSamplerStats.getStatisticsType();
+    statisticsFactory = mock(StatisticsFactory.class);
 
     statistics = mock(Statistics.class);
 
@@ -58,6 +62,19 @@ public class StatSamplerStatsTest {
     statSamplerStats.incJvmPauses();
 
     verify(statistics).incLong(eq(jvmPausesId), eq(1L));
+  }
+
+  @Test
+  public void jvmPausesIsALong() {
+    StatisticsManager statisticsManager = mock(StatisticsManager.class);
+    statistics = spy(new LocalStatisticsImpl(statisticsType, TEXT_ID, 1, 1, false, 0,
+        statisticsManager));
+    when(statisticsFactory.createStatistics(eq(statisticsType), eq(TEXT_ID), eq(1L)))
+        .thenReturn(statistics);
+    statSamplerStats = new StatSamplerStats(statisticsFactory, 1);
+    statistics.incLong(jvmPausesId, Integer.MAX_VALUE + 1L);
+
+    assertThat(statSamplerStats.getJvmPauses()).isPositive();
   }
 
 }
