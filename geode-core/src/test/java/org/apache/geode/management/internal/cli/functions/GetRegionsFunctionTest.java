476663f66bd2fb29709e4677190fca359a2c2054
Kirk Lund <klund@apache.org>
2019-02-15 13:52:14 -0800

    GEODE-6420: Cleanup GetRegionsFunctionTest
    * Reimplement GetRegionsFunctionCacheFactoryTest with Mockito.verify.
    * Combine GetRegionsFunctionCacheFactoryTest and GetRegionsFunctionTest.
    * Cleanup GetRegionsFunctionTest including addition of assertions.
    * Give type to GetRegionsFunction and its ResultSender results.

--- geode-core/src/test/java/org/apache/geode/management/internal/cli/functions/GetRegionsFunctionTest.java
+++ geode-core/src/test/java/org/apache/geode/management/internal/cli/functions/GetRegionsFunctionTest.java
@@ -14,102 +14,107 @@
  */
 package org.apache.geode.management.internal.cli.functions;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.isNull;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Collections;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
-import org.junit.contrib.java.lang.system.RestoreSystemProperties;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 
-import org.apache.geode.CancelCriterion;
+import org.apache.geode.cache.Cache;
 import org.apache.geode.cache.DataPolicy;
 import org.apache.geode.cache.Region;
 import org.apache.geode.cache.RegionAttributes;
 import org.apache.geode.cache.Scope;
 import org.apache.geode.cache.execute.FunctionContext;
 import org.apache.geode.cache.execute.ResultSender;
-import org.apache.geode.distributed.internal.DistributionConfig;
-import org.apache.geode.distributed.internal.InternalDistributedSystem;
-import org.apache.geode.internal.cache.GemFireCacheImpl;
-import org.apache.geode.internal.cache.LocalRegion;
-import org.apache.geode.internal.cache.control.InternalResourceManager;
+import org.apache.geode.management.internal.cli.domain.RegionInformation;
 
+/**
+ * Characterization unit tests for {@code GetRegionsFunction};
+ */
 public class GetRegionsFunctionTest {
 
-  @Rule
-  public RestoreSystemProperties restoreSystemProperties = new RestoreSystemProperties();
+  @Mock
+  private RegionAttributes<Object, Object> regionAttributes;
 
   @Mock
-  private RegionAttributes regionAttributes;
+  private Region<Object, Object> region;
 
   @Mock
-  private LocalRegion region;
+  private FunctionContext<Void> functionContext;
 
   @Mock
-  private GemFireCacheImpl cache;
+  private ResultSender<RegionInformation[]> resultSender;
 
-  @Mock
-  private InternalResourceManager internalResourceManager;
-
-  @Mock
-  private FunctionContext functionContext;
-
-  private final TestResultSender testResultSender = new TestResultSender();
-  private final Set<Region<?, ?>> regions = new HashSet<>();
-  private final Set<Region<?, ?>> subregions = new HashSet<>();
   private final GetRegionsFunction getRegionsFunction = new GetRegionsFunction();
 
   @Before
-  public void before() {
-    System.setProperty(DistributionConfig.GEMFIRE_PREFIX + "statsDisabled", "true");
-
+  public void setUp() {
     initMocks(this);
 
-    when(cache.getCancelCriterion()).thenReturn(mock(CancelCriterion.class));
-    when(cache.getDistributedSystem()).thenReturn(mock(InternalDistributedSystem.class));
-    when(cache.getResourceManager()).thenReturn(internalResourceManager);
-    when(functionContext.getResultSender()).thenReturn(testResultSender);
-    when(functionContext.getCache()).thenReturn(cache);
+    when(functionContext.<RegionInformation[]>getResultSender()).thenReturn(resultSender);
   }
 
   @Test
-  public void testExecuteWithoutRegions() {
+  public void lastResultIsNullWhenThereAreNoRegions() {
+    Cache cacheFromFunctionContext = mock(Cache.class);
+    when(cacheFromFunctionContext.rootRegions()).thenReturn(Collections.emptySet());
+    when(functionContext.getCache()).thenReturn(cacheFromFunctionContext);
+
     getRegionsFunction.execute(functionContext);
+
+    verify(resultSender).lastResult(isNull());
   }
 
   @Test
-  public void testExecuteWithRegions() {
-    when(cache.rootRegions()).thenReturn(regions);
-    when(region.getFullPath()).thenReturn("/MyRegion");
+  public void lastResultHasRegionInformationForRegion() {
+    String regionNameInCacheFromFunctionContext = "MyRegion";
+    Cache cacheFromFunctionContext = cacheWithOneRootRegion(regionNameInCacheFromFunctionContext);
+    when(functionContext.getCache()).thenReturn(cacheFromFunctionContext);
 
-    when(region.getParentRegion()).thenReturn(null);
-    when(region.subregions(true)).thenReturn(subregions);
-    when(region.subregions(false)).thenReturn(subregions);
-    when(region.getAttributes()).thenReturn(regionAttributes);
+    getRegionsFunction.execute(functionContext);
+
+    assertThat(lastResultFrom(resultSender).getValue())
+        .extracting(r -> r.getPath())
+        .containsExactly(regionNameInCacheFromFunctionContext);
+  }
+
+  @Test
+  public void getsCacheFromFunctionContext() {
+    Cache cacheFromFunctionContext = mock(Cache.class);
+    when(cacheFromFunctionContext.rootRegions()).thenReturn(Collections.emptySet());
+    when(functionContext.getCache()).thenReturn(cacheFromFunctionContext);
+
+    getRegionsFunction.execute(functionContext);
+
+    verify(functionContext).getCache();
+  }
+
+  private Cache cacheWithOneRootRegion(String regionName) {
+    Cache cache = mock(Cache.class);
     when(regionAttributes.getDataPolicy()).thenReturn(mock(DataPolicy.class));
     when(regionAttributes.getScope()).thenReturn(mock(Scope.class));
-    regions.add(region);
-    getRegionsFunction.execute(functionContext);
+    when(region.getFullPath()).thenReturn(Region.SEPARATOR + regionName);
+    when(region.subregions(anyBoolean())).thenReturn(Collections.emptySet());
+    when(region.getAttributes()).thenReturn(regionAttributes);
+    when(cache.rootRegions()).thenReturn(Collections.singleton(region));
+    return cache;
   }
 
-  private static class TestResultSender implements ResultSender {
-
-    @Override
-    public void lastResult(final Object lastResult) {}
-
-    @Override
-    public void sendResult(final Object oneResult) {}
-
-    @Override
-    public void sendException(final Throwable t) {
-      throw new RuntimeException(t);
-    }
+  private ArgumentCaptor<RegionInformation[]> lastResultFrom(
+      ResultSender<RegionInformation[]> resultSender) {
+    ArgumentCaptor<RegionInformation[]> lastResult =
+        ArgumentCaptor.forClass(RegionInformation[].class);
+    verify(resultSender).lastResult(lastResult.capture());
+    return lastResult;
   }
-
 }
