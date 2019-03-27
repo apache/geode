f343f230a5b196e6e5431ba1a72eb41a63f13c25
Darrel Schneider <dschneider@pivotal.io>
2019-03-27 11:00:22 -0700

    disabled other changes to LoopRunner to see if they are causing the test failure

--- geode-concurrency-test/src/main/java/org/apache/geode/test/concurrency/loop/LoopRunner.java
+++ geode-concurrency-test/src/main/java/org/apache/geode/test/concurrency/loop/LoopRunner.java
@@ -21,12 +21,10 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.Callable;
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.geode.test.concurrency.ParallelExecutor;
 import org.apache.geode.test.concurrency.Runner;
@@ -77,8 +75,8 @@ public class LoopRunner implements Runner {
   private static class DelegatingExecutor implements ParallelExecutor {
     private final ExecutorService executorService;
     private List<Future<?>> futures;
-    private final AtomicInteger callablesStarting = new AtomicInteger(0);
-    private final CountDownLatch start = new CountDownLatch(1);
+    // private final AtomicInteger callablesStarting = new AtomicInteger(0);
+    // private final CountDownLatch start = new CountDownLatch(1);
 
     public DelegatingExecutor(ExecutorService executorService) {
       this.executorService = executorService;
@@ -87,26 +85,25 @@ public class LoopRunner implements Runner {
 
     @Override
     public <T> Future<T> inParallel(Callable<T> callable) {
-      callablesStarting.getAndIncrement();
-      Future<T> future = executorService.submit(() -> {
-        callablesStarting.getAndDecrement();
-        start.await();
-        return callable.call();
-      });
+      Future<T> future = executorService.submit(callable);
+      // callablesStarting.getAndIncrement();
+      // Future<T> future = executorService.submit(() -> {
+      // callablesStarting.getAndDecrement();
+      // start.await();
+      // return callable.call();
+      // });
       futures.add(future);
       return future;
     }
 
     @Override
     public void execute() throws ExecutionException, InterruptedException {
-      while (callablesStarting.get() > 0);
-
-      start.countDown();
+      // while (callablesStarting.get() > 0);
+      // start.countDown();
       for (Future future : futures) {
         future.get();
       }
-      futures.clear();
+      // futures.clear();
     }
-
   }
 }
