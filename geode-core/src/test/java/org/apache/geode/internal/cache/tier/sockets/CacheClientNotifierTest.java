0f8d4630594ab516fa6995c1b1c956a034cdf56f
Ryan McMahon <rmcmahon@pivotal.io>
2019-04-16 10:24:17 -0700

    Cleaning up test

--- geode-core/src/test/java/org/apache/geode/internal/cache/tier/sockets/CacheClientNotifierTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/tier/sockets/CacheClientNotifierTest.java
@@ -36,7 +36,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.junit.Test;
 
 import org.apache.geode.cache.query.internal.cq.ServerCQ;
@@ -60,82 +59,110 @@ public class CacheClientNotifierTest {
     when(clientRegistrationMetadata.getClientProxyMembershipID()).thenReturn(
         clientProxyMembershipID);
     Socket socket = mock(Socket.class);
+    CacheClientProxy cacheClientProxy = mock(CacheClientProxy.class);
+    ClientUpdateMessageImpl clientUpdateMessage = mock(ClientUpdateMessageImpl.class);
 
     CacheClientNotifier cacheClientNotifier = CacheClientNotifier.getInstance(internalCache,
         cacheServerStats, 0, 0, connectionListener, null, false);
     final CacheClientNotifier cacheClientNotifierSpy = spy(cacheClientNotifier);
-    ClientUpdateMessageImpl clientUpdateMessage = mock(ClientUpdateMessageImpl.class);
 
     CountDownLatch waitForEventDispatchCountdownLatch = new CountDownLatch(1);
-    CountDownLatch waitForRegistrationCoundownLatch = new CountDownLatch(1);
-    CacheClientProxy cacheClientProxy = mock(CacheClientProxy.class);
-
-    doAnswer((i) -> {
-      when(cacheClientProxy.getProxyID()).thenReturn(clientProxyMembershipID);
-      cacheClientNotifierSpy.addClientProxy(cacheClientProxy);
-      waitForRegistrationCoundownLatch.countDown();
-      waitForEventDispatchCountdownLatch.await();
-      return null;
-    }).when(cacheClientNotifierSpy).registerClientInternal(clientRegistrationMetadata, socket,
-        false, 0, true);
-
-    ExecutorService executor = Executors.newFixedThreadPool(2);
+    CountDownLatch waitForRegistrationCountdownLatch = new CountDownLatch(1);
+    ExecutorService registerAndNotifyExecutor = Executors.newFixedThreadPool(2);
 
     try {
+      // We stub out the CacheClientNotifier.registerClientInternal() to do some "work" until
+      // a new event is received and queued, as triggered by the waitForEventDispatchCountdownLatch
+      doAnswer((i) -> {
+        when(cacheClientProxy.getProxyID()).thenReturn(clientProxyMembershipID);
+        cacheClientNotifierSpy.addClientProxy(cacheClientProxy);
+        waitForRegistrationCountdownLatch.countDown();
+        waitForEventDispatchCountdownLatch.await();
+        return null;
+      }).when(cacheClientNotifierSpy).registerClientInternal(clientRegistrationMetadata, socket,
+          false, 0, true);
       List<Callable<Void>> registerAndNotifyTasks = new ArrayList<>();
 
+      // In one thread, we register the new client which should create the temporary client
+      // registration event queue. Events will be passed to that queue while registration is
+      // underway. Once registration is complete, the queue is drained and the event is processed
+      // as normal.
       registerAndNotifyTasks.add(() -> {
         cacheClientNotifierSpy.registerClient(clientRegistrationMetadata, socket, false, 0, true);
         return null;
       });
 
+      // In a second thread, we mock the arrival of a new event. We want to ensure this event
+      // goes into the temporary client registration event queue. To do that, we wait on the
+      // waitForRegistrationCountdownLatch until registration is underway and the temp queue is
+      // created. Once it is, we process the event and notify clients, which should add the event
+      // to the temp queue. Finally, we resume registration and after it is complete, we verify
+      // that the event was drained, processed, and "delivered" (note message delivery is mocked
+      // and results in a no-op).
       registerAndNotifyTasks.add(() -> {
         try {
-          waitForRegistrationCoundownLatch.await();
-          InternalCacheEvent internalCacheEvent = mock(InternalCacheEvent.class);
-          DistributedRegion region = mock(DistributedRegion.class);
-          when(internalCacheEvent.getRegion()).thenReturn(region);
-          FilterRoutingInfo.FilterInfo filterInfo = mock(FilterRoutingInfo.FilterInfo.class);
-          final Long cqId = 0L;
-          final String cqName = "testCQ";
-          HashMap cqs = new HashMap<Long, Integer>() {
-            {
-              put(cqId, 123);
-            }
-          };
-          when(filterInfo.getCQs()).thenReturn(cqs);
-          when(internalCacheEvent.getLocalFilterInfo()).thenReturn(
-              filterInfo);
-          final FilterProfile filterProfile = mock(FilterProfile.class);
-          when(filterProfile.getRealCqID(cqId)).thenReturn(cqName);
-          final ServerCQ serverCQ = mock(ServerCQ.class);
-          when(serverCQ.getClientProxyId()).thenReturn(clientProxyMembershipID);
-          when(filterProfile.getCq(cqName)).thenReturn(serverCQ);
-          when(region.getFilterProfile()).thenReturn(filterProfile);
-          FilterRoutingInfo filterRoutingInfo = mock(FilterRoutingInfo.class);
-          when(filterRoutingInfo.getLocalFilterInfo()).thenReturn(filterInfo);
-          when(filterProfile.getFilterRoutingInfoPart2(null, internalCacheEvent))
-              .thenReturn(filterRoutingInfo);
-          doReturn(clientUpdateMessage).when(cacheClientNotifierSpy)
-              .constructClientMessage(internalCacheEvent);
+          InternalCacheEvent internalCacheEvent =
+              createMockInternalCacheEvent(clientProxyMembershipID, clientUpdateMessage,
+                  cacheClientNotifierSpy,
+                  waitForRegistrationCountdownLatch);
+
           CacheClientNotifier.notifyClients(internalCacheEvent, clientUpdateMessage);
-        } catch (Exception ex) {
-          System.out.println(ExceptionUtils.getStackTrace(ex));
         } finally {
+          // Ensure we always countdown so if the test fails it won't hang due to
+          // awaiting on this countdown latch.
           waitForEventDispatchCountdownLatch.countDown();
         }
         return null;
       });
 
-      final List<Future<Void>> futures = executor.invokeAll(registerAndNotifyTasks);
+      final List<Future<Void>> futures =
+          registerAndNotifyExecutor.invokeAll(registerAndNotifyTasks);
 
       for (Future future : futures) {
         future.get();
       }
     } finally {
-      executor.shutdownNow();
+      // To prevent not cleaning up test resources in case an unexpected exception occurs
+      waitForEventDispatchCountdownLatch.countDown();
+      waitForRegistrationCountdownLatch.countDown();
+      registerAndNotifyExecutor.shutdownNow();
     }
 
     verify(cacheClientProxy, times(1)).deliverMessage(clientUpdateMessage);
+  }
+
+  private InternalCacheEvent createMockInternalCacheEvent(
+      final ClientProxyMembershipID clientProxyMembershipID,
+      final ClientUpdateMessageImpl clientUpdateMessage,
+      final CacheClientNotifier cacheClientNotifierSpy,
+      final CountDownLatch waitForRegistrationCountdownLatch) throws InterruptedException {
+    waitForRegistrationCountdownLatch.await();
+    InternalCacheEvent internalCacheEvent = mock(InternalCacheEvent.class);
+    DistributedRegion region = mock(DistributedRegion.class);
+    when(internalCacheEvent.getRegion()).thenReturn(region);
+    FilterRoutingInfo.FilterInfo filterInfo = mock(FilterRoutingInfo.FilterInfo.class);
+    final Long cqId = 0L;
+    final String cqName = "testCQ";
+    HashMap cqs = new HashMap<Long, Integer>() {
+      {
+        put(cqId, 123);
+      }
+    };
+    when(filterInfo.getCQs()).thenReturn(cqs);
+    when(internalCacheEvent.getLocalFilterInfo()).thenReturn(
+        filterInfo);
+    FilterProfile filterProfile = mock(FilterProfile.class);
+    when(filterProfile.getRealCqID(cqId)).thenReturn(cqName);
+    ServerCQ serverCQ = mock(ServerCQ.class);
+    when(serverCQ.getClientProxyId()).thenReturn(clientProxyMembershipID);
+    when(filterProfile.getCq(cqName)).thenReturn(serverCQ);
+    when(region.getFilterProfile()).thenReturn(filterProfile);
+    FilterRoutingInfo filterRoutingInfo = mock(FilterRoutingInfo.class);
+    when(filterRoutingInfo.getLocalFilterInfo()).thenReturn(filterInfo);
+    when(filterProfile.getFilterRoutingInfoPart2(null, internalCacheEvent))
+        .thenReturn(filterRoutingInfo);
+    doReturn(clientUpdateMessage).when(cacheClientNotifierSpy)
+        .constructClientMessage(internalCacheEvent);
+    return internalCacheEvent;
   }
 }
