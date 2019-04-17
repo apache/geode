a8654eb1df3b6f3ee87d2a24e5efe48a011c3206
Darrel Schneider <dschneider@pivotal.io>
2019-04-17 16:11:40 -0700

    getCacheServers no longer allocates an ArrayList everytime it is called

--- geode-core/src/main/java/org/apache/geode/internal/cache/GemFireCacheImpl.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/GemFireCacheImpl.java
@@ -394,6 +394,10 @@ public class GemFireCacheImpl implements InternalCache, InternalClientCache, Has
    * outnumber the mutative operations such as add, remove.
    */
   private final List<CacheServerImpl> allCacheServers = new CopyOnWriteArrayList<>();
+  /**
+   * This list has all the cache servers that are not gateway receivers
+   */
+  private final List<CacheServer> cacheServersNotGatewayReceivers = new CopyOnWriteArrayList<>();
 
   /**
    * Controls updates to the list of all gateway senders
@@ -2562,6 +2566,7 @@ public class GemFireCacheImpl implements InternalCache, InternalClientCache, Has
         }
       }
       this.allCacheServers.remove(cacheServer);
+      cacheServersNotGatewayReceivers.remove(cacheServer);
       stoppedCacheServer = true;
     }
     if (stoppedCacheServer) {
@@ -3717,6 +3722,9 @@ public class GemFireCacheImpl implements InternalCache, InternalClientCache, Has
 
     CacheServerImpl cacheServer = new CacheServerImpl(this, isGatewayReceiver);
     this.allCacheServers.add(cacheServer);
+    if (!isGatewayReceiver) {
+      cacheServersNotGatewayReceivers.add(cacheServer);
+    }
 
     sendAddCacheServerProfileMessage();
     return cacheServer;
@@ -3725,6 +3733,7 @@ public class GemFireCacheImpl implements InternalCache, InternalClientCache, Has
   @Override
   public boolean removeCacheServer(CacheServer cacheServer) {
     boolean removed = this.allCacheServers.remove(cacheServer);
+    cacheServersNotGatewayReceivers.remove(cacheServer);
     sendRemoveCacheServerProfileMessage();
     return removed;
   }
@@ -3929,22 +3938,7 @@ public class GemFireCacheImpl implements InternalCache, InternalClientCache, Has
 
   @Override
   public List<CacheServer> getCacheServers() {
-    List<CacheServer> cacheServersWithoutReceiver = null;
-    if (!this.allCacheServers.isEmpty()) {
-      for (CacheServerImpl cacheServer : this.allCacheServers) {
-        // If CacheServer is a GatewayReceiver, don't return as part of CacheServers
-        if (!cacheServer.isGatewayReceiver()) {
-          if (cacheServersWithoutReceiver == null) {
-            cacheServersWithoutReceiver = new ArrayList<>();
-          }
-          cacheServersWithoutReceiver.add(cacheServer);
-        }
-      }
-    }
-    if (cacheServersWithoutReceiver == null) {
-      cacheServersWithoutReceiver = Collections.emptyList();
-    }
-    return cacheServersWithoutReceiver;
+    return this.cacheServersNotGatewayReceivers;
   }
 
   @Override
