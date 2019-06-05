66d5fa9c0ada1aa74d521e488c4f36f661583626
Mario Ivanac <mario.ivanac@est.tech>
2019-06-05 11:37:25 +0200

    GEODE-6785: Retry time - system property read optimized             Updated

--- geode-core/src/main/java/org/apache/geode/internal/util/concurrent/StoppableCountDownLatch.java
+++ geode-core/src/main/java/org/apache/geode/internal/util/concurrent/StoppableCountDownLatch.java
@@ -32,8 +32,8 @@ public class StoppableCountDownLatch {
   static final String RETRY_TIME_MILLIS_PROPERTY = GEMFIRE_PREFIX + "stoppable-retry-interval";
   static final long RETRY_TIME_MILLIS_DEFAULT = 2000;
 
-  static final long RETRY_TIME_MILLIS =
-      Long.getLong(RETRY_TIME_MILLIS_PROPERTY, RETRY_TIME_MILLIS_DEFAULT);
+  static final long RETRY_TIME_NANOS = MILLISECONDS.toNanos(
+      Long.getLong(RETRY_TIME_MILLIS_PROPERTY, RETRY_TIME_MILLIS_DEFAULT));
 
   private final CountDownLatch delegate;
 
@@ -54,9 +54,7 @@ public class StoppableCountDownLatch {
    * @throws IllegalArgumentException if {@code count} is negative
    */
   public StoppableCountDownLatch(final CancelCriterion stopper, final int count) {
-    this(stopper, count,
-        MILLISECONDS.toNanos(RETRY_TIME_MILLIS),
-        System::nanoTime);
+    this(stopper, count, RETRY_TIME_NANOS, System::nanoTime);
   }
 
   StoppableCountDownLatch(final CancelCriterion stopper, final int count,
