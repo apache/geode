93b8ac5fe41d9fba9b0c62008c5f64cffae3dc68
Alberto Bustamante Reyes <alberto.bustamante.reyes@est.tech>
2019-06-05 17:10:58 +0200

    HostStatHelper cleaning

--- geode-core/src/main/java/org/apache/geode/internal/statistics/HostStatHelper.java
+++ geode-core/src/main/java/org/apache/geode/internal/statistics/HostStatHelper.java
@@ -19,7 +19,6 @@ import java.net.UnknownHostException;
 
 import org.apache.geode.InternalGemFireException;
 import org.apache.geode.Statistics;
-import org.apache.geode.internal.PureJavaMode;
 import org.apache.geode.internal.net.SocketCreator;
 import org.apache.geode.internal.statistics.platform.LinuxProcFsStatistics;
 import org.apache.geode.internal.statistics.platform.LinuxProcessStats;
@@ -31,75 +30,35 @@ import org.apache.geode.internal.statistics.platform.ProcessStats;
  * Provides native methods which fetch operating system statistics.
  */
 public class HostStatHelper {
-  private static final int LINUX_CODE = 3; // x86 Linux
-
   private static final int PROCESS_STAT_FLAG = 1;
   private static final int SYSTEM_STAT_FLAG = 2;
 
-  private static final int osCode;
-
   static {
     String osName = System.getProperty("os.name", "unknown");
-    if (!PureJavaMode.osStatsAreAvailable()) {
-      throw new RuntimeException("HostStatHelper not allowed in pure java mode");
-    } else if (osName.startsWith("Linux")) {
-      osCode = LINUX_CODE;
-    } else {
-      throw new InternalGemFireException(
-          String.format(
-              "Unsupported OS %s. Only Linux(x86) OSs is supported.",
-              osName));
+    if (!osName.startsWith("Linux")){
+      throw new InternalGemFireException(String.format("Unsupported OS %s. Only Linux(x86) OSs is supported.",osName));
     }
   }
 
   public static boolean isLinux() {
-    return osCode == LINUX_CODE;
+    return true;
   }
-
+  
   private HostStatHelper() {
     // instances are not allowed
   }
 
   static int initOSStats() {
-    if (isLinux()) {
-      return LinuxProcFsStatistics.init();
-    } else {
-      return init();
-    }
+    return LinuxProcFsStatistics.init();
   }
 
   static void closeOSStats() {
-    if (isLinux()) {
-      LinuxProcFsStatistics.close();
-    } else {
-      close();
-    }
+    LinuxProcFsStatistics.close();
   }
 
   static void readyRefreshOSStats() {
-    if (isLinux()) {
-      LinuxProcFsStatistics.readyRefresh();
-    } else {
-      readyRefresh();
-    }
+    LinuxProcFsStatistics.readyRefresh();
   }
-
-  /**
-   * Allocates and initializes any resources required to sample operating system statistics. returns
-   * 0 if initialization succeeded
-   */
-  private static native int init();
-
-  /**
-   * Frees up resources used by this class. Once close is called this class can no longer be used.
-   */
-  private static native void close();
-
-  /**
-   * Should be called before any calls to the refresh methods. On some platforms if this is not
-   * called then the refesh methods will just keep returning the same old data.
-   */
-  private static native void readyRefresh();
 
   /**
    * Refreshes the specified process stats instance by fetching the current OS values for the given
@@ -107,32 +66,18 @@ public class HostStatHelper {
    */
   private static void refreshProcess(LocalStatisticsImpl statistics) {
     int pid = (int) statistics.getNumericId();
-    if (isLinux()) {
-      LinuxProcFsStatistics.refreshProcess(pid, statistics._getIntStorage(),
+    LinuxProcFsStatistics.refreshProcess(pid, statistics._getIntStorage(),
           statistics._getLongStorage(), statistics._getDoubleStorage());
-    } else {
-      refreshProcess(pid, statistics._getIntStorage(), statistics._getLongStorage(),
-          statistics._getDoubleStorage());
-    }
   }
-
-  private static native void refreshProcess(int pid, int[] ints, long[] longs, double[] doubles);
 
   /**
    * Refreshes the specified system stats instance by fetching the current OS values for the local
    * machine and storing them in the instance.
    */
   private static void refreshSystem(LocalStatisticsImpl statistics) {
-    if (isLinux()) {
-      LinuxProcFsStatistics.refreshSystem(statistics._getIntStorage(), statistics._getLongStorage(),
+    LinuxProcFsStatistics.refreshSystem(statistics._getIntStorage(), statistics._getLongStorage(),
           statistics._getDoubleStorage());
-    } else {
-      refreshSystem(statistics._getIntStorage(), statistics._getLongStorage(),
-          statistics._getDoubleStorage());
-    }
   }
-
-  private static native void refreshSystem(int[] ints, long[] longs, double[] doubles);
 
   /**
    * The call should have already checked to make sure usesSystemCalls returns true.
@@ -154,10 +99,6 @@ public class HostStatHelper {
    */
   static Statistics newProcess(OsStatisticsFactory osStatisticsFactory, long pid, String name) {
     Statistics statistics;
-    if (osCode != LINUX_CODE) {
-      throw new InternalGemFireException(
-          String.format("unhandled osCode= %s HostStatHelper:newProcess", osCode));
-    }
     statistics = osStatisticsFactory.createOsStatistics(LinuxProcessStats.getType(), name, pid,
         PROCESS_STAT_FLAG);
     // Note we don't call refreshProcess since we only want the manager to do that
@@ -171,10 +112,6 @@ public class HostStatHelper {
    * @since GemFire 3.5
    */
   static ProcessStats newProcessStats(Statistics statistics) {
-    if (osCode != LINUX_CODE) {
-      throw new InternalGemFireException(
-          String.format("unhandled osCode= %s HostStatHelper:newProcessStats", osCode));
-    }
     return LinuxProcessStats.createProcessStats(statistics);
   }
 
@@ -184,10 +121,6 @@ public class HostStatHelper {
    */
   static void newSystem(OsStatisticsFactory osStatisticsFactory, long id) {
     Statistics statistics;
-    if (osCode != LINUX_CODE) {
-      throw new InternalGemFireException(
-          String.format("unhandled osCode= %s HostStatHelper:newSystem", osCode));
-    }
     statistics = osStatisticsFactory.createOsStatistics(LinuxSystemStats.getType(),
         getHostSystemName(), id, SYSTEM_STAT_FLAG);
     if (statistics instanceof LocalStatisticsImpl) {
