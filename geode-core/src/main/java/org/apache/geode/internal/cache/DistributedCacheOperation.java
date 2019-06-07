b824a4eff063d5553423bf64a365f8db31553dde
Mario Ivanac <mario.ivanac@est.tech>
2019-06-07 15:45:48 +0200

    GEODE-6828: check synchronize only for debug/trace

--- geode-core/src/main/java/org/apache/geode/internal/cache/DistributedCacheOperation.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/DistributedCacheOperation.java
@@ -415,15 +415,17 @@ public abstract class DistributedCacheOperation {
       if (recipients.isEmpty() && adjunctRecipients.isEmpty() && needsOldValueInCacheOp.isEmpty()
           && cachelessNodes.isEmpty()) {
         if (region.isInternalRegion()) {
-          if (mgr.getNormalDistributionManagerIds().size() > 1) {
-            // suppress this msg if we are the only member.
-            if (logger.isTraceEnabled()) {
-              logger.trace("<No Recipients> {}", this);
-            }
-          } else {
-            // suppress this msg if we are the only member.
-            if (logger.isDebugEnabled()) {
-              logger.debug("<No Recipients> {}", this);
+          if (logger.isDebugEnabled() || logger.isTraceEnabled()) {
+            if (mgr.getNormalDistributionManagerIds().size() > 1) {
+              // suppress this msg if we are the only member.
+              if (logger.isTraceEnabled()) {
+                logger.trace("<No Recipients> {}", this);
+              }
+            } else {
+              // suppress this msg if we are the only member.
+              if (logger.isDebugEnabled()) {
+                logger.debug("<No Recipients> {}", this);
+              }
             }
           }
         }
