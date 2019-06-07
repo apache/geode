496048581aabfac33a068cdc0d98967f8bffd6ba
Mario Ivanac <mario.ivanac@est.tech>
2019-06-07 22:10:06 +0200

    GEODE-6837: optimize read system property "statsDisabled"             Update

--- geode-core/src/main/java/org/apache/geode/internal/cache/execute/FunctionStats.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/execute/FunctionStats.java
@@ -20,7 +20,6 @@ import org.apache.geode.StatisticsFactory;
 import org.apache.geode.StatisticsType;
 import org.apache.geode.StatisticsTypeFactory;
 import org.apache.geode.annotations.Immutable;
-import org.apache.geode.distributed.internal.DistributionConfig;
 import org.apache.geode.distributed.internal.DistributionStats;
 import org.apache.geode.distributed.internal.InternalDistributedSystem;
 import org.apache.geode.internal.statistics.DummyStatisticsImpl;
@@ -89,12 +88,6 @@ public class FunctionStats {
    * exceptions statistic
    */
   private static final String FUNCTION_EXECUTION_EXCEPTIONS = "functionExecutionsExceptions";
-
-  /**
-   * Creation of GemFireStatSampler disabled: true/false
-   */
-  private static final boolean STATS_DISABLED =
-      Boolean.getBoolean(DistributionConfig.GEMFIRE_PREFIX + "statsDisabled");
 
   /** Id of the FUNCTION_EXECUTIONS_COMPLETED statistic */
   private static final int _functionExecutionsCompletedId;
@@ -185,7 +178,7 @@ public class FunctionStats {
 
   /** This is an instance of the FunctionStats when the statsDisabled = true; */
   @Immutable
-  private static final FunctionStats dummy = createDummy();
+  public static final FunctionStats dummy = createDummy();
 
   // ///////////////////// Constructors ///////////////////////
 
@@ -444,23 +437,14 @@ public class FunctionStats {
    * @return object of the FunctionStats
    */
   public static FunctionStats getFunctionStats(String functionID, InternalDistributedSystem ds) {
-    if (isDisabled()) {
-      return dummy;
-    } else {
-      return ds.getFunctionStats(functionID);
-    }
+    return ds.getFunctionStats(functionID);
   }
 
   public static FunctionStats getFunctionStats(String functionID) {
-    if (isDisabled()) {
+    InternalDistributedSystem ds = InternalDistributedSystem.getAnyInstance();
+    if (ds == null) {
       return dummy;
-    } else {
-      InternalDistributedSystem ds = InternalDistributedSystem.getAnyInstance();
-      return ds.getFunctionStats(functionID);
     }
-  }
-
-  private static boolean isDisabled() {
-    return STATS_DISABLED;
+    return ds.getFunctionStats(functionID);
   }
 }
