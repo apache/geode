872c67c9d9e9a28d0fec9a62cab0e6b30c95344d
Jinmei Liao <jiliao@pivotal.io>
2019-06-27 07:21:30 -0700

    fix test

--- geode-web-management/src/distributedTest/java/org/apache/geode/management/client/ConfigurePDXDUnitTest.java
+++ geode-web-management/src/distributedTest/java/org/apache/geode/management/client/ConfigurePDXDUnitTest.java
@@ -86,7 +86,7 @@ public class ConfigurePDXDUnitTest {
 
     assertThat(result.isSuccessful()).isTrue();
     assertThat(result.getStatusCode()).isEqualTo(ClusterManagementResult.StatusCode.OK);
-    assertThat(result.getUri()).isEqualTo("/geode-management/v2/configurations/pdx");
+    assertThat(result.getUri()).isEqualTo("/management/v2/configurations/pdx");
 
     RealizationResult status = result.getMemberStatuses().get(0);
     assertThat(status.getMemberName()).isEqualTo("server-1");
