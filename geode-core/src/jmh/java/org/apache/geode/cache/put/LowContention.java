f0464b2af058c6fb78a29ae765ebf5fda7761cb1
Murtuza Boxwala <mboxwala@pivotal.io>
2019-06-27 11:07:03 -0400

    benchmarks

---
+++ geode-core/src/jmh/java/org/apache/geode/cache/put/LowContention.java
@@ -0,0 +1,88 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.geode.cache.put;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+
+import org.apache.geode.cache.Cache;
+import org.apache.geode.cache.CacheFactory;
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.RegionShortcut;
+
+@Fork(1)
+public class LowContention {
+
+  @State(Scope.Benchmark)
+  public static class CacheState {
+    private Region replicateRegion;
+    private Region localRegion;
+    private Region partitionedRegion;
+
+    @Setup
+    public void setup() {
+      Cache cache = new CacheFactory().set("mcast-port", "0").set("locators", "").create();
+
+      replicateRegion = cache.createRegionFactory(RegionShortcut.REPLICATE).create("ReplicateRegion");
+      localRegion = cache.createRegionFactory(RegionShortcut.LOCAL).create("LocalRegion");
+      partitionedRegion = cache.createRegionFactory(RegionShortcut.PARTITION).create("PartitionedRegion");
+    }
+  }
+
+  @Benchmark
+  @Threads(100)
+  @Warmup(iterations = 1)
+  @Measurement(iterations = 5)
+  public Object replicate(CacheState state) {
+    return state.replicateRegion.put(getKey(), "value");
+  }
+
+  @Benchmark
+  @Threads(100)
+  @Warmup(iterations = 1)
+  @Measurement(iterations = 5)
+  public Object local(CacheState state) {
+    return state.localRegion.put(getKey(), "value");
+  }
+
+  @Benchmark
+  @Threads(100)
+  @Warmup(iterations = 1)
+  @Measurement(iterations = 5)
+  public Object partition(CacheState state) {
+    return state.partitionedRegion.put(getKey(), "value");
+  }
+
+  @Benchmark
+  @Threads(100)
+  @Warmup(iterations = 1)
+  @Measurement(iterations = 5)
+  public void noop(CacheState state) {
+    return;
+  }
+
+  private Integer getKey() {
+    int randomInt = ThreadLocalRandom.current().nextInt(-127, 127);
+    return Integer.valueOf(randomInt);
+  }
+}
---
+++ geode-core/src/jmh/java/org/apache/geode/cache/put/ModerateContention.java
@@ -0,0 +1,73 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.geode.cache.put;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Threads;
+import org.openjdk.jmh.annotations.Warmup;
+
+import org.apache.geode.cache.Cache;
+import org.apache.geode.cache.CacheFactory;
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.RegionShortcut;
+
+@Fork(1)
+public class ModerateContention {
+
+  @State(Scope.Benchmark)
+  public static class CacheState {
+    private Region replicateRegion;
+    private Region localRegion;
+    private Region partitionedRegion;
+
+    @Setup
+    public void setup() {
+      Cache cache = new CacheFactory().set("mcast-port", "0").set("locators", "").create();
+
+      replicateRegion = cache.createRegionFactory(RegionShortcut.REPLICATE).create("ReplicateRegion");
+      localRegion = cache.createRegionFactory(RegionShortcut.LOCAL).create("LocalRegion");
+      partitionedRegion = cache.createRegionFactory(RegionShortcut.PARTITION).create("PartitionedRegion");
+    }
+  }
+
+  @Benchmark
+  @Threads(100)
+  @Warmup(iterations = 2)
+  @Measurement(iterations = 20)
+  public Object replicate(CacheState state) {
+    return state.replicateRegion.put("key", "value");
+  }
+
+  @Benchmark
+  @Threads(100)
+  @Warmup(iterations = 2)
+  @Measurement(iterations = 20)
+  public Object local(CacheState state) {
+    return state.localRegion.put("key", "value");
+  }
+
+  @Benchmark
+  @Threads(100)
+  @Warmup(iterations = 2)
+  @Measurement(iterations = 20)
+  public Object partition(CacheState state) {
+    return state.partitionedRegion.put("key", "value");
+  }
+}
