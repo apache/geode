9b20ca99dfac9f2b7326656d4ceeec4b38c5dc79
Kirk Lund <klund@apache.org>
2019-07-05 15:10:49 -0700

    GEODE-6918: Restore use of System.out and max in PersistentOplogSet

--- geode-core/src/main/java/org/apache/geode/internal/cache/DiskStoreImpl.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/DiskStoreImpl.java
@@ -329,7 +329,7 @@ public class DiskStoreImpl implements DiskStore {
   private final AtomicReference<DiskAccessException> diskException =
       new AtomicReference<DiskAccessException>();
 
-  private PersistentOplogSet persistentOplogs = new PersistentOplogSet(this);
+  private PersistentOplogSet persistentOplogs = new PersistentOplogSet(this, System.out);
 
   OverflowOplogSet overflowOplogs = new OverflowOplogSet(this);
 
@@ -2756,7 +2756,7 @@ public class DiskStoreImpl implements DiskStore {
     if (!all && max > MAX_OPLOGS_PER_COMPACTION && MAX_OPLOGS_PER_COMPACTION > 0) {
       max = MAX_OPLOGS_PER_COMPACTION;
     }
-    getPersistentOplogs().getCompactableOplogs(l);
+    getPersistentOplogs().getCompactableOplogs(l, max);
 
     // Note this always puts overflow oplogs on the end of the list.
     // They may get starved.
