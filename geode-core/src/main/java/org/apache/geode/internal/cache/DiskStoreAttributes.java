ec5abc61fb8e379b1758164720063d5313703303
Alberto Bustamante Reyes <alb3rtobr@users.noreply.github.com>
2019-07-18 11:40:54 +0200

    Revert "Remove redundant annotation"
    This reverts commit 5eeb52f396bfdc993c6ac98e3bfbe582a3341fe0.

--- geode-core/src/main/java/org/apache/geode/internal/cache/DiskStoreAttributes.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/DiskStoreAttributes.java
@@ -19,6 +19,7 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.UUID;
 
+import org.apache.geode.annotations.internal.MakeImmutable;
 import org.apache.geode.cache.DiskStore;
 import org.apache.geode.cache.DiskStoreFactory;
 import org.apache.geode.internal.cache.persistence.DefaultDiskDirs;
@@ -54,6 +55,7 @@ public class DiskStoreAttributes implements Serializable, DiskStore {
   /**
    * The default disk directory size unit.
    */
+  @MakeImmutable
   static final DiskDirSizesUnit DEFAULT_DISK_DIR_SIZES_UNIT = DiskDirSizesUnit.MEGABYTES;
 
   public DiskStoreAttributes() {
