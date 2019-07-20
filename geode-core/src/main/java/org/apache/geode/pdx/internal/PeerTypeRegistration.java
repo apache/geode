5c8c728fbe51828dfc5db3c8b2e96e7b8b253557
Donal Evans <doevans@pivotal.io>
2019-07-19 20:05:34 -0700

    GEODE-6973: Use cachelistener to synchronize typeToId with IdToType region in             PeerTypeRegistrationm, then when creating a new json pdxType, no need             to look up in IdToType region. This will speed up creating new pdxType.
        Co-authored-by: Xiaojian Zhou <gzhou@pivotal.io>
        Co-authored-by: Donal Evans <doevans@pivotal.io>

--- geode-core/src/main/java/org/apache/geode/pdx/internal/PeerTypeRegistration.java
+++ geode-core/src/main/java/org/apache/geode/pdx/internal/PeerTypeRegistration.java
@@ -350,23 +350,25 @@ public class PeerTypeRegistration implements TypeRegistration {
   public int defineType(PdxType newType) {
     verifyConfiguration();
     Integer existingId = typeToId.get(newType);
+
     if (existingId != null) {
       return existingId;
     }
     lock();
     try {
-      int id = getExistingIdForType(newType);
-      if (id != -1) {
-        return id;
+      if (typeToId.isEmpty()) {
+        buildTypeToIdFromRegion();
+      }
+      // double check if my type is in region in case the typeToId map has been updated while
+      // waiting to obtain a lock
+      existingId = typeToId.get(newType);
+      if (existingId != null) {
+        return existingId;
       }
 
-      id = allocateTypeId(newType);
+      int id = allocateTypeId(newType);
       newType.setTypeId(id);
-
       updateIdToTypeRegion(newType);
-
-      typeToId.put(newType, id);
-
       return newType.getTypeId();
     } finally {
       unlock();
@@ -538,12 +540,10 @@ public class PeerTypeRegistration implements TypeRegistration {
     }
   }
 
-  /** Should be called holding the dlock */
-  private int getExistingIdForType(PdxType newType) {
+  private void buildTypeToIdFromRegion() {
     int totalPdxTypeIdInDS = 0;
     TXStateProxy currentState = suspendTX();
     try {
-      int result = -1;
       for (Map.Entry<Object, Object> entry : getIdToType().entrySet()) {
         Object v = entry.getValue();
         Object k = entry.getKey();
@@ -560,9 +560,6 @@ public class PeerTypeRegistration implements TypeRegistration {
           }
 
           typeToId.put(foundType, id);
-          if (foundType.equals(newType)) {
-            result = foundType.getTypeId();
-          }
         }
       }
       if (totalPdxTypeIdInDS == this.maxTypeId) {
@@ -570,7 +567,6 @@ public class PeerTypeRegistration implements TypeRegistration {
             "Used up all of the PDX type ids for this distributed system. The maximum number of PDX types is "
                 + this.maxTypeId);
       }
-      return result;
     } finally {
       resumeTX(currentState);
     }
@@ -740,6 +736,7 @@ public class PeerTypeRegistration implements TypeRegistration {
   private void updateClassToTypeMap(PdxType type) {
     if (type != null) {
       synchronized (this.classToType) {
+        typeToId.put(type, type.getTypeId());
         if (type.getClassName().equals(JSONFormatter.JSON_CLASSNAME)) {
           return; // no need to include here
         }
@@ -806,5 +803,9 @@ public class PeerTypeRegistration implements TypeRegistration {
   @Override
   public int getLocalSize() {
     return idToType.size();
+  }
+
+  public int getTypeToIdSize() {
+    return typeToId.size();
   }
 }
