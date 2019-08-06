7f8cfc7e3145fc0fd6099708dae6a686f3973037
Kirk Lund <klund@apache.org>
2019-08-06 12:38:52 -0700

    GEODE-7051: Upgrade to log4j 2.12.0
    (cherry picked from commit e1b61cdcaf38966bbb732b0b3db5223f2cc4f5e4)

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -40,7 +40,7 @@ class DependencyConstraints implements Plugin<Project> {
     deps.put("fastutil.version", "8.2.2")
     deps.put("javax.transaction-api.version", "1.3")
     deps.put("jgroups.version", "3.6.14.Final")
-    deps.put("log4j.version", "2.11.1")
+    deps.put("log4j.version", "2.12.0")
     deps.put("micrometer.version", "1.2.0")
     deps.put("shiro.version", "1.4.0")
     deps.put("slf4j-api.version", "1.7.25")
