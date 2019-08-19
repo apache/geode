d480ba765beeacb847d7e68e182abf978f63e47f
eshu <eshu@pivotal.io>
2019-08-19 13:35:47 -0700

    Refactoring code and fix review comments.

--- geode-core/src/main/java/org/apache/geode/internal/cache/TXState.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/TXState.java
@@ -898,7 +898,20 @@ public class TXState implements TXStateInterface {
           this.proxy.getTxMgr().getCachePerfStats()
               .incTxConflictCheckTime(statisticsClock.getTime() - conflictStart);
       }
-      Iterator<Map.Entry<InternalRegion, TXRegionState>> it = this.regions.entrySet().iterator();
+      cleanupTXRegionState();
+    } finally {
+      synchronized (this.completionGuard) {
+        this.completionGuard.notifyAll();
+      }
+      if (exception != null && !this.proxy.getCache().isClosed()) {
+        throw exception;
+      }
+    }
+  }
+
+  private void cleanupTXRegionState() {
+    Iterator<Map.Entry<InternalRegion, TXRegionState>> it = this.regions.entrySet().iterator();
+    try {
       while (it.hasNext()) {
         Map.Entry<InternalRegion, TXRegionState> me = it.next();
         InternalRegion r = me.getKey();
@@ -907,36 +920,35 @@ public class TXState implements TXStateInterface {
          * Need to unlock the primary lock for rebalancing so that rebalancing can resume.
          */
         if (isBucketLocks()) {
-          if (r instanceof BucketRegion && (((BucketRegion) r).getBucketAdvisor().isPrimary())) {
-            try {
-              ((BucketRegion) r).doUnlockForPrimary();
-            } catch (RegionDestroyedException rde) {
-              // ignore
-              if (logger.isDebugEnabled()) {
-                logger.debug("RegionDestroyedException while unlocking bucket region {}",
-                    r.getFullPath(), rde);
-              }
-            } catch (Exception rde) {
-              // ignore
-              if (logger.isDebugEnabled()) {
-                logger.debug(
-                    "Exception while unlocking bucket region {} this is probably because the bucket was destroyed and never locked initially.",
-                    r.getFullPath(), rde);
-              }
-            }
-          }
+          unlockPrimaryMoveReadLock(r);
         }
         txrs.cleanup(r);
       }
     } finally {
-      synchronized (this.completionGuard) {
-        this.completionGuard.notifyAll();
-      }
       if (isBucketLocks()) {
         gotBucketLocks = false;
       }
-      if (exception != null && !this.proxy.getCache().isClosed()) {
-        throw exception;
+    }
+  }
+
+  private void unlockPrimaryMoveReadLock(InternalRegion internalRegion) {
+    if (internalRegion.isUsedForPartitionedRegionBucket()
+        && (((BucketRegion) internalRegion).getBucketAdvisor().isPrimary())) {
+      try {
+        ((BucketRegion) internalRegion).doUnlockForPrimary();
+      } catch (RegionDestroyedException rde) {
+        // ignore
+        if (logger.isDebugEnabled()) {
+          logger.debug("RegionDestroyedException while unlocking bucket region {}",
+              internalRegion.getFullPath(), rde);
+        }
+      } catch (Exception rde) {
+        // ignore
+        if (logger.isDebugEnabled()) {
+          logger.debug(
+              "Exception while unlocking bucket region {} this is probably because the bucket was destroyed and never locked initially.",
+              internalRegion.getFullPath(), rde);
+        }
       }
     }
   }
