59572d115c52cdd04d13a2d04ec230955deb175a
Donal Evans <doevans@pivotal.io>
2019-08-23 16:28:37 -0700

    WIP Added TouchPartitioned and TouchReplicated function unit tests
    Co-authored-by: Benjamin Ross <bross@pivotal.io>
    Co-authored-by: Donal Evans <doevans@pivotal.io>

---
+++ extensions/geode-modules/src/test/java/org/apache/geode/modules/util/TouchPartitionedRegionEntriesFunctionJUnitTest.java
@@ -0,0 +1,67 @@
+package org.apache.geode.modules.util;
+
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.apache.geode.cache.Cache;
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.execute.FunctionContext;
+import org.apache.geode.cache.execute.RegionFunctionContext;
+import org.apache.geode.cache.execute.ResultSender;
+import org.apache.geode.internal.cache.GemFireCacheImpl;
+import org.apache.geode.internal.logging.log4j.LogWriterLogger;
+
+public class TouchPartitionedRegionEntriesFunctionJUnitTest {
+
+  private TouchPartitionedRegionEntriesFunction function = spy(new TouchPartitionedRegionEntriesFunction());
+  private FunctionContext context = mock(RegionFunctionContext.class);
+  private Cache cache = mock(GemFireCacheImpl.class);
+  private LogWriterLogger logger = mock(LogWriterLogger.class);
+  private Region primaryDataSet = mock(Region.class);
+  private ResultSender resultSender = mock(ResultSender.class);
+
+  @Before
+  public void setUp() {
+    when(context.getCache()).thenReturn(cache);
+    when(context.getResultSender()).thenReturn(resultSender);
+    when(cache.getLogger()).thenReturn(logger);
+    when(logger.fineEnabled()).thenReturn(false);
+    doReturn(primaryDataSet).when(function).getLocalDataForContextViaRegionHelper((RegionFunctionContext)context);
+  }
+
+  @Test
+  public void executeDoesNotThrowExceptionWithProperlyDefinedContext() {
+    doReturn(new HashSet() {}).when((RegionFunctionContext)context).getFilter();
+
+    function.execute(context);
+
+    verify(primaryDataSet, times(0)).get(any());
+    verify(resultSender).lastResult(true);
+  }
+
+  @Test
+  public void executeDoesNotThrowExceptionWithProperlyDefinedContextAndMultipleKeys() {
+    HashSet<String> keys = new HashSet();
+    keys.add("Key1");
+    keys.add("Key2");
+
+    doReturn(keys).when((RegionFunctionContext)context).getFilter();
+
+    function.execute(context);
+
+    verify(primaryDataSet, times(keys.size())).get(anyString());
+    verify(resultSender).lastResult(true);
+  }
+}
---
+++ extensions/geode-modules/src/test/java/org/apache/geode/modules/util/TouchReplicatedRegionEntriesFunctionJUnitTest.java
@@ -0,0 +1,65 @@
+package org.apache.geode.modules.util;
+
+import static org.assertj.core.internal.bytebuddy.matcher.ElementMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+import java.util.HashSet;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.apache.geode.cache.Cache;
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.execute.FunctionContext;
+import org.apache.geode.cache.execute.RegionFunctionContext;
+import org.apache.geode.cache.execute.ResultSender;
+import org.apache.geode.internal.cache.GemFireCacheImpl;
+import org.apache.geode.internal.logging.log4j.LogWriterLogger;
+
+public class TouchReplicatedRegionEntriesFunctionJUnitTest {
+  private TouchReplicatedRegionEntriesFunction function = spy(new TouchReplicatedRegionEntriesFunction());
+  private FunctionContext context = mock(RegionFunctionContext.class);
+  private Cache cache = mock(GemFireCacheImpl.class);
+  private LogWriterLogger logger = mock(LogWriterLogger.class);
+  private Region region = mock(Region.class);
+  private ResultSender resultSender = mock(ResultSender.class);
+  private String regionName = "regionName";
+  private HashSet<String> keys = new HashSet<>();
+  private Object[] arguments = new Object[] {regionName, keys};
+
+  @Before
+  public void setUp() {
+    when(context.getArguments()).thenReturn(arguments);
+    when(context.getCache()).thenReturn(cache);
+    when(context.getResultSender()).thenReturn(resultSender);
+    when(cache.getLogger()).thenReturn(logger);
+    when(logger.fineEnabled()).thenReturn(false);
+  }
+
+  @Test
+  public void executeDoesNotThrowExceptionWithProperlyDefinedContext() {
+    when(cache.getRegion(regionName)).thenReturn(region);
+
+    function.execute(context);
+
+    verify(region).getAll(keys);
+    verify(resultSender).lastResult(true);
+  }
+
+  @Test
+  public void executeDoesNotThrowExceptionWithProperlyDefinedContextAndNullRegion() {
+    when(cache.getRegion(regionName)).thenReturn(null);
+
+    function.execute(context);
+
+    verify(region, times(0)).getAll(keys);
+    verify(resultSender).lastResult(true);
+  }
+}
