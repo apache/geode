a0b20b83b618ffa45316c8ad62d19072f75ebb06
Owen Nichols <onichols@pivotal.io>
2019-08-23 20:43:34 -0700

    Add 1.9.1 to Version list
    (cherry picked from commit a4e84ad79128c84a50340102068388ed94486117)

--- geode-core/src/main/java/org/apache/geode/internal/cache/tier/sockets/CommandInitializer.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/tier/sockets/CommandInitializer.java
@@ -337,6 +337,7 @@ public class CommandInitializer {
         ExecuteRegionFunctionGeode18.getCommand());
     allCommands.put(Version.GEODE_1_8_0, geode18Commands);
     allCommands.put(Version.GEODE_1_9_0, geode18Commands);
+    allCommands.put(Version.GEODE_1_9_1, geode18Commands);
     allCommands.put(Version.GEODE_1_10_0, geode18Commands);
 
     return Collections.unmodifiableMap(allCommands);
