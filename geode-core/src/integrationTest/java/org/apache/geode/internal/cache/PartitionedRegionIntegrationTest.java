3c83a65e79a9e425748acd1b216d568a9ce33c89
Juan José Ramos Cassella <jujoramos@gmail.com>
2019-08-30 12:22:07 +0100

    GEODE-7106: Changes requested by reviewers
    - Use ListAppender instead of manually checking the log file.

--- geode-core/src/integrationTest/java/org/apache/geode/internal/cache/PartitionedRegionIntegrationTest.java
+++ geode-core/src/integrationTest/java/org/apache/geode/internal/cache/PartitionedRegionIntegrationTest.java
@@ -25,11 +25,13 @@ import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
+import java.util.List;
 import java.util.concurrent.ScheduledExecutorService;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.Logger;
+import org.apache.logging.log4j.test.appender.ListAppender;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -80,27 +82,32 @@ public class PartitionedRegionIntegrationTest {
 
   @Test
   // See GEODE-7106
-  public void generatePRIdShouldNotThrowNumberFormatExceptionIfAnErrorOccursWhileReleasingTheLock()
-      throws IOException {
+  public void generatePRIdShouldNotThrowNumberFormatExceptionIfAnErrorOccursWhileReleasingTheLock() {
+    ListAppender listAppender = new ListAppender("ListAppender");
+    Logger partitionRegionLogger = (Logger) LogManager.getLogger(PartitionedRegion.class);
+    partitionRegionLogger.addAppender(listAppender);
+    listAppender.start();
+
     String methodName = testName.getMethodName();
-    File customLogFile = temporaryFolder.newFile(methodName + ".log");
     DistributedLockService mockLockService = mock(DistributedLockService.class);
     doReturn(true).when(mockLockService).lock(any(), anyLong(), anyLong());
     doThrow(new RuntimeException("Mock Exception")).when(mockLockService).unlock(any());
 
-    server.withProperty("log-level", "FINE")
-        .withProperty("log-file", customLogFile.getAbsolutePath()).startServer();
+    server.withProperty("log-level", "FINE").startServer();
     PartitionedRegion region =
         spy((PartitionedRegion) server.createRegion(RegionShortcut.PARTITION, methodName));
     doReturn(mockLockService).when(region).getPartitionedRegionLockService();
 
     assertThatCode(() -> region.generatePRId(server.getCache().getInternalDistributedSystem()))
         .doesNotThrowAnyException();
-    assertThat(Files.lines(customLogFile.toPath())
-        .anyMatch(line -> line.contains("java.lang.NumberFormatException"))).isFalse();
-    assertThat(Files.lines(customLogFile.toPath())
-        .anyMatch(line -> line.contains(
+    List<LogEvent> logEvents = listAppender.getEvents();
+    assertThat(logEvents.stream().anyMatch(logEvent -> logEvent.getMessage().getFormattedMessage()
+        .contains("java.lang.NumberFormatException"))).isFalse();
+    assertThat(logEvents.stream()
+        .anyMatch(logEvent -> logEvent.getMessage().getFormattedMessage().contains(
             "releasePRIDLock: unlocking " + MAX_PARTITIONED_REGION_ID + " caught an exception")))
                 .isTrue();
+
+    partitionRegionLogger.removeAppender(listAppender);
   }
 }
