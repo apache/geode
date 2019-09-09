8a97238490efea2d7f06f0072c24d9c8cb83975e
Donal Evans <doevans@pivotal.io>
2019-09-09 14:36:57 -0700

    GEODE-7035 - pdx enum registry needs to be improved
    Brought Enum behaviour in line with PdxType behaviour in
    PeerTypeRegistration
    Refactored ReverseMap to not be an internal class of
    PeerTypeRegistration
    Added unit tests for PeerTypeRegistration and
    PeerTypeRegistrationReverseMap
    
    Authored-by: Donal Evans <doevans@pivotal.io>

---
+++ geode-core/src/main/java/org/apache/geode/pdx/internal/PeerTypeRegistrationReverseMap.java
@@ -0,0 +1,123 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.apache.geode.pdx.internal;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.geode.annotations.VisibleForTesting;
+import org.apache.geode.cache.Region;
+
+public class PeerTypeRegistrationReverseMap {
+  /**
+   * When a new pdxType or a new enumInfo is added to idToType region, its
+   * listener will add the new type to the pendingTypeToId first, to make sure
+   * the distribution finished.
+   * Then any member who wants to use this new pdxType has to get the dlock to
+   * flush the pendingTypeToId map into typeToId. This design to guarantee that
+   * when using the new pdxType, it should have been distributed to all members.
+   */
+  private final Map<PdxType, Integer> pendingTypeToId =
+      Collections.synchronizedMap(new HashMap<>());
+  private final Map<EnumInfo, EnumId> pendingEnumToId =
+      Collections.synchronizedMap(new HashMap<>());
+
+  /**
+   * This map serves two purposes. It lets us look up an id based on a type, if we previously
+   * found that type in the region. And, if a type is present in this map, that means we read
+   * the type while holding the dlock, which means the type was distributed to all members.
+   */
+  private final Map<PdxType, Integer> typeToId = Collections.synchronizedMap(new HashMap<>());
+
+  private final Map<EnumInfo, EnumId> enumToId = Collections.synchronizedMap(new HashMap<>());
+
+  void save(Object key, Object value) {
+    if (value instanceof PdxType) {
+      PdxType type = (PdxType) value;
+      typeToId.put(type, (Integer) key);
+    } else if (value instanceof EnumInfo) {
+      EnumInfo info = (EnumInfo) value;
+      enumToId.put(info, (EnumId) key);
+    }
+  }
+
+  void saveToPending(Object key, Object value) {
+    if (value instanceof PdxType) {
+      PdxType type = (PdxType) value;
+      pendingTypeToId.put(type, (Integer) key);
+    } else if (value instanceof EnumInfo) {
+      EnumInfo info = (EnumInfo) value;
+      pendingEnumToId.put(info, (EnumId) key);
+    }
+  }
+
+  int typeToIdSize() {
+    return typeToId.size();
+  }
+
+  int enumToIdSize() {
+    return enumToId.size();
+  }
+
+  Integer getIdFromReverseMap(PdxType newType) {
+    return typeToId.get(newType);
+  }
+
+  EnumId getIdFromReverseMap(EnumInfo newInfo) {
+    return enumToId.get(newInfo);
+  }
+
+  // The reverse maps should only be loaded from the region if there is a mismatch in size between
+  // the region and all reverse maps, which should only occur when initializing a new
+  // PeerTypeRegistration in a system with an existing and not-empty PdxTypes region
+  boolean shouldReloadFromRegion(Region pdxRegion) {
+    if (pdxRegion == null) {
+      return false;
+    }
+    return ((typeToId.size() + pendingTypeToId.size() + enumToId.size()
+        + pendingEnumToId.size()) != pdxRegion.size());
+  }
+
+  void flushPendingReverseMap() {
+    if (!pendingTypeToId.isEmpty()) {
+      typeToId.putAll(pendingTypeToId);
+      pendingTypeToId.clear();
+    }
+    if (!pendingEnumToId.isEmpty()) {
+      enumToId.putAll(pendingEnumToId);
+      pendingEnumToId.clear();
+    }
+  }
+
+  // This should only be called prior to reloading the maps from the region
+  void clear() {
+    typeToId.clear();
+    enumToId.clear();
+    pendingTypeToId.clear();
+    pendingEnumToId.clear();
+  }
+
+  @VisibleForTesting
+  int pendingTypeToIdSize() {
+    return pendingTypeToId.size();
+  }
+
+  @VisibleForTesting
+  int pendingEnumToIdSize() {
+    return pendingEnumToId.size();
+  }
+}
--- geode-core/src/test/java/org/apache/geode/pdx/internal/PeerTypeRegistrationTest.java
+++ geode-core/src/test/java/org/apache/geode/pdx/internal/PeerTypeRegistrationTest.java
@@ -18,19 +18,34 @@ package org.apache.geode.pdx.internal;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.io.IOException;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.internal.util.reflection.FieldSetter;
 
+import org.apache.geode.CancelCriterion;
 import org.apache.geode.Statistics;
 import org.apache.geode.StatisticsType;
+import org.apache.geode.cache.AttributesFactory;
+import org.apache.geode.cache.DataPolicy;
 import org.apache.geode.cache.Region;
+import org.apache.geode.distributed.DistributedLockService;
 import org.apache.geode.distributed.internal.DistributionManager;
 import org.apache.geode.distributed.internal.InternalDistributedSystem;
+import org.apache.geode.internal.cache.CacheConfig;
+import org.apache.geode.internal.cache.DiskStoreImpl;
 import org.apache.geode.internal.cache.InternalCache;
 import org.apache.geode.internal.cache.TXManagerImpl;
 import org.apache.geode.internal.statistics.StatisticsManager;
@@ -38,42 +53,138 @@ import org.apache.geode.pdx.PdxInitializationException;
 
 public class PeerTypeRegistrationTest {
 
-  private final PeerTypeRegistration peerTypeRegistration;
+  private final InternalDistributedSystem internalDistributedSystem =
+      mock(InternalDistributedSystem.class);
+  private final DistributionManager distributionManager = mock(DistributionManager.class);
+  private final StatisticsManager statisticsManager = mock(StatisticsManager.class);
+  private final StatisticsType statisticsType = mock(StatisticsType.class);
+  private final Statistics statistics = mock(Statistics.class);
+  private final InternalCache internalCache = mock(InternalCache.class);
+  @SuppressWarnings("unchecked")
+  private final Region<Object, Object> region = mock(Region.class);
+  private final TXManagerImpl txManager = mock(TXManagerImpl.class);
+  @SuppressWarnings("deprecation")
+  private final AttributesFactory factory = mock(AttributesFactory.class);
 
-  public PeerTypeRegistrationTest() throws IOException, ClassNotFoundException {
-    final InternalDistributedSystem internalDistributedSystem =
-        mock(InternalDistributedSystem.class);
-    final DistributionManager distributionManager = mock(DistributionManager.class);
+  @Before
+  public void setUp() throws IOException, ClassNotFoundException {
     when(internalDistributedSystem.getDistributionManager()).thenReturn(distributionManager);
-    final StatisticsManager statisticsManager = mock(StatisticsManager.class);
-    final StatisticsType statisticsType = mock(StatisticsType.class);
     when(statisticsManager.createType(any(), any(), any())).thenReturn(statisticsType);
-    final Statistics statistics = mock(Statistics.class);
     when(statisticsManager.createAtomicStatistics(any(), any())).thenReturn(statistics);
     when(internalDistributedSystem.getStatisticsManager()).thenReturn(statisticsManager);
-    final InternalCache internalCache = mock(InternalCache.class);
     when(internalCache.getInternalDistributedSystem()).thenReturn(internalDistributedSystem);
-    @SuppressWarnings("unchecked")
-    final Region<Object, Object> region = mock(Region.class);
-    when(region.size()).thenReturn(1);
     when(internalCache.createVMRegion(eq(PeerTypeRegistration.REGION_NAME), any(), any()))
         .thenReturn(region);
     when(region.getRegionService()).thenReturn(internalCache);
-    final TXManagerImpl txManager = mock(TXManagerImpl.class);
     when(internalCache.getCacheTransactionManager()).thenReturn(txManager);
-    peerTypeRegistration = new PeerTypeRegistration(internalCache);
   }
 
   @Test
   public void getLocalSizeThrowsWhenNotInitialized() {
+    PeerTypeRegistration peerTypeRegistration = new PeerTypeRegistration(internalCache);
     assertThatThrownBy(peerTypeRegistration::getLocalSize)
         .isInstanceOf(PdxInitializationException.class);
   }
 
   @Test
   public void getLocalSizeReturnsValueAfterInitialized() {
+    when(region.size()).thenReturn(1);
+    PeerTypeRegistration peerTypeRegistration = new PeerTypeRegistration(internalCache);
     peerTypeRegistration.initialize();
+
     assertThat(peerTypeRegistration.getLocalSize()).isEqualTo(1);
   }
 
+  @Test
+  public void typeRegistryCacheIsFlushedWhenNotNull() {
+    TypeRegistry typeRegistry = mock(TypeRegistry.class);
+    when(internalCache.getPdxRegistry()).thenReturn(typeRegistry);
+
+    PeerTypeRegistration peerTypeRegistration = new PeerTypeRegistration(internalCache);
+    peerTypeRegistration.initialize();
+
+    verify(typeRegistry).flushCache();
+  }
+
+  @Test
+  public void pdxPersistenceIsSetWithUserDefinedDiskStore() throws NoSuchFieldException {
+    PeerTypeRegistration peerTypeRegistration = spy(new PeerTypeRegistration(internalCache));
+    doReturn(factory).when(peerTypeRegistration).getAttributesFactory();
+    when(internalCache.getPdxPersistent()).thenReturn(true);
+    CacheConfig config = mock(CacheConfig.class);
+    when(internalCache.getCacheConfig()).thenReturn(config);
+
+    FieldSetter.setField(config, config.getClass().getField("pdxDiskStoreUserSet"), true);
+    final String diskStoreName = "userDiskStore";
+    when(internalCache.getPdxDiskStore()).thenReturn(diskStoreName);
+
+    peerTypeRegistration.initialize();
+
+    verify(factory).setDiskStoreName(diskStoreName);
+    verify(factory).setDataPolicy(DataPolicy.PERSISTENT_REPLICATE);
+  }
+
+  @Test
+  public void pdxPersistenceIsSetWithDefaultDiskStore() {
+    PeerTypeRegistration peerTypeRegistration = spy(new PeerTypeRegistration(internalCache));
+    doReturn(factory).when(peerTypeRegistration).getAttributesFactory();
+    when(internalCache.getPdxPersistent()).thenReturn(true);
+    CacheConfig config = mock(CacheConfig.class);
+    when(internalCache.getCacheConfig()).thenReturn(config);
+
+    DiskStoreImpl defaultDiskStore = mock(DiskStoreImpl.class);
+    when(internalCache.getOrCreateDefaultDiskStore()).thenReturn(defaultDiskStore);
+    final String diskStoreName = "defaultDiskStoreName";
+    when(defaultDiskStore.getName()).thenReturn(diskStoreName);
+
+    peerTypeRegistration.initialize();
+
+    verify(factory).setDiskStoreName(diskStoreName);
+    verify(factory).setDataPolicy(DataPolicy.PERSISTENT_REPLICATE);
+  }
+
+  @Test
+  public void pdxPersistenceIsNotSetWhenPdxPersistenceIsFalse() {
+    PeerTypeRegistration peerTypeRegistration = spy(new PeerTypeRegistration(internalCache));
+    doReturn(factory).when(peerTypeRegistration).getAttributesFactory();
+
+    peerTypeRegistration.initialize();
+
+    verify(factory, times(0)).setDiskStoreName(any(String.class));
+    verify(factory).setDataPolicy(DataPolicy.REPLICATE);
+  }
+
+  @Test(expected = PdxInitializationException.class)
+  public void pdxInitializationExceptionIsThrownInInitializeWhenRegionCreationFails()
+      throws IOException, ClassNotFoundException {
+    doThrow(new IOException()).when(internalCache).createVMRegion(any(), any(), any());
+    PeerTypeRegistration peerTypeRegistration = new PeerTypeRegistration(internalCache);
+    peerTypeRegistration.initialize();
+  }
+
+  @Test
+  public void buildLocalMapsFromRegionIsNotCalledIfLocalMapsAreUpToDate() {
+    PeerTypeRegistration peerTypeRegistration = spy(new PeerTypeRegistration(internalCache));
+
+    DistributedLockService dlockService = mock(DistributedLockService.class);
+    doReturn(dlockService).when(peerTypeRegistration).getLockService();
+
+    when(dlockService.lock(anyString(), anyLong(), anyLong())).thenReturn(true);
+
+    when(internalCache.getDistributionManager()).thenReturn(distributionManager);
+    InternalDistributedSystem internalDistributedSystem = mock(InternalDistributedSystem.class);
+    when(distributionManager.getSystem()).thenReturn(internalDistributedSystem);
+    CancelCriterion cancelCriterion = mock(CancelCriterion.class);
+    when(distributionManager.getCancelCriterion()).thenReturn(cancelCriterion);
+
+    // buildReverseMapsFromRegion() is called during initialization of the PeerTypeRegistration, so
+    // we expect to see one invocation here
+    peerTypeRegistration.initialize();
+    verify(peerTypeRegistration, times(1)).buildReverseMapsFromRegion();
+
+    PdxType newType = mock(PdxType.class);
+    peerTypeRegistration.defineType(newType);
+
+    verify(peerTypeRegistration, times(1)).buildReverseMapsFromRegion();
+  }
 }
