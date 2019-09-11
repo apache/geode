72ec2cd2ce03ab3c78f62c4a132759699cb82a7d
Sean Goller <sgoller@pivotal.io>
2019-09-11 11:31:36 -0700

    maintenance.
    Authored-by: Sean Goller <sgoller@pivotal.io>

--- ci/images/google-windows-geode-builder/build_image.sh
+++ ci/images/google-windows-geode-builder/build_image.sh
@@ -15,7 +15,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-set -x
 
 PACKER=${PACKER:-packer135}
 PACKER_ARGS="${*}"
