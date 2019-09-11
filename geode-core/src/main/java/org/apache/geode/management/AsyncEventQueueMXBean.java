3d9627ff16443f4aa513a67bcc284e68953aff8a
agingade <agingade@pivotal.io>
2019-09-11 11:09:01 -0700

    GEODE-7149: Changes to support AsyncEventQueue's dispatcher status with AsyncEventQueue beans (#4029)
    Mbean changes to support AsyncEventQueue's pauseDispatcher() and resumeDispatcher() apis.

--- geode-core/src/main/java/org/apache/geode/management/AsyncEventQueueMXBean.java
+++ geode-core/src/main/java/org/apache/geode/management/AsyncEventQueueMXBean.java
@@ -127,4 +127,12 @@ public interface AsyncEventQueueMXBean {
    * Returns the number of bytes overflowed to disk for this Sender.
    */
   long getBytesOverflowedToDisk();
+
+  /**
+   * Returns the state of the event dispatcher.
+   *
+   * @return True if the dispatcher is paused, false otherwise.
+   */
+  boolean isDispatchingPaused();
+
 }
