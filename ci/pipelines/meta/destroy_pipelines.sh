1f84de9f56a0e17c9438d84c73988384897be90c
Owen Nichols <onichols@pivotal.io>
2019-09-20 09:40:55 -0700

    when cleaning up pipelines after a release, also clean the rc pipeline

--- ci/pipelines/meta/destroy_pipelines.sh
+++ ci/pipelines/meta/destroy_pipelines.sh
@@ -80,5 +80,5 @@ function destroyPipelines {
   done
 }
 
-destroyPipelines ${PIPELINE_PREFIX}main ${PIPELINE_PREFIX}pr ${PIPELINE_PREFIX}images ${PIPELINE_PREFIX}reaper ${PIPELINE_PREFIX}metrics ${PIPELINE_PREFIX}examples ${PIPELINE_PREFIX}meta 
+destroyPipelines ${PIPELINE_PREFIX}main ${PIPELINE_PREFIX}pr ${PIPELINE_PREFIX}images ${PIPELINE_PREFIX}reaper ${PIPELINE_PREFIX}metrics ${PIPELINE_PREFIX}examples ${PIPELINE_PREFIX}meta ${PIPELINE_PREFIX}rc
 echo "Destroyed ${CONCOURSE_URL}/teams/main/pipelines/${PIPELINE_PREFIX}main and all related pipelines"
