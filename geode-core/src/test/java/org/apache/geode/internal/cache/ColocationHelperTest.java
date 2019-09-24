4fc477bd78ab2db5e9c5cb68516b62a19785a8c6
Kirk Lund <klund@apache.org>
2019-09-24 14:26:50 -0700

    GEODE-7239: Remove log4j-core from ColocationHelperTest
    * Remove log4j-core code
    * Remove use of Fakes class
    * Use MockitoRule with STRICT_STUBS
    * Convert from JUnit Assert to AssertJ
    * Use catchThrowable for expected exceptions
    * Cleanup and streamline code

--- geode-core/src/test/java/org/apache/geode/internal/cache/ColocationHelperTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/ColocationHelperTest.java
@@ -12,141 +12,90 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package org.apache.geode.internal.cache;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import org.apache.logging.log4j.core.Appender;
-import org.apache.logging.log4j.core.LogEvent;
-import org.apache.logging.log4j.core.Logger;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+import org.mockito.quality.Strictness;
 
 import org.apache.geode.CancelCriterion;
 import org.apache.geode.cache.CacheClosedException;
 import org.apache.geode.cache.PartitionAttributes;
-import org.apache.geode.cache.Region;
-import org.apache.geode.distributed.internal.InternalDistributedSystem;
-import org.apache.geode.test.fake.Fakes;
 
 public class ColocationHelperTest {
-  private GemFireCacheImpl cache;
-  private GemFireCacheImpl oldCacheInstance;
-  private InternalDistributedSystem system;
-  private PartitionedRegion pr;
+
+  private CancelCriterion cancelCriterion;
+  private PartitionedRegion partitionedRegion;
+  private PartitionRegionConfig partitionRegionConfig;
   private DistributedRegion prRoot;
-  private PartitionAttributes pa;
-  private PartitionRegionConfig prc;
-  private Logger logger;
-  private Appender mockAppender;
-  private ArgumentCaptor<LogEvent> loggingEventCaptor;
 
-  /**
-   * @throws java.lang.Exception
-   */
-  @BeforeClass
-  public static void setUpBeforeClass() throws Exception {}
+  @Rule
+  public MockitoRule mockitoRule = MockitoJUnit.rule().strictness(Strictness.STRICT_STUBS);
 
-  /**
-   * @throws java.lang.Exception
-   */
-  @AfterClass
-  public static void tearDownAfterClass() throws Exception {}
-
-  /**
-   * @throws java.lang.Exception
-   */
   @Before
   public void setUp() throws Exception {
-    cache = Fakes.cache();
-    system = (InternalDistributedSystem) cache.getDistributedSystem();
-    pr = mock(PartitionedRegion.class);
+    InternalCache cache = mock(InternalCache.class);
+    PartitionAttributes partitionAttributes = mock(PartitionAttributes.class);
+
+    cancelCriterion = mock(CancelCriterion.class);
+    partitionRegionConfig = mock(PartitionRegionConfig.class);
+    partitionedRegion = mock(PartitionedRegion.class);
     prRoot = mock(DistributedRegion.class);
-    pa = mock(PartitionAttributes.class);
-    prc = mock(PartitionRegionConfig.class);
-    cache = Fakes.cache();
-  }
 
-  /**
-   * Test method for
-   * {@link org.apache.geode.internal.cache.ColocationHelper#getColocatedRegion(org.apache.geode.internal.cache.PartitionedRegion)}.
-   */
-  @Test
-  public void testGetColocatedRegionThrowsIllegalStateExceptionForMissingParentRegion() {
-    when(pr.getCache()).thenReturn(cache);
-    when(cache.getRegion(PartitionedRegionHelper.PR_ROOT_REGION_NAME, true))
-        .thenReturn(mock(DistributedRegion.class));
-    when(pr.getPartitionAttributes()).thenReturn(pa);
-    when(pr.getFullPath()).thenReturn("/region1");
-    when(pa.getColocatedWith()).thenReturn("region2");
-
-    PartitionedRegion colocatedPR;
-    boolean caughtIllegalStateException = false;
-    try {
-      colocatedPR = ColocationHelper.getColocatedRegion(pr);
-    } catch (Exception e) {
-      assertEquals("Expected IllegalStateException for missing colocated parent region",
-          IllegalStateException.class, e.getClass());
-      assertTrue("Expected IllegalStateException to be thrown for missing colocated region: "
-          + e.getMessage(),
-          e.getMessage().matches("Region specified in 'colocated-with' .* does not exist.*"));
-      caughtIllegalStateException = true;
-    }
-    assertTrue(caughtIllegalStateException);
-  }
-
-  /**
-   * Test method for
-   * {@link org.apache.geode.internal.cache.ColocationHelper#getColocatedRegion(org.apache.geode.internal.cache.PartitionedRegion)}.
-   */
-  @Test
-  public void testGetColocatedRegionLogsWarningForMissingRegionWhenPRConfigHasRegion() {
-    when(pr.getCache()).thenReturn(cache);
-    when(cache.getRegion(PartitionedRegionHelper.PR_ROOT_REGION_NAME, true)).thenReturn(prRoot);
-    when(pr.getPartitionAttributes()).thenReturn(pa);
-    when(pr.getFullPath()).thenReturn("/region1");
-    when(pa.getColocatedWith()).thenReturn("region2");
-    when(((Region) prRoot).get("#region2")).thenReturn(prc);
-
-    PartitionedRegion colocatedPR = null;
-    boolean caughtIllegalStateException = false;
-    try {
-      colocatedPR = ColocationHelper.getColocatedRegion(pr);
-    } catch (Exception e) {
-      assertEquals("Expected IllegalStateException for missing colocated parent region",
-          IllegalStateException.class, e.getClass());
-      assertTrue("Expected IllegalStateException to be thrown for missing colocated region",
-          e.getMessage().matches("Region specified in 'colocated-with' .* does not exist.*"));
-      caughtIllegalStateException = true;
-    }
-    assertTrue(caughtIllegalStateException);
-  }
-
-  @Test
-  public void testGetColocatedRegionThrowsCacheClosedExceptionWhenCacheIsClosed() {
-    when(pr.getCache()).thenReturn(cache);
-    DistributedRegion prRoot = mock(DistributedRegion.class);
-    when(cache.getRegion(PartitionedRegionHelper.PR_ROOT_REGION_NAME, true))
-        .thenReturn(prRoot);
-    when(pr.getPartitionAttributes()).thenReturn(pa);
-    when(pa.getColocatedWith()).thenReturn("region2");
-    PartitionRegionConfig partitionRegionConfig = mock(PartitionRegionConfig.class);
-    when(prRoot.get(any())).thenReturn(partitionRegionConfig);
-    CancelCriterion cancelCriterion = mock(CancelCriterion.class);
     when(cache.getCancelCriterion()).thenReturn(cancelCriterion);
-    doThrow(CacheClosedException.class).when(cancelCriterion).checkCancelInProgress(any());
+    when(cache.getRegion(anyString(), anyBoolean())).thenReturn(prRoot);
+    when(partitionedRegion.getCache()).thenReturn(cache);
+    when(partitionedRegion.getPartitionAttributes()).thenReturn(partitionAttributes);
 
-    assertThatThrownBy(() -> ColocationHelper.getColocatedRegion(pr))
+    when(partitionAttributes.getColocatedWith()).thenReturn("region2");
+  }
+
+  @Test
+  public void getColocatedRegion_throwsIllegalStateException_ifNotColocated() {
+    when(partitionedRegion.getFullPath()).thenReturn("/region1");
+
+    Throwable thrown = catchThrowable(() -> ColocationHelper.getColocatedRegion(partitionedRegion));
+
+    assertThat(thrown)
+        .as("Expected IllegalStateException for missing colocated parent region")
+        .isInstanceOf(IllegalStateException.class)
+        .hasMessageMatching("Region specified in 'colocated-with' .* does not exist.*");
+  }
+
+  @Test
+  public void getColocatedRegion_logsWarning_ifMissingRegion_whenPRConfigHasRegion() {
+    when(partitionedRegion.getFullPath()).thenReturn("/region1");
+    when(prRoot.get(eq("#region2"))).thenReturn(partitionRegionConfig);
+
+    Throwable thrown = catchThrowable(() -> ColocationHelper.getColocatedRegion(partitionedRegion));
+
+    assertThat(thrown)
+        .as("Expected IllegalStateException for missing colocated parent region")
+        .isInstanceOf(IllegalStateException.class)
+        .hasMessageMatching("Region specified in 'colocated-with' .* does not exist.*");
+  }
+
+  @Test
+  public void getColocatedRegion_throwsCacheClosedException_whenCacheIsClosed() {
+    doThrow(new CacheClosedException("test")).when(cancelCriterion).checkCancelInProgress(any());
+    when(prRoot.get(any())).thenReturn(partitionRegionConfig);
+
+    Throwable thrown = catchThrowable(() -> ColocationHelper.getColocatedRegion(partitionedRegion));
+
+    assertThat(thrown)
         .isInstanceOf(CacheClosedException.class);
   }
 }
