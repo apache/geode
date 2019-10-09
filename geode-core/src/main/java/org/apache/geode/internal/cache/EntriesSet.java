8bcfde1a1976be5a32fecc3320bc87c1967d195e
Ernie Burghardt <eburghardt@pivotal.io>
2019-10-09 14:33:14 -0700

    Remove marker interface called EntriesCollection completely, replace with LogWithToString.

--- geode-core/src/main/java/org/apache/geode/internal/cache/EntriesSet.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/EntriesSet.java
@@ -26,9 +26,10 @@ import org.apache.geode.cache.EntryDestroyedException;
 import org.apache.geode.cache.Region;
 import org.apache.geode.internal.cache.LocalRegion.IteratorType;
 import org.apache.geode.internal.cache.entries.AbstractRegionEntry;
+import org.apache.geode.internal.logging.LogWithToString;
 
 /** Set view of entries */
-public class EntriesSet extends AbstractSet implements EntriesCollection {
+public class EntriesSet extends AbstractSet implements LogWithToString {
 
   final LocalRegion topRegion;
 
