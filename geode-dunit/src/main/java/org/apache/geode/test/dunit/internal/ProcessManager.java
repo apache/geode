9c854dc4653019058562766e7f40afa6f7d8d21d
Ernie Burghardt <eburghardt@pivotal.io>
2019-10-11 10:09:10 -0700

    fix spA

--- geode-dunit/src/main/java/org/apache/geode/test/dunit/internal/ProcessManager.java
+++ geode-dunit/src/main/java/org/apache/geode/test/dunit/internal/ProcessManager.java
@@ -15,7 +15,6 @@
 package org.apache.geode.test.dunit.internal;
 
 import static org.apache.geode.distributed.ConfigurationProperties.ENABLE_NETWORK_PARTITION_DETECTION;
-import static org.apache.geode.distributed.ConfigurationProperties.LOG_LEVEL;
 import static org.apache.geode.distributed.internal.DistributionConfig.GEMFIRE_PREFIX;
 
 import java.io.BufferedReader;
