d2f158960fce07ae807e41ef9a320c0d2b79cf60
Mario Kevo <mario.kevo@est.tech>
2019-10-16 10:21:02 +0200

    upgrade jetty to a newer version

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -57,7 +57,7 @@ class DependencyConstraints implements Plugin<Project> {
 
     // The jetty version is also hard-coded in geode-assembly:test
     // at o.a.g.sessions.tests.GenericAppServerInstall.java
-    deps.put("jetty.version", "9.4.12.v20180830")
+    deps.put("jetty.version", "9.4.21.v20190926")
 
     // These version numbers are consumed by protobuf configurations that are plugin-specific and not
     // part of the typical Gradle dependency configurations.
