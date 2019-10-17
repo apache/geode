a621cd0e25180adbe92c33b9f5ca2819f731d0f3
Naburun Nag <nag@cs.wisc.edu>
2019-10-17 15:46:38 -0700

    Restoring the context distinct state after a recursion step.

--- geode-core/src/main/java/org/apache/geode/cache/query/internal/CompiledSelect.java
+++ geode-core/src/main/java/org/apache/geode/cache/query/internal/CompiledSelect.java
@@ -393,6 +393,7 @@ public class CompiledSelect extends AbstractCompiledValue {
       TypeMismatchException, NameResolutionException, QueryInvocationTargetException {
     context.newScope((Integer) context.cacheGet(scopeID));
     context.pushExecCache((Integer) context.cacheGet(scopeID));
+    boolean prevDistinctState = context.isDistinct();
     context.setDistinct(this.distinct);
     if (this.hasUnmappedOrderByCols && context.getBucketList() != null) {
       throw new QueryInvalidException(
@@ -615,6 +616,7 @@ public class CompiledSelect extends AbstractCompiledValue {
       }
       return result;
     } finally {
+      context.setDistinct(prevDistinctState);
       context.popScope();
       context.popExecCache();
     }
