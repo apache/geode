ebf497f20c5c2340e5faca7cfe108e2ee78d7d0e
Aaron Lindsey <alindsey@pivotal.io>
2019-10-21 12:17:06 -0700

    GEODE-7326: Add cache gets timers
    - Add timers to record get operations from a Java client
    - Tag each timer with region name and result (hit/miss)
    - Close existing entries gauge when closing RegionPerfStats
    - Improve micrometer assertj assertions
    
    Co-authored-by: Aaron Lindsey <alindsey@pivotal.io>
    Co-authored-by: Kirk Lund <klund@apache.org>

--- geode-core/src/test/java/org/apache/geode/internal/cache/RegionPerfStatsTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/RegionPerfStatsTest.java
@@ -14,7 +14,9 @@
  */
 package org.apache.geode.internal.cache;
 
+import static java.util.concurrent.TimeUnit.NANOSECONDS;
 import static org.apache.geode.internal.statistics.StatisticsClockFactory.disabledClock;
+import static org.apache.geode.test.micrometer.MicrometerAssertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
@@ -23,6 +25,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.quality.Strictness.STRICT_STUBS;
 
+import java.util.Collection;
 import java.util.function.LongSupplier;
 
 import io.micrometer.core.instrument.Gauge;
@@ -41,48 +44,46 @@ import org.mockito.junit.MockitoRule;
 import org.apache.geode.Statistics;
 import org.apache.geode.StatisticsFactory;
 import org.apache.geode.cache.DataPolicy;
+import org.apache.geode.internal.statistics.StatisticsClock;
 
 public class RegionPerfStatsTest {
 
   private static final String REGION_NAME = "region1";
   private static final String TEXT_ID = "textId";
   private static final DataPolicy DATA_POLICY = DataPolicy.PERSISTENT_REPLICATE;
+  private static final long CLOCK_VALUE = 5L;
 
   private MeterRegistry meterRegistry;
   private CachePerfStats cachePerfStats;
   private InternalRegion region;
-
   private RegionPerfStats regionPerfStats;
-  private RegionPerfStats regionPerfStats2;
   private Statistics statistics;
+  private StatisticsClock statisticsClock;
 
   @Rule
   public MockitoRule mockitoRule = MockitoJUnit.rule().strictness(STRICT_STUBS);
+  private StatisticsFactory statisticsFactory;
 
   @Before
   public void setUp() {
     meterRegistry = new SimpleMeterRegistry();
     cachePerfStats = mock(CachePerfStats.class);
-    StatisticsFactory statisticsFactory = mock(StatisticsFactory.class);
     statistics = mock(Statistics.class);
     region = mock(InternalRegion.class);
     when(region.getName()).thenReturn(REGION_NAME);
     when(region.getDataPolicy()).thenReturn(DATA_POLICY);
-
+    statisticsFactory = mock(StatisticsFactory.class);
     when(statisticsFactory.createAtomicStatistics(any(), any())).thenReturn(statistics);
+    statisticsClock = mock(StatisticsClock.class);
 
-    regionPerfStats =
-        new RegionPerfStats(statisticsFactory, TEXT_ID, disabledClock(), cachePerfStats, region,
-            meterRegistry);
+    regionPerfStats = new RegionPerfStats(statisticsFactory, TEXT_ID, statisticsClock,
+        cachePerfStats, region, meterRegistry);
   }
 
   @After
   public void closeStats() {
     if (regionPerfStats != null) {
       regionPerfStats.close();
-    }
-    if (regionPerfStats2 != null) {
-      regionPerfStats2.close();
     }
   }
 
@@ -99,23 +100,30 @@ public class RegionPerfStatsTest {
 
   @Test
   public void constructor_createsEntriesGauge_taggedWithRegionName() {
-    Gauge entriesGauge = meterRegistry
-        .find("geode.cache.entries")
-        .gauge();
-
-    assertThat(entriesGauge.getId().getTag("region"))
-        .as("region tag")
-        .isEqualTo(REGION_NAME);
+    assertThat(entriesGauge())
+        .as("geode.cache.entries gauge")
+        .hasTag("region", REGION_NAME);
   }
 
   @Test
   public void constructor_createsEntriesGauge_taggedWithDataPolicy() {
-    Gauge entriesGauge = meterRegistry
-        .find("geode.cache.entries")
-        .gauge();
-    assertThat(entriesGauge.getId().getTag("data.policy"))
-        .as("data.policy tag")
-        .isEqualTo(DATA_POLICY.toString());
+    assertThat(entriesGauge())
+        .as("geode.cache.entries gauge")
+        .hasTag("data.policy", DATA_POLICY.toString());
+  }
+
+  @Test
+  public void constructor_createsCacheGetsHitTimer_taggedWithRegionName() {
+    assertThat(cacheGetsHitTimer())
+        .as("geode.cache.gets timer with tag result=hit")
+        .hasTag("region", REGION_NAME);
+  }
+
+  @Test
+  public void constructor_createsCacheGetsMissTimer_taggedWithRegionName() {
+    assertThat(cacheGetsMissTimer())
+        .as("geode.cache.gets timer with tag result=miss")
+        .hasTag("region", REGION_NAME);
   }
 
   @Test
@@ -146,20 +154,112 @@ public class RegionPerfStatsTest {
         .find("geode.cache.entries")
         .tag("region", REGION_NAME)
         .gauge();
+
     assertThat(entriesGauge.value()).isEqualTo(3);
   }
 
   @Test
-  public void close_removesItsOwnMetersFromTheRegistry() {
-    assertThat(meterNamed("geode.cache.entries"))
+  public void endGet_recordsHitTimerCountAndTotalTime_ifCacheHitAndClockEnabled() {
+    when(statisticsClock.isEnabled()).thenReturn(true);
+    when(statisticsClock.getTime()).thenReturn(CLOCK_VALUE);
+
+    regionPerfStats.endGet(0, false);
+
+    assertThat(cacheGetsHitTimer())
+        .as("geode.cache.gets timer with tag result=hit")
+        .hasCount(1)
+        .hasTotalTime(NANOSECONDS, CLOCK_VALUE);
+  }
+
+  @Test
+  public void endGet_doesNotRecordMissTimerCountOrTotalTime_ifCacheHitAndClockEnabled() {
+    when(statisticsClock.isEnabled()).thenReturn(true);
+    when(statisticsClock.getTime()).thenReturn(CLOCK_VALUE);
+
+    regionPerfStats.endGet(0, false);
+
+    assertThat(cacheGetsMissTimer())
+        .as("geode.cache.gets timer with tag result=miss")
+        .hasCount(0)
+        .hasTotalTime(NANOSECONDS, 0);
+  }
+
+  @Test
+  public void endGet_recordsHitTimerCountOnly_ifCacheHitAndClockDisabled() {
+    when(statisticsClock.isEnabled()).thenReturn(false);
+
+    regionPerfStats.endGet(0, false);
+
+    assertThat(cacheGetsHitTimer())
+        .as("geode.cache.gets timer with tag result=hit")
+        .hasCount(1)
+        .hasTotalTime(NANOSECONDS, 0);
+  }
+
+  @Test
+  public void endGet_recordsMissTimerCountAndTotalTime_ifCacheMissAndClockEnabled() {
+    when(statisticsClock.isEnabled()).thenReturn(true);
+    when(statisticsClock.getTime()).thenReturn(CLOCK_VALUE);
+
+    regionPerfStats.endGet(0, true);
+
+    assertThat(cacheGetsMissTimer())
+        .as("geode.cache.gets timer with tag result=miss")
+        .hasCount(1)
+        .hasTotalTime(NANOSECONDS, CLOCK_VALUE);
+  }
+
+  @Test
+  public void endGet_doesNotRecordHitTimerCountOrTotalTime_ifCacheMissAndClockEnabled() {
+    when(statisticsClock.isEnabled()).thenReturn(true);
+    when(statisticsClock.getTime()).thenReturn(CLOCK_VALUE);
+
+    regionPerfStats.endGet(0, true);
+
+    assertThat(cacheGetsHitTimer())
+        .as("geode.cache.gets timer with tag result=hit")
+        .hasCount(0)
+        .hasTotalTime(NANOSECONDS, 0);
+  }
+
+  @Test
+  public void endGet_recordsMissTimerCountOnly_ifCacheMissAndClockDisabled() {
+    when(statisticsClock.isEnabled()).thenReturn(false);
+
+    regionPerfStats.endGet(0, true);
+
+    assertThat(cacheGetsMissTimer())
+        .as("geode.cache.gets timer with tag result=miss")
+        .hasCount(1)
+        .hasTotalTime(NANOSECONDS, 0);
+  }
+
+  @Test
+  public void close_removesEntriesGaugeFromTheRegistry() {
+    assertThat(metersNamed("geode.cache.entries"))
         .as("entries gauge before closing the stats")
-        .isNotNull();
+        .hasSize(1);
 
     regionPerfStats.close();
 
-    assertThat(meterNamed("geode.cache.entries"))
+    assertThat(metersNamed("geode.cache.entries"))
         .as("entries gauge after closing the stats")
-        .isNull();
+        .hasSize(0);
+
+    regionPerfStats = null;
+  }
+
+  @Test
+  public void close_removesCacheGetsTimersFromTheRegistry() {
+    assertThat(metersNamed("geode.cache.gets"))
+        .as("cache gets timer before closing the stats")
+        .hasSize(2);
+
+    regionPerfStats.close();
+
+    assertThat(metersNamed("geode.cache.gets"))
+        .as("cache gets timer after closing the stats")
+        .hasSize(0);
 
     regionPerfStats = null;
   }
@@ -173,16 +273,55 @@ public class RegionPerfStatsTest {
 
     regionPerfStats.close();
 
-    assertThat(meterNamed(foreignMeterName))
+    assertThat(metersNamed(foreignMeterName))
         .as("foreign meter after closing the stats")
-        .isNotNull();
+        .hasSize(1);
 
     regionPerfStats = null;
   }
 
-  private Meter meterNamed(String meterName) {
+  @Test
+  public void close_closesMeters() {
+    Gauge entriesGauge = mock(Gauge.class);
+    Timer cacheGetsHitTimer = mock(Timer.class);
+    Timer cacheGetsMissTimer = mock(Timer.class);
+    regionPerfStats = new RegionPerfStats(statisticsFactory, TEXT_ID, statisticsClock,
+        cachePerfStats, region, mock(MeterRegistry.class), entriesGauge, cacheGetsHitTimer,
+        cacheGetsMissTimer);
+
+    regionPerfStats.close();
+
+    verify(entriesGauge).close();
+    verify(cacheGetsHitTimer).close();
+    verify(cacheGetsMissTimer).close();
+
+    regionPerfStats = null;
+  }
+
+  private Collection<Meter> metersNamed(String meterName) {
     return meterRegistry
         .find(meterName)
-        .meter();
+        .meters();
+  }
+
+  private Gauge entriesGauge() {
+    return meterRegistry
+        .find("geode.cache.entries")
+        .gauge();
+  }
+
+  private Timer cacheGetsHitTimer() {
+    return cacheGetsTimer("hit");
+  }
+
+  private Timer cacheGetsMissTimer() {
+    return cacheGetsTimer("miss");
+  }
+
+  private Timer cacheGetsTimer(String resultTagValue) {
+    return meterRegistry
+        .find("geode.cache.gets")
+        .tag("result", resultTagValue)
+        .timer();
   }
 }
