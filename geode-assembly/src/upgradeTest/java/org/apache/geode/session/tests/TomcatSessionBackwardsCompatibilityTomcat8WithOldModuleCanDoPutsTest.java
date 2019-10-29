6d4f1e26d091f243b8113f657e8fba2965d78eec
Benjamin Ross <bross@pivotal.io>
2019-10-29 11:09:15 -0700

    Unignored Tomcat8WithOldModuleCanDoPutsTest
    Co-authored-by: Benjamin Ross <bross@pivotal.io>
    Co-authored-by: Donal Evans <doevans@pivotal.io>

--- geode-assembly/src/upgradeTest/java/org/apache/geode/session/tests/TomcatSessionBackwardsCompatibilityTomcat8WithOldModuleCanDoPutsTest.java
+++ geode-assembly/src/upgradeTest/java/org/apache/geode/session/tests/TomcatSessionBackwardsCompatibilityTomcat8WithOldModuleCanDoPutsTest.java
@@ -14,7 +14,6 @@
  */
 package org.apache.geode.session.tests;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runners.Parameterized;
 
@@ -28,7 +27,6 @@ public class TomcatSessionBackwardsCompatibilityTomcat8WithOldModuleCanDoPutsTes
     super(version);
   }
 
-  @Ignore // GEODE-7336 addressing failures in upgradeTests
   @Test
   public void test() throws Exception {
     startClusterWithTomcat(classPathTomcat8);
