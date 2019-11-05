675a23a2dabdae1fb377a9e5beb7566e8ba176c7
Bruce Schuchardt <bschuchardt@pivotal.io>
2019-11-05 15:23:06 -0800

    reverted another set of investigative test changes

--- geode-core/src/distributedTest/java/org/apache/geode/ClusterCommunicationsDUnitTest.java
+++ geode-core/src/distributedTest/java/org/apache/geode/ClusterCommunicationsDUnitTest.java
@@ -167,17 +167,10 @@ public class ClusterCommunicationsDUnitTest implements Serializable {
       createCacheAndRegion(getVM(i), locatorPort);
     }
     performCreateWithLargeValue(getVM(1));
-    performUpdateWithLargeValue(getVM(1));
-  }
-
-  @Test
-  public void createEntryWithSmallMessage() {
-    int locatorPort = createLocator(getVM(0));
-    for (int i = 1; i <= NUM_SERVERS; i++) {
-      createCacheAndRegion(getVM(i), locatorPort);
+    // fault the value into an empty cache - forces use of message chunking
+    for (int i = 1; i <= NUM_SERVERS - 1; i++) {
+      verifyCreatedEntry(getVM(i));
     }
-    performCreateWithSmallValue(getVM(1));
-    performUpdateWithSmallValue(getVM(1));
   }
 
   @Test
@@ -269,40 +262,9 @@ public class ClusterCommunicationsDUnitTest implements Serializable {
 
   private void performCreateWithLargeValue(VM memberVM) {
     memberVM.invoke("perform create", () -> {
-      byte[] value = new byte[1024];
+      byte[] value = new byte[SMALL_BUFFER_SIZE * 20];
       Arrays.fill(value, (byte) 1);
       cache.getRegion(regionName).put("testKey", value);
-    });
-  }
-
-  private void performUpdateWithLargeValue(VM memberVM) {
-    memberVM.invoke("perform update", () -> {
-      byte[] value = new byte[70000];
-      Arrays.fill(value, (byte) 1);
-      long start = System.currentTimeMillis();
-      for (int i=0; i<50000; i++) {
-        cache.getRegion(regionName).put("testKey", value);
-      }
-      long end = System.currentTimeMillis();
-      System.out.println("BRUCE: elapsed update time: " + (end-start) +"ms");
-    });
-  }
-
-  private void performCreateWithSmallValue(VM memberVM) {
-    memberVM.invoke("perform create", () -> {
-      byte[] value = new byte[512];
-      Arrays.fill(value, (byte) 1);
-      cache.getRegion(regionName).put("testKey", value);
-    });
-  }
-
-  private void performUpdateWithSmallValue(VM memberVM) {
-    memberVM.invoke("perform update", () -> {
-      byte[] value = new byte[512];
-      Arrays.fill(value, (byte) 1);
-      for (int i=0; i<50000; i++) {
-        cache.getRegion(regionName).put("testKey", value);
-      }
     });
   }
 
@@ -359,7 +321,6 @@ public class ClusterCommunicationsDUnitTest implements Serializable {
     properties.setProperty(DISABLE_TCP, "" + disableTcp);
     properties.setProperty(SOCKET_LEASE_TIME, "10000");
     properties.setProperty(SOCKET_BUFFER_SIZE, "" + SMALL_BUFFER_SIZE);
-//    properties.setProperty(SOCKET_BUFFER_SIZE, "80000");
 
     if (useSSL) {
       properties.setProperty(SSL_ENABLED_COMPONENTS, "cluster,locator");
@@ -378,11 +339,11 @@ public class ClusterCommunicationsDUnitTest implements Serializable {
   }
 
   enum RunConfiguration {
-    //    SHARED_CONNECTIONS(true, false, false),
-//    SHARED_CONNECTIONS_WITH_SSL(true, true, false),
-//    UNSHARED_CONNECTIONS(false, false, false),
-    UNSHARED_CONNECTIONS_WITH_SSL(false, true, false);
-//    UDP_CONNECTIONS(true, false, true);
+    SHARED_CONNECTIONS(true, false, false),
+    SHARED_CONNECTIONS_WITH_SSL(true, true, false),
+    UNSHARED_CONNECTIONS(false, false, false),
+    UNSHARED_CONNECTIONS_WITH_SSL(false, true, false),
+    UDP_CONNECTIONS(true, false, true);
 
     boolean useSSL;
     boolean conserveSockets;
@@ -437,14 +398,14 @@ public class ClusterCommunicationsDUnitTest implements Serializable {
 
     @Override
     public void toData(DataOutput out,
-                       SerializationContext context) throws IOException {
+        SerializationContext context) throws IOException {
       super.toData(out, context);
       out.writeInt(processorId);
     }
 
     @Override
     public void fromData(DataInput in,
-                         DeserializationContext context) throws IOException, ClassNotFoundException {
+        DeserializationContext context) throws IOException, ClassNotFoundException {
       super.fromData(in, context);
       processorId = in.readInt();
     }
@@ -495,4 +456,3 @@ public class ClusterCommunicationsDUnitTest implements Serializable {
     }
   }
 }
-
