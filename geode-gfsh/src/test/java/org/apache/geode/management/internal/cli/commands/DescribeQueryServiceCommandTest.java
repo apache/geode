f485e1727e718953e2452988d27d0d057c8fd768
Patrick Johnson <pjohnson@pivotal.io>
2019-11-27 08:29:55 -0800

    Fix conflicts

--- geode-gfsh/src/test/java/org/apache/geode/management/internal/cli/commands/DescribeQueryServiceCommandTest.java
+++ geode-gfsh/src/test/java/org/apache/geode/management/internal/cli/commands/DescribeQueryServiceCommandTest.java
@@ -1,16 +1,18 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
- * agreements. See the NOTICE file distributed with this work for additional information regarding
- * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License. You may obtain a
- * copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
  *
  * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package org.apache.geode.management.internal.cli.commands;
@@ -26,7 +28,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -40,12 +41,13 @@ import java.util.Set;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 import org.apache.geode.cache.query.management.configuration.QueryConfigService;
 import org.apache.geode.distributed.DistributedMember;
-import org.apache.geode.management.internal.cli.functions.CliFunctionResult;
 import org.apache.geode.management.internal.cli.functions.DescribeQueryServiceFunction;
 import org.apache.geode.management.internal.cli.result.model.ResultModel;
+import org.apache.geode.management.internal.functions.CliFunctionResult;
 import org.apache.geode.test.junit.rules.GfshParserRule;
 
 public class DescribeQueryServiceCommandTest {
@@ -58,7 +60,7 @@ public class DescribeQueryServiceCommandTest {
 
   @Before
   public void setup() {
-    command = spy(new DescribeQueryServiceCommand());
+    command = Mockito.spy(new DescribeQueryServiceCommand());
     memberSet = new HashSet<>();
     queryConfigService = new QueryConfigService();
     doReturn(memberSet).when(command).findMembers(null, null);
