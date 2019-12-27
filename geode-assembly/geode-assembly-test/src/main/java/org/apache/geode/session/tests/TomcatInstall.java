b4c4c12f04442292deb9669ed5a86d43e83d2aa9
Anthony Baker <abaker@apache.org>
2019-12-26 16:15:34 -0800

    GEODE-7294: Update dependencies for v1.12
    Update many but not all dependencies.

--- geode-assembly/geode-assembly-test/src/main/java/org/apache/geode/session/tests/TomcatInstall.java
+++ geode-assembly/geode-assembly-test/src/main/java/org/apache/geode/session/tests/TomcatInstall.java
@@ -41,8 +41,8 @@ public class TomcatInstall extends ContainerInstall {
    */
   public enum TomcatVersion {
     TOMCAT6(6, "tomcat-6.0.37.zip"),
-    TOMCAT7(7, "tomcat-7.0.90.zip"),
-    TOMCAT8(8, "tomcat-8.5.34.zip"),
+    TOMCAT7(7, "tomcat-7.0.96.zip"),
+    TOMCAT8(8, "tomcat-8.5.46.zip"),
     TOMCAT9(9, "tomcat-9.0.12.zip");
 
     private final int version;
