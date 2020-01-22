f896f04df291246d420cab88b660fc9736fca49b
Alberto Bustamante Reyes <alb3rtobr@users.noreply.github.com>
2020-01-22 17:27:33 +0100

    GEODE-7565: GW sender connections failover

--- geode-core/src/main/java/org/apache/geode/cache/client/internal/EndpointManager.java
+++ geode-core/src/main/java/org/apache/geode/cache/client/internal/EndpointManager.java
@@ -44,9 +44,9 @@ public interface EndpointManager {
   /**
    * Get the map of all endpoints currently in use.
    *
-   * @return a map for ServerLocation->Endpoint
+   * @return a map for DistributedMember->Endpoint
    */
-  Map<ServerLocation, Endpoint> getEndpointMap();
+  Map<DistributedMember, Endpoint> getEndpointMap();
 
   void close();
 
