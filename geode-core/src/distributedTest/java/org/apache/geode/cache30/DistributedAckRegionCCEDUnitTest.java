a64acb2b06e0a71b68ae40c34ab8c80703e2d07f
Mark Hanson <mhanson@pivotal.io>
2020-01-31 15:15:57 -0800

    GEODE-7600: Disabling a test until the underlying code is implemented.

--- geode-core/src/distributedTest/java/org/apache/geode/cache30/DistributedAckRegionCCEDUnitTest.java
+++ geode-core/src/distributedTest/java/org/apache/geode/cache30/DistributedAckRegionCCEDUnitTest.java
@@ -263,6 +263,7 @@ public class DistributedAckRegionCCEDUnitTest extends DistributedAckRegionDUnitT
     versionTestClearWithConcurrentEventsAsync();
   }
 
+  @Ignore("Enable after fix for bug GEODE-7600")
   @Test
   public void testClearOnNonReplicateWithConcurrentEvents() {
     versionTestClearOnNonReplicateWithConcurrentEvents();
