6fec62ff7b4b6ebc4f0f8079fcd67a2b0c3919b0
Bruce Schuchardt <bschuchardt@pivotal.io>
2020-03-20 09:05:24 -0700

    GEODE-7884: server hangs due to IllegalStateException (#4822)
    * GEODE-7884: server hangs due to IllegalStateException
    
    Added cancellation check before scheduling an idle-timeout or
    ack-wait-threshold timer task.  I had to add a new method to
    SystemTimerTask and then noticed there were no tests for SystemTimer, so
    I cleaned up that class and added tests.
    
    * adding missing copyright header to new test
    
    * fixing LGTM issues
    
    * reinstating 'continue' when encountering a null timer during a sweep
    
    * addressing Bill's comments
    
    renamed swarm everwhere
    made the collection of timers associated with a DistributedSystem into a Set
    made timer task variables in Connection volatile
    added checks in tasks to cancel themselves if their Connection is closed
    
    (cherry picked from commit 2d2a3f80bd5053749963889c1898df48e9aa0be7)

--- geode-core/src/main/java/org/apache/geode/internal/cache/partitioned/PRSanityCheckMessage.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/partitioned/PRSanityCheckMessage.java
@@ -124,7 +124,7 @@ public class PRSanityCheckMessage extends PartitionMessage {
       int sanityCheckInterval = Integer
           .getInteger(GeodeGlossary.GEMFIRE_PREFIX + "PRSanityCheckInterval", 5000).intValue();
       if (sanityCheckInterval != 0) {
-        final SystemTimer tm = new SystemTimer(dm.getSystem(), true);
+        final SystemTimer tm = new SystemTimer(dm.getSystem());
         SystemTimer.SystemTimerTask st = new SystemTimer.SystemTimerTask() {
           @Override
           public void run2() {
