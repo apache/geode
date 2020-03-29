43162fa4bd61b9d59a63c6d9c49ead2b44732921
Nabarun Nag <nabarunnag@users.noreply.github.com>
2020-03-29 16:02:12 -0700

    GEODE-7864: Instance checks in equals methods (#4865)

--- geode-junit/src/main/java/org/apache/geode/internal/cache/PartitionedRegionTestHelper.java
+++ geode-junit/src/main/java/org/apache/geode/internal/cache/PartitionedRegionTestHelper.java
@@ -302,6 +302,9 @@ class SerializableObject implements Serializable {
   }
 
   public boolean equals(Object obj) {
+    if (!(obj instanceof SerializableObject)) {
+      return false;
+    }
     if (obj == null)
       return false;
     if (this.str.equals(((SerializableObject) obj).str) && this.i == ((SerializableObject) obj).i)
