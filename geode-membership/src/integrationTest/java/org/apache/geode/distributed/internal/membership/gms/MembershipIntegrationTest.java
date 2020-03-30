2dfb3a8b1575c79799396c93b1ffffb95fe02aea
Dan Smith <upthewaterspout@apache.org>
2020-03-30 08:57:40 -0700

    Review comments and missing license header

--- geode-membership/src/integrationTest/java/org/apache/geode/distributed/internal/membership/gms/MembershipIntegrationTest.java
+++ geode-membership/src/integrationTest/java/org/apache/geode/distributed/internal/membership/gms/MembershipIntegrationTest.java
@@ -1,3 +1,17 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package org.apache.geode.distributed.internal.membership.gms;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -28,6 +42,7 @@ import org.apache.geode.distributed.internal.tcpserver.TcpClient;
 import org.apache.geode.distributed.internal.tcpserver.TcpSocketCreator;
 import org.apache.geode.distributed.internal.tcpserver.TcpSocketCreatorImpl;
 import org.apache.geode.distributed.internal.tcpserver.TcpSocketFactory;
+import org.apache.geode.internal.inet.LocalHostUtil;
 import org.apache.geode.internal.serialization.DSFIDSerializer;
 import org.apache.geode.internal.serialization.internal.DSFIDSerializerImpl;
 import org.apache.geode.logging.internal.executors.LoggingExecutors;
@@ -46,7 +61,7 @@ public class MembershipIntegrationTest {
 
   @Before
   public void before() throws IOException, MembershipConfigurationException {
-    localHost = InetAddress.getLocalHost();
+    localHost = LocalHostUtil.getLocalHost();
     dsfidSerializer = new DSFIDSerializerImpl();
     socketCreator = new TcpSocketCreatorImpl();
   }
@@ -65,7 +80,7 @@ public class MembershipIntegrationTest {
   }
 
   @Test
-  public void twoMembersCanStartWithOneLocator()
+  public void twoMembershipsCanStartWithOneLocator()
       throws IOException, MemberStartupException {
     final MembershipLocator<MemberIdentifier> locator = createLocator(0);
     locator.start();
