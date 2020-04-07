be80c3a93e6592547d646558342b06768a15dba5
Bill Burcham <bburcham@pivotal.io>
2020-04-07 16:00:30 -0700

    GEODE-7852-test-cluster: 2 node docker test cluster running

--- geode-assembly/src/acceptanceTest/java/org/apache/geode/client/sni/DualServerSNIAcceptanceTest.java
+++ geode-assembly/src/acceptanceTest/java/org/apache/geode/client/sni/DualServerSNIAcceptanceTest.java
@@ -1,17 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
- * agreements. See the NOTICE file distributed with this work for additional information regarding
- * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License. You may obtain a
- * copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
 package org.apache.geode.client.sni;
 
 import static com.palantir.docker.compose.execution.DockerComposeExecArgument.arguments;
@@ -43,7 +29,7 @@ import org.apache.geode.cache.client.ClientRegionShortcut;
 import org.apache.geode.cache.client.proxy.ProxySocketFactories;
 import org.apache.geode.test.junit.rules.IgnoreOnWindowsRule;
 
-public class ClientSNIAcceptanceTest {
+public class DualServerSNIAcceptanceTest {
 
   private static final URL DOCKER_COMPOSE_PATH =
       ClientSNIAcceptanceTest.class.getResource("docker-compose.yml");
@@ -66,13 +52,13 @@ public class ClientSNIAcceptanceTest {
     trustStorePath =
         createTempFileFromResource(ClientSNIAcceptanceTest.class,
             "geode-config/truststore.jks")
-                .getAbsolutePath();
+            .getAbsolutePath();
     docker.exec(options("-T"), "geode",
-        arguments("gfsh", "run", "--file=/geode/scripts/geode-starter.gfsh"));
+        arguments("gfsh", "run", "--file=/geode/scripts/geode-starter-2.gfsh"));
   }
 
   @Test
-  public void connectToSNIProxyDocker() {
+  public void dualServerTest() {
     Properties gemFireProps = new Properties();
     gemFireProps.setProperty(SSL_ENABLED_COMPONENTS, "all");
     gemFireProps.setProperty(SSL_KEYSTORE_TYPE, "jks");
@@ -91,17 +77,12 @@ public class ClientSNIAcceptanceTest {
         .setPoolSocketFactory(ProxySocketFactories.sni("localhost",
             proxyPort))
         .create();
-    // the geode-starter.gfsh script has created a Region named "jellyfish" on the
-    // server sitting behind the haproxy gateway. Show that an empty client cache can
-    // put something in that region and then retrieve it.
     Region<String, String> region =
         cache.<String, String>createClientRegionFactory(ClientRegionShortcut.PROXY)
             .create("jellyfish");
     region.destroy("hello");
     region.put("hello", "world");
     assertThat(region.get("hello")).isEqualTo("world");
-    // the geode-starter.gfsh script put an entry named "foo" into the region with the
-    // value "bar"
-    assertThat(region.get("foo")).isEqualTo("bar");
   }
+
 }
