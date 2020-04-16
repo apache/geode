9db544e876088794de58a0f863e13d17134f1e26
Bruce Schuchardt <bschuchardt@pivotal.io>
2020-04-16 08:12:11 -0700

    GEODE-7852: test ClientHealthMonitor functionality behind a SNI gateway (#4958)
    * GEODE-7852: test ClientHealthMonitor functionality behind a SNI gateway
    
    This ensures that a server sitting behind an SNI gateway detects the
    loss of a client and cleans up after it.  In this case the test detects
    that the server has closed CQs created by the non-durable client.
    
    Since test code is not accessible in the Docker container that's running
    the server I've enhanced the StatArchiveReader to be able to report the
    values of a statistic and have enabled statistics recording in the
    server.
    
    (cherry picked from commit 376df4cc7abc6db38b4b298f8af6f3c53baedf15)
    
    * change the docker rule to be a class-rule in case we add more tests to this class
    
    * removed system.out.println per Bill's review

--- geode-assembly/src/acceptanceTest/java/org/apache/geode/client/sni/ClientSNICQAcceptanceTest.java
+++ geode-assembly/src/acceptanceTest/java/org/apache/geode/client/sni/ClientSNICQAcceptanceTest.java
@@ -22,6 +22,7 @@ import static org.apache.geode.distributed.ConfigurationProperties.SSL_KEYSTORE_
 import static org.apache.geode.distributed.ConfigurationProperties.SSL_REQUIRE_AUTHENTICATION;
 import static org.apache.geode.distributed.ConfigurationProperties.SSL_TRUSTSTORE;
 import static org.apache.geode.distributed.ConfigurationProperties.SSL_TRUSTSTORE_PASSWORD;
+import static org.apache.geode.test.awaitility.GeodeAwaitility.await;
 import static org.apache.geode.test.util.ResourceUtils.createTempFileFromResource;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,9 +32,11 @@ import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import com.palantir.docker.compose.DockerComposeRule;
+import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestRule;
 
@@ -42,6 +45,7 @@ import org.apache.geode.cache.Region;
 import org.apache.geode.cache.client.ClientCache;
 import org.apache.geode.cache.client.ClientCacheFactory;
 import org.apache.geode.cache.client.ClientRegionShortcut;
+import org.apache.geode.cache.client.internal.PoolImpl;
 import org.apache.geode.cache.client.proxy.ProxySocketFactories;
 import org.apache.geode.cache.query.CqAttributes;
 import org.apache.geode.cache.query.CqAttributesFactory;
@@ -64,14 +68,17 @@ public class ClientSNICQAcceptanceTest {
   @ClassRule
   public static TestRule ignoreOnWindowsRule = new IgnoreOnWindowsRule();
 
-  @Rule
-  public DockerComposeRule docker = DockerComposeRule.builder()
-      .file(DOCKER_COMPOSE_PATH.getPath())
-      .build();
+  @ClassRule
+  public static NotOnWindowsDockerRule docker =
+      new NotOnWindowsDockerRule(() -> DockerComposeRule.builder()
+          .file(DOCKER_COMPOSE_PATH.getPath()).build());
+
   private CqQuery cqTracker;
 
   AtomicInteger eventCreateCounter = new AtomicInteger(0);
   AtomicInteger eventUpdateCounter = new AtomicInteger(0);
+  private ClientCache cache;
+  private Region<String, Integer> region;
 
   class SNICQListener implements CqListener {
 
@@ -94,22 +101,29 @@ public class ClientSNICQAcceptanceTest {
     }
   }
 
-  private String trustStorePath;
+  private static String trustStorePath;
 
-  @Before
-  public void before() throws IOException, InterruptedException {
+  @BeforeClass
+  public static void beforeClass() throws IOException, InterruptedException {
     trustStorePath =
         createTempFileFromResource(ClientSNICQAcceptanceTest.class,
             "geode-config/truststore.jks")
                 .getAbsolutePath();
-    docker.exec(options("-T"), "geode",
+    docker.get().exec(options("-T"), "geode",
         arguments("gfsh", "run", "--file=/geode/scripts/geode-starter.gfsh"));
 
   }
 
-  @Test
-  public void performSimpleCQOverSNIProxy()
-      throws CqException, CqExistsException, RegionNotFoundException {
+  @AfterClass
+  public static void afterClass() throws Exception {
+    String output =
+        docker.get().exec(options("-T"), "geode",
+            arguments("cat", "server-dolores/server-dolores.log"));
+    System.out.println("Server log file--------------------------------\n" + output);
+  }
+
+  @Before
+  public void before() throws Exception {
     Properties gemFireProps = new Properties();
     gemFireProps.setProperty(SSL_ENABLED_COMPONENTS, "all");
     gemFireProps.setProperty(SSL_KEYSTORE_TYPE, "jks");
@@ -119,22 +133,30 @@ public class ClientSNICQAcceptanceTest {
     gemFireProps.setProperty(SSL_TRUSTSTORE_PASSWORD, "geode");
     gemFireProps.setProperty(SSL_ENDPOINT_IDENTIFICATION_ENABLED, "true");
 
-    int proxyPort = docker.containers()
+    int proxyPort = docker.get().containers()
         .container("haproxy")
         .port(15443)
         .getExternalPort();
-    ClientCache cache = new ClientCacheFactory(gemFireProps)
+    cache = new ClientCacheFactory(gemFireProps)
         .addPoolLocator("locator-maeve", 10334)
         .setPoolSocketFactory(ProxySocketFactories.sni("localhost",
             proxyPort))
         .setPoolSubscriptionEnabled(true)
         .create();
-    Region<String, Integer> region =
-        cache.<String, Integer>createClientRegionFactory(ClientRegionShortcut.PROXY)
-            .create("jellyfish");
+    region = cache.<String, Integer>createClientRegionFactory(ClientRegionShortcut.PROXY)
+        .create("jellyfish");
+  }
 
+  @After
+  public void after() throws Exception {
+    if (cache != null) {
+      cache.close();
+    }
+  }
+
+  @Test
+  public void performSimpleCQOverSNIProxy() throws Exception {
     startCQ(region);
-
     populateRegion(region);
     assertThat(region.get("key0")).isEqualTo(0);
     assertThat(region.get("key1")).isEqualTo(1);
@@ -151,6 +173,20 @@ public class ClientSNICQAcceptanceTest {
     assertThat(region.get("key99")).isEqualTo(109);
 
     assertThat(eventUpdateCounter.get()).isEqualTo(62);
+
+    // verify that the server cleans up when the client connection to the gateway is destroyed
+    ((PoolImpl) cache.getDefaultPool()).killPrimaryEndpoint();
+    // since we can't run code in the server let's grab the CQ statistics and verify that
+    // the CQ has been closed. StatArchiveReader has a main() that we can use to get a printout
+    // of stat values
+    await().untilAsserted(() -> {
+      String stats = docker.get().exec(options("-T"), "geode",
+          arguments("java", "-cp", "/geode/lib/geode-dependencies.jar",
+              "org.apache.geode.internal.statistics.StatArchiveReader",
+              "stat", "server-dolores/statArchive.gfs", "CqServiceStats.numCqsClosed"));
+      // the stat should transition from zero to one at some point
+      assertThat(stats).contains("0.0 1.0");
+    });
 
   }
 
