aab948a5e6c82319a631c901a17cc01692da14c2
mhansonp <hansonm@vmware.com>
2020-04-17 16:46:23 -0700

    GEODE-5428 (#4972)
    * Made a few asserts await.

--- geode-cq/src/distributedTest/java/org/apache/geode/internal/cache/tier/sockets/DurableClientTestCase.java
+++ geode-cq/src/distributedTest/java/org/apache/geode/internal/cache/tier/sockets/DurableClientTestCase.java
@@ -16,6 +16,7 @@ package org.apache.geode.internal.cache.tier.sockets;
 
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.MINUTES;
+import static org.apache.geode.internal.cache.tier.sockets.CacheServerTestUtil.getCache;
 import static org.apache.geode.test.awaitility.GeodeAwaitility.await;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
@@ -73,8 +74,6 @@ public class DurableClientTestCase extends DurableClientTestBase {
   public void testSpecialDurableProperty() throws InterruptedException {
     final Properties jp = new Properties();
     jp.setProperty(GeodeGlossary.GEMFIRE_PREFIX + "SPECIAL_DURABLE", "true");
-
-
 
     try {
 
@@ -518,16 +517,14 @@ public class DurableClientTestCase extends DurableClientTestBase {
     // Verify the durable client received the updates before failover
     this.checkListenerEvents(2, 1, -1, this.durableClientVM);
 
-    this.durableClientVM.invoke(new CacheSerializableRunnable("Get") {
-      @Override
-      public void run2() throws CacheException {
-
-        Region<Object, Object> region = CacheServerTestUtil.getCache().getRegion(regionName);
+    this.durableClientVM.invoke("Get", () -> {
+      await().untilAsserted(() -> {
+        Region<Object, Object> region = getCache().getRegion(regionName);
         assertThat(region).isNotNull();
 
         assertThat(region.getEntry("0")).isNull();
         assertThat(region.getEntry("2")).isNotNull();
-      }
+      });
     });
 
     // Stop server 1
@@ -644,15 +641,13 @@ public class DurableClientTestCase extends DurableClientTestBase {
       this.checkListenerEvents(2, 1, -1, this.durableClientVM);
     }
 
-    this.durableClientVM.invoke(new CacheSerializableRunnable("Get") {
-      @Override
-      public void run2() throws CacheException {
-        Region<Object, Object> region = CacheServerTestUtil.getCache().getRegion(regionName);
+    this.durableClientVM.invoke("Get", () -> {
+      await().untilAsserted(() -> {
+        Region<Object, Object> region = getCache().getRegion(regionName);
         assertThat(region).isNotNull();
-
         // Register interest in all keys
         assertThat(region.getEntry("0")).isNull();
-      }
+      });
     });
 
     publishEntries(4, 1);
