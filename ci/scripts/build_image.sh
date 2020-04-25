9a8dd949f92a0499855c6ef34564c6bf056be5f8
Robert Houghton <rhoughton@pivotal.io>
2020-04-24 19:57:42 -0700

    GEODE-8022: Fix windows image build via pinning. (#4993)
    DRY the packer creation scripts.
    Tested by pinning a working windows image ID.
    
    Signed-off-by: Sean Goller <sgoller@pivotal.io>
    (cherry picked from commit 0aae6cbce71a9c4e4ca4b93589c0fbe164d8ac99)

--- ci/scripts/build_image.sh
+++ ci/scripts/build_image.sh
@@ -16,20 +16,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-SCRIPTDIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+PACKER=${PACKER:-packer}
+PACKER_JSON_SCRIPT="$(pwd)/${1}"
+PACKER_ARGS="${@:2}"
+INTERNAL=${INTERNAL:-false}
 
+PACKERDIR="$( dirname ${PACKER_JSON_SCRIPT} )"
 
-pushd ${SCRIPTDIR}
+BASE_IMAGE="$(cat base-family/name)"
+
+pushd ${PACKERDIR}
 
 if [[ -n "${CONCOURSE_GCP_KEY}" ]]; then
   dd of=credentials.json <<< "${CONCOURSE_GCP_KEY}"
-  export GOOGLE_APPLICATION_CREDENTIALS=${SCRIPTDIR}/credentials.json
+  export GOOGLE_APPLICATION_CREDENTIALS=${PACKERDIR}/credentials.json
 fi
 
 GCP_NETWORK="default"
@@ -44,20 +44,26 @@ if [[ -n "${MY_NAME}" ]]; then
   GCP_NETWORK=${GCP_NETWORK##*/}
   GCP_SUBNETWORK=$(echo ${NETWORK_INTERFACE_INFO} | jq -r '.networkInterfaces[0].subnetwork')
   GCP_SUBNETWORK=${GCP_SUBNETWORK##*/}
+  INTERNAL=true
+fi
+
+if [[ -z "${GCP_PROJECT}" ]]; then
+  echo "GCP_PROJECT is unset. Cowardly refusing to continue."
+  exit 1
 fi
 
 HASHED_PIPELINE_PREFIX="i$(uuidgen -n @dns -s -N "${PIPELINE_PREFIX}")-"
 
 echo "Running packer"
-PACKER_LOG=1 packer build \
-  --var "base_family=${BASE_FAMILY}" \
+PACKER_LOG=1 ${PACKER} build ${PACKER_ARGS} \
+  --var "base_image=${BASE_IMAGE}" \
   --var "geode_docker_image=${GEODE_DOCKER_IMAGE}" \
   --var "pipeline_prefix=${PIPELINE_PREFIX}" \
   --var "hashed_pipeline_prefix=${HASHED_PIPELINE_PREFIX}" \
-  --var "gcp_project=${GCP_PROJECT}" \
   --var "java_build_version=${JAVA_BUILD_VERSION}" \
+  --var "gcp_project=${GCP_PROJECT}" \
   --var "gcp_network=${GCP_NETWORK}" \
   --var "gcp_subnetwork=${GCP_SUBNETWORK}" \
-  --var "use_internal_ip=true" \
+  --var "use_internal_ip=${INTERNAL}" \
   --var "packer_ttl=$(($(date +%s) + 60 * 60 * 12))" \
-  packer.json
+  ${PACKER_JSON_SCRIPT}
