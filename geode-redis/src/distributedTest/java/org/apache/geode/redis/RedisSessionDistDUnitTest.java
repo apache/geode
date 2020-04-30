d9d7eeb632385e189a123665ea6ad3bc18e47312
Ray Ingles <ringles@pivotal.io>
2020-04-30 16:26:46 -0400

    refactor

--- geode-redis/src/distributedTest/java/org/apache/geode/redis/RedisSessionDistDUnitTest.java
+++ geode-redis/src/distributedTest/java/org/apache/geode/redis/RedisSessionDistDUnitTest.java
@@ -19,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.Serializable;
 import java.net.HttpCookie;
 import java.util.Base64;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -44,15 +45,13 @@ import org.apache.geode.test.awaitility.GeodeAwaitility;
 import org.apache.geode.test.dunit.VM;
 import org.apache.geode.test.dunit.rules.ClusterStartupRule;
 import org.apache.geode.test.dunit.rules.DistributedRestoreSystemProperties;
-import org.apache.geode.test.dunit.rules.MemberVM;
 import org.apache.geode.test.junit.categories.RedisTest;
 
 @Category({RedisTest.class})
 public class RedisSessionDistDUnitTest implements Serializable {
 
   @ClassRule
-  public static ClusterStartupRule cluster =
-      new ClusterStartupRule(5);
+  public static ClusterStartupRule cluster = new ClusterStartupRule(5);
 
   @Rule
   public DistributedRestoreSystemProperties restoreSystemProperties =
@@ -60,85 +59,31 @@ public class RedisSessionDistDUnitTest implements Serializable {
 
   public static ConfigurableApplicationContext springApplicationContext;
 
-  private String LOCALHOST = "http://127.0.0.1";
-
-  private static int indexOfLocator = 0;
-  private static int indexOfServer1 = 1;
-  private static int indexOfServer2 = 2;
-  private static int indexOfClient1 = 3;
-  private static int indexOfClient2 = 4;
-
-  private static VM client1;
-  private static VM client2;
-
-  private static int server1Port;
-  private static int server2Port;
-  private static int client1Port;
-  private static int client2Port;
-
-  private static Properties redisPropsForServer1;
-  private static Properties redisPropsForServer2;
-  private static MemberVM locator;
-
-  private static RestTemplate restTemplate;
+  private static final int LOCATOR = 0;
+  private static final int SERVER1 = 1;
+  private static final int SERVER2 = 2;
+  private static final int APP1 = 3;
+  private static final int APP2 = 4;
+  private static final Map<Integer, Integer> ports = new HashMap<>();
 
   private static Jedis jedis;
-
-  private static final int JEDIS_TIMEOUT =
-      Math.toIntExact(GeodeAwaitility.getTimeout().toMillis());
+  private static final int JEDIS_TIMEOUT = Math.toIntExact(GeodeAwaitility.getTimeout().toMillis());
 
   @BeforeClass
   public static void setup() {
+    int[] availablePorts = AvailablePortHelper.getRandomAvailableTCPPorts(4);
+    ports.put(SERVER1, availablePorts[0]);
+    ports.put(SERVER2, availablePorts[1]);
+    ports.put(APP1, availablePorts[2]);
+    ports.put(APP2, availablePorts[3]);
 
-    int[] availablePorts =
-        AvailablePortHelper.getRandomAvailableTCPPorts(4);
+    cluster.startLocatorVM(LOCATOR);
+    startRedisServer(SERVER1);
+    startRedisServer(SERVER2);
+    startSpringApp(APP1, SERVER1, SERVER2);
+    startSpringApp(APP2, SERVER2, SERVER1);
 
-    server1Port = availablePorts[0];
-    server2Port = availablePorts[1];
-    client1Port = availablePorts[2];
-    client2Port = availablePorts[3];
-
-    int localServer1Port = server1Port;
-    int localServer2Port = server2Port;
-    int localClient1Port = client1Port;
-    int localClient2Port = client2Port;
-
-    locator = cluster.startLocatorVM(indexOfLocator);
-
-    redisPropsForServer1 = new Properties();
-    redisPropsForServer1.setProperty("redis-bind-address", "localHost");
-    redisPropsForServer1.setProperty("redis-port", Integer.toString(server1Port));
-    redisPropsForServer1.setProperty("log-level", "warn");
-
-    redisPropsForServer2 = new Properties();
-    redisPropsForServer2.setProperty("redis-bind-address", "localHost");
-    redisPropsForServer2.setProperty("redis-port", Integer.toString(server2Port));
-    redisPropsForServer2.setProperty("log-level", "warn");
-
-    cluster.startServerVM(indexOfServer1, redisPropsForServer1, locator.getPort());
-    cluster.startServerVM(indexOfServer2, redisPropsForServer2, locator.getPort());
-
-    client1 = cluster.getVM(indexOfClient1);
-    client2 = cluster.getVM(indexOfClient2);
-
-    client1.invoke("start a spring app", () -> {
-      System.setProperty("server.port", Integer.toString(localClient1Port));
-      System.setProperty("spring.redis.port", Integer.toString(localServer1Port));
-      SpringApplication.run(
-          RedisSpringTestApplication.class,
-          "" + localServer1Port, "" + localServer2Port);
-    });
-
-    client2.invoke("start a spring app", () -> {
-      System.setProperty("server.port", Integer.toString(localClient2Port));
-      System.setProperty("spring.redis.port", Integer.toString(localServer2Port));
-      springApplicationContext = SpringApplication.run(
-          RedisSpringTestApplication.class,
-          "" + localServer1Port, "" + localServer2Port);
-    });
-
-    restTemplate = new RestTemplate();
-    jedis = new Jedis("localHost", server1Port, JEDIS_TIMEOUT);
+    jedis = new Jedis("localhost", ports.get(SERVER1), JEDIS_TIMEOUT);
   }
 
   @After
@@ -152,244 +97,160 @@ public class RedisSessionDistDUnitTest implements Serializable {
   }
 
   @Test
-  public void should_beAbleToCreateASession() {
-    HttpEntity<String> request = new HttpEntity<>("note1");
-    cluster.getMember(0);
-    HttpHeaders resultHeaders = restTemplate
-        .postForEntity(
-            LOCALHOST + ":" + client1Port + "/addSessionNote",
-            request,
-            String.class)
-        .getHeaders();
-
-    assertThat(resultHeaders).isNotNull();
-
-    String cookieString = resultHeaders.get("Set-Cookie").get(0);
-    HttpHeaders requestHeaders = new HttpHeaders();
-    requestHeaders.add("Cookie", cookieString);
-
-    List<HttpCookie> cookies = HttpCookie.parse(cookieString);
-    byte[] decodedCookie = Base64.getDecoder().decode(cookies.get(0).getValue());
-
-    assertThat(jedis.hgetAll("spring:session:sessions:" + new String(decodedCookie))).isNotEmpty();
-  }
-
-  @Test
-  public void should_storeSessionDataInRedis() {
-    HttpEntity<String> request = new HttpEntity<>("note1");
-    HttpHeaders responseHeaders = restTemplate
-        .postForEntity(
-            LOCALHOST + ":" + client1Port + "/addSessionNote",
-            request,
-            String.class)
-        .getHeaders();
-
-    String sessionAsCookie = responseHeaders.get("Set-Cookie").get(0);
-
-    HttpHeaders requestHeaders = new HttpHeaders();
-    requestHeaders.add("Cookie", sessionAsCookie);
-    HttpEntity<String> request2 = new HttpEntity<>("", requestHeaders);
-
-    String[] sessionNotes = restTemplate
-        .exchange(
-            LOCALHOST + ":" + client2Port + "/getSessionNotes",
-            HttpMethod.GET,
-            request2,
-            String[].class)
-        .getBody();
-
-    assertThat(sessionNotes[0]).isEqualTo("note1");
-
-    List<HttpCookie> cookies = HttpCookie.parse(sessionAsCookie);
-    byte[] decodedCookie = Base64.getDecoder().decode(cookies.get(0).getValue());
+  public void should_beAbleToCreateASession_storedInRedis() {
+    String sessionCookie = createNewSessionWithNote(APP1, "note1");
+    String sessionId = getSessionId(sessionCookie);
 
     Map<String, String> sessionInfo =
-        jedis.hgetAll("spring:session:sessions:" + new String(decodedCookie));
+        jedis.hgetAll("spring:session:sessions:" + sessionId);
 
-    assertThat(sessionInfo).isNotNull();
-    assertThat(sessionInfo.get("sessionAttr:NOTES")).isNotNull();
-
-    jedis.disconnect();
+    assertThat(sessionInfo.get("sessionAttr:NOTES")).contains("note1");
   }
 
   @Test
-  public void should_propagateSessionData_toOtherServers() {
+  public void should_storeSession() {
+    String sessionCookie = createNewSessionWithNote(APP1, "note1");
 
-    HttpEntity<String> request = new HttpEntity<>("noteFromClient1");
-    HttpHeaders resultHeaders = restTemplate
-        .postForEntity(
-            LOCALHOST + ":" + client1Port + "/addSessionNote",
-            request,
-            String.class)
-        .getHeaders();
+    String[] sessionNotes = getSessionNotes(APP2, sessionCookie);
 
-    String sessionAsCookie = resultHeaders.get("Set-Cookie").get(0);
-    HttpHeaders requestHeaders = new HttpHeaders();
-    requestHeaders.add("Cookie", sessionAsCookie);
-
-    HttpEntity<String> request2 = new HttpEntity<>("", requestHeaders);
-
-    String[] sessionNotes = restTemplate
-        .exchange(
-            LOCALHOST + ":" + client2Port + "/getSessionNotes",
-            HttpMethod.GET,
-            request2,
-            String[].class)
-        .getBody();
-
-    assertThat(sessionNotes[0]).isEqualTo("noteFromClient1");
+    assertThat(sessionNotes).containsExactly("note1");
   }
 
   @Test
-  public void should_getSessionDataFromOtherGeodeRedisServer_whenOriginalServerGoesDown() {
-    HttpEntity<String> request = new HttpEntity<>("noteFromClient2");
-    HttpHeaders responseHeaders = restTemplate
-        .postForEntity(
-            LOCALHOST + ":" + client2Port + "/addSessionNote",
-            request,
-            String.class)
-        .getHeaders();
+  public void should_propagateSession_toOtherServers() {
+    String sessionCookie = createNewSessionWithNote(APP1, "noteFromClient1");
 
-    String sessionAsCookie = responseHeaders.get("Set-Cookie").get(0);
-    HttpHeaders requestHeaders = new HttpHeaders();
-    requestHeaders.add("Cookie", sessionAsCookie);
-    HttpEntity<String> request2 = new HttpEntity<>("", requestHeaders);
+    String[] sessionNotes = getSessionNotes(APP2, sessionCookie);
 
-    cluster.crashVM(indexOfServer2);
-
-    String[] sessionNotes = restTemplate
-        .exchange(
-            LOCALHOST + ":" + client1Port + "/getSessionNotes",
-            HttpMethod.GET,
-            request2,
-            String[].class)
-        .getBody();
-
-    assertThat(sessionNotes[0]).isEqualTo("noteFromClient2");
-
-    cluster.startServerVM(indexOfServer2, redisPropsForServer2, locator.getPort());
+    assertThat(sessionNotes).containsExactly("noteFromClient1");
   }
 
   @Test
-  public void should_getSessionDataFromServer_whenServerGoesDownAndIsRestarted() {
-    HttpEntity<String> request = new HttpEntity<>("noteFromClient2");
+  public void should_getSessionFromServer1_whenServer2GoesDown() {
+    String sessionCookie = createNewSessionWithNote(APP2, "noteFromClient2");
+    cluster.crashVM(SERVER2);
+    try {
+      String[] sessionNotes = getSessionNotes(APP1, sessionCookie);
 
-    HttpHeaders resultHeaders = restTemplate
-        .postForEntity(
-            LOCALHOST + ":" + client2Port + "/addSessionNote",
-            request,
-            String.class)
-        .getHeaders();
+      assertThat(sessionNotes).containsExactly("noteFromClient2");
+    } finally {
+      startRedisServer(SERVER2);
+    }
+  }
 
-    String cookieString = resultHeaders.get("Set-Cookie").get(0);
-    HttpHeaders requestHeaders = new HttpHeaders();
-    requestHeaders.add("Cookie", cookieString);
-    HttpEntity<String> request2 = new HttpEntity<>("noteFromClient1", requestHeaders);
+  @Test
+  public void should_getSessionFromServer_whenServerGoesDownAndIsRestarted() {
+    String sessionCookie = createNewSessionWithNote(APP2, "noteFromClient2");
+    cluster.crashVM(SERVER2);
+    addNoteToSession(APP1, sessionCookie);
+    startRedisServer(SERVER2);
 
-    cluster.crashVM(indexOfServer2);
-
-    restTemplate
-        .postForEntity(
-            LOCALHOST + ":" + client1Port + "/addSessionNote",
-            request2,
-            String.class);
-
-    cluster.startServerVM(indexOfServer2,
-        redisPropsForServer2,
-        locator.getPort());
-
-    HttpEntity<String> request3 = new HttpEntity<>("", requestHeaders);
-
-    String[] sessionNotes = restTemplate
-        .exchange(
-            LOCALHOST + ":" + client2Port + "/getSessionNotes",
-            HttpMethod.GET,
-            request3,
-            String[].class)
-        .getBody();
+    String[] sessionNotes = getSessionNotes(APP2, sessionCookie);
 
     assertThat(sessionNotes).containsExactly("noteFromClient2", "noteFromClient1");
   }
 
   @Test
-  public void should_getCorrectSessionData_whenGeodeRedisServerGoesDown_andClientConnectsToDifferentServer() {
-    HttpEntity<String> request = new HttpEntity<>("noteFromClient2");
+  public void should_getSession_whenServer2GoesDown_andAppFailsOverToServer1() {
+    String sessionCookie = createNewSessionWithNote(APP2, "noteFromClient2");
+    cluster.crashVM(SERVER2);
 
-    HttpHeaders resultHeaders = restTemplate
+    try {
+      String[] sessionNotes = getSessionNotes(APP2, sessionCookie);
+
+      assertThat(sessionNotes).containsExactly("noteFromClient2");
+    } finally {
+      startRedisServer(SERVER2);
+    }
+  }
+
+  @Test
+  public void should_getSessionCreatedByApp2_whenApp2GoesDown_andClientConnectsToApp1() {
+    String sessionCookie = createNewSessionWithNote(APP2, "noteFromClient2");
+    stopSpringApp(APP2);
+
+    try {
+      String[] sessionNotes = getSessionNotes(APP1, sessionCookie);
+
+      assertThat(sessionNotes).containsExactly("noteFromClient2");
+    } finally {
+      startSpringApp(APP2, SERVER1, SERVER2);
+    }
+
+  }
+
+  private static void startRedisServer(int server1) {
+    cluster.startServerVM(server1, redisProperties(server1),
+        cluster.getMember(LOCATOR).getPort());
+  }
+
+  private static Properties redisProperties(int server2) {
+    Properties redisPropsForServer = new Properties();
+    redisPropsForServer.setProperty("redis-bind-address", "localHost");
+    redisPropsForServer.setProperty("redis-port", "" + ports.get(server2));
+    redisPropsForServer.setProperty("log-level", "warn");
+    return redisPropsForServer;
+  }
+
+  private static void startSpringApp(int sessionApp, int primaryServer, int secondaryServer) {
+    int primaryRedisPort = ports.get(primaryServer);
+    int failoverRedisPort = ports.get(secondaryServer);
+    int httpPort = ports.get(sessionApp);
+    VM host = cluster.getVM(sessionApp);
+    host.invoke("start a spring app", () -> {
+      System.setProperty("server.port", "" + httpPort);
+      System.setProperty("spring.redis.port", "" + primaryRedisPort);
+      springApplicationContext = SpringApplication.run(
+          RedisSpringTestApplication.class,
+          "" + primaryRedisPort, "" + failoverRedisPort);
+    });
+  }
+
+  private static void stopSpringApp(int sessionApp) {
+    cluster.getVM(sessionApp).invoke(() -> springApplicationContext.close());
+  }
+
+  private String createNewSessionWithNote(int sessionApp, String note) {
+    HttpEntity<String> request = new HttpEntity<>(note);
+    HttpHeaders resultHeaders = new RestTemplate()
         .postForEntity(
-            LOCALHOST + ":" + client2Port + "/addSessionNote",
+            "http://localhost:" + ports.get(sessionApp) + "/addSessionNote",
             request,
             String.class)
         .getHeaders();
 
-    String cookieString = resultHeaders.get("Set-Cookie").get(0);
+    return resultHeaders.getFirst("Set-Cookie");
+  }
+
+  private String[] getSessionNotes(int sessionApp, String sessionCookie) {
     HttpHeaders requestHeaders = new HttpHeaders();
-    requestHeaders.add("Cookie", cookieString);
+    requestHeaders.add("Cookie", sessionCookie);
     HttpEntity<String> request2 = new HttpEntity<>("", requestHeaders);
 
-    cluster.crashVM(indexOfServer2);
-
-    String[] sessionNotes = restTemplate
+    return new RestTemplate()
         .exchange(
-            LOCALHOST + ":" + client2Port + "/getSessionNotes",
+            "http://localhost:" + ports.get(sessionApp) + "/getSessionNotes",
             HttpMethod.GET,
             request2,
             String[].class)
         .getBody();
-
-    assertThat(sessionNotes[0]).isEqualTo("noteFromClient2");
-
-    cluster.startServerVM(indexOfServer2, redisPropsForServer2, locator.getPort());
   }
 
-
-  @Test
-  public void should_getCorrectSessionData_whenAppInstanceGoesDown_andClientConnectsToDifferentAppInstance() {
-
-    HttpEntity<String> request = new HttpEntity<>("noteFromClient2");
-    HttpHeaders responseHeaders = restTemplate
+  private void addNoteToSession(int sessionApp, String sessionCookie) {
+    HttpHeaders requestHeaders = new HttpHeaders();
+    requestHeaders.add("Cookie", sessionCookie);
+    HttpEntity<String> request = new HttpEntity<>("noteFromClient1", requestHeaders);
+    new RestTemplate()
         .postForEntity(
-            LOCALHOST + ":" + client2Port + "/addSessionNote",
+            "http://localhost:" + ports.get(sessionApp) + "/addSessionNote",
             request,
             String.class)
         .getHeaders();
-
-    String sessionAsCookie = responseHeaders.get("Set-Cookie").get(0);
-    HttpHeaders requestHeaders = new HttpHeaders();
-    requestHeaders.add("Cookie", sessionAsCookie);
-    HttpEntity<String> request2 = new HttpEntity<>("", requestHeaders);
-
-    client2.invoke(() -> {
-      springApplicationContext.close();
-    });
-
-    String[] sessionNotes;
-
-    try {
-      sessionNotes = restTemplate
-          .exchange(
-              LOCALHOST + ":" + client1Port + "/getSessionNotes",
-              HttpMethod.GET,
-              request2,
-              String[].class)
-          .getBody();
-
-    } finally {
-
-      int localServer1Port = server1Port;
-      int localServer2Port = server2Port;
-      int localClient2Port = client2Port;
-
-      client2.invoke("start a spring app", () -> {
-        System.setProperty("server.port", Integer.toString(localClient2Port));
-        System.setProperty("spring.redis.port", Integer.toString(localServer2Port));
-        springApplicationContext = SpringApplication.run(
-            RedisSpringTestApplication.class,
-            "" + localServer1Port, "" + localServer2Port);
-      });
-    }
-    assertThat(sessionNotes[0]).isEqualTo("noteFromClient2");
-
   }
 
+  private String getSessionId(String sessionCookie) {
+    List<HttpCookie> cookies = HttpCookie.parse(sessionCookie);
+    byte[] decodedCookie = Base64.getDecoder().decode(cookies.get(0).getValue());
+    return new String(decodedCookie);
+  }
 }
