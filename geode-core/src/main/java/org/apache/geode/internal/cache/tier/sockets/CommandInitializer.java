02839eae79ae078c30a422b1b0ff3c23866ccdbd
Dave Barnes <dbarnes@pivotal.io>
2020-05-04 18:55:12 -0700

    roll develop to 1.14 now that support/1.13 has been created (#5044)

--- geode-core/src/main/java/org/apache/geode/internal/cache/tier/sockets/CommandInitializer.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/tier/sockets/CommandInitializer.java
@@ -340,6 +340,7 @@ public class CommandInitializer {
     allCommands.put(Version.GEODE_1_12_0, geode18Commands);
     allCommands.put(Version.GEODE_1_13_0, geode18Commands);
 
+    allCommands.put(Version.GEODE_1_14_0, geode18Commands);
     return Collections.unmodifiableMap(allCommands);
   }
 
