c6213f59b83636f3d852406e444825fd19dc96b3
Jinmei Liao <jiliao@pivotal.io>
2020-05-06 09:47:39 -0700

    GEODE-8055: create index command should work on sub regions (#5034) - spotless

--- geode-gfsh/src/test/java/org/apache/geode/management/internal/cli/commands/CreateIndexCommandTest.java
+++ geode-gfsh/src/test/java/org/apache/geode/management/internal/cli/commands/CreateIndexCommandTest.java
@@ -28,7 +28,6 @@ import static org.mockito.Mockito.when;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import com.google.common.collect.Sets;
