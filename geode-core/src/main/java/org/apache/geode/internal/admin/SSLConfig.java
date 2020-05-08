ac3b3d0cca23641311785c971adf2143df30cf2e
Robert Houghton <rhoughton@pivotal.io>
2020-05-08 16:22:12 -0700

    Revert "GEODE-7414_2: modify init() method argument (#5040)"
    This reverts commit 5f9800b291d3a11140eb5a1a972459207e07b50c.

--- geode-core/src/main/java/org/apache/geode/internal/admin/SSLConfig.java
+++ geode-core/src/main/java/org/apache/geode/internal/admin/SSLConfig.java
@@ -338,7 +338,7 @@ public class SSLConfig {
         this.sslParameterExtension = null;
         return this;
       }
-      sslParameterExtension.init(ids.getConfig().toProperties());
+      sslParameterExtension.init(ids.getConfig());
       this.sslParameterExtension = sslParameterExtension;
       return this;
     }
