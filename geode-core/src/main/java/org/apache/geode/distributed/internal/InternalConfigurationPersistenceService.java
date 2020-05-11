36b2f397e2a72fe9f9a27b30e6a9be9b3d5049c1
Jinmei Liao <jiliao@pivotal.io>
2020-05-11 16:05:14 -0700

    GEODE-8091: fix AuthenticationRequiredException when starting a locat… (#5071)
    (cherry picked from commit 4c5788d3b1b37bd284e93fdda4b78486aad7c9b8)

--- geode-core/src/main/java/org/apache/geode/distributed/internal/InternalConfigurationPersistenceService.java
+++ geode-core/src/main/java/org/apache/geode/distributed/internal/InternalConfigurationPersistenceService.java
@@ -88,6 +88,7 @@ import org.apache.geode.management.internal.configuration.domain.XmlEntity;
 import org.apache.geode.management.internal.configuration.messages.ConfigurationResponse;
 import org.apache.geode.management.internal.configuration.messages.SharedConfigurationStatusResponse;
 import org.apache.geode.management.internal.configuration.utils.XmlUtils;
+import org.apache.geode.security.AuthenticationRequiredException;
 
 public class InternalConfigurationPersistenceService implements ConfigurationPersistenceService {
   private static final Logger logger = LogService.getLogger();
@@ -849,8 +850,16 @@ public class InternalConfigurationPersistenceService implements ConfigurationPer
     return configuration;
   }
 
-  private String getDeployedBy() {
-    Subject subject = cache.getSecurityService().getSubject();
+  String getDeployedBy() {
+    Subject subject = null;
+    try {
+      subject = cache.getSecurityService().getSubject();
+    } catch (AuthenticationRequiredException e) {
+      // ignored. No user logged in for the deployment
+      // this would happen for offline commands like "start locator" and loading the cluster config
+      // from a directory
+      logger.debug("getDeployedBy: no user information is found.", e);
+    }
     return subject == null ? null : subject.getPrincipal().toString();
   }
 
