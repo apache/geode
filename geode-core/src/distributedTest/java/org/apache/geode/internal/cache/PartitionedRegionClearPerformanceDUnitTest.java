df56523c5550df30d71a7b58a691487ca9ec0998
Jianxia Chen <jchen21@apache.org>
2020-06-02 16:17:01 -0700

    Initial commit
    Authored-by: Jianxia Chen <jchen21@apache.org>

---
+++ geode-core/src/distributedTest/java/org/apache/geode/internal/cache/PartitionedRegionClearPerformanceDUnitTest.java
@@ -0,0 +1,115 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.geode.internal.cache;
+
+import static org.apache.geode.test.dunit.VM.getVM;
+
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.stream.LongStream;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.apache.geode.cache.PartitionAttributesFactory;
+import org.apache.geode.cache.Region;
+import org.apache.geode.cache.RegionShortcut;
+import org.apache.geode.cache.client.ClientCacheFactory;
+import org.apache.geode.cache.client.ClientRegionShortcut;
+import org.apache.geode.cache.server.CacheServer;
+import org.apache.geode.test.dunit.DistributedTestUtils;
+import org.apache.geode.test.dunit.VM;
+import org.apache.geode.test.dunit.rules.CacheRule;
+import org.apache.geode.test.dunit.rules.ClientCacheRule;
+import org.apache.geode.test.dunit.rules.DistributedRule;
+
+public class PartitionedRegionClearPerformanceDUnitTest implements Serializable {
+
+  @Rule
+  public DistributedRule distributedRule = new DistributedRule();
+
+  @Rule
+  public CacheRule cacheRule = new CacheRule();
+
+  @Rule
+  public ClientCacheRule clientCacheRule = new ClientCacheRule();
+
+  private static final String REGION_NAME="testRegion";
+
+  private static final long NUM_ENTRIES = 1000;
+
+  VM server1;
+
+  VM server2;
+
+  VM server3;
+
+  VM client;
+
+  @Before
+  public void setUp() {
+    server1 = getVM(0);
+    server2 = getVM(1);
+    server3 = getVM(2);
+    client = getVM(3);
+  }
+
+  @Test
+  public void testPerformance() {
+    createRegionOnServers(RegionShortcut.PARTITION, 2, 113);
+    populationRegion();
+    long start = System.nanoTime();
+    clearRegion();
+    long end = System.nanoTime();
+    System.out.println();
+  }
+
+  private void createRegionOnServers(RegionShortcut shortcut, int redundancy, int numBuckets) {
+    for (VM vm : new VM[]{server1, server2, server3}) {
+      vm.invoke(() -> {
+        createRegion(shortcut, redundancy, numBuckets);
+      });
+    }
+  }
+
+  private void createRegion(RegionShortcut shortcut, int redundancy, int numBuckets)
+      throws IOException {
+    cacheRule.createCache();
+    final CacheServer cacheServer = cacheRule.getCache().addCacheServer();
+    cacheServer.setPort(0);
+    cacheServer.start();
+    cacheRule.getCache().createRegionFactory(shortcut).setPartitionAttributes(new PartitionAttributesFactory<>().setRedundantCopies(redundancy).setTotalNumBuckets(numBuckets).create()).create(REGION_NAME);
+  }
+
+  private void populationRegion() {
+    client.invoke(() -> {
+      final ClientCacheFactory clientCacheFactory = new ClientCacheFactory();
+      clientCacheFactory.addPoolLocator("localhost", DistributedTestUtils.getLocatorPort());
+      clientCacheRule.createClientCache(clientCacheFactory);
+      Region region = clientCacheRule.getClientCache().createClientRegionFactory(ClientRegionShortcut.CACHING_PROXY).create(REGION_NAME);
+      LongStream.range(0, NUM_ENTRIES).forEach(i -> {
+        region.put("key" + i, "value" + i);
+      });
+    });
+  }
+
+  private void clearRegion() {
+    client.invoke(() -> {
+      clientCacheRule.getClientCache().getRegion(REGION_NAME).clear();
+    });
+  }
+
+}
