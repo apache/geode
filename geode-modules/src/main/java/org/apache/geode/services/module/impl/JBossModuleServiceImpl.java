8f30f774f96f4844debfd3cd4a5c1902ca4b36fb
Patrick Johnson <pjohnson@pivotal.io>
2020-06-10 19:51:34 -0700

    GEODE-8190 - Completed GeodeJarModuleFinder to register and load Modules. (#5234)
    JBossModuleServiceImpl can now register and load Modules using ModuleDescriptor. The ModuleDescriptor
    can now contain source paths to be included in the module as code sources or
    have dependencies on other modules.
    
    The same functionality can now be represented/retrieved from the Manifest file
    contained within the jar files.
    
    Amended gradle scripts for geode-common-services and geode-common to now generate
    manifest files containing "Class-Path" dependencies and project module dependencies
    described in the "Dependent-Modules" attribute in the manifest file.

--- geode-modules/src/main/java/org/apache/geode/services/module/impl/JBossModuleServiceImpl.java
+++ geode-modules/src/main/java/org/apache/geode/services/module/impl/JBossModuleServiceImpl.java
@@ -15,162 +15,232 @@
 
 package org.apache.geode.services.module.impl;
 
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
-import java.util.jar.JarFile;
+import java.util.Optional;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.ConcurrentHashMap;
 
 import org.apache.logging.log4j.Logger;
-import org.jboss.modules.DependencySpec;
-import org.jboss.modules.LocalDependencySpecBuilder;
 import org.jboss.modules.Module;
-import org.jboss.modules.ModuleDependencySpecBuilder;
+import org.jboss.modules.ModuleClassLoader;
 import org.jboss.modules.ModuleLoadException;
-import org.jboss.modules.ModuleSpec;
-import org.jboss.modules.PathUtils;
-import org.jboss.modules.ResourceLoader;
-import org.jboss.modules.ResourceLoaderSpec;
-import org.jboss.modules.ResourceLoaders;
-import org.jboss.modules.Version;
 
 import org.apache.geode.annotations.Experimental;
-import org.apache.geode.logging.internal.log4j.api.LogService;
 import org.apache.geode.services.module.ModuleDescriptor;
 import org.apache.geode.services.module.ModuleService;
+import org.apache.geode.services.module.internal.loader.GeodeModuleLoader;
+import org.apache.geode.services.result.ModuleServiceResult;
+import org.apache.geode.services.result.impl.Failure;
+import org.apache.geode.services.result.impl.Success;
 
 /**
- * Implementation of {@link ModuleService} using JBoss-Modules.
+ * Implementation of {@link ModuleService} using JBoss-Modules. This implementation uses
+ * JBossModules
+ * to load classes in a ClassLoader isolated manner.
+ *
+ * @see <a href="https://github.com/jboss-modules/jboss-modules">JBoss Modules</a>
+ *
+ * @since 1.14.0
  */
 @Experimental
 public class JBossModuleServiceImpl implements ModuleService {
 
-  private final Map<String, Module> modules = new HashMap<>();
+  private final Map<String, Module> modules = new ConcurrentHashMap<>();
 
-  private final GeodeModuleLoader moduleLoader = new GeodeModuleLoader();
+  private final GeodeModuleLoader moduleLoader;
 
   private final Logger logger;
 
-  public JBossModuleServiceImpl() {
-    this(LogService.getLogger());
-  }
-
   public JBossModuleServiceImpl(Logger logger) {
     this.logger = logger;
+    this.moduleLoader = new GeodeModuleLoader(logger);
   }
 
-  public Module getModule(String name) {
-    return modules.get(name);
-  }
-
+  /**
+   * {@inheritDoc}
+   */
   @Override
-  public boolean loadModule(ModuleDescriptor moduleDescriptor) {
-    logger.debug(String.format("Beginning to load module %s", moduleDescriptor.getVersionedName()));
+  public ModuleServiceResult<Boolean> registerModule(ModuleDescriptor moduleDescriptor) {
+    return moduleLoader.registerModuleDescriptor(moduleDescriptor);
+  }
 
-    if (modules.containsKey(moduleDescriptor.getVersionedName())) {
-      logger
-          .warn(String.format("Module %s is already loaded.", moduleDescriptor.getVersionedName()));
-      return false;
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ModuleServiceResult<Boolean> loadModule(ModuleDescriptor moduleDescriptor) {
+    if (moduleDescriptor == null) {
+      return Failure.of("Load module failed due to moduleDescriptor being null");
     }
 
-    // Setting up new module.
-    ModuleSpec.Builder builder = ModuleSpec.build(moduleDescriptor.getVersionedName());
-    builder.setVersion(Version.parse(moduleDescriptor.getVersion()));
-    builder.addDependency(new LocalDependencySpecBuilder()
-        .setImportServices(true)
-        .setExport(true)
-        .build());
+    String versionedName = moduleDescriptor.getName();
+    logger.debug(String.format("Beginning to load module %s", versionedName));
 
-    // Add dependencies to the module.
-    moduleDescriptor.getDependedOnModules().forEach(dependency -> {
-      logger.debug(String.format("Adding dependency on module %s", dependency));
-      builder.addDependency(new ModuleDependencySpecBuilder()
-          .setExport(true)
-          .setImportServices(true)
-          .setName(dependency)
-          .build());
-    });
-
-    // Add resources to the module.
-    try {
-      for (String source : moduleDescriptor.getSources()) {
-        logger.debug(String.format("Adding resource %s to module", source));
-        ResourceLoader resourceLoader =
-            ResourceLoaders.createJarResourceLoader(new JarFile(source));
-        builder.addResourceRoot(ResourceLoaderSpec.createResourceLoaderSpec(resourceLoader));
-      }
-    } catch (IOException e) {
-      logger.error(e.getMessage(), e);
-      return false;
+    if (modules.containsKey(versionedName)) {
+      String errorMessage = String.format("Module %s is already loaded.", versionedName);
+      logger.warn(errorMessage);
+      return Failure.of(errorMessage);
     }
 
-    // Add dependency on the system classloader so modules can access classes that aren't in
-    // modules.
-    builder.addDependency(DependencySpec.createSystemDependencySpec(PathUtils.getPathSet(null)));
+    return loadRegisteredModule(moduleDescriptor);
+  }
 
-    // Build and register the ModuleSpec
-    ModuleSpec moduleSpec = builder.create();
-    moduleLoader.addModuleSpec(moduleSpec);
-
-    // Load the module and add it to the modules map.
+  /**
+   * Tries to load a module.
+   *
+   * @param moduleDescriptor the {@link ModuleDescriptor} describing the module.
+   * @return {@link Success} in the event of success and {@link Failure} in case of failure. Failure
+   *         to load a module usually happens due to the {@link ModuleDescriptor} not having been
+   *         registered.
+   *         {@link ModuleDescriptor} can be registered using
+   *         {@link ModuleService#registerModule(ModuleDescriptor)}
+   */
+  private ModuleServiceResult<Boolean> loadRegisteredModule(ModuleDescriptor moduleDescriptor) {
+    String versionedName = moduleDescriptor.getName();
     try {
-      modules.put(moduleDescriptor.getVersionedName(),
-          moduleLoader.loadModule(moduleSpec.getName()));
+      modules.put(versionedName, moduleLoader.loadModule(versionedName));
+      return Success.of(true);
     } catch (ModuleLoadException e) {
       logger.error(e.getMessage(), e);
-      return false;
+      return Failure.of(e.getMessage());
     }
-
-    logger
-        .debug(String.format("Module %s successfully loaded", moduleDescriptor.getVersionedName()));
-
-    return true;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
-  public boolean unloadModule(String moduleName) {
+  public ModuleServiceResult<Boolean> unloadModule(String moduleName) {
     logger.debug(String.format("Unloading module %s", moduleName));
     if (!modules.containsKey(moduleName)) {
-      logger.warn(
-          String.format("Module %s could not be unloaded because it is not loaded", moduleName));
-      return false;
+      String errorMessage =
+          String.format("Module %s could not be unloaded because it is not loaded", moduleName);
+      logger.warn(errorMessage);
+      return Failure.of(errorMessage);
     }
 
-    if (moduleLoader.unloadModule(modules.get(moduleName))) {
+    ModuleServiceResult<Boolean> unloadModuleResult =
+        moduleLoader.unloadModule(modules.get(moduleName));
+    if (unloadModuleResult.isSuccessful()) {
       modules.remove(moduleName);
       logger.debug(String.format("Module %s was successfully unloaded", moduleName));
-      return true;
     }
 
-    logger.debug(String.format("Module %s could not be unloaded", moduleName));
-    return false;
+    return unloadModuleResult;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
-  public <T> List<T> loadService(Class<T> service) {
-    List<T> serviceImpls = new LinkedList<>();
+  public <T> ModuleServiceResult<Map<String, Set<T>>> loadService(Class<T> service) {
+    Map<String, Set<T>> serviceImpls = new HashMap<>();
 
     // Iterate over all the modules looking for implementations of service.
     modules.values().forEach((module) -> {
-      module.loadService(service).forEach((impl) -> {
-        // Check if class is already loaded.
-        // Modules with dependencies can cause duplicates without this check.
-        boolean duplicate = false;
-        for (T serviceImpl : serviceImpls) {
-          if (serviceImpl.getClass() == impl.getClass()) {
-            duplicate = true;
-            break;
-          }
-        }
-
-        // If impl is not a duplicate, add it to the list to return.
-        if (!duplicate) {
-          serviceImpls.add(impl);
-        }
+      module.loadService(service).forEach((serviceImpl) -> {
+        String moduleName = ((ModuleClassLoader) serviceImpl.getClass().getClassLoader()).getName();
+        Set<T> listOfServices = Optional.ofNullable(serviceImpls.get(moduleName))
+            .orElseGet(() -> createTreeSetWithClassLoaderComparator());
+        listOfServices.add(serviceImpl);
+        serviceImpls.put(moduleName, listOfServices);
       });
     });
 
-    return serviceImpls;
+    return Success.of(serviceImpls);
+  }
+
+  /**
+   *
+   * Create a Set<T> with a comparator on classname and ClassLoader to remove duplicates.
+   *
+   * @return empty set of type T
+   */
+  private <T> Set<T> createTreeSetWithClassLoaderComparator() {
+    return new TreeSet<>((o1, o2) -> {
+      if (o1 == null && o2 == null) {
+        return 0;
+      }
+      if (o1 == null) {
+        return 1;
+      }
+      if (o2 == null) {
+        return -1;
+      }
+      int classLoaderCompare = ((ModuleClassLoader) o1.getClass().getClassLoader()).getName()
+          .compareTo(((ModuleClassLoader) o2.getClass().getClassLoader()).getName());
+      if (classLoaderCompare == 0) {
+        return o1.getClass().getName().compareTo(o2.getClass().getName());
+      }
+      return classLoaderCompare;
+    });
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ModuleServiceResult<Class<?>> loadClass(String className,
+      ModuleDescriptor moduleDescriptor) {
+    Module module = modules.get(moduleDescriptor.getName());
+
+    if (module != null) {
+      return loadClassFromModule(className, module);
+    }
+    if (moduleLoader.hasRegisteredModule(moduleDescriptor.getName())) {
+      try {
+        module = moduleLoader.loadModule(moduleDescriptor.getName());
+        return loadClassFromModule(className, module);
+      } catch (ModuleLoadException e) {
+        logger.error(e);
+        return Failure.of(e.getMessage());
+      }
+    }
+    return Failure
+        .of(String.format("Module named: %s could be found. Please ensure it is registered",
+            moduleDescriptor.getName()));
+  }
+
+  /**
+   * Tries to load a class for name from a given module.
+   *
+   * @param className the classname that is to be loaded
+   * @param module the module from which the class is to be loaded from.
+   * @return {@link Success} with result type {@literal Class} or {@link Failure} with
+   *         {@literal String}
+   *         error message describing the reason for failure.
+   */
+  private ModuleServiceResult<Class<?>> loadClassFromModule(String className, Module module) {
+    try {
+      Class<?> loadedClass = module.getClassLoader().loadClass(className);
+      return Success.of(loadedClass);
+    } catch (ClassNotFoundException e) {
+      String errorMessage =
+          String.format("Could not find class for name: %s in module: %s", className,
+              module.getName());
+      logger.debug(errorMessage);
+      return Failure.of(errorMessage);
+    }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ModuleServiceResult<Map<String, Class<?>>> loadClass(String className) {
+    Map<String, Class<?>> classes = new HashMap<>();
+    modules.values().forEach((module) -> {
+      try {
+        Class<?> loadedClass = module.getClassLoader().loadClass(className);
+        classes.put(module.getName(), loadedClass);
+      } catch (ClassNotFoundException e) {
+        logger.debug(String.format("Could not find class for name: %s in module: %s", className,
+            module.getName()));
+      }
+    });
+
+    return Success.of(classes);
   }
 }
