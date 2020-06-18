14c087c0f6da7f9cfeffe49acad42ef3ad70feaa
Jinmei Liao <jiliao@pivotal.io>
2020-06-18 13:45:55 -0700

    review udpates

--- geode-core/src/upgradeTest/java/org/apache/geode/internal/cache/rollingupgrade/RollingUpgradeWithGfshDUnitTest.java
+++ geode-core/src/upgradeTest/java/org/apache/geode/internal/cache/rollingupgrade/RollingUpgradeWithGfshDUnitTest.java
@@ -14,21 +14,6 @@
  */
 package org.apache.geode.internal.cache.rollingupgrade;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
- * agreements. See the NOTICE file distributed with this work for additional information regarding
- * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License. You may obtain a
- * copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the License
- * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
- * or implied. See the License for the specific language governing permissions and limitations under
- * the License.
- */
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
@@ -139,14 +124,14 @@ public class RollingUpgradeWithGfshDUnitTest {
     return "deploy --dir=" + jarsDir.getAbsolutePath();
   }
 
-  protected String startServerCommand(String name, int port, int connectedLocatorPort) {
+  private String startServerCommand(String name, int port, int connectedLocatorPort) {
     String command = "start server --name=" + name
         + " --server-port=" + port
         + " --locators=localhost[" + connectedLocatorPort + "]";
     return command;
   }
 
-  protected String startLocatorCommand(String name, int port, int jmxPort,
+  private String startLocatorCommand(String name, int port, int jmxPort,
       int connectedLocatorPort) {
     String command = "start locator --name=" + name
         + " --port=" + port
