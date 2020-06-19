16e2c7baec63a15fe2e56060a14e4c83ec963a40
Murtuza Boxwala <mboxwala@pivotal.io>
2020-06-19 13:18:24 -0400

    GEODE-8280: Return correct Redis AUTH errors (#5276)

--- geode-redis/src/main/java/org/apache/geode/redis/internal/RedisConstants.java
+++ geode-redis/src/main/java/org/apache/geode/redis/internal/RedisConstants.java
@@ -37,10 +37,10 @@ public class RedisConstants {
   public static final String ERROR_OUT_OF_RANGE = "The number provided is out of range";
   public static final String ERROR_NAN_INF_INCR = "increment would produce NaN or Infinity";
   public static final String ERROR_NO_PASS =
-      "Attempting to authenticate when no password has been set";
+      "Client sent AUTH, but no password is set";
   public static final String ERROR_INVALID_PWD =
-      "Attemping to authenticate with an invalid password";
-  public static final String ERROR_NOT_AUTH = "Must authenticate before sending any requests";
+      "invalid password";
+  public static final String ERROR_NOT_AUTH = "NOAUTH Authentication required.";
   public static final String ERROR_WRONG_TYPE =
       "Operation against a key holding the wrong kind of value";
   public static final String ERROR_NOT_INTEGER = "value is not an integer or out of range";
