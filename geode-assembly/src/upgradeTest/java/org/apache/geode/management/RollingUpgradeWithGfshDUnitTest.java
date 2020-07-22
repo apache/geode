a8295bce3a0ebf19b6e52ee63da63aaf470706f0
Jinmei Liao <jiliao@pivotal.io>
2020-07-22 10:10:39 -0700

    GEODE-8316: move RollingUpgradeWithGfshDUnitTest to geode-assembly since it needs to do installDist first.

--- geode-assembly/src/upgradeTest/java/org/apache/geode/management/RollingUpgradeWithGfshDUnitTest.java
+++ geode-assembly/src/upgradeTest/java/org/apache/geode/management/RollingUpgradeWithGfshDUnitTest.java
@@ -12,7 +12,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.apache.geode.internal.cache.rollingupgrade;
+package org.apache.geode.management;
 
 import static org.apache.geode.test.junit.rules.gfsh.GfshRule.startLocatorCommand;
 import static org.apache.geode.test.junit.rules.gfsh.GfshRule.startServerCommand;
