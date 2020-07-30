e30b0165e3554d7a6e24a647c72291eda465a437
Sarah <sabbey@pivotal.io>
2020-07-30 12:31:45 -0400

    errz

--- ci/scripts/execute_redis_tests.sh
+++ ci/scripts/execute_redis_tests.sh
@@ -17,6 +17,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 cd ..
 git clone https://github.com/prettyClouds/redis.git
 pushd redis
