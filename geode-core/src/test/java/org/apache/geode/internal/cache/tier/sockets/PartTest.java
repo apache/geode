a6332c4576e82b5620d53ed78b768b2c47570d55
agingade <agingade@pivotal.io>
2020-08-07 10:12:27 -0700

    GEODE-8394: Rewind the message Part on command failure (#5424)
    GEODE-8394: Rewind the message Part on failure
    
    Co-authored-by: anilkumar gingade <anil@anilg.local>
    (cherry picked from commit 83d1e28a953b7d73e7f499f9013540bedd0bd472)

--- geode-core/src/test/java/org/apache/geode/internal/cache/tier/sockets/PartTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/tier/sockets/PartTest.java
@@ -16,16 +16,24 @@ package org.apache.geode.internal.cache.tier.sockets;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.io.EOFException;
 import java.io.OutputStream;
+import java.nio.BufferOverflowException;
 import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
 
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import org.apache.geode.internal.HeapDataOutputStream;
 import org.apache.geode.test.junit.categories.ClientServerTest;
 
 @Category({ClientServerTest.class})
@@ -76,4 +84,101 @@ public class PartTest {
     assertThatThrownBy(() -> part.getCachedString())
         .hasMessageContaining("expected String part to be of type BYTE, part =");
   }
+
+  @Test
+  public void writeToOutputStreamResetsPartOnException() throws Exception {
+    HeapDataOutputStream heapDataOutputStream = mock(HeapDataOutputStream.class);
+    when(heapDataOutputStream.size()).thenReturn(1000);
+    OutputStream outputStream = mock(OutputStream.class);
+    ByteBuffer byteBuffer = mock(ByteBuffer.class);
+    doThrow(new EOFException("test")).when(heapDataOutputStream).sendTo(eq(outputStream),
+        eq(byteBuffer));
+
+    Part part = new Part();
+    part.setPartState(heapDataOutputStream, false);
+
+    Throwable thrown = catchThrowable(() -> part.writeTo(outputStream, byteBuffer));
+
+    assertThat(thrown).isInstanceOf(EOFException.class);
+    verify(heapDataOutputStream, times(1)).rewind();
+  }
+
+  @Test
+  public void writeToOutputStreamResetsPartOnSuccess() throws Exception {
+    HeapDataOutputStream heapDataOutputStream = mock(HeapDataOutputStream.class);
+    when(heapDataOutputStream.size()).thenReturn(1000);
+    OutputStream outputStream = mock(OutputStream.class);
+    ByteBuffer byteBuffer = mock(ByteBuffer.class);
+
+    Part part = new Part();
+    part.setPartState(heapDataOutputStream, false);
+
+    part.writeTo(outputStream, byteBuffer);
+
+    verify(heapDataOutputStream, times(1)).rewind();
+  }
+
+  @Test
+  public void writeToByteBufferResetsPartOnException() throws Exception {
+    HeapDataOutputStream heapDataOutputStream = mock(HeapDataOutputStream.class);
+    when(heapDataOutputStream.size()).thenReturn(1000);
+    ByteBuffer byteBuffer = mock(ByteBuffer.class);
+    doThrow(new BufferOverflowException()).when(heapDataOutputStream).sendTo(eq(byteBuffer));
+
+    Part part = new Part();
+    part.setPartState(heapDataOutputStream, false);
+
+    Throwable thrown = catchThrowable(() -> part.writeTo(byteBuffer));
+
+    assertThat(thrown).isInstanceOf(BufferOverflowException.class);
+    verify(heapDataOutputStream, times(1)).rewind();
+  }
+
+  @Test
+  public void writeToByteBufferResetsPartOnSuccess() throws Exception {
+    HeapDataOutputStream heapDataOutputStream = mock(HeapDataOutputStream.class);
+    when(heapDataOutputStream.size()).thenReturn(1000);
+    ByteBuffer byteBuffer = mock(ByteBuffer.class);
+
+    Part part = new Part();
+    part.setPartState(heapDataOutputStream, false);
+
+    part.writeTo(byteBuffer);
+
+    verify(heapDataOutputStream, times(1)).rewind();
+  }
+
+  @Test
+  public void writeToSocketChannelResetsPartOnException() throws Exception {
+    HeapDataOutputStream heapDataOutputStream = mock(HeapDataOutputStream.class);
+    when(heapDataOutputStream.size()).thenReturn(1000);
+    SocketChannel socketChannel = mock(SocketChannel.class);
+    ByteBuffer byteBuffer = mock(ByteBuffer.class);
+    doThrow(new BufferOverflowException()).when(heapDataOutputStream).sendTo(eq(socketChannel),
+        eq(byteBuffer));
+
+    Part part = new Part();
+    part.setPartState(heapDataOutputStream, false);
+
+    Throwable thrown = catchThrowable(() -> part.writeTo(socketChannel, byteBuffer));
+
+    assertThat(thrown).isInstanceOf(BufferOverflowException.class);
+    verify(heapDataOutputStream, times(1)).rewind();
+  }
+
+  @Test
+  public void writeToSocketChannelResetsPartOnSuccess() throws Exception {
+    HeapDataOutputStream heapDataOutputStream = mock(HeapDataOutputStream.class);
+    when(heapDataOutputStream.size()).thenReturn(1000);
+    SocketChannel socketChannel = mock(SocketChannel.class);
+    ByteBuffer byteBuffer = mock(ByteBuffer.class);
+
+    Part part = new Part();
+    part.setPartState(heapDataOutputStream, false);
+
+    part.writeTo(socketChannel, byteBuffer);
+
+    verify(heapDataOutputStream, times(1)).rewind();
+  }
+
 }
