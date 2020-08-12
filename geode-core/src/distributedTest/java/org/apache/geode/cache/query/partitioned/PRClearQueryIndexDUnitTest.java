425f11a5292417688e7a7ce74bfad01ff4aadcac
Jinmei Liao <jiliao@pivotal.io>
2020-08-12 15:12:11 -0700

    add more tests

--- geode-core/src/distributedTest/java/org/apache/geode/cache/query/partitioned/PRClearQueryIndexDUnitTest.java
+++ geode-core/src/distributedTest/java/org/apache/geode/cache/query/partitioned/PRClearQueryIndexDUnitTest.java
@@ -17,6 +17,7 @@ package org.apache.geode.cache.query.partitioned;
 
 import static org.apache.geode.distributed.ConfigurationProperties.SERIALIZABLE_OBJECT_FILTER;
 import static org.apache.geode.test.awaitility.GeodeAwaitility.await;
+import static org.apache.geode.test.junit.rules.VMProvider.invokeInEveryMember;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.concurrent.TimeUnit;
@@ -39,6 +40,7 @@ import org.apache.geode.cache.query.QueryService;
 import org.apache.geode.cache.query.SelectResults;
 import org.apache.geode.cache.query.data.City;
 import org.apache.geode.internal.cache.InternalCache;
+import org.apache.geode.test.dunit.AsyncInvocation;
 import org.apache.geode.test.dunit.rules.ClusterStartupRule;
 import org.apache.geode.test.dunit.rules.MemberVM;
 import org.apache.geode.test.junit.rules.ClientCacheRule;
@@ -56,6 +58,7 @@ public class PRClearQueryIndexDUnitTest {
   public ClientCacheRule clientCacheRule = new ClientCacheRule();
 
   private ClientCache clientCache;
+  private Region cities;
 
   // class test setup. set up the servers, regions and indexes on the servers
   @BeforeClass
@@ -75,13 +78,15 @@ public class PRClearQueryIndexDUnitTest {
       QueryService queryService = cache.getQueryService();
       queryService.createKeyIndex("cityId", "c.id", "/cities c");
       queryService.createIndex("cityName", "c.name", "/cities c");
-      assertThat(cache.getQueryService().getIndexes(region)).hasSize(2);
+      assertThat(cache.getQueryService().getIndexes(region))
+          .extracting(Index::getName).containsExactlyInAnyOrder("cityId", "cityName");
     });
 
     server2.invoke(() -> {
       Cache cache = ClusterStartupRule.getCache();
       Region region = cache.getRegion("cities");
-      assertThat(cache.getQueryService().getIndexes(region)).hasSize(2);
+      assertThat(cache.getQueryService().getIndexes(region))
+          .extracting(Index::getName).containsExactlyInAnyOrder("cityId", "cityName");
     });
   }
 
@@ -90,19 +95,85 @@ public class PRClearQueryIndexDUnitTest {
   public void before() throws Exception {
     int locatorPort = ClusterStartupRule.getDUnitLocatorPort();
     clientCache = clientCacheRule.withLocatorConnection(locatorPort).createCache();
+    cities = clientCacheRule.createProxyRegion("cities");
+  }
+
+  @Test
+  public void clearOnEmptyRegion() throws Exception {
+    cities.clear();
+    invokeInEveryMember(() -> {
+      verifyIndexesAfterClear();
+    }, server1, server2);
+
+    IntStream.range(0, 10).forEach(i -> cities.put(i, new City(i)));
+    cities.clear();
+    invokeInEveryMember(() -> {
+      verifyIndexesAfterClear();
+    }, server1, server2);
+  }
+
+  @Test
+  public void createAndRemoveIndexWhileClear() throws Exception {
+    IntStream.range(0, 20).forEach(i -> cities.put(i, new City(i)));
+
+    // create index while clear
+    AsyncInvocation createIndex = server1.invokeAsync("create index", () -> {
+      Cache cache = ClusterStartupRule.getCache();
+      QueryService queryService = cache.getQueryService();
+      Index cityZip = queryService.createIndex("cityZip", "c.zip", "/cities c");
+      assertThat(cityZip).isNotNull();
+    });
+
+    cities.clear();
+    createIndex.await();
+
+    invokeInEveryMember(() -> {
+      verifyIndexesAfterClear();
+    }, server1, server2);
+
+    QueryService queryService = clientCache.getQueryService();
+    Query query =
+        queryService.newQuery("select * from /cities c where c.zip < " + (City.ZIP_START + 10));
+    assertThat(((SelectResults) query.execute()).size()).isEqualTo(0);
+
+    IntStream.range(0, 10).forEach(i -> cities.put(i, new City(i)));
+    assertThat(((SelectResults) query.execute()).size()).isEqualTo(10);
+
+    // remove index while clear
+    // removeIndex has to be invoked on each server. It's not distributed
+    AsyncInvocation removeIndex1 = server1.invokeAsync("remove index",
+        PRClearQueryIndexDUnitTest::removeIndex);
+    AsyncInvocation removeIndex2 = server2.invokeAsync("remove index",
+        PRClearQueryIndexDUnitTest::removeIndex);
+
+    cities.clear();
+    removeIndex1.await();
+    removeIndex2.await();
+
+    // make sure removeIndex and clear operations are successful
+    invokeInEveryMember(() -> {
+      InternalCache internalCache = ClusterStartupRule.getCache();
+      QueryService qs = internalCache.getQueryService();
+      Region region = internalCache.getRegion("cities");
+      assertThat(region.size()).isEqualTo(0);
+      // verify only 2 indexes created in the beginning of the tests exist
+      assertThat(qs.getIndexes(region)).extracting(Index::getName)
+          .containsExactlyInAnyOrder("cityId", "cityName");
+    }, server1, server2);
+  }
+
+  private static void removeIndex() {
+    Cache cache = ClusterStartupRule.getCache();
+    QueryService qs = cache.getQueryService();
+    Region<Object, Object> region = cache.getRegion("cities");
+    Index cityZip = qs.getIndex(region, "cityZip");
+    qs.removeIndex(cityZip);
   }
 
   @Test
   public void verifyQuerySucceedsAfterClear() throws Exception {
-    Region cities = clientCacheRule.createProxyRegion("cities");
     // put in some data
     IntStream.range(0, 100).forEach(i -> cities.put(i, new City(i)));
-
-    MemberVM.invokeInEveryMember(() -> {
-      InternalCache internalCache = ClusterStartupRule.getCache();
-      Region region = internalCache.getRegion("cities");
-      assertThat(region.size()).isEqualTo(100);
-    }, server1, server2);
 
     QueryService queryService = clientCache.getQueryService();
     Query query = queryService.newQuery(MUMBAI_QUERY);
@@ -111,28 +182,28 @@ public class PRClearQueryIndexDUnitTest {
     assertThat(((SelectResults) query2.execute()).size()).isEqualTo(1);
 
     cities.clear();
-    MemberVM.invokeInEveryMember(() -> {
-      InternalCache internalCache = ClusterStartupRule.getCache();
-      Region region = internalCache.getRegion("cities");
-      assertThat(region.size()).isEqualTo(0);
+    invokeInEveryMember(() -> {
+      verifyIndexesAfterClear();
     }, server1, server2);
 
     assertThat(((SelectResults) query.execute()).size()).isEqualTo(0);
     assertThat(((SelectResults) query2.execute()).size()).isEqualTo(0);
+  }
 
-    MemberVM.invokeInEveryMember(() -> {
-      InternalCache internalCache = ClusterStartupRule.getCache();
-      QueryService qs = internalCache.getQueryService();
-      Region region = internalCache.getRegion("cities");
-      Index cityId = qs.getIndex(region, "cityId");
-      IndexStatistics statistics = cityId.getStatistics();
-      assertThat(statistics.getNumberOfKeys()).isEqualTo(0);
-      assertThat(statistics.getNumberOfValues()).isEqualTo(0);
-      Index cityName = qs.getIndex(region, "cityName");
-      IndexStatistics statistics1 = cityName.getStatistics();
-      assertThat(statistics1.getNumberOfKeys()).isEqualTo(0);
-      assertThat(statistics1.getNumberOfValues()).isEqualTo(0);
-    }, server1, server2);
+  private static void verifyIndexesAfterClear() {
+    InternalCache internalCache = ClusterStartupRule.getCache();
+    QueryService qs = internalCache.getQueryService();
+    Region region = internalCache.getRegion("cities");
+    assertThat(region.size()).isEqualTo(0);
+    Index cityId = qs.getIndex(region, "cityId");
+    IndexStatistics cityIdStats = cityId.getStatistics();
+    Index cityName = qs.getIndex(region, "cityName");
+    IndexStatistics cityNameStats = cityName.getStatistics();
+
+    assertThat(cityIdStats.getNumberOfKeys()).isEqualTo(0);
+    assertThat(cityIdStats.getNumberOfValues()).isEqualTo(0);
+    assertThat(cityNameStats.getNumberOfKeys()).isEqualTo(0);
+    assertThat(cityNameStats.getNumberOfValues()).isEqualTo(0);
   }
 
   @Test
@@ -153,5 +224,4 @@ public class PRClearQueryIndexDUnitTest {
       assertThat(((SelectResults) query2.execute()).size()).isEqualTo(0);
     });
   }
-
 }
