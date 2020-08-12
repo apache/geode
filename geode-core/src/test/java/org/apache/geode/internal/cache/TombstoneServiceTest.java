975cc84c5670c41b2b0d23f7cef4928f989bfaaf
Mark Hanson <hansonm@vmware.com>
2020-08-12 14:11:44 -0700

    GEODE-8422: Cleanup unit test.

--- geode-core/src/test/java/org/apache/geode/internal/cache/TombstoneServiceTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/TombstoneServiceTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.when;
 
 import java.util.concurrent.ExecutorService;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -18,36 +17,42 @@ import org.apache.geode.internal.cache.versions.RegionVersionVector;
 import org.apache.geode.internal.cache.versions.VersionTag;
 
 public class TombstoneServiceTest {
-  CacheTime cacheTime = mock(CacheTime.class);
-  CachePerfStats stats = mock(CachePerfStats.class);
-  CancelCriterion cancelCriterion = mock(CancelCriterion.class);
-  ExecutorService executor = mock(ExecutorService.class);
-  RegionMap regionMap = mock(RegionMap.class);
-  RegionEntry entry = mock(RegionEntry.class);
-  DistributedRegion region = mock(DistributedRegion.class);
-  VersionTag destroyedVersion = mock(VersionTag.class);
+  CacheTime cacheTime;
+  CachePerfStats stats;
+  CancelCriterion cancelCriterion;
+  ExecutorService executor;
+  RegionMap regionMap;
+  RegionEntry entry;
+  DistributedRegion region;
+  VersionTag destroyedVersion;
+  private TombstoneService.ReplicateTombstoneSweeper replicateTombstoneSweeper;
+  private TombstoneService.Tombstone tombstone;
+
 
   @Before
-  public void setUp() throws Exception {}
-
-  @After
-  public void tearDown() throws Exception {}
+  public void setUp() throws Exception {
+    cacheTime = mock(CacheTime.class);
+    stats = mock(CachePerfStats.class);
+    cancelCriterion = mock(CancelCriterion.class);
+    executor = mock(ExecutorService.class);
+    regionMap = mock(RegionMap.class);
+    entry = mock(RegionEntry.class);
+    region = mock(DistributedRegion.class);
+    destroyedVersion = mock(VersionTag.class);
+    replicateTombstoneSweeper = new TombstoneService.ReplicateTombstoneSweeper(cacheTime, stats,
+        cancelCriterion, executor);
+    tombstone = new TombstoneService.Tombstone(entry, region, destroyedVersion);
+    tombstone.entry = entry;
+  }
 
   @Test
   public void validateThatRemoveIsNotCalledOnTombstoneInRegionThatIsNotInitialized() {
-
     when(region.isInitialized()).thenReturn(false);
     when(region.getRegionMap()).thenReturn(regionMap);
-    TombstoneService.ReplicateTombstoneSweeper replicateTombstoneSweeper =
-        new TombstoneService.ReplicateTombstoneSweeper(cacheTime, stats,
-            cancelCriterion, executor);
-    TombstoneService.Tombstone tombstone =
-        new TombstoneService.Tombstone(entry, region, destroyedVersion);
-    tombstone.entry = entry;
 
     replicateTombstoneSweeper.expireTombstone(tombstone);
     replicateTombstoneSweeper.expireBatch();
-    verify(regionMap, Mockito.never()).removeTombstone(tombstone.entry, destroyedVersion, false,
+    verify(regionMap, Mockito.never()).removeTombstone(tombstone.entry, tombstone, false,
         true);
   }
 
@@ -61,19 +66,10 @@ public class TombstoneServiceTest {
     when(region.getDataPolicy()).thenReturn(DataPolicy.PERSISTENT_REPLICATE);
     when(region.getDiskRegion()).thenReturn(mock(DiskRegion.class));
 
-    TombstoneService.ReplicateTombstoneSweeper replicateTombstoneSweeper =
-        new TombstoneService.ReplicateTombstoneSweeper(cacheTime, stats,
-            cancelCriterion, executor);
-    TombstoneService.Tombstone tombstone =
-        new TombstoneService.Tombstone(entry, region, destroyedVersion);
-    tombstone.entry = entry;
 
     replicateTombstoneSweeper.expireTombstone(tombstone);
     replicateTombstoneSweeper.expireBatch();
-    verify(region, Mockito.atLeastOnce()).getVersionVector();
-    verify(regionVersionVector, Mockito.atLeastOnce()).recordGCVersion(tombstone.getMemberID(),
-        tombstone.getRegionVersion());
-
-
+    verify(regionMap, Mockito.times(1)).removeTombstone(tombstone.entry, tombstone, false,
+        true);
   }
 }
