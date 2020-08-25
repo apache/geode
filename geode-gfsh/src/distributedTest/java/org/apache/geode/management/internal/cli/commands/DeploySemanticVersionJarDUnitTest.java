f9290046c893940f75c2a76aa97fb6984abd1bc4
Jacob Barrett <jbarrett@pivotal.io>
2020-08-25 10:30:44 -0700

    Fixes jar filtering issue.

--- geode-gfsh/src/distributedTest/java/org/apache/geode/management/internal/cli/commands/DeploySemanticVersionJarDUnitTest.java
+++ geode-gfsh/src/distributedTest/java/org/apache/geode/management/internal/cli/commands/DeploySemanticVersionJarDUnitTest.java
@@ -18,6 +18,7 @@ package org.apache.geode.management.internal.cli.commands;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
+import java.io.FilenameFilter;
 import java.nio.file.Paths;
 import java.util.Set;
 
@@ -45,6 +46,7 @@ public class DeploySemanticVersionJarDUnitTest {
   private static File stagedDir;
   private static File semanticJarVersion0, semanticJarVersion1, semanticJarVersion2,
       semanticJarVersion0b, semanticJarVersion0c;
+  public static final FilenameFilter JAR_FILES = (d, f) -> f.endsWith(".jar");
 
   @BeforeClass
   public static void beforeClass() throws Exception {
@@ -107,12 +109,12 @@ public class DeploySemanticVersionJarDUnitTest {
       assertThat(deployedJars).containsExactly("def-1.1.jar");
     }, locator0, locator1);
 
-    assertThat(server2.getWorkingDir().list())
+    assertThat(server2.getWorkingDir().list(JAR_FILES))
         .containsExactlyInAnyOrder("def-1.0.v1.jar", "def-1.1.v2.jar");
     server2.invoke(() -> verifyLoadAndHasVersion("def", "jddunit.function.Def", "version2"));
 
     MemberVM server3 = cluster.startServerVM(3, locator0.getPort(), locator1.getPort());
-    assertThat(server3.getWorkingDir().list())
+    assertThat(server3.getWorkingDir().list(JAR_FILES))
         .containsExactlyInAnyOrder("def-1.1.v1.jar");
     server3.invoke(() -> verifyLoadAndHasVersion("def", "jddunit.function.Def", "version2"));
 
@@ -132,7 +134,7 @@ public class DeploySemanticVersionJarDUnitTest {
 
     // restart server3 and make sure it will get the def.1.2
     server3 = cluster.startServerVM(3, locator0.getPort(), locator1.getPort());
-    assertThat(server3.getWorkingDir().list()).containsExactly("def-1.2.v1.jar");
+    assertThat(server3.getWorkingDir().list(JAR_FILES)).containsExactly("def-1.2.v1.jar");
     server3.invoke(() -> verifyLoadAndHasVersion("def", "jddunit.function.Def", "version3"));
 
     // redeploy def-1.2 would not result in error but report already deployed
@@ -169,7 +171,7 @@ public class DeploySemanticVersionJarDUnitTest {
       Set<String> deployedJars = getDeployedJarsFromClusterConfig();
       assertThat(deployedJars).containsExactly("def.jar");
     }, locator0, locator1);
-    assertThat(server2.getWorkingDir().list())
+    assertThat(server2.getWorkingDir().list(JAR_FILES))
         .containsExactlyInAnyOrder("def-1.0.v1.jar", "def.v2.jar");
     server2.invoke(() -> verifyLoadAndHasVersion("def", "jddunit.function.Def", "version1c"));
 
@@ -193,7 +195,7 @@ public class DeploySemanticVersionJarDUnitTest {
       Set<String> deployedJars = getDeployedJarsFromClusterConfig();
       assertThat(deployedJars).isEmpty();
     }, locator0, locator1);
-    assertThat(server2.getWorkingDir().list()).isEmpty();
+    assertThat(server2.getWorkingDir().list(JAR_FILES)).isEmpty();
   }
 
   static Set<String> getDeployedJarsFromClusterConfig() {
