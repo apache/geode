ceacdbf0d06a9d8f2868eace7d86424a48e6e435
Jinmei Liao <jiliao@pivotal.io>
2020-08-26 10:39:16 -0700

    GEODE-8447: QueryResultFormatter should show dates in localized strings (#5469)
    (cherry picked from commit 6ca548f8f81a60853d4b49def20d6eb69432bf24)
    
    * support java.util.Date, java.sql.Date and java.time.* date objects in the formatter

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -90,6 +90,7 @@ class DependencyConstraints implements Plugin<Project> {
         api(group: 'cglib', name: 'cglib', version: get('cglib.version'))
         api(group: 'com.carrotsearch.randomizedtesting', name: 'randomizedtesting-runner', version: '2.7.3')
         api(group: 'com.fasterxml.jackson.module', name: 'jackson-module-scala_2.10', version: '2.10.0')
+        api(group: 'com.fasterxml.jackson.datatype', name: 'jackson-datatype-jsr310', version: '2.11.2')
         api(group: 'com.github.davidmoten', name: 'geo', version: '0.7.1')
         api(group: 'com.github.stefanbirkner', name: 'system-rules', version: '1.19.0')
         api(group: 'com.github.stephenc.findbugs', name: 'findbugs-annotations', version: '1.3.9-1')
