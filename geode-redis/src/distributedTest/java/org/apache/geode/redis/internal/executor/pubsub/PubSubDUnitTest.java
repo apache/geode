2c893941d8c31535822d5f66ae34d5c9842a39ae
Sarah <sabbey@pivotal.io>
2020-09-17 09:44:32 -0400

    reverts thread change in pubsub test

--- geode-redis/src/distributedTest/java/org/apache/geode/redis/internal/executor/pubsub/PubSubDUnitTest.java
+++ geode-redis/src/distributedTest/java/org/apache/geode/redis/internal/executor/pubsub/PubSubDUnitTest.java
@@ -388,7 +388,7 @@ public class PubSubDUnitTest {
 
   @Test
   public void testPubSubWithMoreSubscribersThanNettyWorkerThreads() throws Exception {
-    int CLIENT_COUNT = 200;
+    int CLIENT_COUNT = 1000;
     String CHANNEL_NAME = "best_channel_ever";
 
     List<Jedis> clients = new ArrayList<>();
