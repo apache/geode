5ba878744c3c3e76b81a0e566558aa60871ac354
Owen Nichols <onichols@apache.org>
2020-09-21 01:20:36 -0700

    Bump spring-security from 5.3.2.RELEASE to 5.4.0

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -241,7 +241,7 @@ class DependencyConstraints implements Plugin<Project> {
       entry('selenium-support')
     }
 
-    dependencySet(group: 'org.springframework.security', version: '5.3.2.RELEASE') {
+    dependencySet(group: 'org.springframework.security', version: '5.4.0') {
       entry('spring-security-config')
       entry('spring-security-core')
       entry('spring-security-ldap')
