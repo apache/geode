3cd6bd59f89eddd7b6091d0a2bc1c8dd89f10d34
mkevo <mario.kevo@est.tech>
2020-09-21 10:10:58 +0200

    update some 3pp

--- geode-assembly/src/distributedTest/java/org/apache/geode/session/tests/GenericAppServerInstall.java
+++ geode-assembly/src/distributedTest/java/org/apache/geode/session/tests/GenericAppServerInstall.java
@@ -33,7 +33,7 @@ import java.util.function.IntSupplier;
  * specific code outside of the {@link GenericAppServerVersion}.
  */
 public class GenericAppServerInstall extends ContainerInstall {
-  private static final String JETTY_VERSION = "9.4.21.v20190926";
+  private static final String JETTY_VERSION = "9.4.24.v20191120";
 
   /**
    * Get the version number, download URL, and container name of a generic app server using
