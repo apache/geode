277344141d7f2ad66f8624e07c96066179b058ce
Owen Nichols <onichols@apache.org>
2020-09-21 03:16:18 -0700

    Bump cargo-core-uberjar from 1.7.11 to 1.8.1

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -151,7 +151,7 @@ class DependencyConstraints implements Plugin<Project> {
         api(group: 'org.assertj', name: 'assertj-core', version: '3.15.0')
         api(group: 'org.awaitility', name: 'awaitility', version: '4.0.3')
         api(group: 'org.bouncycastle', name: 'bcpkix-jdk15on', version: '1.64')
-        api(group: 'org.codehaus.cargo', name: 'cargo-core-uberjar', version: '1.7.11')
+        api(group: 'org.codehaus.cargo', name: 'cargo-core-uberjar', version: '1.8.1')
         api(group: 'org.eclipse.jetty', name: 'jetty-server', version: get('jetty.version'))
         api(group: 'org.eclipse.jetty', name: 'jetty-webapp', version: get('jetty.version'))
         api(group: 'org.eclipse.persistence', name: 'javax.persistence', version: '2.2.1')
