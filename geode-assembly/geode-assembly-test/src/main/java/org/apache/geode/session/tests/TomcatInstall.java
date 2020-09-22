fc5008d35db2b7022d8567c33ddda872d394c8f3
Owen Nichols <onichols@apache.org>
2020-09-21 19:13:48 -0700

    Bump tomcat-catalina from 7.0.99 to 7.0.106

--- geode-assembly/geode-assembly-test/src/main/java/org/apache/geode/session/tests/TomcatInstall.java
+++ geode-assembly/geode-assembly-test/src/main/java/org/apache/geode/session/tests/TomcatInstall.java
@@ -41,7 +41,7 @@ public class TomcatInstall extends ContainerInstall {
    */
   public enum TomcatVersion {
     TOMCAT6(6, "tomcat-6.0.53.zip"),
-    TOMCAT7(7, "tomcat-7.0.99.zip"),
+    TOMCAT7(7, "tomcat-7.0.106.zip"),
     TOMCAT8(8, "tomcat-8.5.50.zip"),
     TOMCAT9(9, "tomcat-9.0.33.zip");
 
