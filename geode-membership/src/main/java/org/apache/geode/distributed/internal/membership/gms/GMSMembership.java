c05118a5cf31cdd20e595ea9093809eb9f6872fc
Ernest Burghardt <eburghardt@pivotal.io>
2020-09-25 13:13:15 -0700

    spotless

--- geode-membership/src/main/java/org/apache/geode/distributed/internal/membership/gms/GMSMembership.java
+++ geode-membership/src/main/java/org/apache/geode/distributed/internal/membership/gms/GMSMembership.java
@@ -42,7 +42,6 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import org.apache.logging.log4j.Logger;
-import sun.print.resources.serviceui;
 
 import org.apache.geode.annotations.VisibleForTesting;
 import org.apache.geode.annotations.internal.MakeNotStatic;
@@ -1408,7 +1407,8 @@ public class GMSMembership<ID extends MemberIdentifier> implements Membership<ID
    */
   public void checkCancelled() throws MembershipClosedException {
     if (services.getCancelCriterion().isCancelInProgress()) {
-      logger.info("*** EB: GMSMembership.checkCancelled services.getShutdownCause(): " + services.getShutdownCause().getStackTrace());
+      logger.info("*** EB: GMSMembership.checkCancelled services.getShutdownCause(): "
+          + services.getShutdownCause().getStackTrace());
       throw new MembershipClosedException("Distributed System is shutting down",
           services.getCancelCriterion().generateCancelledException(services.getShutdownCause()));
     }
