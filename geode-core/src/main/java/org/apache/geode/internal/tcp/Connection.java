467ee63578b136c4fd58becaaddc2e274138253b
Bill Burcham <bill.burcham@gmail.com>
2020-10-02 15:22:08 -0700

    GEODE-8419: SSL/TLS protocol and cipher suite configuration is ignored (#5465)
    Configure cipher suites when creating an SSLEngine
    
    (cherry picked from commit 537721ff815cf40eff85fde65db9b5e787471c89)

--- geode-core/src/main/java/org/apache/geode/internal/tcp/Connection.java
+++ geode-core/src/main/java/org/apache/geode/internal/tcp/Connection.java
@@ -1727,7 +1727,8 @@ public class Connection implements Runnable {
     if (getConduit().useSSL() && channel != null) {
       InetSocketAddress address = (InetSocketAddress) channel.getRemoteAddress();
       SSLEngine engine =
-          getConduit().getSocketCreator().createSSLEngine(address.getHostName(), address.getPort());
+          getConduit().getSocketCreator().createSSLEngine(address.getHostName(), address.getPort(),
+              clientSocket);
 
       int packetBufferSize = engine.getSession().getPacketBufferSize();
       if (inputBuffer == null || inputBuffer.capacity() < packetBufferSize) {
