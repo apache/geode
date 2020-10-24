a6d04f18e46aee388a94ab9d78dedb5e55443c61
mkevo <mario.kevo@est.tech>
2020-10-24 11:02:19 +0200

    fix intgeration test

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -173,7 +173,7 @@ class DependencyConstraints implements Plugin<Project> {
       }
     }
 
-    dependencySet(group: 'com.fasterxml.jackson.core', version: '2.10.1') {
+    dependencySet(group: 'com.fasterxml.jackson.core', version: '2.10.2') {
       entry('jackson-annotations')
       entry('jackson-core')
       entry('jackson-databind')
