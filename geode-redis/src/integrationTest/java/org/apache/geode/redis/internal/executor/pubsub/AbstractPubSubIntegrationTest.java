e3efe552bae5b07188caff4d0d8738931f4134e7
Jens Deppe <jdeppe@pivotal.io>
2020-10-26 06:55:20 -0700

    GEODE-8654: Remove debug logging from AbstractPubSubIntegrationTest (#5668)
    - When this is present, it causes OOM errors when run under
      StressNewTest.

--- geode-redis/src/integrationTest/java/org/apache/geode/redis/internal/executor/pubsub/AbstractPubSubIntegrationTest.java
+++ geode-redis/src/integrationTest/java/org/apache/geode/redis/internal/executor/pubsub/AbstractPubSubIntegrationTest.java
@@ -32,9 +32,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.ClassRule;
@@ -42,7 +39,6 @@ import org.junit.Test;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.Protocol;
 
-import org.apache.geode.logging.internal.log4j.api.FastLogger;
 import org.apache.geode.logging.internal.log4j.api.LogService;
 import org.apache.geode.redis.mocks.MockBinarySubscriber;
 import org.apache.geode.redis.mocks.MockSubscriber;
@@ -829,10 +825,6 @@ public abstract class AbstractPubSubIntegrationTest implements RedisPortSupplier
   @Test
   public void concurrentSubscribers_andPublishers_doesNotHang()
       throws InterruptedException, ExecutionException {
-    Logger logger = LogService.getLogger("org.apache.geode.redis");
-    Configurator.setAllLevels(logger.getName(), Level.getLevel("DEBUG"));
-    FastLogger.setDelegating(true);
-
     AtomicBoolean running = new AtomicBoolean(true);
 
     Future<Integer> makeSubscribersFuture1 =
