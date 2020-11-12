909fe896a664460a52bc27e3b61b19a4f329e892
Mario Salazar de Torres <mario.salazar.de.torres@est.tech>
2020-11-12 21:34:35 +0100

    Revert "GEODE-8664: Nest errors in DistributionImpl.start (#5725)"
    This reverts commit f6605e0820ba9b858b8128cd31a03a29067b7710.

--- geode-core/src/main/java/org/apache/geode/distributed/internal/DistributionImpl.java
+++ geode-core/src/main/java/org/apache/geode/distributed/internal/DistributionImpl.java
@@ -181,9 +181,9 @@ public class DistributionImpl implements Distribution {
       throw new GemFireSecurityException(e.getMessage(),
           e);
     } catch (MembershipConfigurationException e) {
-      throw new GemFireConfigException("Problem configuring membership services", e);
+      throw new GemFireConfigException(e.getMessage());
     } catch (MemberStartupException e) {
-      throw new SystemConnectException("Problem starting up membership services", e);
+      throw new SystemConnectException(e.getMessage());
     } catch (RuntimeException e) {
       logger.error("Unexpected problem starting up membership services", e);
       throw new SystemConnectException("Problem starting up membership services: " + e.getMessage()
