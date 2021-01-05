fe230cab375f46c3afb6229efa79f4ebfcb57cdd
Donal Evans <doevans@vmware.com>
2021-01-05 10:44:26 -0800

    Change MAX_THREADS default to 1000
    Authored-by: Donal Evans <doevans@vmware.com>

--- geode-core/src/main/java/org/apache/geode/distributed/internal/OperationExecutors.java
+++ geode-core/src/main/java/org/apache/geode/distributed/internal/OperationExecutors.java
@@ -26,7 +26,7 @@ import org.apache.geode.distributed.internal.membership.InternalDistributedMembe
  */
 public interface OperationExecutors {
   int MAX_THREADS =
-      Integer.getInteger("DistributionManager.MAX_THREADS", 300);
+      Integer.getInteger("DistributionManager.MAX_THREADS", 1000);
 
   int MAX_FE_THREADS = Integer.getInteger("DistributionManager.MAX_FE_THREADS",
       Math.max(Runtime.getRuntime().availableProcessors() * 16, 100));
