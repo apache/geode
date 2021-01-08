53d32c325c808d6e965a45cdf36aca1a71db2183
Kamilla Aslami <kaslami@vmware.com>
2021-01-08 14:57:02 -0800

    GEODE-7861: Improve error reporting in GMSJoinLeave.join() (#5839)
    * GEODE-7861: Improve error reporting in GMSJoinLeave.join()
    
    * Fix LocatorDUnitTest.testNoLocator
    
    * Changes after the code review
    
    * Fix typo
    
    (cherry picked from commit 089c1ba7e20606f8201a4cd8f7221f6adc60ba5c)

--- geode-membership/src/main/java/org/apache/geode/distributed/internal/membership/gms/interfaces/JoinLeave.java
+++ geode-membership/src/main/java/org/apache/geode/distributed/internal/membership/gms/interfaces/JoinLeave.java
@@ -16,6 +16,7 @@ package org.apache.geode.distributed.internal.membership.gms.interfaces;
 
 import org.apache.geode.distributed.internal.membership.api.MemberIdentifier;
 import org.apache.geode.distributed.internal.membership.api.MemberStartupException;
+import org.apache.geode.distributed.internal.membership.api.MembershipConfigurationException;
 import org.apache.geode.distributed.internal.membership.gms.GMSMembershipView;
 
 /**
@@ -26,10 +27,15 @@ import org.apache.geode.distributed.internal.membership.gms.GMSMembershipView;
 public interface JoinLeave<ID extends MemberIdentifier> extends Service<ID> {
 
   /**
-   * joins the distributed system and returns true if successful, false if not. Throws
-   * MemberStartupException and MemberConfigurationException
+   * joins the distributed system.
+   *
+   * @throws MemberStartupException if there was a problem joining the cluster after membership
+   *         configuration has
+   *         completed.
+   * @throws MembershipConfigurationException if operation either timed out, was stopped or locator
+   *         does not exist.
    */
-  boolean join() throws MemberStartupException;
+  void join() throws MemberStartupException;
 
   /**
    * leaves the distributed system. Should be invoked before stop()
