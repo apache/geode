82afcbcda5c91a2e93022fb7b9464e42f4cce6b9
Owen Nichols <onichols@apache.org>
2021-01-18 20:51:34 -0800

    GEODE-8496: Bump mysql-connector-java from 8.0.22 to 8.0.23

--- geode-connectors/src/distributedTest/java/org/apache/geode/connectors/jdbc/internal/cli/RegisterDriverCommandDUnitTest.java
+++ geode-connectors/src/distributedTest/java/org/apache/geode/connectors/jdbc/internal/cli/RegisterDriverCommandDUnitTest.java
@@ -61,7 +61,7 @@ public class RegisterDriverCommandDUnitTest {
   public void testRegisterDriverDoesNotThrowException() {
 
     // aquire the jar to be used
-    final String jdbcJarName = "mysql-connector-java-8.0.22.jar";
+    final String jdbcJarName = "mysql-connector-java-8.0.23.jar";
     File mySqlDriverFile = loadTestResource("/" + jdbcJarName);
     assertThat(mySqlDriverFile).exists();
     String jarFile = mySqlDriverFile.getAbsolutePath();
