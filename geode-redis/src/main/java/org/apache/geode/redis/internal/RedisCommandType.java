c2dcd9dbbe8b302caeaf35cacaff723adf244852
Jens Deppe <jdeppe@pivotal.io>
2021-02-02 14:22:29 -0800

    GEODE-8912: Support Redis MGET command

--- geode-redis/src/main/java/org/apache/geode/redis/internal/RedisCommandType.java
+++ geode-redis/src/main/java/org/apache/geode/redis/internal/RedisCommandType.java
@@ -152,6 +152,7 @@ public enum RedisCommandType {
   APPEND(new AppendExecutor(), SUPPORTED, new ExactParameterRequirements(3)),
   GET(new GetExecutor(), SUPPORTED, new ExactParameterRequirements(2)),
   INCRBYFLOAT(new IncrByFloatExecutor(), SUPPORTED, new ExactParameterRequirements(3)),
+  MGET(new MGetExecutor(), SUPPORTED, new MinimumParameterRequirements(2)),
   SET(new SetExecutor(), SUPPORTED, new MinimumParameterRequirements(3)),
 
   /************* Hashes *****************/
@@ -221,7 +222,6 @@ public enum RedisCommandType {
   GETSET(new GetSetExecutor(), UNSUPPORTED, new ExactParameterRequirements(3)),
   INCR(new IncrExecutor(), UNSUPPORTED, new ExactParameterRequirements(2)),
   INCRBY(new IncrByExecutor(), UNSUPPORTED, new ExactParameterRequirements(3)),
-  MGET(new MGetExecutor(), UNSUPPORTED, new MinimumParameterRequirements(2)),
   MSET(new MSetExecutor(), UNSUPPORTED,
       new MinimumParameterRequirements(3).and(new OddParameterRequirements())),
   MSETNX(new MSetNXExecutor(), UNSUPPORTED,
