c26a49145366ae700379d8a3c7958b3820c6252b
jvarenina <jakov.varenina@est.tech>
2021-02-16 15:24:53 +0100

    Remove unused code

--- geode-core/src/main/java/org/apache/geode/cache/configuration/CacheConfig.java
+++ geode-core/src/main/java/org/apache/geode/cache/configuration/CacheConfig.java
@@ -2612,7 +2612,7 @@ public class CacheConfig {
   @XmlAccessorType(XmlAccessType.FIELD)
   @XmlType(name = "", propOrder = {"gatewayEventFilters", "gatewayEventSubstitutionFilter",
       "gatewayTransportFilters"})
-  public static class GatewaySender implements Identifiable<String> {
+  public static class GatewaySender {
 
     @XmlElement(name = "gateway-event-filter", namespace = "http://geode.apache.org/schema/cache")
     protected List<DeclarableType> gatewayEventFilters;
