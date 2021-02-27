0afcd2ce685f5a74bc548970cda62121ac2cadbf
Kamilla Aslami <kaslami@vmware.com>
2021-02-26 18:34:26 -0600

    Update documentation

--- geode-membership/src/main/java/org/apache/geode/distributed/internal/membership/gms/GMSMembership.java
+++ geode-membership/src/main/java/org/apache/geode/distributed/internal/membership/gms/GMSMembership.java
@@ -1488,7 +1488,7 @@ public class GMSMembership<ID extends MemberIdentifier> implements Membership<ID
    * <p>
    * Like isShunned, this method holds the view lock while executing
    *
-   * Concurrency: protected by {@link #latestViewReadLock} ReentrantReadWriteLock
+   * Concurrency: protected by {@link #latestViewReadLock} ReentrantReadLock
    *
    * @param m the member in question
    * @return true if the given member is a surprise member
