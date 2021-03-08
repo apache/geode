c8435d002f3456edd74114ca1519e40192f258fc
Mark Hanson <hansonm@vmware.com>
2021-03-08 10:26:11 -0800

    New test

--- geode-wan/src/upgradeTest/java/org/apache/geode/cache/wan/WANRollingUpgradeVerifyGatewaySenderProfile.java
+++ geode-wan/src/upgradeTest/java/org/apache/geode/cache/wan/WANRollingUpgradeVerifyGatewaySenderProfile.java
@@ -73,4 +73,50 @@ public class WANRollingUpgradeVerifyGatewaySenderProfile extends WANRollingUpgra
       ie.remove();
     }
   }
+
+  @Test
+  // This test verifies that a GatewaySenderProfile serializes properly between versions.
+  public void testOldServerCanUnderstandNewGatewaySenderProfile() {
+
+    VM oldLocator = VM.getVM(oldVersion, 0);
+    VM oldServer = VM.getVM(oldVersion, 1);
+    VM currentServer = VM.getVM(VersionManager.CURRENT_VERSION, 2);
+
+    // Start locator
+    final int port = getRandomAvailableTCPPort();
+    oldLocator.invoke(() -> DistributedTestUtils.deleteLocatorStateFile(port));
+    final String locators = VM.getHostName() + "[" + port + "]";
+    oldLocator.invoke(() -> startLocator(port, 0, locators, ""));
+
+    IgnoredException ie =
+        IgnoredException.addIgnoredException("could not get remote locator information");
+    try {
+      String senderId = getName() + "_gatewaysender";
+
+      // Start current server
+      currentServer.invoke(() -> createCache(locators));
+
+      // Create GatewaySender in new server
+      currentServer.invoke(() -> createGatewaySender(senderId, 10,
+          ParallelGatewaySenderQueue.DEFAULT_MESSAGE_SYNC_INTERVAL));
+
+      // Locators before 1.4 handled configuration asynchronously.
+      // We must wait for configuration configuration to be ready, or confirm that it is disabled.
+      oldLocator.invoke(
+          () -> await()
+              .untilAsserted(() -> assertTrue(
+                  !InternalLocator.getLocator().getConfig().getEnableClusterConfiguration()
+                      || InternalLocator.getLocator().isSharedConfigurationRunning())));
+
+      // Start old server
+      oldServer.invoke(() -> createCache(locators));
+
+      // Attempt to create GatewaySender in old server
+      oldServer.invoke(() -> createGatewaySender(senderId, 10,
+          ParallelGatewaySenderQueue.DEFAULT_MESSAGE_SYNC_INTERVAL));
+
+    } finally {
+      ie.remove();
+    }
+  }
 }
