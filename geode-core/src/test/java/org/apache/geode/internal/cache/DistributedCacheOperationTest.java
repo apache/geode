592c69f2873ae9265273fa8044574612c2acebba
Jianxia Chen <11181423+jchen21@users.noreply.github.com>
2021-03-11 11:19:39 -0800

    GEODE-9016: Fix the NPE for PutAll with CQ LOCAL_DESTROY message type (#6104)
    For PutAll and RemoveAll, when removing destroy token from CQ result keys, use the keys in the individual entry events, instead of using the key in the base event.
    
    (cherry picked from commit 4e0c8aa6937ad2b5935a11994138381fa29a8644)

--- geode-core/src/test/java/org/apache/geode/internal/cache/DistributedCacheOperationTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/DistributedCacheOperationTest.java
@@ -17,6 +17,8 @@ package org.apache.geode.internal.cache;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.isA;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -28,9 +30,11 @@ import java.util.Map;
 import org.junit.Test;
 
 import org.apache.geode.cache.CacheEvent;
+import org.apache.geode.cache.query.internal.cq.ServerCQ;
 import org.apache.geode.distributed.internal.membership.InternalDistributedMember;
 import org.apache.geode.internal.cache.DistributedCacheOperation.CacheOperationMessage;
 import org.apache.geode.internal.cache.persistence.PersistentMemberID;
+import org.apache.geode.internal.cache.tier.MessageType;
 
 public class DistributedCacheOperationTest {
 
@@ -105,5 +109,25 @@ public class DistributedCacheOperationTest {
     protected void _distribute() {
       throw new RuntimeException("boom");
     }
+  }
+
+  @Test
+  public void testDoRemoveDestroyTokensFromCqResultKeys() {
+    Object key = new Object();
+    HashMap hashMap = new HashMap();
+    hashMap.put(1L, MessageType.LOCAL_DESTROY);
+    EntryEventImpl baseEvent = mock(EntryEventImpl.class);
+    ServerCQ serverCQ = mock(ServerCQ.class);
+    FilterRoutingInfo.FilterInfo filterInfo = mock(FilterRoutingInfo.FilterInfo.class);
+    DistributedCacheOperation distributedCacheOperation =
+        new DestroyOperation(baseEvent);
+    when(baseEvent.getKey()).thenReturn(key);
+    when(filterInfo.getCQs()).thenReturn(hashMap);
+    when(serverCQ.getFilterID()).thenReturn(new Long(1L));
+    doNothing().when(serverCQ).removeFromCqResultKeys(isA(Object.class), isA(Boolean.class));
+
+    distributedCacheOperation.doRemoveDestroyTokensFromCqResultKeys(filterInfo, serverCQ);
+
+    verify(serverCQ, times(1)).removeFromCqResultKeys(key, true);
   }
 }
