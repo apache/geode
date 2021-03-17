407576f6a0719f9729862d4bec112b15a070fe2c
Owen Nichols <onichols@apache.org>
2021-03-17 15:18:15 -0700

    GEODE-8980: Bump jedis from 3.5.1 to 3.5.2

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -170,7 +170,7 @@ class DependencyConstraints implements Plugin<Project> {
         api(group: 'org.springframework.shell', name: 'spring-shell', version: get('springshell.version'))
         api(group: 'org.testcontainers', name: 'testcontainers', version: '1.14.3')
         api(group: 'pl.pragmatists', name: 'JUnitParams', version: '1.1.0')
-        api(group: 'redis.clients', name: 'jedis', version: '3.5.1')
+        api(group: 'redis.clients', name: 'jedis', version: '3.5.2')
         api(group: 'io.lettuce', name: 'lettuce-core', version: '6.0.3.RELEASE')
         api(group: 'xerces', name: 'xercesImpl', version: '2.12.0')
       }
