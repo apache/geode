529f0eb54359eed0c5941573ff94b612b68ef99a
Robert Houghton <rhoughton@pivotal.io>
2021-04-09 12:45:35 -0700

    pip3 is not playing nicely

--- ci/images/google-geode-builder/scripts/setup.sh
+++ ci/images/google-geode-builder/scripts/setup.sh
@@ -67,7 +67,8 @@ rm -rf /etc/alternatives
 mv /etc/keep-alternatives /etc/alternatives
 
 pip3 install --upgrade pip
-pip3 install setuptools docker-compose
+pip3 install setuptools
+pip3 install docker-compose
 
 pushd /tmp
   curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-${CLOUD_SDK_VERSION}-linux-x86_64.tar.gz
