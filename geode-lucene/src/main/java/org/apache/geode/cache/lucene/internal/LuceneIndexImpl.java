a4852fee94354ef8b9b3dea62ef7ddbe1bf0d459
Xiaojian Zhou <gesterzhou@users.noreply.github.com>
2021-04-13 14:21:34 -0700

    GEODE-7674: Clear on PR with lucene index should throw exception (#6317)

--- geode-lucene/src/main/java/org/apache/geode/cache/lucene/internal/LuceneIndexImpl.java
+++ geode-lucene/src/main/java/org/apache/geode/cache/lucene/internal/LuceneIndexImpl.java
@@ -200,6 +200,7 @@ public abstract class LuceneIndexImpl implements InternalLuceneIndex {
     factory.setDiskStoreName(attributes.getDiskStoreName());
     factory.setDiskSynchronous(true);
     factory.setForwardExpirationDestroy(true);
+    factory.setPartitionedRegionClearUnsupported(true);
     return factory;
   }
 
