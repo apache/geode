b100a78eef5fa23b4c467cd00fa0f81fea5fb2a2
Owen Nichols <onichols@apache.org>
2021-04-17 02:33:58 -0700

    GEODE-8980: Bump spring-session-data-redis from 2.4.2 to 2.4.3

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -263,7 +263,7 @@ class DependencyConstraints implements Plugin<Project> {
       entry('spring-boot-starter-data-redis')
     }
 
-    dependencySet(group: 'org.springframework.session', version: '2.4.2') {
+    dependencySet(group: 'org.springframework.session', version: '2.4.3') {
       entry('spring-session-data-redis')
     }
 
