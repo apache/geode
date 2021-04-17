c4e8de47844ec13d0cb35f26dea5d00bfae3d5ea
Owen Nichols <onichols@apache.org>
2021-04-17 02:23:04 -0700

    GEODE-8980: Bump mockito-core from 3.8.0 to 3.9.0

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -152,7 +152,7 @@ class DependencyConstraints implements Plugin<Project> {
         api(group: 'org.iq80.snappy', name: 'snappy', version: '0.4')
         api(group: 'org.jboss.modules', name: 'jboss-modules', version: get('jboss-modules.version'))
         api(group: 'org.jgroups', name: 'jgroups', version: get('jgroups.version'))
-        api(group: 'org.mockito', name: 'mockito-core', version: '3.8.0')
+        api(group: 'org.mockito', name: 'mockito-core', version: '3.9.0')
         api(group: 'org.mortbay.jetty', name: 'servlet-api', version: '3.0.20100224')
         api(group: 'org.openjdk.jmh', name: 'jmh-core', version: '1.26')
         api(group: 'org.postgresql', name: 'postgresql', version: '42.2.8')
