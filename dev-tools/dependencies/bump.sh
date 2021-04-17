7c9483f88cb682e83a11f873811264d6d6903546
Owen Nichols <onichols@apache.org>
2021-04-17 02:32:48 -0700

    make it easier to batch bumps

--- dev-tools/dependencies/bump.sh
+++ dev-tools/dependencies/bump.sh
@@ -57,4 +57,4 @@ if [ $(git diff | wc -l) -gt 0 ] ; then
   git stash
   git stash drop
 fi
-./gradlew devBuild checkPom :geode-assembly:integrationTest --tests AssemblyContentsIntegrationTest --tests GeodeDependencyJarIntegrationTest --tests BundledJarsJUnitTest --tests GemfireCoreClasspathTest
+[ -n "$SKIP" ] || ./gradlew devBuild checkPom :geode-assembly:integrationTest --tests AssemblyContentsIntegrationTest --tests GeodeDependencyJarIntegrationTest --tests BundledJarsJUnitTest --tests GemfireCoreClasspathTest
