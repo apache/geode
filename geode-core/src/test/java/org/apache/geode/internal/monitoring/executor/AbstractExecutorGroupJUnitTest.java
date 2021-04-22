9d93cba3d9b0c0f8786571fece1220953cb7cdb9
Darrel Schneider <darrel@vmware.com>
2021-04-22 15:40:52 -0700

    Revert "Revert "GEODE-8977: include syncs in thread monitor stack (#6248)" (#6368)"
    This reverts commit f366940bb4172084412ff6943c3793a6e6ed1b77.

--- geode-core/src/test/java/org/apache/geode/internal/monitoring/executor/AbstractExecutorGroupJUnitTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/monitoring/executor/AbstractExecutorGroupJUnitTest.java
@@ -15,9 +15,9 @@
 package org.apache.geode.internal.monitoring.executor;
 
 import static org.apache.geode.test.awaitility.GeodeAwaitility.await;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
 
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 
 import org.apache.geode.test.awaitility.GeodeAwaitility;
@@ -78,11 +78,6 @@ public class AbstractExecutorGroupJUnitTest {
       public void run() {
         blockedThreadWaiting[0] = true;
         synchronized (syncObject) {
-          try {
-            syncObject.wait(timeoutInMilliseconds);
-          } catch (InterruptedException e) {
-            return;
-          }
         }
       }
     };
@@ -99,7 +94,11 @@ public class AbstractExecutorGroupJUnitTest {
       };
       await().untilAsserted(() -> {
         String threadReport = executor.createThreadReport(60000);
-        Assertions.assertThat(threadReport).contains(AbstractExecutor.LOCK_OWNER_THREAD_STACK);
+        assertThat(threadReport)
+            .contains(AbstractExecutor.LOCK_OWNER_THREAD_STACK + " for \"blocking thread\"");
+        assertThat(threadReport).contains("Waiting on <" + syncObject + ">");
+        assertThat(threadReport).contains("Owned By <blocking thread>");
+        assertThat(threadReport).contains("- locked " + syncObject);
       });
     } finally {
       blockingThread.interrupt();
