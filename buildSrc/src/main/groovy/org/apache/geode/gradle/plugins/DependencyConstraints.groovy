d2064678c4df85f2fafe77f102ff1c19c5634760
mkevo <mario.kevo@est.tech>
2021-05-03 09:58:43 +0200

    add dependency

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -193,6 +193,7 @@ class DependencyConstraints implements Plugin<Project> {
     dependencySet(group: 'io.springfox', version: '3.0.0') {
       entry('springfox-swagger-ui')
       entry('springfox-swagger2')
+      entry('springfox-schema')
     }
 
     dependencySet(group: 'mx4j', version: '3.0.2') {
