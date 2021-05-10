d9b323fa18dc969fb7e737652c00a200c297de17
Owen Nichols <34043438+onichols-pivotal@users.noreply.github.com>
2021-05-10 14:32:11 -0700

    GEODE-8321: part 3 - remove adoptopenjdk (#6457)
    This is the final cleanup following successful conversion in Part 2 (#5474)
    
    (all three phases were 1: add liberica  2: switch  3: remove adoptopenjdk)

--- ci/scripts/execute_build.sh
+++ ci/scripts/execute_build.sh
@@ -85,7 +85,6 @@ else
 fi
 
 
-# SET_JAVA_HOME="export JAVA_HOME=/usr/lib/jvm/java-${JAVA_BUILD_VERSION}-openjdk-amd64"
 SET_JAVA_HOME="export JAVA_HOME=/usr/lib/jvm/bellsoft-java${JAVA_BUILD_VERSION}-amd64"
 
 if [ -v CALL_STACK_TIMEOUT ]; then
