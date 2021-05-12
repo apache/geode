34ca38f2750f9a1c63b76a6d4f7b322a7c430363
Robert Houghton <rhoughton@pivotal.io>
2021-05-12 11:49:56 -0700

    GEODE-9251
    capture gradle exit code for exit. Subshell taskkill in case of error.
    make the windows optional command only impact windows builds

--- ci/scripts/execute_tests.sh
+++ ci/scripts/execute_tests.sh
@@ -79,7 +79,11 @@ case $ARTIFACT_SLUG in
     JAVA_TEST_PATH=C:/java${JAVA_TEST_VERSION}
     GRADLE_SKIP_TASK_OPTIONS="${GRADLE_SKIP_TASK_OPTIONS} -x docker"
     SEP=";"
-    OPTIONAL_KILL_ALL_CMD="taskkill //F //IM java.exe //T"
+    OPTIONAL_KILL_ALL_CMD="${SEP} \
+      exit_code=\$? ${SEP} \
+      (taskkill //F //IM java.exe //T) ${SEP} \
+      exit \${exit_code}
+      "
     ;;
   *)
     JAVA_BUILD_PATH=/usr/lib/jvm/bellsoft-java${JAVA_BUILD_VERSION}-amd64
@@ -117,7 +121,7 @@ EXEC_COMMAND="bash -c 'echo Building with: $SEP \
   cd geode $SEP \
   cp gradlew gradlewStrict $SEP \
   sed -e 's/JAVA_HOME/GRADLE_JVM/g' -i.bak gradlewStrict $SEP \
-  GRADLE_JVM=${JAVA_BUILD_PATH} JAVA_TEST_PATH=${JAVA_TEST_PATH} ./gradlewStrict ${GRADLE_ARGS} $SEP \
+  GRADLE_JVM=${JAVA_BUILD_PATH} JAVA_TEST_PATH=${JAVA_TEST_PATH} ./gradlewStrict ${GRADLE_ARGS} \
   ${OPTIONAL_KILL_ALL_CMD}'"
 
 echo "${EXEC_COMMAND}"
