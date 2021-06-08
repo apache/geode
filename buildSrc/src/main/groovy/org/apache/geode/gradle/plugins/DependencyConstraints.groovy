05e24353f95cc83903aa82b4211ea17f1903c7f7
Owen Nichols <onichols@apache.org>
2021-06-08 09:30:40 -0700

    GEODE-9363: Bump spring from 5.2.9 to 5.2.10

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -235,7 +235,7 @@ class DependencyConstraints implements Plugin<Project> {
       entry('selenium-support')
     }
 
-    dependencySet(group: 'org.springframework.security', version: '5.2.9.RELEASE') {
+    dependencySet(group: 'org.springframework.security', version: '5.2.10.RELEASE') {
       entry('spring-security-config')
       entry('spring-security-core')
       entry('spring-security-ldap')
@@ -243,7 +243,7 @@ class DependencyConstraints implements Plugin<Project> {
       entry('spring-security-web')
     }
 
-    dependencySet(group: 'org.springframework', version: '5.2.9.RELEASE') {
+    dependencySet(group: 'org.springframework', version: '5.2.10.RELEASE') {
       entry('spring-aspects')
       entry('spring-beans')
       entry('spring-context')
