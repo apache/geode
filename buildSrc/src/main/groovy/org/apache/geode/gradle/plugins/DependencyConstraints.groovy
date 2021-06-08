44fac1ca96b970f6adc5d5c90ba62577dd2ac87b
Owen Nichols <onichols@apache.org>
2021-06-08 09:17:15 -0700

    GEODE-9363: Bump spring from 5.3.3 to 5.3.8

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -253,7 +253,7 @@ class DependencyConstraints implements Plugin<Project> {
       entry('spring-security-oauth2-jose')
     }
 
-    dependencySet(group: 'org.springframework', version: '5.3.3') {
+    dependencySet(group: 'org.springframework', version: '5.3.8') {
       entry('spring-aspects')
       entry('spring-beans')
       entry('spring-context')
