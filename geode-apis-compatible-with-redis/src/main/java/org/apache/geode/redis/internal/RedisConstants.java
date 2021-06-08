7a2fee92450fa2fd3846fb92b8eb90fceb0d5170
John Hutchison <hutchisonjo@vmware.com>
2021-06-08 11:44:35 -0400

    GEODE-9202: implement PUBSUB CHANNELS subcommand (#6493)
    - implement methods for finding channel names in subscriptions class
    - implement pattern matching for finding channel names

--- geode-apis-compatible-with-redis/src/main/java/org/apache/geode/redis/internal/RedisConstants.java
+++ geode-apis-compatible-with-redis/src/main/java/org/apache/geode/redis/internal/RedisConstants.java
@@ -59,4 +59,7 @@ public class RedisConstants {
       "Unknown subcommand or wrong number of arguments for '%s'. Try CLUSTER HELP.";
   public static final String ERROR_INVALID_ZADD_OPTION_NX_XX =
       "XX and NX options at the same time are not compatible";
+
+  public static final String ERROR_UNKNOWN_PUBSUB_SUBCOMMAND =
+      "Unknown subcommand or wrong number of arguments for '%s'";
 }
