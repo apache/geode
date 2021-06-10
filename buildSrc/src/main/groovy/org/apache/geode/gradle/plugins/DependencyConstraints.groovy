dc61c7571cdb292ba12c21dc70f467e48934b1dc
Owen Nichols <onichols@apache.org>
2021-06-09 20:30:00 -0700

    GEODE-8980: Bump tomcat-catalina from 8.5.65 to 8.5.66

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -56,7 +56,7 @@ class DependencyConstraints implements Plugin<Project> {
     // This would possibly be corrected if they were proper source sets.
     deps.put("tomcat6.version", "6.0.37")
     deps.put("tomcat7.version", "7.0.109")
-    deps.put("tomcat8.version", "8.5.65")
+    deps.put("tomcat8.version", "8.5.66")
     deps.put("tomcat9.version", "9.0.46")
 
     // The jetty version is also hard-coded in geode-assembly:test
