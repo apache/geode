2e0440df1c5ae96baa80aa6a3c9c53866ae0d5fe
Owen Nichols <onichols@apache.org>
2021-06-09 19:23:19 -0700

    GEODE-8980: Bump commons-io from 2.8.0 to 2.9.0

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -34,7 +34,7 @@ class DependencyConstraints implements Plugin<Project> {
     // These version numbers are consumed by :geode-modules-assembly:distAppServer filtering
     // Some of these are referenced below as well
     deps.put("antlr.version", "2.7.7")
-    deps.put("commons-io.version", "2.8.0")
+    deps.put("commons-io.version", "2.9.0")
     deps.put("commons-lang3.version", "3.12.0")
     deps.put("commons-validator.version", "1.7")
     deps.put("fastutil.version", "8.5.4")
