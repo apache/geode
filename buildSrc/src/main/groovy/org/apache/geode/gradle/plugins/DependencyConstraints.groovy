bcf1c72d783e2795548a9145e7f248659332db7d
Owen Nichols <onichols@apache.org>
2021-06-09 20:33:54 -0700

    GEODE-8980: Bump jetty from 9.4.40.v20210413 to 9.4.42.v20210604

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -61,7 +61,7 @@ class DependencyConstraints implements Plugin<Project> {
 
     // The jetty version is also hard-coded in geode-assembly:test
     // at o.a.g.sessions.tests.GenericAppServerInstall.java
-    deps.put("jetty.version", "9.4.40.v20210413")
+    deps.put("jetty.version", "9.4.42.v20210604")
 
     // These versions are referenced in test.gradle, which is aggressively injected into all projects.
     deps.put("junit.version", "4.13.2")
