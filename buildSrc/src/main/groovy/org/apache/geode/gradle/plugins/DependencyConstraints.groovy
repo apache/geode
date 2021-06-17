e16a87330872f6b82a37a499ad9a4ed783ecf5be
mkevo <mario.kevo@est.tech>
2021-06-17 16:31:58 +0200

    uplift oauth2-oidc-sdk to 9.3.1

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -99,7 +99,7 @@ class DependencyConstraints implements Plugin<Project> {
         api(group: 'com.mockrunner', name: 'mockrunner-servlet', version: '2.0.4')
         api(group: 'com.nimbusds', name:'nimbus-jose-jwt', version:'8.11')
         // Pinning transitive dependency from spring-security-oauth2 to clean up our licenses.
-        api(group: 'com.nimbusds', name: 'oauth2-oidc-sdk', version: '8.9')
+        api(group: 'com.nimbusds', name: 'oauth2-oidc-sdk', version: '9.3.1')
         api(group: 'com.sun.istack', name: 'istack-commons-runtime', version: '3.0.11')
         api(group: 'com.sun.mail', name: 'javax.mail', version: '1.6.2')
         api(group: 'com.sun.xml.bind', name: 'jaxb-impl', version: '2.3.2')
