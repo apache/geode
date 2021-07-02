00dfe1f88bb611480eaa0e724474936d9ace28b4
Ray Ingles <ringles@vmware.com>
2021-07-02 10:46:59 -0400

    Fix failed merge

--- geode-apis-compatible-with-redis/src/acceptanceTest/java/org/apache/geode/redis/internal/executor/sortedset/ZRangeNativeRedisAcceptanceTest.java
+++ geode-apis-compatible-with-redis/src/acceptanceTest/java/org/apache/geode/redis/internal/executor/sortedset/ZRangeNativeRedisAcceptanceTest.java
@@ -12,28 +12,6 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-<<<<<<< HEAD:geode-apis-compatible-with-redis/src/main/java/org/apache/geode/redis/internal/RedisException.java
-package org.apache.geode.redis.internal;
-
-/**
- * A general exception that can be thrown during command execution and will result in a Redis
- * protocol exception message being returned to the client.
- */
-public class RedisException extends RuntimeException {
-
-  private static final long serialVersionUID = 6530956425269820304L;
-
-  public RedisException() {
-    super();
-  }
-
-  public RedisException(String message) {
-    super(message);
-  }
-
-  public RedisException(String message, Throwable throwable) {
-    super(message, throwable);
-=======
 package org.apache.geode.redis.internal.executor.sortedset;
 
 import org.junit.ClassRule;
@@ -53,7 +31,6 @@ public class ZRangeNativeRedisAcceptanceTest extends AbstractZRangeIntegrationTe
   @Override
   public void flushAll() {
     server.flushAll();
->>>>>>> 9aa758f0c2 (Revert "GEODE-8971: Add grace period when stopping gateway sender with group-… (#6052)" (#6634)):geode-apis-compatible-with-redis/src/acceptanceTest/java/org/apache/geode/redis/internal/executor/sortedset/ZRangeNativeRedisAcceptanceTest.java
   }
 
 }
--- geode-apis-compatible-with-redis/src/main/java/org/apache/geode/redis/internal/RedisException.java
+++ geode-apis-compatible-with-redis/src/main/java/org/apache/geode/redis/internal/RedisException.java
@@ -12,7 +12,6 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-<<<<<<< HEAD:geode-apis-compatible-with-redis/src/main/java/org/apache/geode/redis/internal/RedisException.java
 package org.apache.geode.redis.internal;
 
 /**
@@ -33,27 +32,6 @@ public class RedisException extends RuntimeException {
 
   public RedisException(String message, Throwable throwable) {
     super(message, throwable);
-=======
-package org.apache.geode.redis.internal.executor.sortedset;
-
-import org.junit.ClassRule;
-
-import org.apache.geode.redis.NativeRedisClusterTestRule;
-
-public class ZRangeNativeRedisAcceptanceTest extends AbstractZRangeIntegrationTest {
-
-  @ClassRule
-  public static NativeRedisClusterTestRule server = new NativeRedisClusterTestRule();
-
-  @Override
-  public int getPort() {
-    return server.getExposedPorts().get(0);
-  }
-
-  @Override
-  public void flushAll() {
-    server.flushAll();
->>>>>>> 9aa758f0c2 (Revert "GEODE-8971: Add grace period when stopping gateway sender with group-… (#6052)" (#6634)):geode-apis-compatible-with-redis/src/acceptanceTest/java/org/apache/geode/redis/internal/executor/sortedset/ZRangeNativeRedisAcceptanceTest.java
   }
 
 }
