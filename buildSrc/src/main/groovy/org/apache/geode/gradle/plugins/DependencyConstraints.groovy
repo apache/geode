e8cc2c9ccd19d21f802dc4fa038b75da9e1ca2ca
Owen Nichols <onichols@apache.org>
2021-07-30 20:05:36 -0700

    GEODE-8980: Bump spring-boot-starter from 2.5.2 to 2.5.3

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -252,7 +252,7 @@ class DependencyConstraints implements Plugin<Project> {
       entry('spring-webmvc')
     }
 
-    dependencySet(group: 'org.springframework.boot', version: '2.5.2') {
+    dependencySet(group: 'org.springframework.boot', version: '2.5.3') {
       entry('spring-boot-starter')
       entry('spring-boot-starter-jetty')
       entry('spring-boot-starter-web')
