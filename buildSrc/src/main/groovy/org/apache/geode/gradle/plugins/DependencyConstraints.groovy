2c438d8f800443c8985a9d2ef334165b6b55805f
Owen Nichols <onichols@apache.org>
2021-08-27 17:51:30 -0700

    GEODE-8980: Bump json-smart from 2.3.1 to 2.4.7

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -131,7 +131,7 @@ class DependencyConstraints implements Plugin<Project> {
         api(group: 'mysql', name: 'mysql-connector-java', version: '5.1.46')
         api(group: 'net.java.dev.jna', name: 'jna', version: '5.9.0')
         api(group: 'net.java.dev.jna', name: 'jna-platform', version: '5.9.0')
-        api(group: 'net.minidev', name: 'json-smart', version: '2.3.1')
+        api(group: 'net.minidev', name: 'json-smart', version: '2.4.7')
         api(group: 'net.openhft', name: 'compiler', version: '2.4.1')
         api(group: 'net.sf.jopt-simple', name: 'jopt-simple', version: '5.0.4')
         api(group: 'net.sourceforge.pmd', name: 'pmd-java', version: '6.37.0')
