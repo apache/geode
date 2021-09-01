f934afa3c9c847bce955d85ae5eed219c60170ae
Jens Deppe <jdeppe@vmware.com>
2021-09-01 13:58:35 -0700

    Add tests for cert rotation

--- geode-apis-compatible-with-redis/src/distributedTest/java/org/apache/geode/redis/SSLDUnitTest.java
+++ geode-apis-compatible-with-redis/src/distributedTest/java/org/apache/geode/redis/SSLDUnitTest.java
@@ -21,17 +21,25 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.io.FileInputStream;
 import java.security.KeyStore;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.Properties;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.KeyManager;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLHandshakeException;
+import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManagerFactory;
 
 import io.netty.handler.ssl.NotSslRecordException;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.exceptions.JedisConnectionException;
@@ -39,21 +47,27 @@ import redis.clients.jedis.exceptions.JedisConnectionException;
 import org.apache.geode.cache.ssl.CertStores;
 import org.apache.geode.cache.ssl.CertificateBuilder;
 import org.apache.geode.cache.ssl.CertificateMaterial;
+import org.apache.geode.internal.net.filewatch.PollingFileWatcher;
+import org.apache.geode.test.awaitility.GeodeAwaitility;
 import org.apache.geode.test.dunit.IgnoredException;
 import org.apache.geode.test.dunit.rules.MemberVM;
 import org.apache.geode.test.dunit.rules.RedisClusterStartupRule;
 
 public class SSLDUnitTest {
 
-  @ClassRule
-  public static RedisClusterStartupRule cluster = new RedisClusterStartupRule();
+  @Rule
+  public RedisClusterStartupRule cluster = new RedisClusterStartupRule();
 
-  private static int redisPort;
-  private static CertificateMaterial ca;
+  private static String commonPassword = "password";
+  private static SANCapturingHostnameVerifier hostnameVerifier = new SANCapturingHostnameVerifier();
 
+  private int redisPort;
+  private CertificateMaterial ca;
+  private String serverKeyStoreFilename;
+  private String serverTrustStoreFilename;
 
-  @BeforeClass
-  public static void setupClass() throws Exception {
+  @Before
+  public void setup() throws Exception {
     ca = new CertificateBuilder()
         .commonName("Test CA")
         .isCA()
@@ -72,6 +86,8 @@ public class SSLDUnitTest {
     MemberVM server = cluster.startRedisVM(0, x -> x.withProperties(serverProperties));
 
     redisPort = cluster.getRedisPort(server);
+    serverKeyStoreFilename = serverProperties.getProperty("ssl-keystore");
+    serverTrustStoreFilename = serverProperties.getProperty("ssl-truststore");
   }
 
   @Test
@@ -86,11 +102,8 @@ public class SSLDUnitTest {
     IgnoredException.addIgnoredException(SSLHandshakeException.class);
 
     // Create the client without a keystore
-    try (Jedis jedis = createClient(false, false)) {
-      assertThatThrownBy(jedis::ping).satisfiesAnyOf(
-          e -> assertThat(e.getMessage()).contains("SSLException"),
-          e -> assertThat(e.getMessage()).contains("SSLHandshakeException"));
-    }
+    assertThatThrownBy(() -> createClient(false, false))
+        .isInstanceOf(JedisConnectionException.class);
 
     IgnoredException.removeAllExpectedExceptions();
   }
@@ -100,11 +113,8 @@ public class SSLDUnitTest {
     IgnoredException.addIgnoredException(SSLHandshakeException.class);
 
     // Create the client with a self-signed certificate
-    try (Jedis jedis = createClient(true, true)) {
-      assertThatThrownBy(jedis::ping).satisfiesAnyOf(
-          e -> assertThat(e.getMessage()).contains("SSLException"),
-          e -> assertThat(e.getMessage()).contains("SSLHandshakeException"));
-    }
+    assertThatThrownBy(() -> createClient(false, false))
+        .isInstanceOf(JedisConnectionException.class);
 
     IgnoredException.removeAllExpectedExceptions();
   }
@@ -121,6 +131,78 @@ public class SSLDUnitTest {
     IgnoredException.removeAllExpectedExceptions();
   }
 
+  @Test
+  public void givenServerCertificateIsRotated_clientCanStillConnect() throws Exception {
+    String newServerName = "updated-server";
+
+    try (Jedis jedis = createClient(true, false)) {
+      assertThat(jedis.ping()).isEqualTo("PONG");
+    }
+
+    // create a new certificate for the server
+    CertificateMaterial serverCertificate = new CertificateBuilder()
+        .commonName(newServerName)
+        .issuedBy(ca)
+        .sanDnsName(newServerName)
+        .generate();
+
+    CertStores serverStore = CertStores.serverStore();
+    serverStore.withCertificate("server", serverCertificate);
+    serverStore.trust("ca", ca);
+
+    // Wait for one second since file timestamp granularity may only be seconds depending on the
+    // platform.
+    GeodeAwaitility.await().during(Duration.ofSeconds(1)).until(() -> true);
+    serverStore.createKeyStore(serverKeyStoreFilename, commonPassword);
+
+    // Wait long enough for the file change to be detected
+    GeodeAwaitility.await().during(Duration.ofSeconds(PollingFileWatcher.PERIOD_SECONDS + 2))
+        .until(() -> true);
+
+    try (Jedis jedis = createClient(true, false)) {
+      assertThat(jedis.ping()).isEqualTo("PONG");
+    }
+    assertThat(hostnameVerifier.getSubjectAltNames()).contains(newServerName);
+  }
+
+  @Test
+  public void givenServerCAandKeyIsRotated_clientCannotConnect() throws Exception {
+    try (Jedis jedis = createClient(true, false)) {
+      assertThat(jedis.ping()).isEqualTo("PONG");
+    }
+
+    CertificateMaterial newCA = new CertificateBuilder()
+        .commonName("New Test CA")
+        .isCA()
+        .generate();
+
+    CertificateMaterial serverCertificate = new CertificateBuilder()
+        .commonName("server")
+        .issuedBy(newCA)
+        .generate();
+
+    CertStores serverStore = CertStores.serverStore();
+    serverStore.withCertificate("server", serverCertificate);
+    serverStore.trust("ca", newCA);
+
+    // Wait for one second since file timestamp granularity may only be seconds depending on the
+    // platform.
+    GeodeAwaitility.await().during(Duration.ofSeconds(1)).until(() -> true);
+    serverStore.createKeyStore(serverKeyStoreFilename, commonPassword);
+    serverStore.createTrustStore(serverTrustStoreFilename, commonPassword);
+
+    // Wait long enough for the file change to be detected
+    GeodeAwaitility.await().during(Duration.ofSeconds(PollingFileWatcher.PERIOD_SECONDS + 2))
+        .until(() -> true);
+
+    IgnoredException.addIgnoredException(SSLHandshakeException.class);
+
+    assertThatThrownBy(() -> createClient(true, false))
+        .isInstanceOf(JedisConnectionException.class);
+
+    IgnoredException.removeAllExpectedExceptions();
+  }
+
   private Jedis createClient(boolean mutualAuthentication, boolean isSelfSigned) throws Exception {
     CertificateMaterial clientCertificate = new CertificateBuilder()
         .commonName("redis-client")
@@ -133,23 +215,22 @@ public class SSLDUnitTest {
 
     Properties clientProperties = clientStore.propertiesWith("all");
 
-    // The generated password is the same everywhere
-    char[] password = clientProperties.getProperty("ssl-keystore-password").toCharArray();
-
     KeyManager[] keyManagers = null;
 
     if (mutualAuthentication) {
       KeyStore keyStore = KeyStore.getInstance("JKS");
-      keyStore.load(new FileInputStream(clientProperties.getProperty("ssl-keystore")), password);
+      keyStore.load(new FileInputStream(clientProperties.getProperty("ssl-keystore")),
+          commonPassword.toCharArray());
 
       KeyManagerFactory keyManagerFactory =
           KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
-      keyManagerFactory.init(keyStore, password);
+      keyManagerFactory.init(keyStore, commonPassword.toCharArray());
       keyManagers = keyManagerFactory.getKeyManagers();
     }
 
     KeyStore trustStore = KeyStore.getInstance("JKS");
-    trustStore.load(new FileInputStream(clientProperties.getProperty("ssl-truststore")), password);
+    trustStore.load(new FileInputStream(clientProperties.getProperty("ssl-truststore")),
+        commonPassword.toCharArray());
 
     TrustManagerFactory trustManagerFactory =
         TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
@@ -159,6 +240,39 @@ public class SSLDUnitTest {
     sslContext.init(keyManagers, trustManagerFactory.getTrustManagers(), null);
 
     return new Jedis(BIND_ADDRESS, redisPort, true, sslContext.getSocketFactory(),
-        sslContext.getSupportedSSLParameters(), null);
+        sslContext.getSupportedSSLParameters(), hostnameVerifier);
   }
+
+  /**
+   * {@link HostnameVerifier} that captures the Subject Alternative Names (SANs).
+   */
+  private static class SANCapturingHostnameVerifier implements HostnameVerifier {
+
+    private final List<String> subjectAltNames = new ArrayList<>();
+
+    @Override
+    public boolean verify(String s, SSLSession sslSession) {
+      subjectAltNames.clear();
+
+      try {
+        Certificate[] certs = sslSession.getPeerCertificates();
+        X509Certificate x509 = (X509Certificate) certs[0];
+        Collection<List<?>> entries = x509.getSubjectAlternativeNames();
+
+        for (List<?> entry : entries) {
+          if (entry.size() > 1) {
+            subjectAltNames.add((String) entry.get(1));
+          }
+        }
+      } catch (Exception ignored) {
+      }
+
+      return true;
+    }
+
+    public List<String> getSubjectAltNames() {
+      return subjectAltNames;
+    }
+  }
+
 }
