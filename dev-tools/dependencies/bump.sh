292ae50ae510f31e6891e002c40d1161d29fd970
Owen Nichols <onichols@apache.org>
2021-08-31 19:19:44 -0700

    run the license review after bumping dependencies, in case indirect bumps require license updates

--- dev-tools/dependencies/bump.sh
+++ dev-tools/dependencies/bump.sh
@@ -26,6 +26,7 @@ if [ "$2" = "-l" ] ; then
   ./gradlew dependencyUpdates -Drevision=release ; find . | grep build/dependencyUpdates/report.txt | xargs cat \
    | grep ' -> ' | egrep -v '(Gradle|antlr|lucene|JUnitParams|docker-compose-rule|javax.servlet-api|springfox|derby|selenium|jgroups|jmh|\[6.0.37|commons-collections|jaxb|testcontainers|gradle-tooling-api|slf4j|archunit)' \
    | sort -u | tr -d '][' | sed -e 's/ -> / /' -e 's#.*:#'"$0 $1"' #'
+  echo "cd .. ; geode/dev-tools/release/license_review.sh -v HEAD ; cd $(pwd)"
   exit 0
 fi
 
