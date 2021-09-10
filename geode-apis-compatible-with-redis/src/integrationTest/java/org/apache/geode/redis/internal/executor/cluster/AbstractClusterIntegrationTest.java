572fbc4ecbe0041e35e61dc8bbaae1a7e382d324
Dan Smith <dasmith@vmware.com>
2021-09-10 16:28:55 -0700

    Adding a randomized test to the AbstractClusterIntegrationTest
    Co-authored-by: Eric Zoerner <zoernere@vmware.com>

--- geode-apis-compatible-with-redis/src/integrationTest/java/org/apache/geode/redis/internal/executor/cluster/AbstractClusterIntegrationTest.java
+++ geode-apis-compatible-with-redis/src/integrationTest/java/org/apache/geode/redis/internal/executor/cluster/AbstractClusterIntegrationTest.java
@@ -15,19 +15,25 @@
 
 package org.apache.geode.redis.internal.executor.cluster;
 
+import static org.apache.geode.redis.internal.RegionProvider.REDIS_SLOTS;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import redis.clients.jedis.HostAndPort;
+import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisCluster;
 import redis.clients.jedis.Protocol;
+import redis.clients.jedis.util.JedisClusterCRC16;
 
 import org.apache.geode.redis.RedisIntegrationTest;
 
 public abstract class AbstractClusterIntegrationTest implements RedisIntegrationTest {
+  private static final int NUM_KEYS_TO_TEST = 1000;
+  private static final int MAX_KEY_LENGTH = 255;
 
   private JedisCluster jedis;
 
@@ -43,26 +49,27 @@ public abstract class AbstractClusterIntegrationTest implements RedisIntegration
 
   @Test
   public void testCluster_givenWrongNumberOfArguments() {
-    assertThatThrownBy(() -> jedis.getConnectionFromSlot(0).sendCommand(Protocol.Command.CLUSTER))
+    final Jedis connection = jedis.getConnectionFromSlot(0);
+    assertThatThrownBy(() -> connection.sendCommand(Protocol.Command.CLUSTER))
         .hasMessage("ERR wrong number of arguments for 'cluster' command");
     assertThatThrownBy(
-        () -> jedis.getConnectionFromSlot(0).sendCommand(Protocol.Command.CLUSTER, "1", "2"))
+        () -> connection.sendCommand(Protocol.Command.CLUSTER, "1", "2"))
             .hasMessage(
                 "ERR Unknown subcommand or wrong number of arguments for '1'. Try CLUSTER HELP.");
     assertThatThrownBy(
-        () -> jedis.getConnectionFromSlot(0).sendCommand(Protocol.Command.CLUSTER, "SLOTS", "1"))
+        () -> connection.sendCommand(Protocol.Command.CLUSTER, "SLOTS", "1"))
             .hasMessage(
                 "ERR Unknown subcommand or wrong number of arguments for 'SLOTS'. Try CLUSTER HELP.");
     assertThatThrownBy(
-        () -> jedis.getConnectionFromSlot(0).sendCommand(Protocol.Command.CLUSTER, "NOTACOMMAND"))
+        () -> connection.sendCommand(Protocol.Command.CLUSTER, "NOTACOMMAND"))
             .hasMessage(
                 "ERR Unknown subcommand or wrong number of arguments for 'NOTACOMMAND'. Try CLUSTER HELP.");
     assertThatThrownBy(
-        () -> jedis.getConnectionFromSlot(0).sendCommand(Protocol.Command.CLUSTER, "KEYSLOT"))
+        () -> connection.sendCommand(Protocol.Command.CLUSTER, "KEYSLOT"))
             .hasMessage(
                 "ERR Unknown subcommand or wrong number of arguments for 'KEYSLOT'. Try CLUSTER HELP.");
     assertThatThrownBy(
-        () -> jedis.getConnectionFromSlot(0).sendCommand(Protocol.Command.CLUSTER, "KEYSLOT",
+        () -> connection.sendCommand(Protocol.Command.CLUSTER, "KEYSLOT",
             "blah", "fo"))
                 .hasMessage(
                     "ERR Unknown subcommand or wrong number of arguments for 'KEYSLOT'. Try CLUSTER HELP.");
@@ -70,14 +77,24 @@ public abstract class AbstractClusterIntegrationTest implements RedisIntegration
 
   @Test
   public void keyslot_ReturnsCorrectSlot() {
-    assertThat(jedis.getConnectionFromSlot(0).clusterKeySlot("nohash")).isEqualTo(9072);
-    assertThat(jedis.getConnectionFromSlot(0).clusterKeySlot("with{hash}")).isEqualTo(238);
-    assertThat(jedis.getConnectionFromSlot(0).clusterKeySlot("with{two}{hashes}")).isEqualTo(2127);
-    assertThat(jedis.getConnectionFromSlot(0).clusterKeySlot("with{borked{hashes}"))
-        .isEqualTo(1058);
-    assertThat(jedis.getConnectionFromSlot(0).clusterKeySlot("with{unmatched")).isEqualTo(10479);
-    assertThat(jedis.getConnectionFromSlot(0).clusterKeySlot("aaa}bbb{tag}ccc")).isEqualTo(8338);
-    assertThat(jedis.getConnectionFromSlot(0).clusterKeySlot("withunmatchedright}"))
-        .isEqualTo(10331);
+    final Jedis connection = jedis.getConnectionFromSlot(0);
+    assertThat(connection.clusterKeySlot("nohash")).isEqualTo(9072);
+    assertThat(connection.clusterKeySlot("with{hash}")).isEqualTo(238);
+    assertThat(connection.clusterKeySlot("with{two}{hashes}")).isEqualTo(2127);
+    assertThat(connection.clusterKeySlot("with{borked{hashes}")).isEqualTo(1058);
+    assertThat(connection.clusterKeySlot("with{unmatched")).isEqualTo(10479);
+    assertThat(connection.clusterKeySlot("aaa}bbb{tag}ccc")).isEqualTo(8338);
+    assertThat(connection.clusterKeySlot("withunmatchedright}")).isEqualTo(10331);
+    assertThat(connection.clusterKeySlot("somekey")).isEqualTo(11058L);
+    assertThat(connection.clusterKeySlot("foo{hash_tag}")).isEqualTo(2515L);
+    assertThat(connection.clusterKeySlot("bar{hash_tag}")).isEqualTo(2515L);
+    assertThat(connection.clusterKeySlot("hash_tag")).isEqualTo(2515L);
+
+    for (int i = 0; i < NUM_KEYS_TO_TEST; i++) {
+      String key = RandomStringUtils.random(i % MAX_KEY_LENGTH + 1);
+      assertThat(connection.clusterKeySlot(key))
+          .withFailMessage("Failure for key %s", key)
+          .isEqualTo(JedisClusterCRC16.getCRC16(key) % (long) REDIS_SLOTS);
+    }
   }
 }
