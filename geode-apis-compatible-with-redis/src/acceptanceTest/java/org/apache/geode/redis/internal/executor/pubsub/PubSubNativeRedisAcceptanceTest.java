2744ab8a902b665e655c02871c43db45562d800e
Ray Ingles <ringles@pivotal.io>
2021-09-28 16:53:27 -0400

    GEODE-9495: limit thread sleep in pub sub native redis acceptance test (#6899)
    Looks up the actual TIME_WAIT timeout value on the operating systems generally used for development and in the CI pipeline. Updated to read the value in a different way on Linux.
    
    Co-authored-by: Ray Ingles <ringles@vmware.com>

--- geode-apis-compatible-with-redis/src/acceptanceTest/java/org/apache/geode/redis/internal/executor/pubsub/PubSubNativeRedisAcceptanceTest.java
+++ geode-apis-compatible-with-redis/src/acceptanceTest/java/org/apache/geode/redis/internal/executor/pubsub/PubSubNativeRedisAcceptanceTest.java
@@ -15,30 +15,70 @@
 
 package org.apache.geode.redis.internal.executor.pubsub;
 
-import org.apache.logging.log4j.Logger;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.apache.commons.lang3.SystemUtils;
 import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.ClassRule;
 
 import org.apache.geode.NativeRedisTestRule;
-import org.apache.geode.logging.internal.log4j.api.LogService;
 
 public class PubSubNativeRedisAcceptanceTest extends AbstractPubSubIntegrationTest {
-
-  private static final Logger logger = LogService.getLogger();
+  private static long socketTimeWaitMsec = 90000;
 
   @ClassRule
   public static NativeRedisTestRule redis = new NativeRedisTestRule();
+
+  @BeforeClass
+  public static void runOnce() throws IOException {
+    if (SystemUtils.IS_OS_LINUX) {
+      try {
+        BufferedReader bufferedReader =
+            new BufferedReader(new FileReader("/proc/sys/net/ipv4/tcp_fin_timeout"));
+        String line = bufferedReader.readLine();
+        if (line != null) {
+          socketTimeWaitMsec = 1000 * Long.parseLong(line.trim());
+        }
+      } catch (NumberFormatException | IOException ignored) {
+      }
+    } else if (SystemUtils.IS_OS_MAC) {
+      try {
+        String line = getCommandOutput("sysctl", "net.inet.tcp.msl");
+        String[] parts = line.split(":");
+        if (parts.length == 2) {
+          socketTimeWaitMsec = 2 * Long.parseLong(parts[1].trim());
+        }
+      } catch (NumberFormatException | IOException ignored) {
+      }
+    }
+    // Just leave timeout at the default if it's some other OS or there's a problem getting OS value
+  }
+
+  private static String getCommandOutput(String... commandStringElements) throws IOException {
+    Process process = new ProcessBuilder(commandStringElements).start();
+    try (BufferedReader reader = new BufferedReader(
+        new InputStreamReader(process.getInputStream()))) {
+      return reader.readLine();
+    } finally {
+      // Probably overkill but ensures test leaves no orphaned processes
+      process.destroy();
+    }
+  }
 
   @AfterClass
   public static void cleanup() throws InterruptedException {
     // This test consumes a lot of sockets and any subsequent tests may fail because of spurious
     // bind exceptions. Even though sockets are closed, they will remain in TIME_WAIT state so we
     // need to wait for that to clear up. It shouldn't take more than a minute or so.
-    // There will be a better solution for this from GEODE-9495, but for now a thread sleep is the
-    // simplest way to wait for the sockets to be out of the TIME_WAIT state. The timeout of 240 sec
-    // was chosen because that is the default duration for TIME_WAIT on Windows. The timeouts for
-    // both mac and linux are significantly shorter.
-    Thread.sleep(240000);
+    // For now a thread sleep is the simplest way to wait for the sockets to be out of the TIME_WAIT
+    // state. The default timeout of 90 sec was chosen because that is comfortably larger than the
+    // default TIME_WAIT on Linux (60 sec) or OSX (30 sec).
+    // Windows has a default TIME_WAIT of 240 sec, but the CI pipelines do not current use Windows.
+    Thread.sleep(socketTimeWaitMsec);
   }
 
   @Override
