1bdf51ca576a6eb0900834443e4962f68d6dece6
Ray Ingles <ringles@vmware.com>
2021-09-28 17:21:11 -0400

    Refactor and some polish

--- geode-apis-compatible-with-redis/src/main/java/org/apache/geode/redis/internal/executor/server/LolWutExecutor.java
+++ geode-apis-compatible-with-redis/src/main/java/org/apache/geode/redis/internal/executor/server/LolWutExecutor.java
@@ -16,6 +16,8 @@
 package org.apache.geode.redis.internal.executor.server;
 
 
+import static org.apache.geode.redis.internal.RedisConstants.ERROR_NOT_INTEGER;
+
 import java.util.List;
 import java.util.Random;
 
@@ -27,46 +29,70 @@ import org.apache.geode.redis.internal.netty.Command;
 import org.apache.geode.redis.internal.netty.ExecutionHandlerContext;
 
 public class LolWutExecutor extends AbstractExecutor {
+
+  public static final int DEFAULT_WIDTH = 40;
+  public static final int DEFAULT_HEIGHT = 10;
+  private static int width = DEFAULT_WIDTH;
+  private static int height = DEFAULT_HEIGHT;
+
   @Override
   public RedisResponse executeCommand(Command command,
       ExecutionHandlerContext context) {
-    long height = 10;
+
+    long inputWidth = -1;
+    long inputHeight = -1;
+
     List<byte[]> commands = command.getProcessedCommand();
     if (commands.size() > 1) {
       for (int i = 1; i < commands.size(); i++) {
         if (Coder.bytesToString(commands.get(i)).equalsIgnoreCase("version")) {
           i += 1; // skip next arg, we only have one version for now
         } else {
-          height = Coder.bytesToLong(commands.get(i));
+          try {
+            if (inputWidth < 0) {
+              inputWidth = Coder.bytesToLong(commands.get(i));
+            } else if (inputHeight < 0) {
+              inputHeight = Coder.bytesToLong(commands.get(i));
+            } else {
+              break; // all required args filled
+            }
+          } catch (NumberFormatException ignored) {
+            return RedisResponse.error(ERROR_NOT_INTEGER);
+          }
         }
       }
     }
+    if (inputHeight >= 0) {
+      height = (int) inputHeight;
+    }
+    if (inputWidth >= 0) {
+      width = (int) inputWidth;
+    }
 
-    return RedisResponse.bulkString(makeArbitraryHeightMaze(height));
+    return RedisResponse.bulkString(makeArbitraryHeightMaze());
   }
 
   // Adapted from code here: https://tromp.github.io/maze.html
-  public static String makeArbitraryHeightMaze(long height) {
+  public static String makeArbitraryHeightMaze() {
     StringBuilder mazeString = new StringBuilder();
-    final int width = 40;
     int[] leftLinks = new int[width];
     int[] rightLinks = new int[width];
 
     Random rand = new Random();
     leftLinks[0] = 1;
 
-    mazeTopAndEntrance(mazeString, width, leftLinks, rightLinks);
+    mazeTopAndEntrance(mazeString, leftLinks, rightLinks);
 
-    mazeRows(mazeString, height, width, leftLinks, rightLinks, rand);
+    mazeRows(mazeString, leftLinks, rightLinks, rand);
 
-    mazeBottomRow(mazeString, width, leftLinks, rightLinks, rand);
+    mazeBottomRow(mazeString, leftLinks, rightLinks, rand);
 
     mazeString.append("\n " + KnownVersion.getCurrentVersion().toString() + "\n");
 
     return mazeString.toString();
   }
 
-  private static void mazeTopAndEntrance(StringBuilder mazeString, int width, int[] leftLinks,
+  private static void mazeTopAndEntrance(StringBuilder mazeString, int[] leftLinks,
       int[] rightLinks) {
     int tempIndex;
     for (tempIndex = width; --tempIndex > 0; leftLinks[tempIndex] =
@@ -76,13 +102,14 @@ public class LolWutExecutor extends AbstractExecutor {
     mazeString.append("\n "); // Open wall for entrance at top left
   }
 
-  private static void mazeRows(StringBuilder mazeString, long height,
-      int width, int[] leftLinks,
+  private static void mazeRows(StringBuilder mazeString,
+      int[] leftLinks,
       int[] rightLinks, Random rand) {
     int currentCell;
+    int tempIndex;
     String first;
     String second;
-    int tempIndex;
+
     while (--height > 0) {
       for (currentCell = width; --currentCell > 0;) {
         if (currentCell != (tempIndex = leftLinks[currentCell - 1])
@@ -91,7 +118,7 @@ public class LolWutExecutor extends AbstractExecutor {
           leftLinks[rightLinks[currentCell]] = tempIndex;
           rightLinks[currentCell] = currentCell - 1;
           leftLinks[currentCell - 1] = currentCell;
-          second = ".";
+          second = "."; // no wall to right
         } else {
           second = "|"; // wall to the right
         }
@@ -112,10 +139,11 @@ public class LolWutExecutor extends AbstractExecutor {
     }
   }
 
-  private static void mazeBottomRow(StringBuilder mazeString, int width, int[] leftLinks,
+  private static void mazeBottomRow(StringBuilder mazeString, int[] leftLinks,
       int[] rightLinks, Random rand) {
     int currentCell;
     int tempIndex;
+
     for (currentCell = width; --currentCell > 0;) {
       if (currentCell != (tempIndex = leftLinks[currentCell - 1])
           && (currentCell == rightLinks[currentCell] || rand.nextBoolean())) {
@@ -124,7 +152,7 @@ public class LolWutExecutor extends AbstractExecutor {
         mazeString.append("_.");
       } else {
         if (currentCell == 1) {
-          mazeString.append("_ "); // maze exit
+          mazeString.append("_."); // maze exit
         } else {
           mazeString.append("_|"); // regular wall
         }
