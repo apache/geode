2ecd20f79b821d0943fe9059ad44bff32f656e85
Jacob Barrett <jbarrett@pivotal.io>
2021-10-13 14:14:38 -0700

    MODULE: Create TX grouping sender impls.

--- geode-wan/src/main/java/org/apache/geode/cache/wan/internal/txgrouping/parallel/TxGroupingParallelGatewaySenderImpl.java
+++ geode-wan/src/main/java/org/apache/geode/cache/wan/internal/txgrouping/parallel/TxGroupingParallelGatewaySenderImpl.java
@@ -17,40 +17,22 @@ package org.apache.geode.cache.wan.internal.txgrouping.parallel;
 
 import org.jetbrains.annotations.NotNull;
 
-import org.apache.geode.cache.wan.GatewaySender;
-import org.apache.geode.cache.wan.internal.parallel.ParallelGatewaySenderTypeFactory;
-import org.apache.geode.cache.wan.internal.txgrouping.CommonTxGroupingGatewaySenderFactory;
+import org.apache.geode.cache.wan.internal.parallel.ParallelGatewaySenderImpl;
 import org.apache.geode.internal.cache.InternalCache;
 import org.apache.geode.internal.cache.wan.GatewaySenderAttributes;
-import org.apache.geode.internal.cache.wan.GatewaySenderException;
-import org.apache.geode.internal.cache.wan.MutableGatewaySenderAttributes;
 import org.apache.geode.internal.statistics.StatisticsClock;
 
-public class TxGroupingParallelGatewaySenderTypeFactory extends ParallelGatewaySenderTypeFactory {
+public class TxGroupingParallelGatewaySenderImpl extends ParallelGatewaySenderImpl {
 
-  @Override
-  public @NotNull String getType() {
-    return "TxGroupingParallelGatewaySender";
-  }
-
-  @Override
-  public void validate(final @NotNull MutableGatewaySenderAttributes attributes)
-      throws GatewaySenderException {
-    super.validate(attributes);
-
-    CommonTxGroupingGatewaySenderFactory.validate(this, attributes);
-  }
-
-  @Override
-  public GatewaySender create(final @NotNull InternalCache cache,
+  public TxGroupingParallelGatewaySenderImpl(final @NotNull InternalCache cache,
       final @NotNull StatisticsClock clock,
       final @NotNull GatewaySenderAttributes attributes) {
-    return new TxGroupingParallelGatewaySenderImpl(cache, clock, attributes);
+    super(cache, clock, attributes);
   }
 
   @Override
-  public GatewaySender createCreation(final @NotNull InternalCache cache,
-      final @NotNull GatewaySenderAttributes attributes) {
-    return new TxGroupingParallelGatewaySenderCreation(cache, attributes);
+  public boolean mustGroupTransactionEvents() {
+    return true;
   }
+
 }
