00f92b660b6eff5cbd1dc349e0b795955bde728f
Xiaojian Zhou <gesterzhou@users.noreply.github.com>
2021-10-13 14:05:30 -0700

    GEODE-9677: After provider sends ImageReply, requester could finish e… (#6992)

--- geode-core/src/distributedTest/java/org/apache/geode/internal/cache/GIIDeltaDUnitTest.java
+++ geode-core/src/distributedTest/java/org/apache/geode/internal/cache/GIIDeltaDUnitTest.java
@@ -1530,8 +1530,7 @@ public class GIIDeltaDUnitTest extends JUnit4CacheTestCase {
     // let GII continue
     P.invoke(() -> resetGIITestHook(DuringPackingImage, true));
     async3.join(MAX_WAIT * 2);
-    count = getDeltaGIICount(P);
-    assertEquals(0, count);
+    GeodeAwaitility.await().untilAsserted(() -> assertEquals(0, getDeltaGIICount(P)));
     verifyDeltaSizeFromStats(R, 1, 1); // deltaGII, key1 in delta
 
     // tombstone key2, key5 should still exist and expired at R
