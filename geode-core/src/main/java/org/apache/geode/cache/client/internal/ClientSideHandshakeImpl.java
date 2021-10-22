c865ea66151dfcc3c16b1d86f0ee0bd1ea04ea89
Mario Ivanac <48509724+mivanac@users.noreply.github.com>
2021-10-22 06:15:14 +0200

    GEODE-9743: updated ClientSideHandshakeImpl and added UT (#7008)
    * GEODE-9743: updated ClientSideHandshakeImpl and added UT
    
    * GEODE-9743: update after comments

--- geode-core/src/main/java/org/apache/geode/cache/client/internal/ClientSideHandshakeImpl.java
+++ geode-core/src/main/java/org/apache/geode/cache/client/internal/ClientSideHandshakeImpl.java
@@ -211,7 +211,8 @@ public class ClientSideHandshakeImpl extends Handshake implements ClientSideHand
 
       // Successful handshake for GATEWAY_TO_GATEWAY mode sets the peer version in connection
       if (communicationMode.isWAN() && !(acceptanceCode == REPLY_EXCEPTION_AUTHENTICATION_REQUIRED
-          || acceptanceCode == REPLY_EXCEPTION_AUTHENTICATION_FAILED)) {
+          || acceptanceCode == REPLY_EXCEPTION_AUTHENTICATION_FAILED
+          || acceptanceCode == REPLY_REFUSED || acceptanceCode == REPLY_INVALID)) {
         short wanSiteVersion = Version.readOrdinal(dis);
         conn.setWanSiteVersion(wanSiteVersion);
         // establish a versioned stream for the other site, if necessary
