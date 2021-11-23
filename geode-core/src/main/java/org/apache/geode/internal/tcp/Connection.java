5955adb12dea0f29941ca596adca92ccb3b1c32d
Bill Burcham <bill.burcham@gmail.com>
2021-11-23 08:29:11 -0800

    GEODE-9825: processInputBuffer resize retains data (#7131)
    (cherry picked from commit fb142e1bbd42d6af2463fd9b9b49ef3e5519cfcb)
    (cherry picked from commit 62bc9aee3f929103411fc2dbe66cc750e56c06c1)

--- geode-core/src/main/java/org/apache/geode/internal/tcp/Connection.java
+++ geode-core/src/main/java/org/apache/geode/internal/tcp/Connection.java
@@ -2750,6 +2750,9 @@ public class Connection implements Runnable {
   /**
    * processes the current NIO buffer. If there are complete messages in the buffer, they are
    * deserialized and passed to TCPConduit for further processing
+   *
+   * pre-condition: inputBuffer (from inputSharing.getBuffer()) is in WRITABLE mode
+   * post-condition: inputBuffer is in WRITABLE mode
    */
   private void processInputBuffer() throws ConnectionException, IOException {
     try (final ByteBufferSharing inputSharing = inputBufferVendor.open()) {
@@ -2825,12 +2828,12 @@ public class Connection implements Runnable {
                       "Allocating larger network read buffer, new size is {} old size was {}.",
                       allocSize, oldBufferSize);
                   inputBuffer = inputSharing.expandReadBufferIfNeeded(allocSize);
+                  makeReadableBufferWriteable(inputBuffer);
                 } else {
                   if (inputBuffer.position() != 0) {
                     inputBuffer.compact();
                   } else {
-                    inputBuffer.position(inputBuffer.limit());
-                    inputBuffer.limit(inputBuffer.capacity());
+                    makeReadableBufferWriteable(inputBuffer);
                   }
                 }
               }
@@ -2842,6 +2845,11 @@ public class Connection implements Runnable {
         }
       }
     }
+  }
+
+  private void makeReadableBufferWriteable(final ByteBuffer inputBuffer) {
+    inputBuffer.position(inputBuffer.limit());
+    inputBuffer.limit(inputBuffer.capacity());
   }
 
   private boolean readHandshakeForReceiver(DataInput dis) {
