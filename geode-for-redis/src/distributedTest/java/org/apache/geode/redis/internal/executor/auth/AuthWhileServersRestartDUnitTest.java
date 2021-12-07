645df9373ca82fcfb3560c04df69e23c1a531320
Jens Deppe <jdeppe@vmware.com>
2021-12-07 11:02:05 -0800

    GEODE-9870: Avoid Radish MOVED error during flushAll in AuthWhileServersRestartDUnitTest
    - Since servers have been stopped and restarted, as part of the test,
      buckets may be moving. Occasionally this may still be the case when
      the test is done and flushAll is cleaning up. To avoid that, make sure
      that everything is rebalanced before flushAll is called.

--- geode-for-redis/src/distributedTest/java/org/apache/geode/redis/internal/executor/auth/AuthWhileServersRestartDUnitTest.java
+++ geode-for-redis/src/distributedTest/java/org/apache/geode/redis/internal/executor/auth/AuthWhileServersRestartDUnitTest.java
@@ -50,6 +50,7 @@ public class AuthWhileServersRestartDUnitTest {
   @ClassRule
   public static ExecutorServiceRule executor = new ExecutorServiceRule();
 
+  private static MemberVM server1;
   private static int redisServerPort;
   private static SerializableFunction<ServerStarterRule> operatorForVM3;
   private static final String KEY = "key";
@@ -65,7 +66,7 @@ public class AuthWhileServersRestartDUnitTest {
         .withCredential("cluster", "cluster")
         .withConnectionToLocator(locatorPort);
 
-    clusterStartUp.startRedisVM(1, serverOperator);
+    server1 = clusterStartUp.startRedisVM(1, serverOperator);
     clusterStartUp.startRedisVM(2, serverOperator);
 
     int server3Port = AvailablePortHelper.getRandomAvailableTCPPort();
@@ -85,6 +86,9 @@ public class AuthWhileServersRestartDUnitTest {
 
   @After
   public void after() {
+    // Make sure that no buckets are moving before calling flushAll, otherwise we might get
+    // a MOVED exception.
+    clusterStartUp.rebalanceAllRegions();
     clusterStartUp.flushAll("data", "data");
   }
 
