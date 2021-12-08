927eaddd39a82b378c032566403866a3b1b14ed8
Jens Deppe <jdeppe@vmware.com>
2021-12-07 18:27:58 -0800

    Removed unnecessary field

--- geode-for-redis/src/distributedTest/java/org/apache/geode/redis/internal/executor/auth/AuthWhileServersRestartDUnitTest.java
+++ geode-for-redis/src/distributedTest/java/org/apache/geode/redis/internal/executor/auth/AuthWhileServersRestartDUnitTest.java
@@ -50,7 +50,6 @@ public class AuthWhileServersRestartDUnitTest {
   @ClassRule
   public static ExecutorServiceRule executor = new ExecutorServiceRule();
 
-  private static MemberVM server1;
   private static int redisServerPort;
   private static SerializableFunction<ServerStarterRule> operatorForVM3;
   private static final String KEY = "key";
@@ -66,7 +65,7 @@ public class AuthWhileServersRestartDUnitTest {
         .withCredential("cluster", "cluster")
         .withConnectionToLocator(locatorPort);
 
-    server1 = clusterStartUp.startRedisVM(1, serverOperator);
+    clusterStartUp.startRedisVM(1, serverOperator);
     clusterStartUp.startRedisVM(2, serverOperator);
 
     int server3Port = AvailablePortHelper.getRandomAvailableTCPPort();
