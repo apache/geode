29a9984d5a0d4f703e40a961a0690ba9d6e1329a
Donal Evans <doevans@vmware.com>
2022-01-07 14:32:55 -0800

    GEODE-9934: Move Redis MemoryOverheadIntegration test classes
     - Change package for AbstractMemoryOverheadIntegrationTest and child
     classes from org.apache.geode.redis.internal.commands.executor.hash to
     org.apache.geode.redis.internal.data
    
    Authored-by: Donal Evans <doevans@vmware.com>

--- geode-for-redis/src/acceptanceTest/java/org/apache/geode/redis/internal/data/MemoryOverheadNativeRedisAcceptanceTest.java
+++ geode-for-redis/src/acceptanceTest/java/org/apache/geode/redis/internal/data/MemoryOverheadNativeRedisAcceptanceTest.java
@@ -12,7 +12,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package org.apache.geode.redis.internal.commands.executor.hash;
+package org.apache.geode.redis.internal.data;
 
 import static org.junit.Assert.assertTrue;
 
