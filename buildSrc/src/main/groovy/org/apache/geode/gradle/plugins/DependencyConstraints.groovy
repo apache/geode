d9f3a1fdde433196443077086d072f7fd140ed77
Owen Nichols <onichols@apache.org>
2022-01-07 19:23:06 -0800

    GEODE-8980: Bump spring-security from 5.5.2 to 5.6.1

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -250,7 +250,7 @@ class DependencyConstraints implements Plugin<Project> {
       entry('selenium-support')
     }
 
-    dependencySet(group: 'org.springframework.security', version: '5.5.2') {
+    dependencySet(group: 'org.springframework.security', version: '5.6.1') {
       entry('spring-security-config')
       entry('spring-security-core')
       entry('spring-security-ldap')
