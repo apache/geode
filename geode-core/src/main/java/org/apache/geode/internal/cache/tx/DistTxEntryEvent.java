fe6920f14848adf7ebda7c46c61c4b865fbfcbe3
Jacob Barrett <jbarrett@pivotal.io>
2022-01-13 20:59:53 -0800

    GEODE-6588: Cleanup static analyzer warnings.

--- geode-core/src/main/java/org/apache/geode/internal/cache/tx/DistTxEntryEvent.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/tx/DistTxEntryEvent.java
@@ -30,11 +30,9 @@ import org.apache.geode.internal.cache.DistributedRemoveAllOperation.RemoveAllEn
 import org.apache.geode.internal.cache.EntryEventImpl;
 import org.apache.geode.internal.cache.versions.VersionTag;
 import org.apache.geode.internal.offheap.annotations.Retained;
-import org.apache.geode.internal.serialization.ByteArrayDataInput;
 import org.apache.geode.internal.serialization.DeserializationContext;
 import org.apache.geode.internal.serialization.KnownVersion;
 import org.apache.geode.internal.serialization.SerializationContext;
-import org.apache.geode.internal.serialization.StaticSerialization;
 
 public class DistTxEntryEvent extends EntryEventImpl {
 
@@ -152,8 +150,6 @@ public class DistTxEntryEvent extends EntryEventImpl {
     int putAllSize = DataSerializer.readInteger(in);
     PutAllEntryData[] putAllEntries = new PutAllEntryData[putAllSize];
     if (putAllSize > 0) {
-      final KnownVersion version = StaticSerialization.getVersionForDataStreamOrNull(in);
-      final ByteArrayDataInput bytesIn = new ByteArrayDataInput();
       for (int i = 0; i < putAllSize; i++) {
         putAllEntries[i] = new PutAllEntryData(in, context, eventID, i);
       }
@@ -202,8 +198,6 @@ public class DistTxEntryEvent extends EntryEventImpl {
       DeserializationContext context) throws IOException, ClassNotFoundException {
     int removeAllSize = DataSerializer.readInteger(in);
     final RemoveAllEntryData[] removeAllData = new RemoveAllEntryData[removeAllSize];
-    final KnownVersion version = StaticSerialization.getVersionForDataStreamOrNull(in);
-    final ByteArrayDataInput bytesIn = new ByteArrayDataInput();
     for (int i = 0; i < removeAllSize; i++) {
       removeAllData[i] = new RemoveAllEntryData(in, eventID, i, context);
     }
@@ -246,10 +240,10 @@ public class DistTxEntryEvent extends EntryEventImpl {
     buf.append(getKeyInfo().getBucketId());
     buf.append(";oldValue=");
     if (putAllOp != null) {
-      buf.append(";putAllDataSize :" + putAllOp.putAllDataSize);
+      buf.append(";putAllDataSize :").append(putAllOp.putAllDataSize);
     }
     if (removeAllOp != null) {
-      buf.append(";removeAllDataSize :" + removeAllOp.removeAllDataSize);
+      buf.append(";removeAllDataSize :").append(removeAllOp.removeAllDataSize);
     }
     buf.append("]");
     return buf.toString();
