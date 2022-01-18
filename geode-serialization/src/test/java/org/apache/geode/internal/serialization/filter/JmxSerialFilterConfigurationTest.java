03fe0cb9734046a1c018501306c694e642f8ccdd
Kirk Lund <klund@apache.org>
2022-01-18 11:01:25 -0800

    GEODE-9758: Add internal serial filter API (#7217)
    GEODE-9758: Add internal serial filter API #7217
    
    Expand ObjectInputStreamFilterWrapper to be an internal API which
    supports all of Geode's uses of Java's ObjectInputFilter.
    
    Introduce a new system property, geode.enableGlobalSerialFilter, to
    enable a process-wide filter with all serializable Geode classes on the
    classpath and the value of serializable-object-filter accept-listed.
    
    To enable the process-wide filter with GFSH start commands, add:
    
    * --J=-Dgeode.enableGlobalSerialFilter=true
    
    Functional Capabilities
    
    The internal API lives in geode-serialization and works on OpenJDK
    based JREs providing a facade for Java's ObjectInputFilter in Java 8
    and Java 9 or greater using reflection. The API provides the following
    capabilities:
    
    * creating an ObjectInputFilter
    * setting an ObjectInputFilter on an ObjectInputStream
    * getting an ObjectInputFilter from a ObjectInputStream
    * setting a process-wide ObjectInputFilter
    * getting a process-wide ObjectInputFilter
    
    Design Notes
    
    The API defines the following primary interface types:
    
    * factory interfaces for creating instances of types within the API
    * filter interfaces to split out single ops from Java's
      ObjectInputFilter
    * configuration interfaces for handling system properties, logging,
      and config validation
    
    The concrete classes in the API receive parameters injected via a
    constructor for any collaborators that are not specified by the
    interfaces. This is intentional even when the instance is only used
    once before de-referencing it. All collaborators that are defined in
    the interface are passed in as parameters to the implementing
    method; all others are passed in via the constructor and stored as
    fields.
    
    (cherry picked from commit 7978abf34707d11da45cff0b7cb7445f18d21438)
    (cherry picked from commit f9742384edf23b8b2dd23c8d37e947a894836651)

---
+++ geode-serialization/src/test/java/org/apache/geode/internal/serialization/filter/JmxSerialFilterConfigurationTest.java
@@ -0,0 +1,155 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.geode.internal.serialization.filter;
+
+import static org.apache.geode.util.internal.UncheckedUtils.uncheckedCast;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+import java.util.function.Consumer;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.contrib.java.lang.system.RestoreSystemProperties;
+
+public class JmxSerialFilterConfigurationTest {
+
+  private static final String SYSTEM_PROPERTY = "system.property.name";
+
+  private String pattern;
+  private Consumer<String> loggerConsumer;
+
+  @Rule
+  public RestoreSystemProperties restoreSystemProperties = new RestoreSystemProperties();
+
+  @Before
+  public void setUp() {
+    pattern = "the-filter-pattern";
+    loggerConsumer = uncheckedCast(mock(Consumer.class));
+  }
+
+  @Test
+  public void propertyValue_isNullByDefault() {
+    assertThat(System.getProperty(SYSTEM_PROPERTY))
+        .as(SYSTEM_PROPERTY)
+        .isNull();
+  }
+
+  @Test
+  public void setsPropertyValue() {
+    FilterConfiguration filterConfiguration =
+        new JmxSerialFilterConfiguration(SYSTEM_PROPERTY, pattern);
+
+    filterConfiguration.configure();
+
+    assertThat(System.getProperty(SYSTEM_PROPERTY))
+        .as(SYSTEM_PROPERTY)
+        .isNotEmpty();
+  }
+
+  @Test
+  public void setsPropertyValue_ifExistingValueIsNull() {
+    System.clearProperty(SYSTEM_PROPERTY);
+    FilterConfiguration filterConfiguration =
+        new JmxSerialFilterConfiguration(SYSTEM_PROPERTY, pattern);
+
+    filterConfiguration.configure();
+
+    assertThat(System.getProperty(SYSTEM_PROPERTY))
+        .as(SYSTEM_PROPERTY)
+        .isEqualTo(pattern);
+  }
+
+  @Test
+  public void setsPropertyValue_ifExistingValueIsEmpty() {
+    System.setProperty(SYSTEM_PROPERTY, "");
+    FilterConfiguration filterConfiguration =
+        new JmxSerialFilterConfiguration(SYSTEM_PROPERTY, pattern);
+
+    filterConfiguration.configure();
+
+    assertThat(System.getProperty(SYSTEM_PROPERTY))
+        .as(SYSTEM_PROPERTY)
+        .isEqualTo(pattern);
+  }
+
+  @Test
+  public void setsPropertyValue_ifExistingValueIsBlank() {
+    System.setProperty(SYSTEM_PROPERTY, " ");
+    FilterConfiguration filterConfiguration =
+        new JmxSerialFilterConfiguration(SYSTEM_PROPERTY, pattern);
+
+    filterConfiguration.configure();
+
+    assertThat(System.getProperty(SYSTEM_PROPERTY))
+        .as(SYSTEM_PROPERTY)
+        .isEqualTo(pattern);
+  }
+
+  @Test
+  public void logsNowConfigured_ifExistingValueIsEmpty() {
+    System.setProperty(SYSTEM_PROPERTY, "");
+    FilterConfiguration filterConfiguration =
+        new JmxSerialFilterConfiguration(SYSTEM_PROPERTY, pattern, loggerConsumer);
+
+    filterConfiguration.configure();
+
+    verify(loggerConsumer)
+        .accept("System property '" + SYSTEM_PROPERTY + "' is now configured with '" +
+            pattern + "'.");
+  }
+
+  @Test
+  public void logsNowConfigured_ifExistingValueIsBlank() {
+    System.setProperty(SYSTEM_PROPERTY, " ");
+    FilterConfiguration filterConfiguration =
+        new JmxSerialFilterConfiguration(SYSTEM_PROPERTY, pattern, loggerConsumer);
+
+    filterConfiguration.configure();
+
+    verify(loggerConsumer)
+        .accept("System property '" + SYSTEM_PROPERTY + "' is now configured with '" +
+            pattern + "'.");
+  }
+
+  @Test
+  public void doesNotSetPropertyValue_ifExistingValueIsNotEmpty() {
+    String existingValue = "existing-value-of-property";
+    System.setProperty(SYSTEM_PROPERTY, existingValue);
+    FilterConfiguration filterConfiguration =
+        new JmxSerialFilterConfiguration(SYSTEM_PROPERTY, pattern);
+
+    filterConfiguration.configure();
+
+    assertThat(System.getProperty(SYSTEM_PROPERTY))
+        .as(SYSTEM_PROPERTY)
+        .isEqualTo(existingValue);
+  }
+
+  @Test
+  public void logsAlreadyConfiguredMessage_ifExistingPropertyValueIsNotEmpty() {
+    String existingValue = "existing-value-of-property";
+    System.setProperty(SYSTEM_PROPERTY, existingValue);
+    FilterConfiguration filterConfiguration =
+        new JmxSerialFilterConfiguration(SYSTEM_PROPERTY, pattern, loggerConsumer);
+
+    filterConfiguration.configure();
+
+    verify(loggerConsumer)
+        .accept("System property '" + SYSTEM_PROPERTY + "' is already configured.");
+  }
+}
