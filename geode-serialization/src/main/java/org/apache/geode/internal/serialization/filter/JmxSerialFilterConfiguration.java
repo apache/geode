c7ebf4da3431741c8a420aff2b2555242365ba54
Kirk Lund <klund@apache.org>
2022-01-18 11:01:25 -0800

    GEODE-9758: Add internal serial filter API (#7217)
    GEODE-9758: Add internal serial filter API #7217
    
    Expand ObjectInputStreamFilterWrapper to be an internal API which
    supports all of Geode's uses of Java's ObjectInputFilter.
    
    Introduce a new system property, geode.enableGlobalSerialFilter, to
    enable a process-wide filter with all serializable Geode classes on the
    classpath and the value of serializable-object-filter accept-listed.
    
    To enable the process-wide filter with GFSH start commands, add:
    
    * --J=-Dgeode.enableGlobalSerialFilter=true
    
    Functional Capabilities
    
    The internal API lives in geode-serialization and works on OpenJDK
    based JREs providing a facade for Java's ObjectInputFilter in Java 8
    and Java 9 or greater using reflection. The API provides the following
    capabilities:
    
    * creating an ObjectInputFilter
    * setting an ObjectInputFilter on an ObjectInputStream
    * getting an ObjectInputFilter from a ObjectInputStream
    * setting a process-wide ObjectInputFilter
    * getting a process-wide ObjectInputFilter
    
    Design Notes
    
    The API defines the following primary interface types:
    
    * factory interfaces for creating instances of types within the API
    * filter interfaces to split out single ops from Java's
      ObjectInputFilter
    * configuration interfaces for handling system properties, logging,
      and config validation
    
    The concrete classes in the API receive parameters injected via a
    constructor for any collaborators that are not specified by the
    interfaces. This is intentional even when the instance is only used
    once before de-referencing it. All collaborators that are defined in
    the interface are passed in as parameters to the implementing
    method; all others are passed in via the constructor and stored as
    fields.
    
    (cherry picked from commit 7978abf34707d11da45cff0b7cb7445f18d21438)

---
+++ geode-serialization/src/main/java/org/apache/geode/internal/serialization/filter/JmxSerialFilterConfiguration.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor license
+ * agreements. See the NOTICE file distributed with this work for additional information regarding
+ * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License. You may obtain a
+ * copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.apache.geode.internal.serialization.filter;
+
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
+import java.util.function.Consumer;
+
+import org.apache.logging.log4j.Logger;
+
+import org.apache.geode.annotations.VisibleForTesting;
+import org.apache.geode.logging.internal.log4j.api.LogService;
+
+/**
+ * Implementation of {@code FilterConfiguration} for configuring the JVMs JMX serialization filter.
+ *
+ * <p>
+ * Configure the “jmx.remote.rmi.server.serial.filter.pattern” system property if Java version is
+ * Java 9 or greater. The serial pattern will be configured to accept only standard JMX open-types.
+ * If the system property already has a non-null value, then leave it as is.
+ *
+ * <p>
+ * Configure the {@code jdk.serialFilter} system property if Java version is Java 8. The serial
+ * pattern will be configured to accept only geode sanctioned serializables and standard JMX
+ * open-types. If the system property already has a non-null value, then leave it as is.
+ */
+class JmxSerialFilterConfiguration implements FilterConfiguration {
+
+  private static final Logger LOGGER = LogService.getLogger();
+
+  private final String key;
+  private final String value;
+  private final Consumer<String> logger;
+
+  /**
+   * Constructs instance for the specified system property and filter pattern.
+   */
+  JmxSerialFilterConfiguration(String property, String pattern) {
+    this(property, pattern, LOGGER::info);
+  }
+
+  @VisibleForTesting
+  JmxSerialFilterConfiguration(String property, String pattern, Consumer<String> logger) {
+    key = property;
+    value = pattern;
+    this.logger = logger;
+  }
+
+  @Override
+  public boolean configure() {
+    if (isBlank(System.getProperty(key))) {
+      System.setProperty(key, value);
+      logger.accept("System property '" + key + "' is now configured with '" + value + "'.");
+      return true;
+    }
+
+    logger.accept("System property '" + key + "' is already configured.");
+    return false;
+  }
+}
