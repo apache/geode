aafc3bb24c3148f1b0135ade0c95bf65a67c29c4
Dan Smith <dasmith@vmware.com>
2022-01-28 10:14:52 -0800

    Adding license header and applying review comments

--- dev-tools/release-testing/smoketest_rc.sh
+++ dev-tools/release-testing/smoketest_rc.sh
@@ -1,6 +1,19 @@
-#!/bin/bash
-
-
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 set -e
 
@@ -140,10 +153,14 @@ set +x
 echo ""
 echo "============================================================"
 echo "Verifying Signatures"
+echo "The PGP verification requires that you have imported geodes KEYS file"
+echo "as described on https://geode.apache.org/releases/"
+echo "If PGP verification fails, ensure you have imported the KEYS file"
+echo "in the geode home directory"
+echo "gpg --import KEYS"
 echo "============================================================"
 
 set -x
-cd $WORKSPACE
 cd $DOWNLOAD_DIR
 ls *.zip *.tar.gz *.tgz | xargs -n1 -I {} bash -c "shasum -c {}.sha*"
 ls *.zip *.tar.gz *.tgz | xargs -n1 -I {} bash -c "gpg --verify {}.asc"
