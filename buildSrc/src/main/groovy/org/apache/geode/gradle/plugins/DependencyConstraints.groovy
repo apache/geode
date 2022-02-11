095aae72ca97d6aab4fd7df86e7e276c9641917f
Owen Nichols <onichols@apache.org>
2022-02-11 11:02:22 -0800

    GEODE-10046: Bump jproc from 2.6.2 to 2.8.0

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -150,7 +150,7 @@ class DependencyConstraints implements Plugin<Project> {
         api(group: 'org.apache.shiro', name: 'shiro-core', version: get('shiro.version'))
         api(group: 'org.assertj', name: 'assertj-core', version: '3.22.0')
         api(group: 'org.awaitility', name: 'awaitility', version: '4.1.1')
-        api(group: 'org.buildobjects', name: 'jproc', version: '2.6.2')
+        api(group: 'org.buildobjects', name: 'jproc', version: '2.8.0')
         api(group: 'org.codehaus.cargo', name: 'cargo-core-uberjar', version: '1.9.9')
         api(group: 'org.eclipse.jetty', name: 'jetty-server', version: get('jetty.version'))
         api(group: 'org.eclipse.jetty', name: 'jetty-webapp', version: get('jetty.version'))
