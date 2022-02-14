07a6c6f1d5da833107999c63bb79ddd1e247fde2
Kirk Lund <klund@apache.org>
2022-02-14 13:57:25 -0800

    Fix Gfsh and Redis packages for 1.14

--- geode-apis-compatible-with-redis/src/integrationTest/java/org/apache/geode/redis/internal/RedisSanctionedSerializablesServiceIntegrationTest.java
+++ geode-apis-compatible-with-redis/src/integrationTest/java/org/apache/geode/redis/internal/RedisSanctionedSerializablesServiceIntegrationTest.java
@@ -18,7 +18,6 @@ import org.junit.experimental.categories.Category;
 
 import org.apache.geode.codeAnalysis.SanctionedSerializablesServiceIntegrationTestBase;
 import org.apache.geode.internal.serialization.filter.SanctionedSerializablesService;
-import org.apache.geode.redis.internal.services.RedisSanctionedSerializablesService;
 import org.apache.geode.test.junit.categories.SanctionedSerializablesTest;
 import org.apache.geode.test.junit.categories.SerializationTest;
 
