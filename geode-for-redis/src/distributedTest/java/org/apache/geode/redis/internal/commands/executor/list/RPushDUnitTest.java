08ac2311323ae717656546b23585e37cb33a710c
Jens Deppe <jdeppe@vmware.com>
2022-03-15 13:39:54 -0700

    Rework and simplify tests in RPushDUnitTest

--- geode-for-redis/src/distributedTest/java/org/apache/geode/redis/internal/commands/executor/list/RPushDUnitTest.java
+++ geode-for-redis/src/distributedTest/java/org/apache/geode/redis/internal/commands/executor/list/RPushDUnitTest.java
@@ -15,117 +15,104 @@
 
 package org.apache.geode.redis.internal.commands.executor.list;
 
+import static org.apache.geode.distributed.ConfigurationProperties.GEODE_FOR_REDIS_PORT;
 import static org.apache.geode.test.dunit.rules.RedisClusterStartupRule.BIND_ADDRESS;
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.ExecutionException;
+import java.util.Map;
 import java.util.concurrent.Future;
-import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.IntStream;
 
 import com.google.common.collect.Streams;
 import org.junit.After;
-import org.junit.Before;
-import org.junit.Rule;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Test;
 import redis.clients.jedis.HostAndPort;
 import redis.clients.jedis.JedisCluster;
 
+import org.apache.geode.internal.cache.BucketDump;
+import org.apache.geode.internal.cache.InternalCache;
+import org.apache.geode.internal.cache.PartitionedRegion;
 import org.apache.geode.redis.ConcurrentLoopingThreads;
+import org.apache.geode.redis.internal.services.RegionProvider;
+import org.apache.geode.test.dunit.rules.ClusterStartupRule;
 import org.apache.geode.test.dunit.rules.MemberVM;
 import org.apache.geode.test.dunit.rules.RedisClusterStartupRule;
 import org.apache.geode.test.junit.rules.ExecutorServiceRule;
 
 public class RPushDUnitTest {
-  @Rule
-  public RedisClusterStartupRule clusterStartUp = new RedisClusterStartupRule();
+  @ClassRule
+  public static RedisClusterStartupRule clusterStartUp = new RedisClusterStartupRule();
 
-  @Rule
-  public ExecutorServiceRule executor = new ExecutorServiceRule();
+  @ClassRule
+  public static ExecutorServiceRule executor = new ExecutorServiceRule();
 
-  public static final int PUSHER_COUNT = 6;
-  public static final int PUSH_LIST_SIZE = 3;
-  private static final int MINIMUM_ITERATIONS = 10000;
+  private static JedisCluster jedis;
+  private static int locatorPort;
+  private static int redisServerPort;
+  private static MemberVM server1;
 
-  private static final AtomicLong runningCount = new AtomicLong(PUSHER_COUNT);
-  private static List<String> listHashtags;
-  private static List<String> keys;
-  private static HashMap<String, List<String>> keyToElementListMap;
-  private static List<Runnable> taskList;
-
-  private JedisCluster jedis;
-
-  @Before
-  public void testSetup() {
+  @BeforeClass
+  public static void testSetup() {
     MemberVM locator = clusterStartUp.startLocatorVM(0);
-    clusterStartUp.startRedisVM(1, locator.getPort());
-    clusterStartUp.startRedisVM(2, locator.getPort());
-    clusterStartUp.startRedisVM(3, locator.getPort());
+    locatorPort = locator.getPort();
+    server1 = clusterStartUp.startRedisVM(1, locatorPort);
+    clusterStartUp.startRedisVM(2, locatorPort);
+    clusterStartUp.startRedisVM(3, locatorPort);
 
-    int redisServerPort = clusterStartUp.getRedisPort(1);
+    redisServerPort = clusterStartUp.getRedisPort(3);
     jedis = new JedisCluster(new HostAndPort(BIND_ADDRESS, redisServerPort), 10_000);
-
-    listHashtags = makeListHashtags();
-    keys = makeListKeys(listHashtags);
-    keyToElementListMap = new HashMap<>();
-    for (String key : keys) {
-      keyToElementListMap.put(key, makeElementList(PUSH_LIST_SIZE, key));
-    }
-
-    taskList = new ArrayList<>();
-    taskList.add(() -> rpushPerformAndVerify(keys.get(0), keyToElementListMap.get(keys.get(0)),
-        runningCount));
-    taskList.add(() -> rpushPerformAndVerify(keys.get(0), keyToElementListMap.get(keys.get(0)),
-        runningCount));
-    taskList.add(() -> rpushPerformAndVerify(keys.get(1), keyToElementListMap.get(keys.get(1)),
-        runningCount));
-    taskList.add(() -> rpushPerformAndVerify(keys.get(1), keyToElementListMap.get(keys.get(1)),
-        runningCount));
-    taskList.add(() -> rpushPerformAndVerify(keys.get(2), keyToElementListMap.get(keys.get(2)),
-        runningCount));
-    taskList.add(() -> rpushPerformAndVerify(keys.get(2), keyToElementListMap.get(keys.get(2)),
-        runningCount));
-    taskList.add(() -> verifyListLengthCondition(keys.get(0), runningCount));
-    taskList.add(() -> verifyListLengthCondition(keys.get(1), runningCount));
-    taskList.add(() -> verifyListLengthCondition(keys.get(2), runningCount));
   }
 
   @After
-  public void tearDown() {
+  public void cleanup() {
+    clusterStartUp.flushAll();
+  }
+
+  @AfterClass
+  public static void teardown() {
     jedis.close();
   }
 
   @Test
-  public void givenBucketsMovedDuringRPush_elementsAreAddedAtomically()
-      throws ExecutionException, InterruptedException {
+  public void givenBucketsMovedDuringRPush_elementsAreAddedAtomically() throws Exception {
+    AtomicBoolean running = new AtomicBoolean(true);
+    String KEY = "key";
 
-    List<Future<Void>> futureList = new ArrayList<>();
-    for (Runnable task : taskList) {
-      futureList.add(executor.runAsync(task));
+    Future<?> movingFuture = executor.submit(() -> {
+      try {
+        for (int i = 0; i < 20; i++) {
+          clusterStartUp.moveBucketForKey(KEY);
+          Thread.sleep(500);
+        }
+      } finally {
+        running.set(false);
+      }
+    });
+
+    int i = 0;
+    while (running.get()) {
+      jedis.rpush(KEY, value(i++), value(i++), value(i++), value(i++), value(i++));
     }
+    movingFuture.get();
 
-    for (int i = 0; i < 10 && runningCount.get() > 0; i++) {
-      clusterStartUp.moveBucketForKey(listHashtags.get(i % listHashtags.size()));
-      Thread.sleep(500);
-    }
+    compareBuckets();
 
-    for (Future<Void> future : futureList) {
-      future.get();
-    }
-
-    for (String key : keys) {
-      long length = jedis.llen(key);
-      assertThat(length).isGreaterThanOrEqualTo(MINIMUM_ITERATIONS * 2 * PUSH_LIST_SIZE);
-      validateListContents(key, keyToElementListMap);
+    String popped;
+    int j = 0;
+    while ((popped = jedis.lpop(KEY)) != null) {
+      assertThat(popped).isEqualTo(value(j));
+      j++;
     }
   }
 
   @Test
-  public void concurrentRpush_behavesCorrectly() {
+  public void concurrentRPush_behavesCorrectly() {
     String KEY = "muggle";
     String[] rpushElements1 = IntStream.range(0, 10)
         .mapToObj(Integer::toString)
@@ -158,83 +145,67 @@ public class RPushDUnitTest {
   }
 
   @Test
-  public void shouldNotLoseData_givenPrimaryServerCrashesDuringOperations()
-      throws ExecutionException, InterruptedException {
-    List<Future<Void>> futureList = new ArrayList<>();
-    for (Runnable task : taskList) {
-      futureList.add(executor.runAsync(task));
+  public void shouldNotLoseData_givenPrimaryServerCrashesDuringOperations() throws Exception {
+    AtomicBoolean running = new AtomicBoolean(true);
+    String KEY = "key";
+    int finalLocatorPort = locatorPort;
+    int finalRedisPort = redisServerPort;
+
+    Future<?> crasherFuture = executor.submit(() -> {
+      try {
+        for (int i = 0; i < 10 && running.get(); i++) {
+          clusterStartUp.moveBucketForKey(KEY, "server-3");
+          // Sleep for a bit so that rename can execute
+          Thread.sleep(1000);
+          clusterStartUp.crashVM(3);
+          clusterStartUp.startRedisVM(3, x -> x
+              .withProperty(GEODE_FOR_REDIS_PORT, Integer.toString(finalRedisPort))
+              .withConnectionToLocator(finalLocatorPort));
+          clusterStartUp.rebalanceAllRegions();
+        }
+      } finally {
+        running.set(false);
+      }
+    });
+
+    int i = 0;
+    while (running.get()) {
+      jedis.rpush(KEY, value(i++));
     }
+    crasherFuture.get();
 
-    Thread.sleep(200);
-    clusterStartUp.crashVM(1); // kill primary server
+    compareBuckets();
 
-    for (Future<Void> future : futureList) {
-      future.get();
-    }
-
-    long length;
-    for (String key : keys) {
-      length = jedis.llen(key);
-      assertThat(length).isGreaterThanOrEqualTo(MINIMUM_ITERATIONS * 2 * PUSH_LIST_SIZE);
-      assertThat(length % 3).isEqualTo(0);
-      validateListContents(key, keyToElementListMap);
-    }
-  }
-
-  private void rpushPerformAndVerify(String key, List<String> elementList,
-      AtomicLong runningCount) {
-    for (int i = 0; i < MINIMUM_ITERATIONS; i++) {
-      long listLength = jedis.llen(key);
-      long newLength = jedis.rpush(key, elementList.toArray(new String[] {}));
-      assertThat((newLength - listLength) % 3).as("RPUSH, list length %s not multiple of 3",
-          newLength).isEqualTo(0);
-    }
-    runningCount.decrementAndGet();
-  }
-
-  private void validateListContents(String key,
-      HashMap<String, List<String>> keyToElementListMap) {
-    while (jedis.llen(key) > 0) {
-      List<String> elementList = keyToElementListMap.get(key);
-      assertThat(jedis.lpop(key)).isEqualTo(elementList.get(0));
-      assertThat(jedis.lpop(key)).isEqualTo(elementList.get(1));
-      assertThat(jedis.lpop(key)).isEqualTo(elementList.get(2));
-    }
-  }
-
-  private List<String> makeListHashtags() {
-    List<String> listHashtags = new ArrayList<>();
-    listHashtags.add(clusterStartUp.getKeyOnServer("rpush", 1));
-    listHashtags.add(clusterStartUp.getKeyOnServer("rpush", 2));
-    listHashtags.add(clusterStartUp.getKeyOnServer("rpush", 3));
-    return listHashtags;
-  }
-
-  private List<String> makeListKeys(List<String> listHashtags) {
-    List<String> keys = new ArrayList<>();
-    keys.add(makeListKeyWithHashtag(1, listHashtags.get(0)));
-    keys.add(makeListKeyWithHashtag(2, listHashtags.get(1)));
-    keys.add(makeListKeyWithHashtag(3, listHashtags.get(2)));
-    return keys;
-  }
-
-  private String makeListKeyWithHashtag(int index, String hashtag) {
-    return "{" + hashtag + "}-key-" + index;
-  }
-
-  private void verifyListLengthCondition(String key, AtomicLong runningCount) {
-    while (runningCount.get() > 0) {
-      long listLength = jedis.llen(key);
-      assertThat(listLength % 3).as("List length not a multiple of three: %s", listLength)
-          .isEqualTo(0);
+    String popped;
+    int j = 0;
+    while ((popped = jedis.lpop(KEY)) != null) {
+      try {
+        assertThat(popped).isEqualTo(value(j));
+        j++;
+      } catch (AssertionError e) {
+        // It's OK if there is a duplicate since any retries are not idempotent.
+        assertThat(popped).as("duplicate check failed")
+            .isEqualTo(value(j - 1));
+      }
     }
   }
 
-  private List<String> makeElementList(int listSize, String baseString) {
-    List<String> elements = new ArrayList<>();
-    for (int i = 0; i < listSize; i++) {
-      elements.add(baseString + i);
-    }
-    return elements;
+  private String value(int i) {
+    return "value-" + i;
+  }
+
+  private void compareBuckets() {
+    server1.invoke(() -> {
+      InternalCache cache = ClusterStartupRule.getCache();
+      PartitionedRegion region =
+          (PartitionedRegion) cache.getRegion(RegionProvider.DEFAULT_REDIS_REGION_NAME);
+      for (int j = 0; j < region.getTotalNumberOfBuckets(); j++) {
+        List<BucketDump> buckets = region.getAllBucketEntries(j);
+        assertThat(buckets.size()).isEqualTo(2);
+        Map<Object, Object> bucket1 = buckets.get(0).getValues();
+        Map<Object, Object> bucket2 = buckets.get(1).getValues();
+        assertThat(bucket1).containsExactlyEntriesOf(bucket2);
+      }
+    });
   }
 }
