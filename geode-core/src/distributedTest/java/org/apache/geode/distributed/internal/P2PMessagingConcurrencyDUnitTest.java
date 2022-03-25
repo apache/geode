3c7b59abe898168c638df59d02a399a7bdeec854
Bill Burcham <bill.burcham@gmail.com>
2022-03-25 15:21:22 -0700

    enhance P2P concurrency test

--- geode-core/src/distributedTest/java/org/apache/geode/distributed/internal/P2PMessagingConcurrencyDUnitTest.java
+++ geode-core/src/distributedTest/java/org/apache/geode/distributed/internal/P2PMessagingConcurrencyDUnitTest.java
@@ -15,14 +15,19 @@
 
 package org.apache.geode.distributed.internal;
 
+import static org.apache.geode.distributed.ConfigurationProperties.SSL_CIPHERS;
+import static org.apache.geode.distributed.ConfigurationProperties.SSL_PROTOCOLS;
 import static org.apache.geode.test.awaitility.GeodeAwaitility.await;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.security.Security;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Properties;
 import java.util.Random;
 import java.util.Set;
@@ -72,11 +77,14 @@ import org.apache.geode.test.version.VersionManager;
 @RunWith(GeodeParamsRunner.class)
 public class P2PMessagingConcurrencyDUnitTest {
 
-  // how many messages will each sender generate?
-  private static final int MESSAGES_PER_SENDER = 1_000;
+  // how many sending member JVMs
+  private static final int SENDERS = 1;
 
-  // number of concurrent (sending) tasks to run
-  private static final int SENDER_COUNT = 10;
+  // number of concurrent (sending) tasks to run in each sending JVM
+  private static final int TASKS_PER_SENDER = 10;
+
+  // how many messages will each sending task generate?
+  private static final int MESSAGES_PER_SENDING_TASK = 1_000;
 
   /*
    * Upper bound (exclusive) of random message size, in bytes.
@@ -88,10 +96,41 @@ public class P2PMessagingConcurrencyDUnitTest {
    */
   private static final int LARGEST_MESSAGE_BOUND = 32 * 1024 + 2;
 
+  /*
+   * Non-random payload content makes debugging easier when a NULL cipher is used.
+   * Null ciphers are not allowed on TLSv1.3 though.
+   */
+  private static boolean RANDOMIZE_PAYLOAD_CONTENT = false;
+
+  private static final byte[] NON_RANDOM_PAYLOAD_PATTERN =
+      "EVERYGOODBOYDOESFINE".getBytes(StandardCharsets.UTF_8);
+
   // random seed
   private static final int RANDOM_SEED = 1234;
 
-  public static final boolean TEST_WITH_TLS_KEY_UPDATE_MESSAGE_PROCESSING = false;
+  /*
+   * On TLSv1.3 using a GCM-based cipher, a KeyUpdate TLS message will be sent
+   * after a preconfigured number of bytes have been encrypted by an SSLEngine.
+   * Setting this to true causes generation of KeyUpdate TLS messages.
+   * See NioSslEngineKeyUpdateTest for more details.
+   */
+  private static final boolean TEST_WITH_TLS_KEY_UPDATE_MESSAGE_PROCESSING = true;
+
+  private static final int ENCRYPTED_BYTES_LIMIT = 1024 * 8; // 2^13
+
+  {
+    /*
+     * ENCRYPTED_BYTES_LIMIT must be large enough to allow non-P2P TLS communication
+     * e.g. locator, with no TLS KeyUpdate message processing. But it must also be small
+     * enough to ensure that KeyUpdate messages will be generated/processed by P2P messaging.
+     *
+     * When changing these constants, if you make ENCRYPTED_BYTES_LIMIT too small then
+     * e.g. locator communication will fail before P2P messaging. The following assertion
+     * will keep you from choosing an ENCRYPTED_BYTES_LIMIT that is too large relative
+     * to the messaging volume. 10 is an arbitrary safety factor.
+     */
+    assert ENCRYPTED_BYTES_LIMIT * 10 < MESSAGES_PER_SENDING_TASK * LARGEST_MESSAGE_BOUND / 2;
+  }
 
   private static Properties securityProperties;
 
@@ -100,9 +139,9 @@ public class P2PMessagingConcurrencyDUnitTest {
 
   @ClassRule
   public static final DistributedExecutorServiceRule senderExecutorServiceRule =
-      new DistributedExecutorServiceRule(SENDER_COUNT, 3);
+      new DistributedExecutorServiceRule(TASKS_PER_SENDER, 3);
 
-  private MemberVM sender;
+  private final Collection<MemberVM> senders = new ArrayList<>();
   private MemberVM receiver;
 
   /*
@@ -123,7 +162,7 @@ public class P2PMessagingConcurrencyDUnitTest {
         gemFireConfiguration(conserveSockets, useTLS, receiveSocketBufferSize);
 
     if (TEST_WITH_TLS_KEY_UPDATE_MESSAGE_PROCESSING) {
-      configureForTestingTLSKeyUpdateMessage();
+      configureSecurityPropertiesForTestingTLSKeyUpdateMessage();
     }
 
     final MemberVM locator =
@@ -131,19 +170,18 @@ public class P2PMessagingConcurrencyDUnitTest {
             x -> x.withProperties(senderConfiguration).withConnectionToLocator()
                 .withoutClusterConfigurationService().withoutManagementRestService());
 
-    sender = clusterStartupRule.startServerVM(1, senderConfiguration, locator.getPort());
-    receiver = clusterStartupRule.startServerVM(2, receiverConfiguration, locator.getPort());
+    receiver = clusterStartupRule.startServerVM(1, receiverConfiguration, locator.getPort());
+
+    for (int i = 2; i - 2 < SENDERS; i++) {
+      senders.add(
+          clusterStartupRule.startServerVM(i, senderConfiguration, locator.getPort()));
+    }
   }
 
-  private void configureForTestingTLSKeyUpdateMessage() {
+  private void configureSecurityPropertiesForTestingTLSKeyUpdateMessage() {
     final SerializableRunnableIF setSystemProperties = () -> {
-      System.setProperty("javax.net.debug", "all");
-      /*
-       * 2^12 is large enough to support non-P2P TLS messaging e.g. locator with no
-       * TLS KeyUpdate message processing, but small enough to ensure that KeyUpdate
-       * messages will be generated/processed by P2P messaging.
-       */
-      Security.setProperty("jdk.tls.keyLimits", "AES/GCM/NoPadding KeyUpdate 2^12");
+      Security.setProperty("jdk.tls.keyLimits",
+          "AES/GCM/NoPadding KeyUpdate " + ENCRYPTED_BYTES_LIMIT);
     };
     clusterStartupRule.getVM(0).invoke(setSystemProperties);
     clusterStartupRule.getVM(1).invoke(setSystemProperties);
@@ -156,24 +194,41 @@ public class P2PMessagingConcurrencyDUnitTest {
        * all combinations of flags with buffer sizes:
        * (equal), larger/smaller, smaller/larger, minimal
        */
-      "true, true, 32768, 32768",
-      "true, true, 65536, 32768",
-      "true, true, 32768, 65536",
-      "true, true, 1024, 1024",
-      "true, false, 32768, 32768",
-      "true, false, 65536, 32768",
-      "true, false, 32768, 65536",
-      "true, false, 1024, 1024",
-      "false, true, 32768, 32768",
-      "false, true, 65536, 32768",
-      "false, true, 32768, 65536",
-      "false, true, 1024, 1024",
-      "false, false, 32768, 32768",
-      "false, false, 65536, 32768",
-      "false, false, 32768, 65536",
-      "false, false, 1024, 1024",
+      "true, true, true, 32768, 32768",
+      "true, true, true, 65536, 32768",
+      "true, true, true, 32768, 65536",
+      "true, true, true, 1024, 1024",
+      "true, true, false, 32768, 32768",
+      "true, true, false, 65536, 32768",
+      "true, true, false, 32768, 65536",
+      "true, true, false, 1024, 1024",
+      "true, false, true, 32768, 32768",
+      "true, false, true, 65536, 32768",
+      "true, false, true, 32768, 65536",
+      "true, false, true, 1024, 1024",
+      "true, false, false, 32768, 32768",
+      "true, false, false, 65536, 32768",
+      "true, false, false, 32768, 65536",
+      "true, false, false, 1024, 1024",
+      "false, true, true, 32768, 32768",
+      "false, true, true, 65536, 32768",
+      "false, true, true, 32768, 65536",
+      "false, true, true, 1024, 1024",
+      "false, true, false, 32768, 32768",
+      "false, true, false, 65536, 32768",
+      "false, true, false, 32768, 65536",
+      "false, true, false, 1024, 1024",
+      "false, false, true, 32768, 32768",
+      "false, false, true, 65536, 32768",
+      "false, false, true, 32768, 65536",
+      "false, false, true, 1024, 1024",
+      "false, false, false, 32768, 32768",
+      "false, false, false, 65536, 32768",
+      "false, false, false, 32768, 65536",
+      "false, false, false, 1024, 1024",
   })
   public void testP2PMessaging(
+      final boolean requireOrderedDelivery,
       final boolean conserveSockets,
       final boolean useTLS,
       final int sendSocketBufferSize,
@@ -192,7 +247,7 @@ public class P2PMessagingConcurrencyDUnitTest {
 
         });
 
-    sender.invoke(() -> {
+    senders.forEach(sender -> sender.invoke(() -> {
 
       bytesTransferredAdder = new LongAdder();
 
@@ -211,8 +266,8 @@ public class P2PMessagingConcurrencyDUnitTest {
        */
       final ExecutorService executor = senderExecutorServiceRule.getExecutorService();
 
-      final CountDownLatch startLatch = new CountDownLatch(SENDER_COUNT);
-      final CountDownLatch stopLatch = new CountDownLatch(SENDER_COUNT);
+      final CountDownLatch startLatch = new CountDownLatch(TASKS_PER_SENDER);
+      final CountDownLatch stopLatch = new CountDownLatch(TASKS_PER_SENDER);
       final LongAdder failedRecipientCount = new LongAdder();
 
       final Runnable doSending = () -> {
@@ -223,10 +278,11 @@ public class P2PMessagingConcurrencyDUnitTest {
         } catch (final InterruptedException e) {
           throw new RuntimeException("doSending failed", e);
         }
-        final int firstMessageId = senderId * SENDER_COUNT;
+        final int firstMessageId = senderId * TASKS_PER_SENDER;
         for (int messageId = firstMessageId; messageId < firstMessageId
-            + MESSAGES_PER_SENDER; messageId++) {
-          final TestMessage msg = new TestMessage(receiverMember, random, messageId);
+            + MESSAGES_PER_SENDING_TASK; messageId++) {
+          final TestMessage msg =
+              new TestMessage(receiverMember, random, messageId, requireOrderedDelivery);
 
           /*
            * HERE is the Geode API entrypoint we intend to test (putOutgoing()).
@@ -240,7 +296,7 @@ public class P2PMessagingConcurrencyDUnitTest {
         stopLatch.countDown();
       };
 
-      for (int i = 0; i < SENDER_COUNT; ++i) {
+      for (int i = 0; i < TASKS_PER_SENDER; ++i) {
         executor.submit(doSending);
       }
 
@@ -248,9 +304,10 @@ public class P2PMessagingConcurrencyDUnitTest {
 
       assertThat(failedRecipientCount.sum()).as("message delivery failed N times").isZero();
 
-    });
+    }));
 
-    final long bytesSent = getByteCount(sender);
+    final long bytesSent = senders.stream().map(sender -> getByteCount(sender))
+        .reduce(0L, Long::sum);
 
     await().untilAsserted(
         () -> assertThat(getByteCount(receiver))
@@ -278,19 +335,29 @@ public class P2PMessagingConcurrencyDUnitTest {
      * Left the field here in case it comes in handy later.
      */
     private volatile int messageId;
-    private final Random random;
+    private volatile Random random;
+    private boolean requireOrderedDelivery;
 
-    TestMessage(final InternalDistributedMember receiver,
-        final Random random, final int messageId) {
+    TestMessage(
+        final InternalDistributedMember receiver,
+        final Random random,
+        final int messageId,
+        final boolean requireOrderedDelivery) {
       setRecipient(receiver);
       this.random = random;
       this.messageId = messageId;
+      this.requireOrderedDelivery = requireOrderedDelivery;
     }
 
     // necessary for deserialization
     public TestMessage() {
       random = null;
       messageId = 0;
+    }
+
+    @Override
+    public boolean orderedDelivery() {
+      return requireOrderedDelivery;
     }
 
     @Override
@@ -313,8 +380,11 @@ public class P2PMessagingConcurrencyDUnitTest {
       out.writeInt(length);
 
       final byte[] payload = new byte[length];
-      random.nextBytes(payload);
-
+      if (RANDOMIZE_PAYLOAD_CONTENT) {
+        random.nextBytes(payload);
+      } else {
+        nextBytesNonRandom(payload);
+      }
       out.write(payload);
 
       /*
@@ -344,6 +414,12 @@ public class P2PMessagingConcurrencyDUnitTest {
     public int getDSFID() {
       return NO_FIXED_ID; // for testing only!
     }
+
+    private void nextBytesNonRandom(byte[] bytes) {
+      for (int i = 0; i < bytes.length; i++) {
+        bytes[i] = NON_RANDOM_PAYLOAD_PATTERN[i % NON_RANDOM_PAYLOAD_PATTERN.length];
+      }
+    }
   }
 
   @NotNull
@@ -371,6 +447,10 @@ public class P2PMessagingConcurrencyDUnitTest {
      */
     props.setProperty("conserve-sockets", String.valueOf(conserveSockets));
 
+    if (TEST_WITH_TLS_KEY_UPDATE_MESSAGE_PROCESSING) {
+      props.setProperty(SSL_PROTOCOLS, "TLSv1.3");
+      props.setProperty(SSL_CIPHERS, "TLS_AES_256_GCM_SHA384");
+    }
     return props;
   }
 
