1714e46ec56659b32f98f66e5a334c727b4d9927
Owen Nichols <onichols@apache.org>
2022-03-28 00:16:46 -0700

    GEODE-10179: Bump jackson-databind to 2.13.2.1
    Geode endeavors to update to the latest version of 3rd-party
    dependencies on develop wherever possible.  Doing so increases the
    shelf life of releases and increases security and reliability.
    Doing so regularly makes the occasional hiccups this can cause easier
    to pinpoint and address.
    
    This bump of jackson-databind from 2.13.2 to micropatch 2.13.2.1 is
    a little clunkly and once 2.13.3 or 2.14.0 is available, this should
    first be reverted...

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -46,6 +46,8 @@ class DependencyConstraints implements Plugin<Project> {
     deps.put("slf4j-api.version", "1.7.32")
     deps.put("jboss-modules.version", "1.11.0.Final")
     deps.put("jackson.version", "2.13.2")
+    deps.put("jackson.databind.version", "2.13.2.1")
+    deps.put("jackson.bom.version", "2.13.2.20220324")
     deps.put("springshell.version", "1.2.0.RELEASE")
     deps.put("springframework.version", "5.3.16")
 
@@ -185,7 +187,14 @@ class DependencyConstraints implements Plugin<Project> {
     dependencySet(group: 'com.fasterxml.jackson.core', version: get('jackson.version')) {
       entry('jackson-annotations')
       entry('jackson-core')
+    }
+
+    dependencySet(group: 'com.fasterxml.jackson.core', version: get('jackson.databind.version')) {
       entry('jackson-databind')
+    }
+
+    dependencySet(group: 'com.fasterxml.jackson', version: get('jackson.bom.version')) {
+      entry('jackson-bom')
     }
 
     dependencySet(group: 'com.fasterxml.jackson.datatype', version: get('jackson.version')) {
