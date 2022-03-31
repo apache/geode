b6090a92054f20b4651814c6753d0d28d24d1a58
Steve Sienkowski <ssienkowski@vmware.com>
2022-03-31 16:15:38 -0400

    Spotless changes.

--- geode-for-redis/src/main/java/org/apache/geode/redis/internal/data/RedisList.java
+++ geode-for-redis/src/main/java/org/apache/geode/redis/internal/data/RedisList.java
@@ -353,7 +353,7 @@ public class RedisList extends AbstractRedisData {
 
   @Override
   public synchronized void applyRemoveElementsByIndex(List<Integer> indexes) {
-    if(indexes.size() == 1) {
+    if (indexes.size() == 1) {
       removeElement(indexes.get(0));
     } else {
       elementList.removeIndexes(indexes);
