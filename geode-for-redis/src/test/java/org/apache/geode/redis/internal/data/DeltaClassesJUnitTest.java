9eed5b2029ace216494c10b958fbbfddb8013c0e
Ray Ingles <ringles@vmware.com>
2022-04-04 16:51:57 -0400

    GEODE-10157: Add Radish Delta Unit Tests

--- geode-for-redis/src/test/java/org/apache/geode/redis/internal/data/DeltaClassesJUnitTest.java
+++ geode-for-redis/src/test/java/org/apache/geode/redis/internal/data/DeltaClassesJUnitTest.java
@@ -21,10 +21,17 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Test;
 
+import org.apache.geode.redis.internal.data.delta.AddByteArrays;
+import org.apache.geode.redis.internal.data.delta.AddByteArraysTail;
+import org.apache.geode.redis.internal.data.delta.InsertByteArray;
+import org.apache.geode.redis.internal.data.delta.RemoveElementsByIndex;
 import org.apache.geode.redis.internal.data.delta.ReplaceByteArrayAtOffset;
+import org.apache.geode.redis.internal.data.delta.RetainElementsByIndexRange;
 
 public class DeltaClassesJUnitTest {
 
@@ -55,10 +62,7 @@ public class DeltaClassesJUnitTest {
 
     source.serializeTo(dos);
 
-    RedisList redisList = new RedisList();
-    redisList.applyAddByteArrayTailDelta("zero".getBytes());
-    redisList.applyAddByteArrayTailDelta("one".getBytes());
-    redisList.applyAddByteArrayTailDelta("two".getBytes());
+    RedisList redisList = makeRedisList();
 
     DataInputStream dis = new DataInputStream(new ByteArrayInputStream(baos.toByteArray()));
     redisList.fromDelta(dis);
@@ -67,5 +71,140 @@ public class DeltaClassesJUnitTest {
     assertThat(redisList.lindex(0)).isEqualTo("zero".getBytes());
     assertThat(redisList.lindex(1)).isEqualTo(payload.getBytes());
     assertThat(redisList.lindex(2)).isEqualTo("two".getBytes());
+  }
+
+  @Test
+  public void testRemoveElementsByIndex() throws Exception {
+    List<Integer> payload = new ArrayList<>();
+    payload.add(0);
+    payload.add(2);
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    DataOutputStream dos = new DataOutputStream(baos);
+    RedisList redisList = makeRedisList();
+
+    RemoveElementsByIndex source =
+        new RemoveElementsByIndex((byte) (redisList.getVersion() + 1), payload);
+
+    source.serializeTo(dos);
+
+    DataInputStream dis = new DataInputStream(new ByteArrayInputStream(baos.toByteArray()));
+    redisList.fromDelta(dis);
+
+    assertThat(redisList.llen()).isEqualTo(1);
+    assertThat(redisList.lindex(0)).isEqualTo("one".getBytes());
+  }
+
+  @Test
+  public void testInsertByteArrayDelta() throws Exception {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    DataOutputStream dos = new DataOutputStream(baos);
+    RedisList redisList = makeRedisList();
+    InsertByteArray source =
+        new InsertByteArray((byte) (redisList.getVersion() + 1), "newElement".getBytes(), 1);
+
+    source.serializeTo(dos);
+
+    DataInputStream dis = new DataInputStream(new ByteArrayInputStream(baos.toByteArray()));
+    redisList.fromDelta(dis);
+
+    assertThat(redisList.llen()).isEqualTo(4);
+    assertThat(redisList.lindex(1)).isEqualTo("newElement".getBytes());
+  }
+
+  @Test
+  public void testAddByteArraysDeltaForRedisList() throws Exception {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    DataOutputStream dos = new DataOutputStream(baos);
+    List<byte[]> deltaList = new ArrayList<>();
+    deltaList.add("secondNew".getBytes());
+    deltaList.add("firstNew".getBytes());
+    RedisList redisList = makeRedisList();
+    AddByteArrays source = new AddByteArrays(deltaList, (byte) (redisList.getVersion() + 1));
+
+    source.serializeTo(dos);
+
+    DataInputStream dis = new DataInputStream(new ByteArrayInputStream(baos.toByteArray()));
+    redisList.fromDelta(dis);
+
+    assertThat(redisList.llen()).isEqualTo(5);
+    assertThat(redisList.lindex(0)).isEqualTo("firstNew".getBytes());
+    assertThat(redisList.lindex(1)).isEqualTo("secondNew".getBytes());
+    assertThat(redisList.lindex(2)).isEqualTo("zero".getBytes());
+  }
+
+  @Test
+  public void testAddByteArraysDeltaForRedisSet() throws Exception {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    DataOutputStream dos = new DataOutputStream(baos);
+    List<byte[]> deltaList = new ArrayList<>();
+    deltaList.add("secondNew".getBytes());
+    deltaList.add("firstNew".getBytes());
+    RedisSet redisSet = makeRedisSet();
+    AddByteArrays source = new AddByteArrays(deltaList, (byte) (redisSet.getVersion() + 1));
+
+    source.serializeTo(dos);
+
+    DataInputStream dis = new DataInputStream(new ByteArrayInputStream(baos.toByteArray()));
+    redisSet.fromDelta(dis);
+
+    assertThat(redisSet.scard()).isEqualTo(5);
+    assertThat(redisSet.sismember("firstNew".getBytes())).isTrue();
+    assertThat(redisSet.sismember("secondNew".getBytes())).isTrue();
+  }
+
+  @Test
+  public void testAddByteArraysTailDelta() throws Exception {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    DataOutputStream dos = new DataOutputStream(baos);
+    List<byte[]> deltaList = new ArrayList<>();
+    deltaList.add("firstNew".getBytes());
+    deltaList.add("secondNew".getBytes());
+    RedisList redisList = makeRedisList();
+    AddByteArraysTail source =
+        new AddByteArraysTail((byte) (redisList.getVersion() + 1), deltaList);
+
+    source.serializeTo(dos);
+
+    DataInputStream dis = new DataInputStream(new ByteArrayInputStream(baos.toByteArray()));
+    redisList.fromDelta(dis);
+
+    assertThat(redisList.llen()).isEqualTo(5);
+    assertThat(redisList.lindex(2)).isEqualTo("two".getBytes());
+    assertThat(redisList.lindex(3)).isEqualTo("firstNew".getBytes());
+    assertThat(redisList.lindex(4)).isEqualTo("secondNew".getBytes());
+  }
+
+  @Test
+  public void testRetainElementsByIndexRangeDelta() throws Exception {
+    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    DataOutputStream dos = new DataOutputStream(baos);
+    RedisList redisList = makeRedisList();
+    RetainElementsByIndexRange source =
+        new RetainElementsByIndexRange((byte) (redisList.getVersion() + 1), 0, 1);
+
+    source.serializeTo(dos);
+
+    DataInputStream dis = new DataInputStream(new ByteArrayInputStream(baos.toByteArray()));
+    redisList.fromDelta(dis);
+
+    assertThat(redisList.llen()).isEqualTo(2);
+    assertThat(redisList.lindex(0)).isEqualTo("zero".getBytes());
+    assertThat(redisList.lindex(1)).isEqualTo("one".getBytes());
+  }
+
+  private RedisList makeRedisList() {
+    RedisList redisList = new RedisList();
+    redisList.applyAddByteArrayTailDelta("zero".getBytes());
+    redisList.applyAddByteArrayTailDelta("one".getBytes());
+    redisList.applyAddByteArrayTailDelta("two".getBytes());
+    return redisList;
+  }
+
+  private RedisSet makeRedisSet() {
+    RedisSet redisSet = new RedisSet(5);
+    redisSet.membersAdd("zero".getBytes());
+    redisSet.membersAdd("one".getBytes());
+    redisSet.membersAdd("two".getBytes());
+    return redisSet;
   }
 }
