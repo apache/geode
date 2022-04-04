83a8230144facc782c4982eca27a2b1fc7cbe083
Ray Ingles <ringles@pivotal.io>
2022-04-04 16:14:51 -0400

    GEODE-10116: Enable Redis TCL tests for Lists (#7521)
    * GEODE-10116: Enable Redis TCL tests for Lists
    
    Co-authored-by: Ray Ingles <ringles@vmware.com>

--- ci/scripts/execute_redis_tests.sh
+++ ci/scripts/execute_redis_tests.sh
@@ -82,7 +82,10 @@ failCount=0
 --single unit/type/zset \
 --single unit/quit \
 --single unit/pubsub \
---single unit/dump
+--single unit/dump \
+--single unit/type/list
+#--single unit/type/list-2 \ TODO enable when GEODE-9953 merged
+#--single unit/type/list-3 TODO enable when GEODE-10160 is fixed
 
 ((failCount += $?))
 
