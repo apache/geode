894f9efda8b09c33ac8425af162914e8001a86ef
Dale Emery <demery@vmware.com>
2022-04-08 17:00:08 -0700

    QueryPdxDataDUnitTest uses InMemoryJavaCompiler

--- geode-junit/src/main/java/org/apache/geode/test/compiler/InMemoryClassFileLoader.java
+++ geode-junit/src/main/java/org/apache/geode/test/compiler/InMemoryClassFileLoader.java
@@ -11,38 +11,28 @@
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
+ *
  */
+
 package org.apache.geode.test.compiler;
 
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.net.URI;
+import static java.util.stream.Collectors.toList;
 
-import javax.tools.SimpleJavaFileObject;
+import java.util.List;
 
 /**
- * In-memory representation of the name and bytecode of a Java class.
+ * Loads classes described by in-memory class files.
  */
-public class InMemoryClassFile extends SimpleJavaFileObject {
-  private final String name;
-  private ByteArrayOutputStream out;
-
-  public InMemoryClassFile(String name) {
-    super(URI.create("string:///" + name.replace('.', '/') + Kind.CLASS.extension), Kind.CLASS);
-    this.name = name;
+public class InMemoryClassFileLoader extends ClassLoader {
+  public List<Class<?>> defineClasses(List<InMemoryClassFile> classFiles) {
+    return classFiles.stream()
+        .map(this::defineClass)
+        .collect(toList());
   }
 
-  @Override
-  public String getName() {
-    return name;
-  }
-
-  @Override
-  public OutputStream openOutputStream() {
-    return out = new ByteArrayOutputStream();
-  }
-
-  public byte[] getByteContent() {
-    return out.toByteArray();
+  public Class<?> defineClass(InMemoryClassFile classFile) {
+    String name = classFile.getName();
+    byte[] bytes = classFile.getByteContent();
+    return defineClass(name, bytes, 0, bytes.length);
   }
 }
