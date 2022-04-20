1319ca0d6656eebf2f5909145a61745240b22bd7
Xiaojian Zhou <gesterzhou@users.noreply.github.com>
2022-04-20 14:35:54 -0700

    GEODE-10229: GII image should fill disk region RVV's exceptions. (#7602)
    * GEODE-10229: GII image should fill disk region RVV's exceptions.
        There're 2 issues: Tombstone in GII image should save version tag into disk region RVV
        even the tombstone is the same as local one. Disk region RVV holder did not use bitset.
        Only bitset based holder can fill special exception. Should do it for non-bitset holder too.
    
    (cherry picked from commit 28dbac1a86dd40ca0c3ceba004263824f6de4653)

--- geode-core/src/test/java/org/apache/geode/internal/cache/versions/RegionVersionHolderJUnitTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/cache/versions/RegionVersionHolderJUnitTest.java
@@ -14,6 +14,7 @@
  */
 package org.apache.geode.internal.cache.versions;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
@@ -51,6 +52,53 @@ public class RegionVersionHolderJUnitTest {
 
   protected InternalDistributedMember member() {
     return member;
+  }
+
+  @Test
+  public void fillSpecialExceptionForRVHWithBitSet() {
+    fillSpecialExceptionForRVH(true);
+  }
+
+  @Test
+  public void fillSpecialExceptionForRVHWithoutBitSet() {
+    fillSpecialExceptionForRVH(false);
+  }
+
+  private void fillSpecialExceptionForRVH(boolean useBitSet) {
+    RegionVersionHolder<InternalDistributedMember> vh1;
+    RegionVersionHolder<InternalDistributedMember> vh2;
+    if (useBitSet) {
+      vh1 = new RegionVersionHolder<>(member);
+      vh2 = new RegionVersionHolder<>(member);
+    } else {
+      vh1 = new RegionVersionHolder<>(0);
+      vh2 = new RegionVersionHolder<>(0);
+    }
+    for (int i = 1; i <= 3; i++) {
+      vh1.recordVersion(i);
+    }
+    for (int i = 1; i <= 5; i++) {
+      vh2.recordVersion(i);
+    }
+
+    // create special exception 5(n=6,p=3)
+    vh2.initializeFrom(vh1);
+    List<RVVException> exceptionList = vh2.getExceptionForTest();
+    assertThat(exceptionList.size()).isEqualTo(1);
+    RVVException exception = exceptionList.get(0);
+    assertThat(exception.previousVersion).isEqualTo(3);
+    assertThat(exception.nextVersion).isEqualTo(6);
+
+    vh2.recordVersion(5);
+    exceptionList = vh2.getExceptionForTest();
+    assertThat(exceptionList.size()).isEqualTo(1);
+    exception = exceptionList.get(0);
+    assertThat(exception.previousVersion).isEqualTo(3);
+    assertThat(exception.nextVersion).isEqualTo(5);
+
+    vh2.recordVersion(4);
+    exceptionList = vh2.getExceptionForTest();
+    assertThat(exceptionList.size()).isEqualTo(0);
   }
 
   @Test
