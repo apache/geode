28920caebe6dc2ac214eb565936fcece74529190
Owen Nichols <34043438+onichols-pivotal@users.noreply.github.com>
2022-05-20 18:32:05 -0700

    Update gfsh.bat
    re-trigger CI since new checks have been added

--- geode-assembly/src/main/dist/bin/gfsh.bat
+++ geode-assembly/src/main/dist/bin/gfsh.bat
@@ -27,7 +27,7 @@ REM
 
 setlocal enableextensions
 set scriptdir=%~dp0
-REM Remove last 4 chars corresponding to bin\
+REM Remove last 4 chars which are presumably "bin\"
 set gf=%scriptdir:~0,-4%
 REM echo %gf%
 REM echo %scriptdir%
