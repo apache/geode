8bb60e6c760d462e338594804e68b9e548a848dc
Owen Nichols <onichols@apache.org>
2022-06-03 12:27:39 -0700

    GEODE-10355: Bump spring-security from 5.4.8 to 5.6.5
    Geode endeavors to update to the latest version of 3rd-party
    dependencies on develop wherever possible.  Doing so increases the
    shelf life of releases and increases security and reliability.
    Doing so regularly makes the occasional hiccups this can cause easier
    to pinpoint and address.
    
    Dependency bumps in this batch:
    * Bump spring-security from 5.4.8 to 5.6.5

--- buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ buildSrc/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -244,7 +244,7 @@ class DependencyConstraints implements Plugin<Project> {
       entry('selenium-support')
     }
 
-    dependencySet(group: 'org.springframework.security', version: '5.4.8') {
+    dependencySet(group: 'org.springframework.security', version: '5.6.5') {
       entry('spring-security-config')
       entry('spring-security-core')
       entry('spring-security-ldap')
