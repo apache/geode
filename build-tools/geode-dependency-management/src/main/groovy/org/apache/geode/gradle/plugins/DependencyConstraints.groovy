2a3518ca3c9af1d66a28446a3a43ca7500bc60fe
mkevo <mario.kevo@est.tech>
2022-09-07 15:03:28 +0200

    GEODE:10418 uplift jetty to 9.4.47.v20220610

--- build-tools/geode-dependency-management/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ build-tools/geode-dependency-management/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -61,7 +61,7 @@ class DependencyConstraints {
 
     // The jetty version is also hard-coded in geode-assembly:test
     // at o.a.g.sessions.tests.GenericAppServerInstall.java
-    deps.put("jetty.version", "9.4.46.v20220331")
+    deps.put("jetty.version", "9.4.47.v20220610")
 
     // These versions are referenced in test.gradle, which is aggressively injected into all projects.
     deps.put("junit.version", "4.13.2")
