f563aee12796eede82b2b52db0020ffaf88a7a3e
mivanac <mario.ivanac@est.tech>
2022-09-13 14:48:20 +0200

    GEODE-10421: added check gw status

--- geode-gfsh/src/main/java/org/apache/geode/management/internal/cli/commands/StartGatewaySenderCommand.java
+++ geode-gfsh/src/main/java/org/apache/geode/management/internal/cli/commands/StartGatewaySenderCommand.java
@@ -77,6 +77,49 @@ public class StartGatewaySenderCommand extends GfshCommand {
       return ResultModel.createError(CliStrings.NO_MEMBERS_FOUND_MESSAGE);
     }
 
+    if (cleanQueues) {
+
+      Set<DistributedMember> allServers = findMembers(null, null);
+
+      if (dsMembers.size() != allServers.size()) {
+        return ResultModel.createError(CliStrings.EXECUTE_ON_ALL_MEMBERS);
+      }
+
+      GatewaySenderMXBean bean;
+      boolean commmandRejected = false;
+
+      ResultModel rejectResultModel =
+          ResultModel.createError(CliStrings.START_GATEWAYSENDER_REJECTED);
+      TabularResultModel rejectResultData =
+          rejectResultModel.addTable(CliStrings.REJECT_START_GATEWAYSENDER_REASON);
+
+      for (DistributedMember member : dsMembers) {
+        if (cache.getDistributedSystem().getDistributedMember().getId().equals(member.getId())) {
+          bean = service.getLocalGatewaySenderMXBean(senderId);
+        } else {
+          ObjectName objectName = service.getGatewaySenderMBeanName(member, senderId);
+          bean = service.getMBeanProxy(objectName, GatewaySenderMXBean.class);
+        }
+        if (bean != null) {
+          if (bean.isRunning()) {
+            commmandRejected = true;
+            rejectResultData.addMemberStatusResultRow(member.getId(), CliStrings.GATEWAY_ERROR,
+                CliStrings.format(
+                    CliStrings.GATEWAY_SENDER_0_IS_ALREADY_STARTED_ON_MEMBER_1, id,
+                    member.getId()));
+          }
+        } else {
+          commmandRejected = true;
+          rejectResultData.addMemberStatusResultRow(member.getId(), CliStrings.GATEWAY_ERROR,
+              CliStrings.format(
+                  CliStrings.GATEWAY_SENDER_0_IS_NOT_AVAILABLE_ON_MEMBER_1, id, member.getId()));
+        }
+      }
+      if (commmandRejected) {
+        return rejectResultModel;
+      }
+    }
+
     ExecutorService execService =
         LoggingExecutors.newCachedThreadPool("Start Sender Command Thread ", true);
 
