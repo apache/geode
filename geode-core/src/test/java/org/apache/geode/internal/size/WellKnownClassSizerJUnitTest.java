ac0bb45b2794af7016af9fdeada7e11044ad78f0
Jinwoo Hwang <92374539+JinwooHwang-SAS@users.noreply.github.com>
2025-08-27 06:35:12 -0400

    Update geode-core/src/test/java/org/apache/geode/internal/size/WellKnownClassSizerJUnitTest.java
    Co-authored-by: Arnout Engelen <arnout@engelen.eu>

--- geode-core/src/test/java/org/apache/geode/internal/size/WellKnownClassSizerJUnitTest.java
+++ geode-core/src/test/java/org/apache/geode/internal/size/WellKnownClassSizerJUnitTest.java
@@ -53,10 +53,8 @@ public class WellKnownClassSizerJUnitTest {
         - ObjectSizer.SIZE_CLASS_ONCE.sizeof(new char[0]);
 
     assertEquals(emptySize + roundup(OBJECT_SIZE + 4 + 3 * 2), WellKnownClassSizer.sizeof(test1));
-    if (SystemUtils.isAzulJVM()) {
-      assertEquals(emptySize + roundup(OBJECT_SIZE + 4 + 9 * 2 + 8), WellKnownClassSizer.sizeof(test2));
-    } else {
-      assertEquals(emptySize + roundup(OBJECT_SIZE + 4 + 9 * 2), WellKnownClassSizer.sizeof(test2));
+    if (!SystemUtils.isAzulJVM()) {
+        assertEquals(emptySize + roundup(OBJECT_SIZE + 4 + 9 * 2), WellKnownClassSizer.sizeof(test2));
     }
   }
 
