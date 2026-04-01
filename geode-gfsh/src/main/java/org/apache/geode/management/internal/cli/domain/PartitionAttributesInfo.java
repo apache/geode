d90a4675f93d1c663b047bf79bc2209746b62a47
VENTSISLAV MARINOV <ventsislav.marinov@sas.com>
2026-04-01 11:13:03 -0400

    GEODE-10533: Fix Deprecated APIs in geode-gfsh Module

--- geode-gfsh/src/main/java/org/apache/geode/management/internal/cli/domain/PartitionAttributesInfo.java
+++ geode-gfsh/src/main/java/org/apache/geode/management/internal/cli/domain/PartitionAttributesInfo.java
@@ -15,7 +15,11 @@
 package org.apache.geode.management.internal.cli.domain;
 
 import java.io.Serializable;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang3.StringUtils;
 
