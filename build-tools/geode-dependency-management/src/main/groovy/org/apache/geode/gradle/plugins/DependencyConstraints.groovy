25a3397eb301cbf5d4b104803f731b08bf19158c
JinwooHwang <jinwoo@apache.org>
2026-08-06 17:43:21 -0400

    resolved merge conflict

--- build-tools/geode-dependency-management/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ build-tools/geode-dependency-management/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -52,7 +52,7 @@ class DependencyConstraints {
     deps.put("shiro.version", "3.0.0")
     // GEODE-10583: Pin Bouncy Castle (transitive via shiro-crypto-hash) to a fixed version
     deps.put("bouncycastle.version", "1.85")
-    deps.put("slf4j-api.version", "2.0.17")
+    deps.put("slf4j-api.version", "2.0.18")
     deps.put("jakarta.transaction-api.version", "2.0.1")
     deps.put("jboss-modules.version", "1.11.0.Final")
     deps.put("jackson.version", "2.21.5")
