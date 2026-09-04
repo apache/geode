49c5b94934f8681e2c474295a84d3a3778650cdd
Jinwoo Hwang <92374539+JinwooHwang@users.noreply.github.com>
2026-09-03 20:56:47 -0400

    [GEODE-10622] Remediation of CVE-2026-47857 (#8049)

--- build-tools/geode-dependency-management/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
+++ build-tools/geode-dependency-management/src/main/groovy/org/apache/geode/gradle/plugins/DependencyConstraints.groovy
@@ -67,6 +67,9 @@ class DependencyConstraints {
     deps.put("springldap.version", "3.2.7")
     deps.put("springdoc.version", "2.6.0")
 
+    // Pin Reactor Core (transitive via spring-shell-core) to a fixed version
+    deps.put("reactor-core.version", "3.8.7")
+
     // These version numbers are used in testing various versions of tomcat and are consumed explicitly
     // in will be called explicitly in the relevant extensions module, and respective configurations
     // in geode-assembly.gradle.  Moreover, dependencyManagement does not seem to play nicely when
@@ -149,6 +152,8 @@ class DependencyConstraints {
         api(group: 'io.github.resilience4j', name: 'resilience4j-retry', version: '1.7.1')
         api(group: 'io.lettuce', name: 'lettuce-core', version: '6.1.8.RELEASE')
         api(group: 'io.micrometer', name: 'micrometer-core', version: get('micrometer.version'))
+        // Pin Reactor Core (pulled in via spring-shell-core) to 3.8.7
+        api(group: 'io.projectreactor', name: 'reactor-core', version: get('reactor-core.version'))
         api(group: 'io.swagger.core.v3', name: 'swagger-annotations', version: '2.2.22')
         api(group: 'org.hdrhistogram', name: 'HdrHistogram', version: '2.2.2')
         api(group: 'it.unimi.dsi', name: 'fastutil', version: get('fastutil.version'))
